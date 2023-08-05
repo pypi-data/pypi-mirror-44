99b5bf9d56fa1c94bc5bd8780bad35c5b55e19ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:19 +0000

    Add package enoslib/2.2.8/enoslib-2.2.8-py3-none-any.whl

---
+++ packages/enoslib/enoslib-2.2.8-py3-none-any.whl/enoslib/infra/enos_g5k/api.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+
+import copy
+
+from itertools import groupby
+from operator import itemgetter
+
+from enoslib.infra.enos_g5k import remote
+from enoslib.infra.enos_g5k import utils
+from enoslib.infra.enos_g5k.driver import get_driver
+from enoslib.infra.enos_g5k.constants import DEFAULT_ENV_NAME, JOB_TYPE_DEPLOY
+
+
+def get_clusters_sites(clusters):
+    """ Returns the name of the site for each cluster.
+
+    Args:
+        clusters (str): list of clusters
+
+    Returns:
+        dict of cluster with its associated site
+
+    """
+
+    sites = {}
+    for cluster in clusters:
+        site = utils.get_cluster_site(cluster)
+        sites.setdefault(cluster, site)
+
+    return sites
+
+
+def get_clusters_interfaces(clusters, extra_cond=lambda nic: True):
+    """ Returns for each cluster the available cluster interfaces
+
+    Args:
+        clusters (str): list of the clusters
+        extra_cond (lambda): extra predicate to filter network card retrieved
+    from the API. E.g lambda nic: not nic['mounted'] will retrieve all the
+    usable network cards that are not mounted by default.
+
+    Returns:
+        dict of cluster with their associated nic names
+
+    Examples:
+        .. code-block:: python
+
+            # pseudo code
+            actual = get_clusters_interfaces(["paravance"])
+            expected = {"paravance": ["eth0", "eth1"]}
+            assertDictEquals(expected, actual)
+    """
+
+    interfaces = {}
+    for cluster in clusters:
+        nics = utils.get_cluster_interfaces(cluster, extra_cond=extra_cond)
+        interfaces.setdefault(cluster, nics)
+
+    return interfaces
+
+
+def exec_command_on_nodes(nodes, cmd, label, conn_params=None):
+    """Execute a command on a node (id or hostname) or on a set of nodes.
+
+    Args:
+        nodes (list):  list of targets of the command cmd. Each must be an
+    execo.Host.
+        cmd (str): string representing the command to run on the remote nodes.
+        label (str):  string for debugging purpose.
+        conn_params (dict): connection parameters passed to the execo.Remote
+    function.
+
+    """
+
+    remote.exec_command_on_nodes(nodes, cmd, label, conn_params)
+
+
+class Resources:
+    """Class to manipulate g5k resource.
+
+    This acts as an entry point to control the deployment life-cycle.
+    A typical workflow of use would be :
+
+    Examples:
+        .. code-block:: python
+
+            configuration = { ... }
+            r = Resources(configuration)
+            r.reserve()
+            r.deploy()
+            r.configure_network()
+
+        Or more concisely :
+
+            configuration = { ... }
+            r = Resources(configuration)
+            r.launch()
+
+    Note that ``configuration`` dict is not validated here, but can be through
+    the :py:func:`enoslib.infra.enos_g5k.schema.validate_schema` function and
+    thus follow the same syntax as the g5k provider.
+    """
+
+    def __init__(self, configuration):
+        self.configuration = configuration
+        # This one will be modified
+        self.c_resources = copy.deepcopy(self.configuration["resources"])
+        # Load the driver that will interact with G5K
+        self.driver = get_driver(configuration)
+
+    def launch(self):
+        self.reserve()
+        if self.configuration.get("job_type") == JOB_TYPE_DEPLOY:
+            self.deploy()
+            self.configure_network()
+        else:
+            # make sure we can connect as root on non-deploy nodes
+            self.grant_root_access()
+
+    def reserve(self):
+        nodes, vlans, subnets = self.driver.reserve()
+
+        # The following as side-effect on self.c_resources
+        self._concretize_resources(nodes, vlans, subnets)
+
+    def deploy(self):
+        def translate_vlan(primary_network, networks, nodes):
+
+            def translate(node, vlan_id):
+                splitted = node.split(".")
+                splitted[0] = "%s-kavlan-%s" % (splitted[0], vlan_id)
+                return ".".join(splitted)
+
+            if utils.is_prod(primary_network, networks):
+                return nodes
+            net = utils.lookup_networks(primary_network, networks)
+            vlan_id = net["_c_network"]["vlan_id"]
+            return [translate(node, vlan_id) for node in nodes]
+
+        env_name = self.configuration.get("env_name", DEFAULT_ENV_NAME)
+        force_deploy = self.configuration.get("force_deploy", False)
+
+        machines = self.c_resources["machines"]
+        networks = self.c_resources["networks"]
+        key = itemgetter("primary_network")
+        s_machines = sorted(machines, key=key)
+        for primary_network, i_descs in groupby(s_machines, key=key):
+            descs = list(i_descs)
+            nodes = [desc.get("_c_nodes", []) for desc in descs]
+            # flatten
+            nodes = sum(nodes, [])
+            options = {
+                "env_name": env_name
+            }
+            if not utils.is_prod(primary_network, networks):
+                net = utils.lookup_networks(primary_network, networks)
+                options.update({"vlan": net["_c_network"]["vlan_id"]})
+            # Yes, this is sequential
+            deployed, undeployed = utils._deploy(nodes, force_deploy, options)
+            for desc in descs:
+                c_nodes = desc.get("_c_nodes", [])
+                desc["_c_deployed"] = list(set(c_nodes) & set(deployed))
+                desc["_c_undeployed"] = list(set(c_nodes) & set(undeployed))
+                desc["_c_ssh_nodes"] = translate_vlan(
+                    primary_network,
+                    networks,
+                    desc["_c_deployed"])
+
+    def configure_network(self):
+        dhcp = self.configuration.get("dhcp", False)
+        utils.mount_nics(self.c_resources)
+        if dhcp:
+            utils.dhcp_interfaces(self.c_resources)
+        return self.c_resources
+
+    def grant_root_access(self):
+        utils.grant_root_access(self.c_resources)
+
+    def get_networks(self):
+        """Get the networks assoiated with the resource description.
+
+        Returns
+            list of networks
+        """
+        networks = self.c_resources["networks"]
+        result = []
+        for net in networks:
+            current = {}
+            current.update(net)
+            _c_network = current.pop("_c_network", None)
+            if _c_network:
+                current.update(_c_network)
+            result.append(current)
+        return result
+
+    def get_roles(self):
+        """Get the roles associated with the hosts.
+
+        Returns
+            dict of role -> [host]
+        """
+
+        machines = self.c_resources["machines"]
+        result = {}
+        for desc in machines:
+            roles = utils.get_roles_as_list(desc)
+            hosts = self._denormalize(desc)
+            for role in roles:
+                result.setdefault(role, [])
+                result[role].extend(hosts)
+        return result
+
+    def destroy(self):
+        """Destroy the associated job."""
+        self.driver.destroy()
+
+    def _denormalize(self, desc):
+        hosts = desc.get("_c_ssh_nodes", desc.get("_c_nodes", []))
+        nics = desc.get("_c_nics", [])
+        hosts = [{"host": h, "nics": nics} for h in hosts]
+        return hosts
+
+    def _concretize_resources(self, nodes, vlans, subnets):
+        self._concretize_nodes(nodes)
+        self._concretize_networks(vlans, subnets)
+
+    def _concretize_nodes(self, nodes):
+        utils.concretize_nodes(self.c_resources, nodes)
+
+    def _concretize_networks(self, vlans, subnets):
+        utils.concretize_networks(self.c_resources, vlans, subnets)
---
+++ packages/enoslib/enoslib-2.2.8-py3-none-any.whl/enoslib/infra/enos_g5k/provider.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+import logging
+import socket
+
+from netaddr import IPAddress, IPNetwork, IPSet
+
+import enoslib.infra.enos_g5k.api as api
+from enoslib.infra.enos_g5k.schema import KAVLAN_TYPE, SUBNET_TYPE
+from enoslib.host import Host
+from enoslib.infra.provider import Provider
+from enoslib.utils import get_roles_as_list
+from .constants import NAMESERVER
+
+
+logger = logging.getLogger(__name__)
+
+
+def _to_enos_roles(roles):
+    """Transform the roles to use enoslib.host.Host hosts.
+
+    Args:
+        roles (dict): roles returned by
+            :py:func:`enoslib.infra.provider.Provider.init`
+    """
+
+    def to_host(h):
+        extra = {}
+        # create extra_vars for the nics
+        # network_role = ethX
+        for nic, roles in h["nics"]:
+            for role in roles:
+                extra[role] = nic
+
+        return Host(h["host"], user="root", extra=extra)
+
+    enos_roles = {}
+    for role, hosts in roles.items():
+        enos_roles[role] = [to_host(h) for h in hosts]
+    logger.debug(enos_roles)
+    return enos_roles
+
+
+def _to_enos_networks(networks):
+    """Transform the networks returned by deploy5k.
+
+    Args:
+        networks (dict): networks returned by
+            :py:func:`enoslib.infra.provider.Provider.init`
+    """
+    nets = []
+    for network in networks:
+        net = {
+            "cidr": str(network["network"]),
+            "gateway": str(network["gateway"]),
+            # NOTE(msimonin): This will point to the nameserver of the site
+            # where the deployment is launched regardless the actual site in
+            # the network description. Until now we used the global DNS IP
+            # here. Finally this information couldn't be found in the API (dec.
+            # 18) otherwise we'd move this logic in utils.concretize_networks
+            # (like network and gateway)
+            "dns": socket.gethostbyname(NAMESERVER),
+            "roles": get_roles_as_list(network)
+        }
+        if network["type"] in KAVLAN_TYPE:
+            # On the network, the first IP are reserved to g5k machines.
+            # For a routed vlan I don't know exactly how many ip are
+            # reserved. However, the specification is clear about global
+            # vlan: "A global VLAN is a /18 subnet (16382 IP addresses).
+            # It is split -- contiguously -- so that every site gets one
+            # /23 (510 ip) in the global VLAN address space". There are 12
+            # site. This means that we could take ip from 13th subnetwork.
+            # Lets consider the strategy is the same for routed vlan. See,
+            # https://www.grid5000.fr/mediawiki/index.php/Grid5000:Network#KaVLAN
+            #
+            # First, split network in /23 this leads to 32 subnetworks.
+            # Then, (i) drops the 12 first subnetworks because they are
+            # dedicated to g5k machines, and (ii) drops the last one
+            # because some of ips are used for specific stuff such as
+            # gateway, kavlan server...
+            subnets = IPNetwork(network["network"])
+            if network["vlan_id"] < 4:
+                # vlan local
+                subnets = list(subnets.subnet(24))
+                subnets = subnets[4:7]
+            else:
+                subnets = list(subnets.subnet(23))
+                subnets = subnets[13:31]
+
+            # Finally, compute the range of available ips
+            ips = IPSet(subnets).iprange()
+
+            net.update({
+                "start": str(IPAddress(ips.first)),
+                "end": str(IPAddress(ips.last))
+            })
+        elif network["type"] in SUBNET_TYPE:
+            start_ip, start_mac = network["ipmac"][0]
+            end_ip, end_mac = network["ipmac"][-1]
+            net.update({
+                "start": start_ip,
+                "end": end_ip,
+                "mac_start": start_mac,
+                "mac_end": end_mac
+            })
+
+        net.update({"roles": get_roles_as_list(network)})
+        nets.append(net)
+    logger.debug(nets)
+    return nets
+
+
+class G5k(Provider):
+    """The provider to use when deploying on Grid'5000."""
+
+    def init(self, force_deploy=False):
+        """Reserve and deploys the nodes according to the resources section
+
+        In comparison to the vagrant provider, networks must be characterized
+        as in the networks key.
+
+        Args:
+            force_deploy (bool): True iff the environment must be redeployed
+        Raises:
+            MissingNetworkError: If one network is missing in comparison to
+                what is claimed.
+            NotEnoughNodesError: If the `min` constraints can't be met.
+
+           """
+        self.provider_conf.force_deploy = force_deploy
+
+        # TODO remove the use of dict
+        self._provider_conf = self.provider_conf.to_dict()
+        r = api.Resources(self._provider_conf)
+        r.launch()
+        roles = r.get_roles()
+        networks = r.get_networks()
+
+        return (_to_enos_roles(roles),
+                _to_enos_networks(networks))
+
+    def destroy(self):
+        """Destroys the jobs."""
+        r = api.Resources(self._provider_conf)
+        # insert force_deploy
+        r.destroy()
+
+    def __str__(self):
+        return 'G5k'
