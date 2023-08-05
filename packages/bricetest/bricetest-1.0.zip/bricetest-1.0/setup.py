3f65d632618aecc9446057ee29bed76e796e696c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:41 +0000

    Add package bricetest/1.0/bricetest-1.0.zip

---
+++ packages/bricetest/bricetest-1.0.zip/bricetest-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'bricetest',
+    version='1.0',
+    author_email='xiaoxiaofeng1119@163.com',
+    url='https://github.com/xiaoxiaofeng999/dabao_test2.git/',
+    description= 'this is a test lib',
+    license='MIT', 
+    packages = ['test_package','test_package.test_package2']
+)
