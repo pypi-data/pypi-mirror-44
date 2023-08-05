895ad92213d128b158d6b2455fa077e45393dc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package SmoothAPI/1.0.3/SmoothAPI-1.0.3.tar.gz

---
+++ packages/SmoothAPI/SmoothAPI-1.0.3.tar.gz/SmoothAPI-1.0.3/SmoothAPI/__init__.py
@@ -0,0 +1,1 @@
+from .smoothapi import SmoothAPI
---
+++ packages/SmoothAPI/SmoothAPI-1.0.3.tar.gz/SmoothAPI-1.0.3/SmoothAPI/proxy_handlers.py
@@ -0,0 +1,32 @@
+class NoProxy:
+    def get(self, _):
+        return None
+
+    def ban_proxy(self, proxies):
+        return None
+
+
+class RateLimitProxy:
+    def __init__(self, proxies, paths, default=None):
+        self.proxies = proxies
+        self.proxy_count = len(proxies)
+        self.access_counter = {
+            path: {"limit": paths[path], "count": 0} for path in paths.keys()
+        }
+        self.default = {"http": default, "https": default}
+
+    def get(self, keyword_arguments):
+        counter = self.access_counter.get(keyword_arguments["path"])
+        if counter is not None:
+            proxy = self.proxies[
+                (counter["count"] // counter["limit"] - self.proxy_count)
+                % self.proxy_count
+            ]
+            counter["count"] += 1
+            return {"http": proxy, "https": proxy}
+
+        return self.default
+
+    def ban_proxy(self, proxies):
+        self.proxies = list(filter(lambda a: a not in [proxies.get("http", ''), proxies.get("https", '')], self.proxies))
+        return None
