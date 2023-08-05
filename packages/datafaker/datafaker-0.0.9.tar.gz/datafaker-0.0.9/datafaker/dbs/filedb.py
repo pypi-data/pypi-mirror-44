0f95a2a8dde67e1b82f6fb11c9d20c4bdae06c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:35 +0000

    Add package datafaker/0.0.9/datafaker-0.0.9.tar.gz

---
+++ packages/datafaker/datafaker-0.0.9.tar.gz/datafaker-0.0.9/datafaker/dbs/filedb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import os
+
+from datafaker.dbs.basedb import BaseDB
+from datafaker.utils import save2file
+
+
+class FileDB(BaseDB):
+
+    def construct_self_rows(self):
+        return []
+
+    def save_data(self, lines):
+        spliter = self.args.outspliter if self.args.outspliter else ','
+        filepath = os.path.join(self.args.connect, self.args.table)
+        save2file(lines, filepath, spliter)
+
