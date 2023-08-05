28d50d31cf5db1b8807b282176014b4c3fb6efd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:03 +0000

    Add package tflibs/0.10.0.dev1/tflibs-0.10.0.dev1-py3-none-any.whl

---
+++ packages/tflibs/tflibs-0.10.0.dev1-py3-none-any.whl/tflibs/training/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tflibs.training.dispatcher import *
+from tflibs.training.optimizer import *
+from tflibs.training.hooks import *
