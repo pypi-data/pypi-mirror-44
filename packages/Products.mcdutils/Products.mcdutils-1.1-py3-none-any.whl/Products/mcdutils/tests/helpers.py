ae4360df753a590922a7cb5b3ffc8f3b40637e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:27 +0000

    Add package Products.mcdutils/1.1/Products.mcdutils-1.1-py3-none-any.whl

---
+++ packages/Products.mcdutils/Products.mcdutils-1.1-py3-none-any.whl/Products/mcdutils/tests/helpers.py
@@ -0,0 +1,44 @@
+""" Unit test helper modules """
+
+
+class DummyMemcache(dict):
+
+    def _assertKeyBinary(self, key):
+        if not isinstance(key, str):
+            raise ValueError('Key must be binary string.')
+        return key
+
+    def set(self, key, value):
+        self._assertKeyBinary(key)
+        self[key] = value
+        return True
+
+    def add(self, key, value):
+        self._assertKeyBinary(key)
+        if key not in self:
+            self[key] = value
+            return True
+
+    def replace(self, key, value):
+        self._assertKeyBinary(key)
+        if key in self:
+            self[key] = value
+            return True
+
+    def delete(self, key, time=0):
+        self._assertKeyBinary(key)
+        if key in self:
+            del self[key]
+            return True
+
+    def get(self, key):
+        self._assertKeyBinary(key)
+        if key in self:
+            return self[key]
+
+    def get_multi(self, keys):
+        res = {}
+        for key in keys:
+            self._assertKeyBinary(key)
+            res[key] = self.get(key)
+        return res
