3e182b6d0344e289f0b2f17fb8c469c876c14a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package idebug/0.0.5/idebug-0.0.5-py3-none-any.whl

---
+++ packages/idebug/idebug-0.0.5-py3-none-any.whl/idebug/__init__.py
@@ -0,0 +1,23 @@
+
+#============================================================
+# External
+#============================================================
+import os
+import sys
+import inspect
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+from datetime import datetime
+import pandas as pd
+import json
+
+#============================================================
+# Internal
+#============================================================
+from .DataStructure import *
+from .etc import *
+from .function import *
+from .html import *
+from .iinspect import *
+from .mongo import *
+from .performance import *
