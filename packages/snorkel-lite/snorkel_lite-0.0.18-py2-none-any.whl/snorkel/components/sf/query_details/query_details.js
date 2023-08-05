c9e34af5a4ea25cfd31a6d8f8e807436d994e38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:04 +0000

    Add package snorkel-lite/0.0.18/snorkel_lite-0.0.18-py2-none-any.whl

---
+++ packages/snorkel-lite/snorkel_lite-0.0.18-py2-none-any.whl/snorkel/components/sf/query_details/query_details.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  tagName: "div",
+  className: "",
+  client: function(options) {
+    if (_.isNaN(options.created)) {
+      options.created = +new Date();
+    }
+
+    var created_str = new Date(options.created).toISOString();
+    var that = this;
+
+    var view = options.query.parsed.baseview || options.query.parsed.viewbase || options.query.parsed.view;
+
+    var count = 0;
+    var w_count = 0;
+    var results = options.query.results;
+    if (view == "samples") {
+      count = results.length;
+
+    }
+
+    if (view == "table" || view == "time" || view == "dist") {
+      _.each(results, function(row) {
+        count += row.samples || row.count;
+        w_count += row.weighted_count || row.samples;
+      });
+
+    }
+
+    if (!_.isNaN(count)) {
+      that.$el.find(".count").text("Sample Count: " + count);
+    }
+
+    if (!_.isNaN(w_count)) {
+      that.$el.find(".weight").text("Weighted Samples: " + w_count || count);
+    }
+
+    $C("timeago", {time: created_str }, function(cmp) {
+      that.$el.find(".timestamp").append(cmp.$el);
+    });
+  }
+};
