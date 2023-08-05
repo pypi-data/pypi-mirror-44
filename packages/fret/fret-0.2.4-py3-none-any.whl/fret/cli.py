95e3808bb6baf979781bf3ab9e7212b9d1f1a7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:09 +0000

    Add package fret/0.2.4/fret-0.2.4-py3-none-any.whl

---
+++ packages/fret/fret-0.2.4-py3-none-any.whl/fret/cli.py
@@ -0,0 +1,17 @@
+from .app import get_app
+
+
+def main(args=None):
+    """``fret`` script cli entry point.
+
+    Args:
+        args (list): command line arguments. If None, get args from
+                     :data:`sys.argv` (default: ``None``)
+    """
+    app = get_app()
+    app.import_modules()
+    return app.main(args)
+
+
+if __name__ == '__main__':
+    main()
