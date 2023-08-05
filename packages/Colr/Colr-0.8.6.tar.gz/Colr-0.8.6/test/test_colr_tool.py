5a2dd4b9ffe5497eaa23ddc32598a141d52942e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:45 +0000

    Add package Colr/0.8.6/Colr-0.8.6.tar.gz

---
+++ packages/Colr/Colr-0.8.6.tar.gz/Colr-0.8.6/test/test_colr_tool.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" test_colr_tool.py
+    Unit tests for colr.py command line tool.
+
+    These tests should be ran with `green -q` to quiet stdout.
+    If you are using nose then stdout should be quiet already.
+    If you are using unittest to run these, then -b should work to quiet them.
+    -Christopher Welborn 12-09-2015
+"""
+
+import json
+import random
+import sys
+import unittest
+
+from colr import (
+    Colr,
+    hex2rgb,
+    name_data,
+    InvalidColr,
+    InvalidStyle,
+    rgb2hex,
+)
+from colr.__main__ import (
+    __version__,
+    get_colr,
+    main
+)
+
+from .testing_tools import ColrTestCase
+
+r = random.SystemRandom()
+
+# Save names in list format, for random.choice().
+name_data_names = list(name_data)
+
+
+class ColrToolTests(ColrTestCase):
+    def setUp(self):
+        # Default argd, when no flags are given.
+        self.argd = {
+            '--auto-disable': False,
+            '--back': None,
+            '--center': None,
+            '--debug': False,
+            '--err': False,
+            '--fore': None,
+            '--frequency': None,
+            '--gradient': None,
+            '--gradientrgb': [],
+            '--help': False,
+            '--listcodes': False,
+            '--ljust': None,
+            '--names': False,
+            '--newline': False,
+            '--offset': None,
+            '--rainbow': False,
+            '--rjust': None,
+            '--spread': None,
+            '--stripcodes': False,
+            '--style': None,
+            '--translate': False,
+            '--truecolor': False,
+            '--unique': False,
+            '--version': False,
+            'BACK': None,
+            'CODE': [],
+            'FORE': None,
+            'STYLE': None,
+            'TEXT': None,
+        }
+        # Number of random values to generate for tests to choose from.
+        max_vals = 50
+
+        # Valid basic color name args for the colr tool.
+        self.valid_basic_vals = (
+            'white',
+            'black',
+            'red',
+            'cyan',
+            'green',
+            'blue',
+        )
+        # Valid extended color names/values.
+        self.valid_ext_vals = [
+            'lightblue',
+            'lightcyan',
+        ]
+        self.valid_ext_vals.extend(
+            set((
+                str(r.randint(0, 255))
+                for _ in range(max_vals)
+            ))
+        )
+
+        # Valid rgb color values.
+        self.valid_rgb_vals = []
+        self.valid_rgb_vals.extend(
+            set((
+                ','.join(str(r.randint(0, 255)) for rgb in range(3))
+                for _ in range(max_vals)
+            ))
+        )
+
+        # Valid hex values.
+        self.valid_hex_vals = ['000', 'fff', 'f0f', 'aba']
+        for rgb in self.valid_rgb_vals:
+            rgbtup = tuple(int(x) for x in rgb.split(','))
+            self.valid_hex_vals.append(rgb2hex(*rgbtup))
+
+        # Valid style names/values.
+        self.valid_style_vals = (
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '7',
+            '22',
+            'b',
+            'bold',
+            'bright',
+            'd',
+            'dim',
+            'f',
+            'flash',
+            'h',
+            'highlight',
+            'hilight',
+            'hilite',
+            'i',
+            'italic',
+            'n',
+            'none',
+            'normal',
+            'reset_all',
+            'reverse',
+            'u',
+            'underline',
+            'underlined',
+        )
+
+    def make_argd(self, argd):
+        """ Make a copy of self.argd and update it with values from argd.
+            Returns the updated copy.
+        """
+        d = self.argd.copy()
+        d.update(argd)
+        return d
+
+    def run_main_test(self, argd, should_fail=False):
+        """ Run main() with the given argd, and fail on any errors. """
+        argd = self.make_argd(argd)
+        try:
+            ret = main(argd)
+        except (InvalidColr, InvalidStyle) as ex:
+            if should_fail:
+                raise
+            # This should not have happened. Show detailed arg/exc info.
+            self.fail(
+                'Colr tool failed to run:\n{}\n    argd: {}'.format(
+                    ex,
+                    json.dumps(argd, sort_keys=True, indent=4)
+                )
+            )
+        return ret
+
+    def test_basic_colors(self):
+        """ colr tool should recognize basic colors. """
+        argd = {'TEXT': 'Hello World', 'FORE': 'green', 'BACK': 'blue'}
+        for _ in range(10):
+            argd['FORE'] = r.choice(self.valid_basic_vals)
+            argd['BACK'] = r.choice(self.valid_basic_vals)
+            self.assertEqual(
+                0,
+                self.run_main_test(argd, should_fail=False)
+            )
+        # Invalid color names should raise a InvalidColr.
+        badargsets = (
+            {'FORE': 'XXX', 'BACK': r.choice(self.valid_basic_vals)},
+            {'BACK': 'XXX', 'FORE': r.choice(self.valid_basic_vals)},
+        )
+        for argset in badargsets:
+            argd.update(argset)
+            with self.assertRaises(InvalidColr):
+                self.run_main_test(argd, should_fail=True)
+
+    def test_extended_colors(self):
+        """ colr tool should recognize extended colors. """
+        argd = {'TEXT': 'Hello World', 'FORE': '235'}
+        for _ in range(10):
+            argd['FORE'] = r.choice(self.valid_ext_vals)
+            argd['BACK'] = r.choice(self.valid_ext_vals)
+            self.assertEqual(
+                0,
+                self.run_main_test(argd, should_fail=False)
+            )
+        # Invalid color values should raise a InvalidColr.
+        badargsets = (
+            {'FORE': '1000', 'BACK': r.choice(self.valid_ext_vals)},
+            {'BACK': '-1', 'FORE': r.choice(self.valid_ext_vals)},
+        )
+        for argset in badargsets:
+            argd.update(argset)
+            with self.assertRaises(InvalidColr):
+                self.run_main_test(argd, should_fail=True)
+
+    def test_hex_colors(self):
+        """ colr tool should recognize hex colors. """
+        argd = {'TEXT': 'Hello World', 'FORE': 'd7d7d7'}
+        for _ in range(10):
+            argd['FORE'] = r.choice(self.valid_hex_vals)
+            argd['BACK'] = r.choice(self.valid_hex_vals)
+            self.assertEqual(
+                0,
+                self.run_main_test(argd, should_fail=False)
+            )
+
+        # Without -T, close matches should be used.
+        argd = {'TEXT': 'Hello World', '--truecolor': False}
+        hexvals = {
+            '010203': '000000',
+            '040506': '000000',
+        }
+        for hexval, closematch in hexvals.items():
+            argd['FORE'] = hexval
+            self.assertEqual(
+                get_colr(argd['TEXT'], self.make_argd(argd)),
+                Colr(argd['TEXT'], closematch),
+                msg='Hex value close match failed without --truecolor.',
+            )
+        # With -T, rgb mode should be used.
+        argd = {'TEXT': 'Hello World', '--truecolor': True}
+        hexvals = (
+            '010203',
+            '040506',
+        )
+        for hexval in hexvals:
+            argd['FORE'] = hexval
+            self.assertEqual(
+                get_colr(argd['TEXT'], self.make_argd(argd)),
+                Colr(argd['TEXT'], hex2rgb(argd['FORE'])),
+                msg='Hex value failed with --truecolor.',
+            )
+        # Invalid color values should raise a InvalidColr.
+        argd['--truecolor'] = False
+
+        argd = {'TEXT': 'Hello World'}
+        badargsets = (
+            {'FORE': 'ffooll', 'BACK': r.choice(self.valid_hex_vals)},
+            {'BACK': 'oopsie', 'FORE': r.choice(self.valid_hex_vals)},
+        )
+        for argset in badargsets:
+            argd.update(argset)
+            with self.assertRaises(InvalidColr):
+                self.run_main_test(argd, should_fail=True)
+
+    def test_rgb_colors(self):
+        """ colr tool should recognize rgb colors. """
+        argd = {'TEXT': 'Hello World', 'FORE': '25, 25, 25'}
+        for _ in range(10):
+            argd['FORE'] = r.choice(self.valid_rgb_vals)
+            argd['BACK'] = r.choice(self.valid_rgb_vals)
+            self.assertEqual(
+                0,
+                self.run_main_test(argd)
+            )
+        # Invalid rgb values should raise a InvalidColr.
+        badargsets = (
+            {'FORE': '-1,25,25', 'BACK': r.choice(self.valid_rgb_vals)},
+            {'BACK': '257,25,25', 'FORE': r.choice(self.valid_rgb_vals)},
+            {'FORE': 'a,255,255', 'BACK': r.choice(self.valid_rgb_vals)},
+            {'BACK': 'xxx', 'FORE': r.choice(self.valid_rgb_vals)},
+        )
+        for argset in badargsets:
+            argd.update(argset)
+            with self.assertRaises(InvalidColr):
+                self.run_main_test(argd, should_fail=True)
+
+    def test_styles(self):
+        """ colr tool should recognize styles. """
+        argd = {'TEXT': 'Hello World', 'FORE': '235', 'STYLE': 'normal'}
+        for _ in range(10):
+            argd['STYLE'] = r.choice(self.valid_style_vals)
+            self.assertEqual(
+                0,
+                self.run_main_test(argd, should_fail=False)
+            )
+        # Invalid style values should raise a InvalidStyle.
+        badargsets = (
+            {'STYLE': 'dimmer'},
+            {'STYLE': 'x'},
+        )
+        for argset in badargsets:
+            argd.update(argset)
+            with self.assertRaises(InvalidStyle):
+                self.run_main_test(argd, should_fail=True)
+
+
+if __name__ == '__main__':
+    print('Testing Colr Tool v. {}'.format(__version__))
+    # unittest.main() calls sys.exit(status_code).
+    unittest.main(argv=sys.argv, verbosity=2)
---
+++ packages/Colr/Colr-0.8.6.tar.gz/Colr-0.8.6/test/testing_tools.py
@@ -0,0 +1,417 @@
+#!/usr/bin/env python3
+""" Colr - Testing Tools
+    These are some unittest funcs/classes to help with testing.
+    -Christopher Welborn 2-27-17
+"""
+import inspect
+import unittest
+from contextlib import suppress
+from io import (
+    BytesIO,
+    StringIO,
+)
+from unittest.case import _AssertRaisesContext
+from typing import Any, Callable, Mapping, Optional, no_type_check
+
+from colr import Colr
+
+
+def _equality_msg(op, a, b, msg=None):
+    """ The ne_msg and eq_msg wrap this function to reduce code duplication.
+        It builds a message suitable for an assert*Equal msg parameter.
+    """
+    fmta = str(Colr(repr(a), 'yellow'))
+    if repr(a) != str(a):
+        fmta = '{} ({})'.format(fmta, a)
+    fmtb = str(Colr(repr(b), 'green'))
+    if repr(b) != str(b):
+        fmtb = '{} ({})'.format(fmtb, b)
+
+    return '\n'.join((
+        '\n  {} {}'.format(
+            fmta,
+            Colr(op, 'red', style='bright')
+        ),
+        '  {}'.format(fmtb),
+        '\n{}'.format(Colr(msg, 'red')) if msg else '',
+    ))
+
+
+def call_msg(s: str, *args: Any, **kwargs: Mapping[Any, Any]):
+    """ Return a message suitable for the `msg` arg in asserts,
+        including the calling function name.
+    """
+    if s.count(':') == 1:
+        stdmsg, _, msg = s.partition(':')
+    else:
+        stdmsg, msg = s, None
+    return '{funcsig}: {stdmsg}{msgdiv}{msg}'.format(
+        funcsig=format_call_str(*args, **kwargs),
+        stdmsg=Colr(stdmsg, 'red', style='bright'),
+        msgdiv=': ' if msg else '',
+        msg=Colr(msg, 'red'),
+    )
+
+
+def format_call_str(*args: Any, **kwargs: Mapping[Any, Any]):
+    """ Build a formatted string for a function signature. """
+    use_func_name = None
+    with suppress(KeyError):
+        use_func = kwargs.pop('_call_func')
+        if use_func is not None:
+            use_func_name = use_func.__qualname__
+        else:
+            # Default level uses the caller of format_call_str.
+            kwargs.setdefault('_level', 3)
+
+    otherargs = None
+    otherkwargs = None
+    with suppress(KeyError):
+        otherargs = kwargs.pop('_other_args')
+    with suppress(KeyError):
+        otherkwargs = kwargs.pop('_other_kwargs')
+    op = 'and'
+    with suppress(KeyError):
+        userop = kwargs.pop('_op')
+        op = userop or op
+    funcsig = format_func_sig(use_func_name, *args, **kwargs)
+    if otherargs or otherkwargs:
+        otherargs = otherargs or []
+        otherkwargs = otherkwargs or {}
+        otherkwargs['_level'] = kwargs.get('_level', None)
+        funcsig = ' {} '.format(op).join((
+            funcsig,
+            format_func_sig(use_func_name, *otherargs, **otherkwargs)
+        ))
+    return funcsig
+
+
+def format_func_sig(name, *args, **kwargs):
+    """ Format a function signature.
+        Pass None for a name and use _level=<frames_backward> to use the
+        calling function.
+    """
+    # Default level uses the caller of format_func_sig.
+    level = 2
+    with suppress(KeyError):
+        level = kwargs.pop('_level')
+
+    argstr = Colr(', ').join(Colr(repr(a), 'cyan') for a in args)
+    kwargstr = ', '.join(
+        '{}={}'.format(Colr(k, 'lightblue'), Colr(repr(v), 'cyan'))
+        for k, v in kwargs.items()
+    )
+    argrepr = Colr(', ').join(s for s in (argstr, kwargstr) if s)
+    return '{funcname}{args}'.format(
+        funcname=Colr(
+            name or func_name(level=level),
+            'blue',
+            style='bright'
+        ),
+        args=Colr(argrepr).join('(', ')', style='bright'),
+    )
+
+
+def func_name(level: Optional[int]=1, parent: Optional[Callable]=None) -> str:
+    """ Return the name of the function that is calling this function. """
+    frame = inspect.currentframe()
+    # Go back a number of frames (usually 1).
+    backlevel = level or 1
+    while backlevel > 0:
+        frame = frame.f_back
+        backlevel -= 1
+    if parent:
+        func = '{}.{}'.format(parent.__class__.__name__, frame.f_code.co_name)
+        return func
+
+    return frame.f_code.co_name
+
+
+@no_type_check
+class ColrAssertRaisesContext(_AssertRaisesContext):
+    # This is how many frames it takes to get back to the test method
+    # that calls the assert method that uses this context.
+    # It's for getting the calling test name when building messages.
+    calling_test_level = 6
+
+    def __init__(
+            self, expected, test_case, expected_regex=None,
+            func=None, args=None, kwargs=None):
+        super().__init__(expected, test_case, expected_regex=expected_regex)
+        self.func = func
+        self.args = args or []
+        self.kwargs = kwargs or {}
+
+    def _raiseFailure(self, std_msg):
+        raise self.test_case.failureException(
+            call_msg(
+                self.test_case._formatMessage(self.msg, std_msg),
+                *self.args,
+                **self.kwargs,
+                _call_func=self.func,
+                _level=self.calling_test_level,
+            )
+        )
+
+
+@no_type_check
+class ColrTestCase(unittest.TestCase):
+    # This is how many frames it takes to get back to the test method
+    # that calls these assert methods.
+    # It's for getting the calling test name when building messages.
+    calling_test_level = 5
+
+    def assertAlmostEqual(self, a, b, places=None, msg=None, delta=None):
+        """ Like self.assertAlmostEqual, with a better message. """
+        try:
+            super().assertAlmostEqual(
+                a,
+                b,
+                places=places,
+                msg=msg,
+                delta=delta,
+            )
+        except self.failureException:
+            raise self.failureException(_equality_msg('!~', a, b, msg=msg))
+
+    def assertAlmostNotEqual(self, a, b, places=None, msg=None, delta=None):
+        """ Like self.assertAlmostNotEqual, with a better message. """
+        try:
+            super().assertAlmostNotEqual(
+                a,
+                b,
+                places=places,
+                msg=msg,
+                delta=delta,
+            )
+        except self.failureException:
+            raise self.failureException(_equality_msg('~', a, b, msg=msg))
+
+    def assertCallEqual(
+            self, a, b, func=None,
+            args=None, kwargs=None,
+            otherargs=None, otherkwargs=None, msg=None):
+        """ Like self.assertEqual, but includes the func call info. """
+        if a == b:
+            return None
+
+        callargs = args or []
+        callkwargs = kwargs or {}
+        raise self.failureException(
+            call_msg(
+                _equality_msg('!=', a, b, msg=msg),
+                *callargs,
+                **callkwargs,
+                _call_func=func,
+                _level=self.calling_test_level,
+                _other_args=otherargs,
+                _other_kwargs=otherkwargs,
+                _op='!=',
+            )
+        )
+
+    def assertCallFalse(
+            self, val, func=None, args=None, kwargs=None, msg=None):
+        """ Like self.assertFalse, but includes the func call info. """
+        if not val:
+            return None
+        callargs = args or []
+        callkwargs = kwargs or {}
+        raise self.failureException(
+            call_msg(
+                _equality_msg('!=', val, False, msg=msg),
+                *callargs,
+                **callkwargs,
+                _call_func=func,
+                _level=self.calling_test_level,
+                _op='!=',
+            )
+        )
+
+    def assertCallNotEqual(
+            self, a, b, func=None,
+            args=None, kwargs=None,
+            otherargs=None, otherkwargs=None, msg=None):
+        """ Like self.assertNotEqual, but includes the func call info. """
+        if a != b:
+            return None
+
+        callargs = args or []
+        callkwargs = kwargs or {}
+        raise self.failureException(
+            call_msg(
+                _equality_msg('==', a, b, msg=msg),
+                *callargs,
+                **callkwargs,
+                _call_func=func,
+                _level=self.calling_test_level,
+                _other_args=otherargs,
+                _other_kwargs=otherkwargs,
+                _op='==',
+            )
+        )
+
+    def assertCallIsInstance(
+            self, obj, cls, func=None,
+            args=None, kwargs=None,
+            otherargs=None, otherkwargs=None, msg=None):
+        try:
+            super().assertIsInstance(obj, cls, msg=msg)
+        except self.failureException as ex:
+            stdmsg = ex.args[0] if ex.args else None
+            callargs = args or []
+            callkwargs = kwargs or {}
+            raise self.failureException(
+                call_msg(
+                    _equality_msg('is not', obj, cls, msg=stdmsg),
+                    *callargs,
+                    **callkwargs,
+                    _other_args=otherargs,
+                    _other_kwargs=otherkwargs,
+                    _call_func=func,
+                    _level=3,
+                    _op='is not',
+                )
+            )
+
+    def assertCallTrue(
+            self, val, func=None, args=None, kwargs=None, msg=None):
+        """ Like self.assertTrue, but includes the func call info. """
+        if val:
+            return None
+        callargs = args or []
+        callkwargs = kwargs or {}
+        raise self.failureException(
+            call_msg(
+                _equality_msg('!=', val, True, msg=msg),
+                *callargs,
+                **callkwargs,
+                _call_func=func,
+                _level=self.calling_test_level,
+                _op='!=',
+            )
+        )
+
+    def assertCallRaises(
+            self, exception, func=None, args=None, kwargs=None, msg=None):
+        """ Like self.assertRaises, but includes the func call info. """
+        context = ColrAssertRaisesContext(
+            exception,
+            self,
+            func=func,
+            args=args,
+            kwargs=kwargs
+        )
+        return context.handle('assertCallRaises', [], {'msg': msg})
+
+    def assertCallTupleEqual(
+            self, a, b, func=None,
+            args=None, kwargs=None,
+            otherargs=None, otherkwargs=None, msg=None):
+        try:
+            super().assertTupleEqual(a, b, msg=msg)
+        except self.failureException as ex:
+            # TODO: The entire exception chain is printed with `green`.
+            #       The point of these calls is to REPLACE the old message,
+            #       not to clutter up the console with their
+            #       `failureException` and mine.
+            stdmsg = ex.args[0] if ex.args else None
+            callargs = args or []
+            callkwargs = kwargs or {}
+            raise self.failureException(
+                call_msg(
+                    _equality_msg('!=', a, b, msg=stdmsg),
+                    *callargs,
+                    **callkwargs,
+                    _other_args=otherargs,
+                    _other_kwargs=otherkwargs,
+                    _call_func=func,
+                    _level=self.calling_test_level,
+                    _op='!=',
+                )
+            )
+
+    def assertEqual(self, a, b, msg=None):
+        if a == b:
+            return None
+        raise self.failureException(_equality_msg('!=', a, b, msg=msg))
+
+    def assertGreater(self, a, b, msg=None):
+        if a > b:
+            return None
+        raise self.failureException(_equality_msg('<=', a, b, msg=msg))
+
+    def assertGreaterEqual(self, a, b, msg=None):
+        if a >= b:
+            return None
+        raise self.failureException(_equality_msg('<', a, b, msg=msg))
+
+    def assertLess(self, a, b, msg=None):
+        if a < b:
+            return None
+        raise self.failureException(_equality_msg('>=', a, b, msg=msg))
+
+    def assertLessEqual(self, a, b, msg=None):
+        if a <= b:
+            return None
+        raise self.failureException(_equality_msg('>', a, b, msg=msg))
+
+    def assertNotEqual(self, a, b, msg=None):
+        if a != b:
+            return None
+        raise self.failureException(_equality_msg('==', a, b, msg=msg))
+
+    def assertTupleEqual(self, a, b, msg=None):
+        try:
+            super().assertTupleEqual(a, b, msg=msg)
+        except self.failureException as ex:
+            stdmsg = ex.args[0] if ex.args else None
+            raise self.failureException(
+                _equality_msg('!=', a, b, msg=stdmsg)
+            )
+
+    def call_msg(self, s, *args, **kwargs):
+        """ Convenience method, a wrapper for `call_msg`. """
+        kwargs.setdefault('_level', 4)
+        with suppress(KeyError):
+            kwargs.setdefault('_call_func', kwargs.pop('func'))
+        return call_msg(s, *args, **kwargs)
+
+
+class TestFileBytes(BytesIO):
+    """ A file object that deletes it's content every time you call
+        str(TestFileBytes).
+    """
+    def __bytes__(self):
+        self.seek(0)
+        s = self.read()
+        self.truncate(0)
+        self.seek(0)
+        return s
+
+    def __str__(self):
+        return repr(bytes(self))
+
+
+class TestFile(StringIO):
+    """ A file object that deletes it's content every time you call
+        str(TestFile).
+    """
+    def __init__(self):
+        self.buffer = TestFileBytes()
+
+    def __str__(self):
+        self.seek(0)
+        s = self.read()
+        self.truncate(0)
+        self.seek(0)
+        return s
+
+    def write(self, s):
+        try:
+            super().write(s)
+        except ValueError as ex:
+            # I/O operation on uninitialized object?
+            if 'uninitialized' not in str(ex).lower():
+                raise
+        self.buffer.write(s.encode() if hasattr(s, 'encode') else s)
