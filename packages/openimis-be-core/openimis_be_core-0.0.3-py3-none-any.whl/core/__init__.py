6cddfb09a29f92c155d7868eab015adf66243e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package openimis-be-core/0.0.3/openimis_be_core-0.0.3-py3-none-any.whl

---
+++ packages/openimis-be-core/openimis_be_core-0.0.3-py3-none-any.whl/core/__init__.py
@@ -0,0 +1,3 @@
+from .datetimes.shared import *
+
+default_app_config = 'core.apps.CoreConfig'
---
+++ packages/openimis-be-core/openimis_be_core-0.0.3-py3-none-any.whl/core/datetimes/test_ad_datetime.py
@@ -0,0 +1,251 @@
+import sys
+import importlib
+import core
+from django.test import TestCase
+from datetime import date as py_date
+from datetime import datetime as py_datetime
+from .shared import datetimedelta
+
+
+class DateTestCase(TestCase):
+    def setUp(self):
+        core.calendar = importlib.import_module(
+            '.calendars.ad_calendar', 'core')
+        core.datetime = importlib.import_module(
+            '.datetimes.ad_datetime', 'core')
+
+    def test_ad_date(self):
+        py_dt = py_date(2020, 1, 13)
+        ad_dt = core.datetime.date(2020, 1, 13)
+        self.assertEqual(py_dt, ad_dt)
+
+    def test_repr(self):
+        ad_dt = core.datetime.date(2020, 1, 13)
+        self.assertEqual(
+            repr(ad_dt), 'core.datetimes.ad_datetime.date(2020, 1, 13)')
+
+    def test_str(self):
+        ad_dt = core.datetime.date(2020, 1, 13)
+        self.assertEqual(str(ad_dt), '2020-01-13')
+
+    def test_from_ad_datetime(self):
+        db_dt = core.datetime.date.from_ad_datetime(py_datetime(2020, 1, 13))
+        ad_dt = core.datetime.date(2020, 1, 13)
+        self.assertEqual(db_dt, ad_dt)
+
+    def test_to_ad_datetime(self):
+        ad_dt = core.datetime.date(2020, 1, 13)
+        db_dt = ad_dt.to_ad_datetime()
+        self.assertEqual(db_dt, py_datetime(2020, 1, 13))
+
+    def test_raw_iso_format(self):
+        dt = core.datetime.date(2019, 3, 22)
+        self.assertEquals("2019-03-22", dt.raw_isoformat())
+
+    def test_ad_iso_format(self):
+        dt = core.datetime.date(2019, 3, 22)
+        self.assertEquals("2019-03-22", dt.ad_isoformat())
+
+    def test_add_sub_years(self):
+        dt = core.datetime.date(2020, 10, 22)
+        self.assertEquals(core.datetime.date(2022, 10, 22),
+                          (dt + datetimedelta(years=2)))
+        self.assertEquals(core.datetime.date(2018, 10, 22),
+                          (dt + datetimedelta(years=-2)))
+        self.assertEquals(core.datetime.date(2022, 10, 22),
+                          (dt - datetimedelta(years=-2)))
+        self.assertEquals(core.datetime.date(2018, 10, 22),
+                          (dt - datetimedelta(years=2)))
+
+    def test_add_sub_months(self):
+        dt = core.datetime.date(2020, 10, 22)
+        self.assertEquals(core.datetime.date(2020, 12, 22),
+                          (dt + datetimedelta(months=2)))
+        self.assertEquals(core.datetime.date(2020, 8, 22),
+                          (dt + datetimedelta(months=-2)))
+        self.assertEquals(core.datetime.date(2020, 8, 22),
+                          (dt - datetimedelta(months=2)))
+        self.assertEquals(core.datetime.date(2020, 12, 22),
+                          (dt - datetimedelta(months=-2)))
+
+    def test_add_sub_days(self):
+        dt = core.datetime.date(2019, 3, 22)
+        self.assertEquals(core.datetime.date(2019, 4, 5),
+                          (dt + core.datetime.timedelta(days=14)))
+        self.assertEquals(core.datetime.date(2019, 4, 5),
+                          (dt - core.datetime.timedelta(days=-14)))
+
+        dt = core.datetime.date(2019, 3, 11)
+        self.assertEquals(core.datetime.date(2019, 2, 25),
+                          (dt - core.datetime.timedelta(days=14)))
+        self.assertEquals(core.datetime.date(2019, 2, 25),
+                          (dt + core.datetime.timedelta(days=-14)))
+
+    def test_add_sub_mixed(self):
+        dt = core.datetime.date(2020, 10, 22)
+        self.assertEquals(
+            core.datetime.date(2020, 12, 6),
+            (dt + datetimedelta(months=1, days=14)))
+        self.assertEquals(
+            core.datetime.date(2021, 2, 28),
+            (dt + 4 * datetimedelta(months=1, days=1, hours=14, seconds=36)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 10, 22),
+            (dt + datetimedelta(days=-1, hours=24)))
+        self.assertEquals(
+            core.datetime.date(2021, 2, 20),
+            (dt + 4 * datetimedelta(months=1, days=-1, hours=14, seconds=36)))
+
+    def test_eq_gt_ge_lt_le(self):
+        dt = core.datetime.date(2019, 3, 11)
+        dt_bis = core.datetime.date(2019, 3, 11)
+        dt_after = core.datetime.date(2019, 3, 12)
+        self.assertTrue(dt == dt_bis)
+        self.assertTrue(dt >= dt_bis)
+        self.assertTrue(dt <= dt_bis)
+        self.assertTrue(dt < dt_after)
+        self.assertTrue(dt <= dt_after)
+        self.assertTrue(dt_after > dt)
+        self.assertTrue(dt_after >= dt)
+
+    def test_diff(self):
+        ad_dt_1 = core.datetime.date(2019, 12, 7)
+        ad_dt_2 = core.datetime.date(2019, 12, 8)
+        self.assertEqual(ad_dt_2 - ad_dt_1, core.datetime.timedelta(1))
+
+        ad_dt_2 = core.datetime.date(2020, 1, 7)
+        self.assertEqual(ad_dt_2 - ad_dt_1, core.datetime.timedelta(days=31))
+
+    def test_from_ad_isoformat(self):
+        ad_dt = core.datetime.date(2019, 4, 2)
+        ad_iso = core.datetime.date.from_ad_isoformat('2019-04-02')
+        self.assertEqual(ad_dt, ad_iso)
+        adt_iso = core.datetime.date.from_ad_isoformat('2019-04-02T12:34:49')
+        self.assertEqual(ad_dt, adt_iso)
+
+    def test_from_raw_isoformat(self):
+        ad_dt = core.datetime.date(2019, 4, 2)
+        ad_iso = core.datetime.date.from_raw_isoformat('2019-04-02')
+        self.assertEqual(ad_dt, ad_iso)
+        adt_iso = core.datetime.date.from_raw_isoformat('2019-04-02T12:34:49')
+        self.assertEqual(ad_dt, adt_iso)
+
+
+class DatetimeTestCase(TestCase):
+    def setUp(self):
+        core.calendar = importlib.import_module(
+            '.calendars.ad_calendar', 'core')
+        core.datetime = importlib.import_module(
+            '.datetimes.ad_datetime', 'core')
+
+    def test_ad_datetime(self):
+        py_dt = py_datetime(2020, 1, 13, 10, 9, 55, 728267)
+        ad_dt = core.datetime.datetime(2020, 1, 13, 10, 9, 55, 728267)
+        self.assertEqual(py_dt, ad_dt)
+
+    def test_repr(self):
+        ad_dt = core.datetime.datetime(2020, 1, 13, 10, 9, 55, 728267)
+        self.assertEqual(repr(
+            ad_dt), 'core.datetimes.ad_datetime.datetime(2020, 1, 13, 10, 9, 55, 728267)')
+
+    def test_str(self):
+        ad_dt = core.datetime.datetime(2020, 1, 13, 10, 9, 55, 728267)
+        self.assertEqual(str(ad_dt), '2020-01-13 10:09:55.728267')
+
+    def test_add_sub_years(self):
+        dt = core.datetime.datetime(2020, 10, 22, 14, 25, 44, 876134)
+        self.assertEquals(
+            core.datetime.datetime(2022, 10, 22, 14, 25, 44, 876134),
+            (dt + datetimedelta(years=2)))
+        self.assertEquals(
+            core.datetime.datetime(2018, 10, 22, 14, 25, 44, 876134),
+            (dt + datetimedelta(years=-2)))
+        self.assertEquals(
+            core.datetime.datetime(2022, 10, 22, 14, 25, 44, 876134),
+            (dt - datetimedelta(years=-2)))
+        self.assertEquals(
+            core.datetime.datetime(2018, 10, 22, 14, 25, 44, 876134),
+            (dt - datetimedelta(years=2)))
+
+    def test_add_sub_months(self):
+        dt = core.datetime.datetime(2020, 10, 22, 14, 25, 44, 876134)
+        self.assertEquals(
+            core.datetime.datetime(2020, 12, 22, 14, 25, 44, 876134),
+            (dt + datetimedelta(months=2)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 8, 22, 14, 25, 44, 876134),
+            (dt + datetimedelta(months=-2)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 8, 22, 14, 25, 44, 876134),
+            (dt - datetimedelta(months=2)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 12, 22, 14, 25, 44, 876134),
+            (dt - datetimedelta(months=-2)))
+
+    def test_add_sub_days(self):
+        dt = core.datetime.datetime(2019, 3, 22)
+        self.assertEquals(core.datetime.datetime(2019, 4, 5),
+                          (dt + core.datetime.timedelta(days=14)))
+        self.assertEquals(core.datetime.datetime(2019, 4, 5),
+                          (dt - core.datetime.timedelta(days=-14)))
+
+        dt = core.datetime.datetime(2019, 3, 11)
+        self.assertEquals(core.datetime.datetime(2019, 2, 25),
+                          (dt - core.datetime.timedelta(days=14)))
+        self.assertEquals(core.datetime.datetime(2019, 2, 25),
+                          (dt + core.datetime.timedelta(days=-14)))
+
+    def test_add_sub_mixed(self):
+        dt = core.datetime.datetime(2019, 10, 22, 5, 33, 24, 786222)
+        self.assertEquals(
+            core.datetime.datetime(2019, 12, 6, 5, 33, 24, 786222),
+            (dt + datetimedelta(months=1, days=14)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 2, 28, 13, 35, 48, 786222),
+            (dt + 4 * datetimedelta(months=1, days=1, hours=14, seconds=36)))
+        self.assertEquals(
+            core.datetime.datetime(2019, 10, 22, 5, 33, 24, 786222),
+            (dt + datetimedelta(days=-1, hours=24)))
+        self.assertEquals(
+            core.datetime.datetime(2020, 2, 20, 13, 35, 48, 786222),
+            (dt + 4 * datetimedelta(months=1, days=-1, hours=14, seconds=36)))
+
+    def test_eq_gt_ge_lt_le(self):
+        dt = core.datetime.datetime(2019, 3, 11)
+        dt_bis = core.datetime.datetime(2019, 3, 11)
+        dt_after = core.datetime.datetime(2019, 3, 12)
+        self.assertTrue(dt == dt_bis)
+        self.assertTrue(dt >= dt_bis)
+        self.assertTrue(dt <= dt_bis)
+        self.assertTrue(dt < dt_after)
+        self.assertTrue(dt <= dt_after)
+        self.assertTrue(dt_after > dt)
+        self.assertTrue(dt_after >= dt)
+
+    def test_diff(self):
+        ad_dt_1 = core.datetime.datetime(2019, 12, 7)
+        ad_dt_2 = core.datetime.datetime(2019, 12, 8)
+        self.assertEqual(ad_dt_2 - ad_dt_1, core.datetime.timedelta(1))
+
+        ad_dt_1 = core.datetime.datetime(2019, 12, 7, 11, 5, 34, 999999)
+        ad_dt_2 = core.datetime.datetime(2020, 1, 7, 11, 7, 34, 999999)
+        self.assertEqual(ad_dt_2 - ad_dt_1,
+                         core.datetime.timedelta(days=31, minutes=2))
+
+    def test_from_ad_isoformat(self):
+        ad_dt = core.datetime.datetime(2019, 4, 2)
+        ad_iso = core.datetime.datetime.from_ad_isoformat('2019-04-02')
+        self.assertEqual(ad_dt, ad_iso)
+        ad_dt = core.datetime.datetime(2019, 4, 2, 12, 34, 49)
+        adt_iso = core.datetime.datetime.from_ad_isoformat(
+            '2019-04-02T12:34:49')
+        self.assertEqual(ad_dt, adt_iso)
+
+    def test_from_raw_isoformat(self):
+        ad_dt = core.datetime.datetime(2019, 4, 2)
+        ad_iso = core.datetime.datetime.from_raw_isoformat('2019-04-02')
+        self.assertEqual(ad_dt, ad_iso)
+        ad_dt = core.datetime.datetime(2019, 4, 2, 12, 34, 49)
+        adt_iso = core.datetime.datetime.from_raw_isoformat(
+            '2019-04-02T12:34:49')
+        self.assertEqual(ad_dt, adt_iso)
