855b23f8ef6c0714ab97af5101c876d242d540b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:16 +0000

    Add package scipion-em-empiar/1.0.3/scipion-em-empiar-1.0.3.tar.gz

---
+++ packages/scipion-em-empiar/scipion-em-empiar-1.0.3.tar.gz/scipion-em-empiar-1.0.3/setup.py
@@ -0,0 +1,35 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='scipion-em-empiar',
+    version='1.0.3',
+    description='A Scipion plugin to make depositions to EMPIAR',
+    long_description=long_description,
+    url='https://github.com/scipion-em/scipion-em-empiar',
+    author='I2PC',
+    author_email='scipion@cnb.csic.es',
+    keywords='scipion empiar scipion-2.0',
+    packages=find_packages(),
+    install_requires=['empiar-depositor', 'jsonschema'],
+    package_data={
+       'empiar': ['EMPIAR_logo.png', 'empiar_deposition.schema.json',
+                  'empiar_deposition_template.json', 'protocols.conf'],
+    }
+)
