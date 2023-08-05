9b0356d83eddd875f189e4aeda3fd302c1112068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package e3db/2.1.3/e3db-2.1.3.tar.gz

---
+++ packages/e3db/e3db-2.1.3.tar.gz/e3db-2.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+version = "2.1.3"
+setup(
+  name="e3db",
+  version=version,
+  packages=find_packages(),
+  install_requires=[
+    'PyNaCl >= 1.3.0, < 2',
+    'requests >= 2.19.1, < 3',
+    'Cryptography >= 2.2',
+  ],
+  url = "https://github.com/tozny/e3db-python",
+  download_url = 'https://github.com/tozny/e3db-python/archive/{0}.tar.gz'.format(version),
+  description  = open("README.md").read(),
+  author = "Tozny, LLC",
+  author_email = "info@tozny.com",
+  license = open("LICENSE.md").read(),
+  keywords = ['e3db', 'encryption', 'encrypted-store'],
+  classifiers = [],
+)
