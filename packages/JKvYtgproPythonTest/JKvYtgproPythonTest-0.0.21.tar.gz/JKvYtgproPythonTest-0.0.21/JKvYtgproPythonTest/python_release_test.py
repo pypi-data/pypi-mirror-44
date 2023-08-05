3593dee8f027b861ef364dee3da98ee6711c14fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package JKvYtgproPythonTest/0.0.21/JKvYtgproPythonTest-0.0.21.tar.gz

---
+++ packages/JKvYtgproPythonTest/JKvYtgproPythonTest-0.0.21.tar.gz/JKvYtgproPythonTest-0.0.21/JKvYtgproPythonTest/python_release_test.py
@@ -0,0 +1,23 @@
+
+__author__ = "Yury D."
+__credits__ = ["Yury D.", "Some Dude", "Some Dudette"]
+__license__ = "MIT"
+__version__ = "0.0.21"
+__maintainer__ = "Yury D."
+__email__ = "SoulGate@yandex.ru"
+__status__ = "Alpha"
+
+class PythonReleaseTest:
+    """
+    Python Release Test Class
+    """
+    def __init__(self):
+        self.variable = 1
+
+    def execute(self):
+        """
+        Some function
+        :return: value of self.variable
+        """
+        print("Python Release Test, variable="+str(self.variable))
+        return self.variable
