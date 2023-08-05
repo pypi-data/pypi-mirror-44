4d464c676cf7d187f717258ec1201e7530107a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package InstagramGraphQL/1.0.10/InstagramGraphQL-1.0.10-py3-none-any.whl

---
+++ packages/InstagramGraphQL/InstagramGraphQL-1.0.10-py3-none-any.whl/InstagramGraphQL/media.py
@@ -0,0 +1,66 @@
+import InstagramGraphQL
+
+from . import constants
+from .utils import get_value_deep_key, paginator
+
+
+class Media:
+    def __init__(self, data, api):
+        self.api = api
+        self.data = data
+
+    def comments(self, variables={}):
+        data = get_value_deep_key(self.data, constants.LOAD_COMMENTS["keys"][1])
+        return paginator(
+            self.api,
+            data,
+            constants.LOAD_COMMENTS["keys"],
+            {
+                "query_hash": constants.LOAD_COMMENTS["hash"],
+                "variables": {
+                    **constants.LOAD_COMMENTS["variables"],
+                    "shortcode": data["shortcode"],
+                    **variables,
+                },
+            },
+        )
+
+    def __first_liked_by(self, variables):
+        return self.api.query.GET(
+            params={
+                "query_hash": constants.LOAD_LIKED_BY["hash"],
+                "variables": InstagramGraphQL.InstagramGraphQL.dumps(
+                    {
+                        **constants.LOAD_LIKED_BY["variables"],
+                        "shortcode": get_value_deep_key(
+                            self.data, constants.LOAD_LIKED_BY["keys"][1]
+                        )["shortcode"],
+                        **variables,
+                    }
+                ),
+            }
+        )
+
+    def liked_by(self, variables={}):
+        get_value_deep_key(self.data, constants.LOAD_LIKED_BY["keys"][1])[
+            constants.LOAD_LIKED_BY["keys"][0]
+        ] = get_value_deep_key(
+            self.__first_liked_by(variables), constants.LOAD_LIKED_BY["keys"][1]
+        )[
+            constants.LOAD_LIKED_BY["keys"][0]
+        ]
+
+        data = get_value_deep_key(self.data, constants.LOAD_LIKED_BY["keys"][1])
+        return paginator(
+            self.api,
+            data,
+            constants.LOAD_LIKED_BY["keys"],
+            {
+                "query_hash": constants.LOAD_LIKED_BY["hash"],
+                "variables": {
+                    **constants.LOAD_LIKED_BY["variables"],
+                    "shortcode": data["shortcode"],
+                    **variables,
+                },
+            },
+        )
---
+++ packages/InstagramGraphQL/InstagramGraphQL-1.0.10-py3-none-any.whl/InstagramGraphQL/user.py
@@ -0,0 +1,26 @@
+import InstagramGraphQL
+
+from . import constants
+from .utils import get_value_deep_key, paginator
+
+
+class User:
+    def __init__(self, data, api):
+        self.api = api
+        self.data = data
+
+    def timeline(self, variables={}):
+        data = get_value_deep_key(self.data, constants.LOAD_TIMELINE["keys"][1])
+        return paginator(
+            self.api,
+            data,
+            constants.LOAD_TIMELINE["keys"],
+            {
+                "query_hash": constants.LOAD_TIMELINE["hash"],
+                "variables": {
+                    **constants.LOAD_TIMELINE["variables"],
+                    "id": data["id"],
+                    **variables,
+                },
+            },
+        )
