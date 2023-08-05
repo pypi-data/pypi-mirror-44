7bd3ff8b1822ffc905cff53a118327cf2821d13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:31 +0000

    Add package refcliq/0.0.6/refcliq-0.0.6.tar.gz

---
+++ packages/refcliq/refcliq-0.0.6.tar.gz/refcliq-0.0.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="refcliq",
+    version="0.0.6",
+    author="Fabio Dias",
+    author_email="fabio.dias@gmail.com",
+    description="Community analysis in bibliographical references",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fabioasdias/RefCliq",
+    packages=setuptools.find_packages(),
+    scripts=['refcliq/refcliq.py'],
+    install_requires=[
+        "python-louvain",
+        "numpy",
+        "pybtex",
+        "nltk",
+        "tqdm",
+        "titlecase",
+        "fuzzywuzzy[speedup]"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
