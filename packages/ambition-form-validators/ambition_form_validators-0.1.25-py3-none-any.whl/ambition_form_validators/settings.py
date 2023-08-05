a8321af40b1122dae0db6be021b13faf0d53b197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package ambition-form-validators/0.1.25/ambition_form_validators-0.1.25-py3-none-any.whl

---
+++ packages/ambition-form-validators/ambition_form_validators-0.1.25-py3-none-any.whl/ambition_form_validators/settings.py
@@ -0,0 +1,149 @@
+import os
+import sys
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APP_NAME = "ambition_form_validators"
+ETC_DIR = os.path.join(BASE_DIR, "etc")
+SITE_ID = 40
+REVIEWER_SITE_ID = 0
+
+# AUTO_CREATE_KEYS = True
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "6_sw)a7sn3tt%tbx$aps62a90dp&yfdri+a#=udt6g_4h--$q9"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django_crypto_fields.apps.AppConfig",
+    "django_revision.apps.AppConfig",
+    "edc_appointment.apps.AppConfig",
+    "edc_base.apps.AppConfig",
+    "edc_device.apps.AppConfig",
+    "edc_metadata.apps.AppConfig",
+    "edc_identifier.apps.AppConfig",
+    "edc_protocol.apps.AppConfig",
+    "edc_registration.apps.AppConfig",
+    "edc_timepoint.apps.AppConfig",
+    "edc_form_validators.apps.AppConfig",
+    "edc_visit_schedule.apps.AppConfig",
+    "ambition_screening.apps.AppConfig",
+    "ambition_labs.apps.AppConfig",
+    "ambition_lists.apps.AppConfig",
+    "ambition_form_validators.apps.AppConfig",
+]
+
+MIDDLEWARE_CLASSES = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "ambition_form_validators.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+WSGI_APPLICATION = "ambition_form_validators.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = "/static/"
+
+DASHBOARD_URL_NAMES = {
+    "subject_models_url": "subject_models_url",
+    "subject_listboard_url": "ambition_dashboard:subject_listboard_url",
+    "screening_listboard_url": "ambition_dashboard:screening_listboard_url",
+    "subject_dashboard_url": "ambition_dashboard:subject_dashboard_url",
+}
+
+
+if "test" in sys.argv:
+
+    COUNTRY = "botswana"
+
+    class DisableMigrations:
+        def __contains__(self, item):
+            return True
+
+        def __getitem__(self, item):
+            return None
+
+    MIGRATION_MODULES = DisableMigrations()
+    PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
+    DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"
