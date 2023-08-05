83123c6876a2fd369fdf5b5f1370b9b9985bf69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package InstagramGraphQL/1.0.10/InstagramGraphQL-1.0.10.tar.gz

---
+++ packages/InstagramGraphQL/InstagramGraphQL-1.0.10.tar.gz/InstagramGraphQL-1.0.10/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / 'README.md').read_text()
+
+# This call to setup() does all the work
+setup(
+    name='InstagramGraphQL',
+    version='1.0.10',
+    description='InstagramGraphQL Unoffical API',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/technerium/InstagramGraphQL',
+    author='Technerium',
+    author_email='technerium@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['InstagramGraphQL'],
+    install_requires=['SmoothAPI==1.0.2']
+)
