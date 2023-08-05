5f9157b702217db8dd9a978c13583a0318947389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package pyECG/0.0.5/pyECG-0.0.5-py2.py3-none-any.whl

---
+++ packages/pyECG/pyECG-0.0.5-py2.py3-none-any.whl/pyecg/importers/ishine.py
@@ -0,0 +1,24 @@
+import os
+
+from ishneholterlib import Holter
+
+from pyecg import ECGRecord, Time, Signal
+from . import Importer
+
+
+class ISHINELoader(Importer):
+
+    def load(self, ecg_file) -> ECGRecord:
+        if not os.path.isfile(ecg_file):
+            raise FileNotFoundError(f"{ecg_file} is not found")
+        record = Holter(ecg_file)
+        record.load_data()
+        time = Time.from_fs_samples(record.sr, len(record.lead[0].data))
+        record_name = os.path.split(ecg_file)[1]
+        record_name = ".".join(record_name.split(".")[:-1])
+        new_record = ECGRecord(name=record_name, time=time)
+
+        for leadi in record.lead:
+            new_record.add_signal(Signal(leadi.data, str(leadi)))
+
+        return new_record
