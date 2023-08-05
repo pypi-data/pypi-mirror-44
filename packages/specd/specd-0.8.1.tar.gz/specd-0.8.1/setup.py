1789cf702c5c0d974f4c35e4384c301aa52d6bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package specd/0.8.1/specd-0.8.1.tar.gz

---
+++ packages/specd/specd-0.8.1.tar.gz/specd-0.8.1/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+history = ""
+
+with open('dev-requirements.txt') as dev_requirements_file:
+    tests_require = [r.strip() for r in dev_requirements_file.readlines()]
+
+setup(
+    name="specd",
+    version='0.8.1',
+    author="Ian Maurer",
+    author_email='ian@genomoncology.com',
+    url="https://github.com/genomoncology/specd",
+
+    package_dir={
+        '': 'src'
+    },
+
+    packages=[
+        "specd",
+        "specd.sdk",
+    ],
+
+    include_package_data=True,
+
+    package_data={
+        '': ['*.yaml'],
+    },
+
+    install_requires=[
+        "click >= 6.7",
+        "flask-swagger-ui >= 3.6.0",
+        "swagger-spec-validator >= 2.1.0",
+        "PyYAML >= 3.12",
+        "dictdiffer >= 0.7.1",
+        "related >= 0.6.3",
+        "inflect >= 0.3.1",
+        "aiobravado == 0.9.3",
+        "bravado == 10.0.0",
+        "bravado-core == 5.0.4",
+        "bravado-asyncio == 1.0.0",
+        "stringcase >= 1.2.0",
+        "jsonschema[format]",
+    ],
+
+    setup_requires=[
+        'pytest-runner',
+    ],
+
+    license="MIT license",
+
+    keywords="Swagger Open API Specification Bravado",
+    description="specd",
+    long_description="%s\n\n%s" % (readme, history),
+
+    entry_points={
+        'console_scripts': [
+            'specd=specd.cli:cli',
+        ],
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
