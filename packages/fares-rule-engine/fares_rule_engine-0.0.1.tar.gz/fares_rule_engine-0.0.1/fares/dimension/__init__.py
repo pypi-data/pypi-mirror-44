c4cf28d7f58bb6932503f905a6571396ad047827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package fares-rule-engine/0.0.1/fares_rule_engine-0.0.1.tar.gz

---
+++ packages/fares-rule-engine/fares_rule_engine-0.0.1.tar.gz/fares_rule_engine-0.0.1/fares/dimension/__init__.py
@@ -0,0 +1,26 @@
+from fares.dimension.dimension import SlotDimensions, UserDimensions
+
+
+def resolve_slot_dimensions(slot):
+    return {
+        "slot.od_cluster_session": (
+            slot[SlotDimensions.ORIGIN_CLUSTER_ID.value],
+            slot[SlotDimensions.DESTINATION_CLUSTER_ID.value],
+            slot[SlotDimensions.SESSION.value],
+        ),
+        "slot.origin_cluster_id": slot[SlotDimensions.ORIGIN_CLUSTER_ID.value],
+        "slot.destination_cluster_id": slot[
+            SlotDimensions.DESTINATION_CLUSTER_ID.value
+        ],
+        "slot.session": slot[SlotDimensions.SESSION.value],
+        "slot.route_id": slot[SlotDimensions.ROUTE_ID.value],
+        "slot.start_time": slot[SlotDimensions.START_TIME.value],
+    }
+
+
+def resolve_user_dimensions(user):
+    return {
+        "user.pass_purchase_count": user[UserDimensions.PASS_PURCHASE_COUNT.value],
+        "user.id": user[UserDimensions.ID.value],
+        "user.gender": user[UserDimensions.GENDER.value],
+    }
