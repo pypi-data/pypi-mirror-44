3d5b5936c758d1f2b68e4f97b689cce53ce659c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package iterchain/0.0.1/iterchain-0.0.1.tar.gz

---
+++ packages/iterchain/iterchain-0.0.1.tar.gz/iterchain-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='iterchain',
+    version='0.0.1',
+    description='Iterator chaining for Python',
+    long_description='',
+    url='https://github.com/Evelyn-H/iterchain',
+    download_url='https://github.com/Evelyn-H/iterchain',
+    author='Evelyn-H',
+    author_email='hobert.evelyn@gmail.com',
+    packages=[],
+    classifiers=[
+      'Development Status :: 1 - Planning',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
