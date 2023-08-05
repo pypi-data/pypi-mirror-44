351b922d359a37afb8edd9119a419617e99fdfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package elit/0.1.29.dev1553713736/elit-0.1.29.dev1553713736-py3-none-any.whl

---
+++ packages/elit/elit-0.1.29.dev1553713736-py3-none-any.whl/elit/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.29'
+version = '0.1.29'
+full_version = '0.1.29.dev1553713736'
+dev_version = '1553713736'
+release = False
+if not release:
+    version = full_version
