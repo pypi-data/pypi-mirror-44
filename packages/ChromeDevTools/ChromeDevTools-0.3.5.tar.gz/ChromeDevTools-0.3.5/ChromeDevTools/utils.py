5afe84e2e9f2c6354613e7a88e70d4e05651bdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:06 +0000

    Add package ChromeDevTools/0.3.5/ChromeDevTools-0.3.5.tar.gz

---
+++ packages/ChromeDevTools/ChromeDevTools-0.3.5.tar.gz/ChromeDevTools-0.3.5/ChromeDevTools/utils.py
@@ -0,0 +1,19 @@
+import re
+from typing import Any, Mapping
+
+__all__ = ('camelize', 'underscore', 'normalize_dict')
+
+
+def camelize(s: str) -> str:
+  return re.sub('_([a-z])', lambda m: m.group(1).upper(), s)
+
+
+def underscore(s: str) -> str:
+  return re.sub('([a-z])([A-Z])', r'\1_\2', s).lower()
+
+
+def normalize_dict(d: Mapping[str, Any]) -> Mapping[str, Any]:
+  return {
+    underscore(k): normalize_dict(v) if isinstance(v, dict) else v
+    for k, v in d.items()
+  }
---
+++ packages/ChromeDevTools/ChromeDevTools-0.3.5.tar.gz/ChromeDevTools-0.3.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+package = 'ChromeDevTools'
+cur_dir = Path(__file__).parent
+with open((cur_dir / 'README.md').resolve()) as f:
+  readme = f.read()
+
+with open((cur_dir / 'LICENSE').resolve()) as f:
+  license = f.read()
+
+about = {}
+with open((cur_dir / package / 'version.py').resolve()) as f:
+  exec(f.read(), about)
+
+with open((cur_dir / 'requirements.txt').resolve()) as f:
+  install_requires = f.read().splitlines()
+
+setup(
+  name=package,
+  description='Async Chrome DevTools',
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  # license=license,
+  author=about['__author__'],
+  author_email=about['__email__'],
+  version=about['__version__'],
+  url='https://github.com/codedumps/async-chrome-dev-tools',
+  install_requires=install_requires,
+  keywords='async chrome devtools',
+  packages=find_packages(),
+  # не помогло нахуй
+  # data_files=[('', ['LICENSE'])],
+  package_dir={package: package},
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Framework :: AsyncIO',
+    'Topic :: Software Development :: Debuggers',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7'
+  ]
+)
