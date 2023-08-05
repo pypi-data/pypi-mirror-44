befe1089c9d6a54eaf9cba7fdb66bd15d28c834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-espeon/0.0.1a0/eevee-espeon-0.0.1a0.tar.gz

---
+++ packages/eevee-espeon/eevee-espeon-0.0.1a0.tar.gz/eevee-espeon-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="eevee-espeon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Espeon Extension for the Eevee Discord Framework",
+    packages=['eevee.ext.espeon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
