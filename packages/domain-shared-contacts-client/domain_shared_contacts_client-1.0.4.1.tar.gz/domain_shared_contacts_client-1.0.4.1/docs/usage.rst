8229eefd04653eb47459d828bffa518c62a7ede3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package domain-shared-contacts-client/1.0.4.1/domain_shared_contacts_client-1.0.4.1.tar.gz

---
+++ packages/domain-shared-contacts-client/domain_shared_contacts_client-1.0.4.1.tar.gz/domain_shared_contacts_client-1.0.4.1/docs/usage.rst
@@ -0,0 +1,178 @@
+=====
+Usage
+=====
+
+
+Use Google Domain Shared Contacts Client in a project to list, create, read, update and delete contacts::
+
+    import domain_shared_contacts_client
+    import json
+    
+    # Create the client. Authenticates to the Google Data API using the service account credentials provided
+    client = domain_shared_contacts_client.Client(
+        domain='example.com', 
+        admin='admin@example.com', 
+        credentials='/path/to/credentials.json')
+    
+    # Fetch the list of contacts currently available
+    contacts = client.get_contacts()
+    print json.dumps(contacts, default=contacts_helper.convert_contacts)
+
+Fetch contacts using the CLI::
+
+    $ domain_shared_contacts_client \
+        example.com \
+        admin@example.com \
+        /path/to/client_secret.json \
+        list
+
+Example new_contact.json::
+
+    {
+        "name": {
+            "given_name": "John",
+            "family_name": "Doe",
+            "full_name": "John Doe"
+        },
+        "email": [
+            {
+                "address": "jdoe@somewhere.com",
+                "primary": "true"
+            }
+        ],
+        "organization": {
+            "name": "Example Pty Ltd",
+            "title": "Senior Manager",
+            "job_description": "Manage development activities",
+            "department": "Software Engineering",
+            "symbol": "EXPL"
+        },
+        "phone_number": [
+            {
+                "text": "(888)555-1212",
+                "primary": "true"
+            }
+        ],
+        "structured_postal_address": [
+            {
+                "street": "1 Example St.",
+                "city": "Springfield",
+                "region": "IL",
+                "postcode": "62701",
+                "country": "United States",
+                "primary": "true"
+            }
+        ]
+    }
+
+
+Create a new contact::
+
+    saved_contact = client.create_contact('/path/to/new_contact.json')
+    print json.dumps(saved_contact)
+
+Create a new contact using the CLI::
+
+    $ domain_shared_contacts_client \
+        example.com \
+        admin@example.com \
+        /path/to/client_secret.json \
+        create /path/to/new_contact.json
+    {"id": "http://www.google.com/m8/feeds/contacts/example.com/base/2ba2136d0e101978"}
+
+Read a contact::
+
+    contact = client.read_contact(saved_contact['id'])
+    print json.dumps(contact, default=contacts_helper.convert_contact)
+
+Read a contact using the CLI::
+
+    $ domain_shared_contacts_client \
+        example.com \
+        admin@example.com \
+        /path/to/client_secret.json \
+        read 2ba2136d0e101978
+
+Example update_contact.json::
+
+    {
+        "name": {
+            "given_name": "Joe",
+            "full_name": "Joe Doe"
+        },
+        "email": [
+            {
+                "address": "jdoe@somewhereelse.com",
+                "primary": "true"
+            },
+            {
+                "address": "jdoe@somewhere.com",
+                "primary": "false"
+            }
+        ]
+    }
+
+
+Update a contact::
+
+    contact = client.update_contact(saved_contact['id'], '/path/to/updated_contact.json')
+    print json.dumps(contact, default=contacts_helper.convert_contact)
+
+Update a contact using the CLI::
+
+    $ domain_shared_contacts_client \
+        example.com \
+        admin@example.com \
+        /path/to/client_secret.json \
+        update /path/to/updated_contact.json
+
+Delete a contact::
+
+    result = client.delete_contact(saved_contact['id'])
+    print json.dumps(result)
+
+Delete a contact using the CLI::
+
+    $ domain_shared_contacts_client
+        example.com \
+        admin@example.com \
+        /path/to/client_secret.json \
+        delete 2ba2136d0e101978
+    {"status": "OK"}
+
+This package assumes the following:
+
+- You have a Google Apps domain account
+
+- You are able to login as the Domain Admin for the domain account
+
+- You have created a `Service Account`_ and enabled G-Suite Domain-wide Delegation for that account
+
+- You have created a key for the service account and downloaded it in JSON format
+
+  - This will be provided in the 'credentials' parameter to instantiate a Client
+
+- You have granted your service account authority to make API calls on your behalf
+
+  - Go to the domain Admin Console
+
+  - Select Security from the list of controls. If you don't see Security listed, select More controls from the gray bar at 
+    the bottom of the page, then select Security from the list of controls. If you can't see the controls, make sure you're 
+    signed in as an administrator for the domain.
+    
+  - Select Show more and then Advanced settings from the list of options.
+
+  - Select Manage API client access in the Authentication section.
+
+  - In the Client Name field enter the service account's Client ID. You can find your service account's client ID in the 
+    Service accounts page.
+
+  - In the One or More API Scopes field enter the list of scopes that your application should be granted access to. 
+    In our case, that is http://www.google.com/m8/feeds/contacts/
+
+  - Click Authorize
+
+  - Your application now has the authority to make API calls as users in your domain (to "impersonate" users).
+
+
+.. _Service Account: https://console.developers.google.com/permissions/serviceaccounts
---
+++ packages/domain-shared-contacts-client/domain_shared_contacts_client-1.0.4.1.tar.gz/domain_shared_contacts_client-1.0.4.1/domain_shared_contacts_client/contacts_helper.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+import gdata.contacts.data
+import gdata.data
+
+GD_ORGANIZATION = (
+    ('department', gdata.data.OrgDepartment),
+    ('job_description', gdata.data.OrgJobDescription),
+    ('name', gdata.data.OrgName),
+    ('symbol', gdata.data.OrgSymbol),
+    ('title', gdata.data.OrgTitle),
+)
+
+
+def fetch_attribute_text(attr):
+    if attr is None:
+        return ''
+    else:
+        return attr.text
+
+
+def fetch_attribute_boolean(attr):
+    if attr is None:
+        return False
+    else:
+        return attr
+
+
+def convert_organization(organization):
+    """
+    Handle organization
+    :param entry:
+    :return:
+    """
+    organization = {
+        key: fetch_attribute_text(getattr(organization, key))
+        for key, cls in GD_ORGANIZATION
+        if getattr(organization, key, None)
+    }
+    return organization
+
+
+def convert_address(entry):
+    """
+    Handle list of structured postal addresses
+    :param entry:
+    :return:
+    """
+    address_list = []
+    for address in entry.structured_postal_address:
+        address_list.append({
+            'primary': fetch_attribute_boolean(address.primary),
+            'street': fetch_attribute_text(address.street),
+            'city': fetch_attribute_text(address.city),
+            'region': fetch_attribute_text(address.region),
+            'postcode': fetch_attribute_text(address.postcode),
+            'country': fetch_attribute_text(address.country)
+        })
+
+    return address_list
+
+
+def convert_email(entry):
+    """
+    Handle list of email addresses
+    :param entry:
+    :return:
+    """
+    email_list = []
+    for email in entry.email:
+        email_list.append({
+            'address': email.address,
+            'primary': fetch_attribute_boolean(email.primary)
+        })
+
+    return email_list
+
+
+def convert_phone_number(entry):
+    """
+    Handle list of phone numbers
+    :param entry:
+    :return:
+    """
+    phone_list = []
+    for phone in entry.phone_number:
+        phone_list.append({
+            'text': fetch_attribute_text(phone),
+            'primary': fetch_attribute_boolean(phone.primary)
+        })
+
+    return phone_list
+
+
+def convert_contacts(o):
+    """
+    Convert the list of ContactEntry objects into a JSON serializable object
+    :param o:
+    :return:
+    """
+    new_contacts = []
+    for i, entry in enumerate(o.entry):
+        new_entry = {
+            'id': fetch_attribute_text(entry.id),
+            'title': fetch_attribute_text(entry.title),
+            'name': {
+                'given_name': fetch_attribute_text(entry.name.given_name),
+                'family_name': fetch_attribute_text(entry.name.family_name),
+                'full_name': fetch_attribute_text(entry.name.full_name)
+            },
+            'organization': convert_organization(entry.organization),
+            'email': convert_email(entry),
+            'phone_number': convert_phone_number(entry),
+            'structured_postal_address': convert_address(entry)
+        }
+        new_contacts.append(new_entry)
+
+    return new_contacts
+
+
+def convert_contact(entry):
+    """
+    Convert a single ContactEntry object into a JSON serializable object
+    :param entry:
+    :return:
+    """
+    new_entry = {
+        'id': fetch_attribute_text(entry.id),
+        'title': fetch_attribute_text(entry.title),
+        'name': {
+            'given_name': fetch_attribute_text(entry.name.given_name),
+            'family_name': fetch_attribute_text(entry.name.family_name),
+            'full_name': fetch_attribute_text(entry.name.full_name)
+        },
+        'organization': convert_organization(entry.organization),
+        'email': convert_email(entry),
+        'phone_number': convert_phone_number(entry),
+        'structured_postal_address': convert_address(entry)
+    }
+    return new_entry
+
+
+def create_organization(organization_object):
+    """
+    Create an Organization object given the dictionary in organization_object
+    :param organization_object:
+    :return:
+    """
+    new_organization = gdata.data.Organization(label=gdata.data.WORK_REL)
+
+    for key, cls in GD_ORGANIZATION:
+        if key in organization_object and organization_object[key]:
+            setattr(new_organization, key, cls(organization_object[key]))
+
+    return new_organization
+
+
+def create_contact_entry(contact_object):
+    """
+    Create a ContactEntry object given the dictionary in contact_object
+    :param contact_object:
+    :return:
+    """
+    new_contact = gdata.contacts.data.ContactEntry()
+    new_contact.name = gdata.data.Name(
+        given_name=gdata.data.GivenName(text=contact_object['name']['given_name']),
+        family_name=gdata.data.FamilyName(text=contact_object['name']['family_name']),
+        full_name=gdata.data.FullName(text=contact_object['name']['full_name'])
+    )
+    new_contact.organization = create_organization(contact_object['organization'])
+    for email in contact_object['email']:
+        new_contact.email.append(gdata.data.Email(
+            address=email['address'],
+            primary=email['primary'],
+            # TODO support different address types
+            rel=gdata.data.WORK_REL
+        ))
+    for phone_number in contact_object['phone_number']:
+        new_contact.phone_number.append(gdata.data.PhoneNumber(
+            text=phone_number['text'],
+            rel=gdata.data.WORK_REL,
+            primary=phone_number['primary']
+        ))
+    for structured_postal_address in contact_object['structured_postal_address']:
+        new_contact.structured_postal_address.append(gdata.data.StructuredPostalAddress(
+            rel=gdata.data.WORK_REL,
+            primary=structured_postal_address['primary'],
+            street=gdata.data.Street(text=structured_postal_address['street']),
+            city=gdata.data.City(text=structured_postal_address['city']),
+            region=gdata.data.Region(text=structured_postal_address['region']),
+            postcode=gdata.data.Postcode(text=structured_postal_address['postcode']),
+            country=gdata.data.Country(text=structured_postal_address['country'])
+        ))
+
+    return new_contact
+
+
+def update_contact_entry(contact, contact_updates):
+    """
+    Update a contact entry with attributes passed in the contact_updates dictionary. Attributes like 'email' and
+    'phone_number' that are lists are replaced with the contents of the list provided in the json data. If you want to
+    add additional email addresses, for instance, pass the original email addresses along with any new ones together
+    in the 'email' attribute of contact_updates.
+    :param contact:
+    :param contact_updates:
+    :return:
+    """
+    if 'organization' in contact_updates:
+        organization = contact_updates['organization']
+        contact.organization = create_organization(organization)
+
+    if 'name' in contact_updates:
+        if 'given_name' in contact_updates['name']:
+            contact.name.given_name.text = contact_updates['name']['given_name']
+        if 'family_name' in contact_updates['name']:
+            contact.name.family_name.text = contact_updates['name']['family_name']
+        if 'full_name' in contact_updates['name']:
+            contact.name.full_name.text = contact_updates['name']['full_name']
+
+    if 'email' in contact_updates:
+        contact.email = []
+        for email in contact_updates['email']:
+            contact.email.append(gdata.data.Email(
+                address=email['address'],
+                primary=email['primary'],
+                # TODO support different address types
+                rel=gdata.data.WORK_REL
+            ))
+
+    if 'phone_number' in contact_updates:
+        contact.phone_number = []
+        for phone_number in contact_updates['phone_number']:
+            contact.phone_number.append(gdata.data.PhoneNumber(
+                text=phone_number['text'],
+                rel=gdata.data.WORK_REL,
+                primary=phone_number['primary']
+            ))
+
+    if 'structured_postal_address' in contact_updates:
+        contact.structured_postal_address = []
+        for structured_postal_address in contact_updates['structured_postal_address']:
+            contact.structured_postal_address.append(gdata.data.StructuredPostalAddress(
+                rel=gdata.data.WORK_REL,
+                primary=structured_postal_address['primary'],
+                street=gdata.data.Street(text=structured_postal_address['street']),
+                city=gdata.data.City(text=structured_postal_address['city']),
+                region=gdata.data.Region(text=structured_postal_address['region']),
+                postcode=gdata.data.Postcode(text=structured_postal_address['postcode']),
+                country=gdata.data.Country(text=structured_postal_address['country'])
+            ))
+
+    return contact
