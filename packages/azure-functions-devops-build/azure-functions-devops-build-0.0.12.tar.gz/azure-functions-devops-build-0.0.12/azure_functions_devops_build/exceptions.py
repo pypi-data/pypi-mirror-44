e1d50845f3472b85ddd43c05adbc6b6f91061529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package azure-functions-devops-build/0.0.12/azure-functions-devops-build-0.0.12.tar.gz

---
+++ packages/azure-functions-devops-build/azure-functions-devops-build-0.0.12.tar.gz/azure-functions-devops-build-0.0.12/azure_functions_devops_build/exceptions.py
@@ -0,0 +1,15 @@
+class BaseException(Exception):
+    def __init__(self, message=None):
+        self.message = message
+
+
+class GitOperationException(BaseException):
+    pass
+
+
+class RoleAssignmentException(BaseException):
+    pass
+
+
+class LanguageNotSupportException(BaseException):
+    pass
