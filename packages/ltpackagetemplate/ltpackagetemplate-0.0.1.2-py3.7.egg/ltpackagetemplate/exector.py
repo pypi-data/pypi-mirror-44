a5f7245039fc2954afbd4c2c93c9777387e2a77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:35 +0000

    Add package ltpackagetemplate/0.0.1.2/ltpackagetemplate-0.0.1.2-py3.7.egg

---
+++ packages/ltpackagetemplate/ltpackagetemplate-0.0.1.2-py3.7.egg/ltpackagetemplate/exector.py
@@ -0,0 +1,61 @@
+#! /usr/local/bin/python3
+# encoding: utf-8
+# Author: LiTing
+
+import os
+import sys
+import getopt
+from .utils import *
+
+
+"""
+USAGE
+    
+    python3 exector.py [-s <string>] [-h]
+    e.g.
+        python3 exector.py -s 'Hi, LT'
+    
+    
+SUPPORT FUNCTIONS
+    
+    1. 输入-处理-输出
+    
+    
+SUPPORT OPTIONS
+    
+    -s, --string
+        [Optional] Input msg
+    
+    -h, --help
+        [Optional] Show this USAGE and exit.
+"""
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, 's:h', ['string', 'help'])
+    except getopt.GetoptError:
+        warning(__doc__)
+        sys.exit(2)
+
+    global out_put
+
+    for opt, arg in opts:
+        if opt in {'-s', '--string'}:
+            out_put = arg
+        elif opt in {'-h', '--help'}:
+            usage(__doc__)
+            return
+
+    __do_exec()
+
+
+out_put = 'hello world'
+
+
+def __do_exec():
+    PrintWithColor.black('').fore_green().style_underline().apply(f'{out_put}', end='\n\n')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
