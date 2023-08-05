6456cbfbb11077074a9fd1943b3457787894a475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package rasaio.integrations/0.0.7/rasaio.integrations-0.0.7.tar.gz

---
+++ packages/rasaio.integrations/rasaio.integrations-0.0.7.tar.gz/rasaio.integrations-0.0.7/rasaio/integrations/constants.py
@@ -0,0 +1,18 @@
+RASA_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DATA = "data"
+RESULTS = "results"
+
+# API_ENDPOINT = "api_endpoint"
+KEY = "key"
+RASA_TOKEN = "rasa-token"
+
+# # Person fields
+ID = "id"
+EXTERNAL_ID = "external_id"
+EMAIL = "email"
+FIRST_NAME = "first_name"
+LAST_NAME = "last_name"
+UPDATED = "updated"
+IS_SUBSCRIBED = "is_subscribed"
+IS_ACTIVE = "is_active"
