8a347783c3cda0ead465581bb1ed75653583a814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package gallery-dl/1.8.1/gallery_dl-1.8.1.tar.gz

---
+++ packages/gallery-dl/gallery_dl-1.8.1.tar.gz/gallery_dl-1.8.1/gallery_dl/cloudflare.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015-2019 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Methods to access sites behind Cloudflare protection"""
+
+import re
+import time
+import operator
+import urllib.parse
+from . import text
+from .cache import memcache
+
+
+def is_challenge(response):
+    return (response.status_code == 503 and
+            response.headers.get("Server", "").startswith("cloudflare") and
+            b"jschl-answer" in response.content)
+
+
+def solve_challenge(session, response, kwargs):
+    """Solve Cloudflare challenge and get cfclearance cookie"""
+    parsed = urllib.parse.urlsplit(response.url)
+    root = parsed.scheme + "://" + parsed.netloc
+
+    cf_kwargs = kwargs.copy()
+    headers = cf_kwargs["headers"] = (
+        kwargs["headers"].copy() if "headers" in kwargs else {})
+    params = cf_kwargs["params"] = (
+        kwargs["params"].copy() if "params" in kwargs else {})
+
+    page = response.text
+    params["s"] = text.extract(page, 'name="s" value="', '"')[0]
+    params["pass"] = text.extract(page, 'name="pass" value="', '"')[0]
+    params["jschl_vc"] = text.extract(page, 'name="jschl_vc" value="', '"')[0]
+    params["jschl_answer"] = solve_js_challenge(page, parsed.netloc)
+    headers["Referer"] = response.url
+
+    time.sleep(4)
+
+    url = root + "/cdn-cgi/l/chk_jschl"
+    cf_kwargs["allow_redirects"] = False
+    cf_response = session.request(response.request.method, url, **cf_kwargs)
+
+    location = cf_response.headers["Location"]
+    if location[0] == "/":
+        location = root + location
+
+    for cookie in cf_response.cookies:
+        if cookie.name == "cf_clearance":
+            return location, cookie.domain, {
+                cookie.name: cookie.value,
+                "__cfduid" : response.cookies.get("__cfduid", ""),
+            }
+    return location, "", {}
+
+
+def solve_js_challenge(page, netloc):
+    """Evaluate JS challenge in 'page' to get 'jschl_answer' value"""
+
+    # build variable name
+    # e.g. '...f, wqnVscP={"DERKbJk":+(...' --> wqnVscP.DERKbJk
+    data, pos = text.extract_all(page, (
+        ('var' , ',f, ', '='),
+        ('key' , '"'   , '"'),
+        ('expr', ':'   , '}'),
+    ))
+    variable = "{}.{}".format(data["var"], data["key"])
+    vlength = len(variable)
+
+    # evaluate the initial expression
+    solution = evaluate_expression(data["expr"])
+
+    # iterator over all remaining expressions
+    # and combine their values in 'solution'
+    expressions = text.extract(
+        page, "'challenge-form');", "f.submit();", pos)[0]
+    for expr in expressions.split(";")[1:]:
+
+        if expr.startswith(variable):
+            # select arithmetc function based on operator (+/-/*)
+            func = OPERATORS[expr[vlength]]
+            # evaluate the rest of the expression
+            value = evaluate_expression(expr[vlength+2:])
+            # combine expression value with our current solution
+            solution = func(solution, value)
+
+        elif expr.startswith("a.value"):
+            # add length of hostname
+            solution += len(netloc)
+
+            if ".toFixed(" in expr:
+                # trim solution to 10 decimal places
+                # and strip trailing zeros
+                solution = "{:.10f}".format(solution).rstrip("0")
+
+            return solution
+
+
+def evaluate_expression(expr, split_re=re.compile(r"\(+([^)]*)\)")):
+    """Evaluate a single Javascript expression for the challenge"""
+
+    if "/" in expr:
+        # split the expression in numerator and denominator subexpressions,
+        # evaluate them separately,
+        # and return their fraction-result
+        num, _, denom = expr.partition("/")
+        return evaluate_expression(num) / evaluate_expression(denom)
+
+    # iterate over all subexpressions,
+    # evaluate them,
+    # and accumulate their values in 'result'
+    result = ""
+    for subexpr in split_re.findall(expr):
+        result += str(sum(
+            VALUES[part]
+            for part in subexpr.split("[]")
+        ))
+    return int(result)
+
+
+OPERATORS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+}
+
+VALUES = {
+    "": 0,
+    "+": 0,
+    "!+": 1,
+    "+!!": 1,
+}
+
+
+@memcache(keyarg=0)
+def cookies(category):
+    return None
---
+++ packages/gallery-dl/gallery_dl-1.8.1.tar.gz/gallery_dl-1.8.1/gallery_dl/extractor/fallenangels.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017-2019 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract manga-chapters from https://www.fascans.com/"""
+
+from .common import ChapterExtractor, MangaExtractor
+from .. import text, util
+import json
+
+
+class FallenangelsChapterExtractor(ChapterExtractor):
+    """Extractor for manga-chapters from fascans.com"""
+    category = "fallenangels"
+    pattern = (r"(?:https?://)?(manga|truyen)\.fascans\.com"
+               r"/manga/([^/]+)/(\d+)(\.[^/?&#]+)?")
+    test = (
+        ("https://manga.fascans.com/manga/chronos-ruler/20/1", {
+            "url": "4604a7914566cc2da0ff789aa178e2d1c8c241e3",
+            "keyword": "2dfcc50020e32cd207be88e2a8fac0933e36bdfb",
+        }),
+        ("http://truyen.fascans.com/manga/hungry-marie/8", {
+            "url": "1f923d9cb337d5e7bbf4323719881794a951c6ae",
+            "keyword": "2bdb7334c0e3eceb9946ffd3132df679b4a94f6a",
+        }),
+        ("http://manga.fascans.com/manga/rakudai-kishi-no-eiyuutan/19.5", {
+            "keyword": "9fcca4c1a90d11f00764f62477ebe10bd408021c",
+        }),
+    )
+
+    def __init__(self, match):
+        self.version, self.manga, self.chapter, self.minor = match.groups()
+        url = "https://{}.fascans.com/manga/{}/{}/1".format(
+            self.version, self.manga, self.chapter)
+        ChapterExtractor.__init__(self, match, url)
+
+    def metadata(self, page):
+        lang = "vi" if self.version == "truyen" else "en"
+        data = {
+            "chapter": self.chapter,
+            "chapter_minor": self.minor or "",
+            "lang": lang,
+            "language": util.code_to_language(lang),
+        }
+        return text.extract_all(page, (
+            ("manga", 'name="description" content="', ' Chapter '),
+            ("title", ':  ', ' - Page 1'),
+        ), values=data)[0]
+
+    @staticmethod
+    def images(page):
+        return [
+            (img["page_image"], None)
+            for img in json.loads(
+                text.extract(page, "var pages = ", ";")[0]
+            )
+        ]
+
+
+class FallenangelsMangaExtractor(MangaExtractor):
+    """Extractor for manga from fascans.com"""
+    chapterclass = FallenangelsChapterExtractor
+    category = "fallenangels"
+    pattern = r"(?:https?://)?((manga|truyen)\.fascans\.com/manga/[^/]+)/?$"
+    test = (
+        ("http://manga.fascans.com/manga/trinity-seven", {
+            "url": "92699a250ff7d5adcf4b06e6a45b0c05f3426643",
+            "keyword": "afc785c37da7c48e639d3a596e8e0401482b628f",
+        }),
+        ("https://truyen.fascans.com/manga/rakudai-kishi-no-eiyuutan", {
+            "url": "51a731a6b82d5eb7a335fbae6b02d06aeb2ab07b",
+            "keyword": "2d2a2a5d9ea5925eb9a47bb13d848967f3af086c",
+        }),
+    )
+
+    def __init__(self, match):
+        url = "https://" + match.group(1)
+        self.lang = "vi" if match.group(2) == "truyen" else "en"
+        MangaExtractor.__init__(self, match, url)
+
+    def chapters(self, page):
+        language = util.code_to_language(self.lang)
+        results = []
+        pos = 0
+        while True:
+            test, pos = text.extract(page, '<li style="', '', pos)
+            if test is None:
+                return results
+            volume , pos = text.extract(page, 'class="volume-', '"', pos)
+            url    , pos = text.extract(page, 'href="', '"', pos)
+            chapter, pos = text.extract(page, '>', '<', pos)
+            title  , pos = text.extract(page, '<em>', '</em>', pos)
+
+            manga, _, chapter = chapter.rpartition(" ")
+            chapter, dot, minor = chapter.partition(".")
+            results.append((url, {
+                "manga": manga,
+                "title": text.unescape(title),
+                "volume": text.parse_int(volume),
+                "chapter": text.parse_int(chapter),
+                "chapter_minor": dot + minor,
+                "lang": self.lang,
+                "language": language,
+            }))
