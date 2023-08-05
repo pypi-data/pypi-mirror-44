47494842025c604f76acb7c6aee907f48e109b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package labeled-enum/1.3.1/labeled-enum-1.3.1.tar.gz

---
+++ packages/labeled-enum/labeled-enum-1.3.1.tar.gz/labeled-enum-1.3.1/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+with open('README.rst') as fin:
+    description = fin.read()
+
+setup(
+    name='labeled-enum',
+    version='1.3.1',
+    description='Django friendly, iterable Enum type with labels.',
+    long_description=description,
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    py_modules=['lenum',],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+    ],
+)
