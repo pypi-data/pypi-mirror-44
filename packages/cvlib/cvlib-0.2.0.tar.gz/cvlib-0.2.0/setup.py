608f60b3fe86f5b1bf227b6cb4a3a35b181c204b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:07 +0000

    Add package cvlib/0.2.0/cvlib-0.2.0.tar.gz

---
+++ packages/cvlib/cvlib-0.2.0.tar.gz/cvlib-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='cvlib',
+      version='0.2.0',
+      description='A high level, easy to use, open source computer vision library for python',
+      url='https://github.com/arunponnusamy/cvlib.git',
+      author='Arun Ponnusamy',
+      author_email='hello@arunponnusamy.com',
+      license='MIT',
+      packages=['cvlib'],
+      include_package_data=True,
+      zip_safe=False
+      )
