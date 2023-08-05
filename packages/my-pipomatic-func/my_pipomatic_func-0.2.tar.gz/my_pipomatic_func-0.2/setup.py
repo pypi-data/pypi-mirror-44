36a8b82b5bc5b288ff7d151357c5c6cc648cc4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:40 +0000

    Add package my-pipomatic-func/0.2/my_pipomatic_func-0.2.tar.gz

---
+++ packages/my-pipomatic-func/my_pipomatic_func-0.2.tar.gz/my_pipomatic_func-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="my_pipomatic_func",
+    version="0.2",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["my_pipomatic_func"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
