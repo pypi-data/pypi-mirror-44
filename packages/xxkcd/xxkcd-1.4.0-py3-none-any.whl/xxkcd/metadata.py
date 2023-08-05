8ec6e8c28ecf135d74933ebffbe5a6bf440327a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package xxkcd/1.4.0/xxkcd-1.4.0-py3-none-any.whl

---
+++ packages/xxkcd/xxkcd-1.4.0-py3-none-any.whl/xxkcd/metadata.py
@@ -0,0 +1,11 @@
+"""Metadata pertaining to the xxkcd Python package"""
+
+__author__ = 'Mital Ashok'
+__credits__ = ['Mital Ashok']
+__license__ = 'MIT'
+__version__ = '1.4.0'
+__maintainer__ = 'Mital Ashok'
+__author_email__ = __email__ = 'mital.vaja@googlemail.com'
+__status__ = 'Production'
+__url__ = 'https://github.com/MitalAshok/xxkcd'
+__description__ = 'An (unofficial) Python wrapper around xkcd APIs'
