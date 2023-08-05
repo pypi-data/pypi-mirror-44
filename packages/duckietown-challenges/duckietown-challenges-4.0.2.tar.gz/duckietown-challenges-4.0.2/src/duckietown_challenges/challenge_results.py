7103b31dc2b28fdbb1c2a26ed80e3694f030ce59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package duckietown-challenges/4.0.2/duckietown-challenges-4.0.2.tar.gz

---
+++ packages/duckietown-challenges/duckietown-challenges-4.0.2.tar.gz/duckietown-challenges-4.0.2/src/duckietown_challenges/challenge_results.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+import os
+from collections import OrderedDict
+
+from .constants import CHALLENGE_RESULTS_YAML, ChallengeResultsStatus
+from .utils import wrap_config_reader2
+from .yaml_utils import write_yaml, read_yaml_file
+
+
+class ChallengeResults:
+
+    def __init__(self, status, msg, scores, stats=None):
+        assert status in ChallengeResultsStatus.ALL, (status, ChallengeResultsStatus.ALL)
+        self.status = status
+        self.msg = msg
+        self.scores = scores
+        if stats is None:
+            stats = {}
+        self.stats = stats
+
+    def to_yaml(self):
+        data = {}
+        data['status'] = self.status
+        data['msg'] = self.msg
+        data['scores'] = self.scores
+        data['stats'] = self.stats
+        return data
+
+    def __repr__(self):
+        return 'ChallengeResults(%s)' % self.to_yaml()
+
+    # noinspection PyArgumentList
+    @classmethod
+    @wrap_config_reader2
+    def from_yaml(cls, d0):
+        status = d0.pop('status')
+        msg = d0.pop('msg')
+        scores = d0.pop('scores')
+        stats = d0.pop('stats', {})
+        return ChallengeResults(status, msg, scores, stats)
+
+    def get_status(self):
+        return self.status
+
+    def get_stats(self):
+        stats = OrderedDict()
+        stats['scores'] = self.scores
+        stats['msg'] = self.msg
+        return stats
+
+
+def declare_challenge_results(root, cr):
+    if root is None:
+        root = '/'
+    data = cr.to_yaml()
+    fn = os.path.join(root, CHALLENGE_RESULTS_YAML)
+    write_yaml(data, fn)
+
+
+class NoResultsFound(Exception):
+    pass
+
+def read_challenge_results(root):
+    fn = os.path.join(root, CHALLENGE_RESULTS_YAML)
+    if not os.path.exists(fn):
+        msg = 'File %r does not exist.' % fn
+        raise NoResultsFound(msg)
+    #
+    # with open(fn) as f:
+    #     contents = f.read()
+    #
+    # if '!!omap' in contents:
+    #     contents = contents.replace('!!omap', '')
+    #
+    # data = yaml.load(contents, Loader=yaml.Loader)
+
+    data = read_yaml_file(fn)
+
+    return ChallengeResults.from_yaml(data)
+
---
+++ packages/duckietown-challenges/duckietown-challenges-4.0.2.tar.gz/duckietown-challenges-4.0.2/src/duckietown_challenges/challenges_constants.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+class ChallengesConstants(object):
+    # status for submission
+    STATUS_SUBMITTED = 'submitted'
+    # STATUS_RETIRED = 'retired'
+    STATUS_EVALUATION = 'evaluating'
+    STATUS_ABORTED = 'aborted'
+    STATUS_SUCCESS = 'success'
+    STATUS_FAILED = 'failed'
+    STATUS_ERROR = 'error'
+
+    ALLOWED_SUB_STATUS = [
+        STATUS_SUBMITTED,
+        # STATUS_RETIRED,
+        STATUS_EVALUATION,
+        STATUS_ABORTED,
+        STATUS_SUCCESS,
+        STATUS_FAILED,
+        STATUS_ERROR,
+    ]
+
+    # status for evaluation jobs
+    STATUS_JOB_TIMEOUT = 'timeout'
+    STATUS_JOB_EVALUATION = 'evaluating'
+    STATUS_JOB_FAILED = 'failed'  # submission failed
+    STATUS_JOB_ERROR = 'error'  # evaluation failed
+    STATUS_JOB_SUCCESS = 'success'
+    STATUS_JOB_ABORTED = 'aborted'
+
+    ALLOWED_JOB_STATUS = [
+        STATUS_JOB_EVALUATION,
+        STATUS_JOB_SUCCESS,
+        STATUS_JOB_TIMEOUT,
+        STATUS_JOB_FAILED,
+        STATUS_JOB_ERROR,
+        STATUS_JOB_ABORTED
+    ]
+
+    JOB_TIMEOUT_MINUTES= 30
