c55200c0ff2e9c6ca38805455b6c4137ef7d0e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package atlassian-python-api/1.12.1/atlassian-python-api-1.12.1.tar.gz

---
+++ packages/atlassian-python-api/atlassian-python-api-1.12.1.tar.gz/atlassian-python-api-1.12.1/atlassian/__init__.py
@@ -0,0 +1,10 @@
+from .confluence import Confluence
+from .jira import Jira
+from .bitbucket import Bitbucket
+from .bitbucket import Bitbucket as Stash
+from .portfolio import Portfolio
+from .bamboo import Bamboo
+from .crowd import Crowd
+from .service_desk import ServiceDesk
+
+__all__ = ['Confluence', 'Jira', 'Bitbucket', 'Portfolio', 'Bamboo', 'Stash', 'Crowd', 'ServiceDesk']
