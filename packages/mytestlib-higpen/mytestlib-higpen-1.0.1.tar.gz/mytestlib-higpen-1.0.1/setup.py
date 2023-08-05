11a7c8efcfb59025e645bdb625f5dc684bf6cd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package mytestlib-higpen/1.0.1/mytestlib-higpen-1.0.1.tar.gz

---
+++ packages/mytestlib-higpen/mytestlib-higpen-1.0.1.tar.gz/mytestlib-higpen-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='mytestlib-higpen',
+	version='1.0.1',
+	url='http://higpen.jellybean.jp/',
+	author='higpen',
+	author_email='test@higpen.jp',
+	description='My Test Library.',
+	packages=find_packages()
+)
