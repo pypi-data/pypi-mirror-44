5661463e72eade3355fb3ad1e14f0cb9119444e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:52 +0000

    Add package cf-plot/2.4.1/cf-plot-2.4.1.tar.gz

---
+++ packages/cf-plot/cf-plot-2.4.1.tar.gz/cf-plot-2.4.1/cfplot/__init__.py
@@ -0,0 +1,18 @@
+'''
+
+cf-plot is a set of Python plotting routines for the contour, vector and line plots that
+climate researchers commonly make.
+
+
+See the cf-plot home page http://ajheaps.github.io/cf-plot for a gallery of plots and how to use cf-plot.
+
+
+'''
+
+__author__ = 'Andy Heaps - andy.heaps@ncas.ac.uk'
+__date__ = '1st April 2019'
+__version__ = '2.4.1'
+
+from cfplot import *
+
+
