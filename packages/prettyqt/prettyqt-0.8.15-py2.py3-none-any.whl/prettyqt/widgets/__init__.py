47dee6b029b65df0c5864200892630d386dcf847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:02 +0000

    Add package prettyqt/0.8.15/prettyqt-0.8.15-py2.py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-0.8.15-py2.py3-none-any.whl/prettyqt/widgets/__init__.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""widgets module
+
+contains QtWidgets-based classes
+"""
+
+from .formlayout import FormLayout
+from .image import Image
+
+from .slider import Slider
+from .styleoptionslider import StyleOptionSlider
+from .spanslider import SpanSlider
+
+from .action import Action
+from .menu import Menu
+from .tabbar import TabBar
+from .tabwidget import TabWidget
+from .toolbar import Toolbar
+from .headerview import HeaderView
+from .dockwidget import DockWidget
+from .pushbutton import PushButton
+from .radiobutton import RadioButton
+from .textedit import TextEdit
+
+from .splashscreen import SplashScreen
+from .progressdialog import ProgressDialog
+from .filedialog import FileDialog
+from .imageviewer import ImageViewer
+from .dialogbuttonbox import DialogButtonBox
+
+from .mainwindow import MainWindow
+
+from .composed.waitingspinner import WaitingSpinner
+from .composed.callout import Callout
+from .popupinfo import PopupInfo
+from .buttondelegate import ButtonDelegate
+
+__all__ = ["FormLayout",
+           "Image",
+           "Slider",
+           "StyleOptionSlider",
+           "SpanSlider",
+           "Action",
+           "Menu",
+           "TabWidget",
+           "TabBar",
+           "Toolbar",
+           "HeaderView",
+           "DockWidget",
+           "PushButton",
+           "RadioButton",
+           "TextEdit",
+           "SplashScreen",
+           "ProgressDialog",
+           "FileDialog",
+           "ImageViewer",
+           "DialogButtonBox",
+           "MainWindow",
+           "WaitingSpinner",
+           "Callout",
+           "PopupInfo",
+           "ButtonDelegate"]
