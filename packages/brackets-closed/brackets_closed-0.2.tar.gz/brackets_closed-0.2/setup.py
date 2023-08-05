378b0337290edaa2009cdacf01591665f93b18ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:20 +0000

    Add package brackets-closed/0.2/brackets_closed-0.2.tar.gz

---
+++ packages/brackets-closed/brackets_closed-0.2.tar.gz/brackets_closed-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='brackets_closed',
+      version='0.2',
+      description='Checks if brackets are closed given a string',
+      url='http://pypi.python.org/pypi/brackets_closed/0.1',
+      author='Daniel Keighley',
+      author_email='daniel.keighley@wpengine.com',
+      license='WP',
+      packages=['brackets_closed'],
+      zip_safe=False)
