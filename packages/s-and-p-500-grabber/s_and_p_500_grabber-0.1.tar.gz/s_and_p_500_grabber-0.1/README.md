6d3f285c85d7a1e75084830d2a07e66e93e79c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:54 +0000

    Add package s-and-p-500-grabber/0.1/s_and_p_500_grabber-0.1.tar.gz

---
+++ packages/s-and-p-500-grabber/s_and_p_500_grabber-0.1.tar.gz/s_and_p_500_grabber-0.1/README.md
@@ -0,0 +1,3 @@
+# s_and_p_500_grabber
+
+Python3 module to generate the list of S&P 500 companies in a machine readable format.
---
+++ packages/s-and-p-500-grabber/s_and_p_500_grabber-0.1.tar.gz/s_and_p_500_grabber-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='s_and_p_500_grabber',
+      version='0.1',
+      description='A free S&P 500 list getter.',
+      url='https://gitlab.com/zeastman/s_and_p_500_grabber',
+      author='Zachariah Eastman',
+      author_email='zachariah.d.eastman@gmail.com',
+      license='MIT',
+      packages=['s_and_p_500_grabber'],
+      zip_safe=False)
