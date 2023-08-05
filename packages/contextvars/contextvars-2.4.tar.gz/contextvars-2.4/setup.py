0d9b786e003748d8112c46a62aae39e2faf16af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:04 +0000

    Add package contextvars/2.4/contextvars-2.4.tar.gz

---
+++ packages/contextvars/contextvars-2.4.tar.gz/contextvars-2.4/setup.py
@@ -0,0 +1,36 @@
+import os.path
+import setuptools
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+
+setuptools.setup(
+    name='contextvars',
+    version="2.4",
+    url='http://github.com/MagicStack/contextvars',
+    description='PEP 567 Backport',
+    long_description=readme,
+    author='MagicStack Inc',
+    author_email='hello@magic.io',
+    packages=['contextvars'],
+    provides=['contextvars'],
+    install_requires=[
+        'immutables>=0.9',
+    ],
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+    ],
+    include_package_data=True,
+    test_suite='tests.suite',
+)
