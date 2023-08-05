53b828114291ea7f560b8a043b07dd370e46bdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:58 +0000

    Add package vredis.py/1.2.3/vredis.py-1.2.3-py3-none-any.whl

---
+++ packages/vredis.py/vredis.py-1.2.3-py3-none-any.whl/vredis/error.py
@@ -0,0 +1,33 @@
+class NotInDefaultsSetting(Exception):
+    pass
+
+class NotInDefaultCommand(Exception):
+	pass
+
+class NotInDefaultType(Exception):
+    pass
+
+class MustDictType(Exception):
+	pass
+
+class MustInSubcommandList(Exception):
+	pass
+
+class MustInCommandList(Exception):
+	pass
+
+class UndevelopmentSubcommand(Exception):
+	pass
+
+class SenderAlreadyStarted(Exception):
+    pass
+
+class PathNotExists(Exception):
+    pass
+
+class SettingError(Exception):
+    pass
+
+class TaskUnstopError(Exception):
+    pass
+
