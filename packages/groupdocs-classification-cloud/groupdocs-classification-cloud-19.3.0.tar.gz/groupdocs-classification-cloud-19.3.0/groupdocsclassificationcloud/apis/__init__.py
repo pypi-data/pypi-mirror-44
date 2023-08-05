99681d6cf8f3ae4d05818f7ecbda2b09214446ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package groupdocs-classification-cloud/19.3.0/groupdocs-classification-cloud-19.3.0.tar.gz

---
+++ packages/groupdocs-classification-cloud/groupdocs-classification-cloud-19.3.0.tar.gz/groupdocs-classification-cloud-19.3.0/groupdocsclassificationcloud/apis/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from groupdocsclassificationcloud.apis.classification_api import ClassificationApi
