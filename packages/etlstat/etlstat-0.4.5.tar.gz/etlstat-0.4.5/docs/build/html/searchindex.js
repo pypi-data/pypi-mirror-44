37d258967ed362dfdb47f5b01c8e3f0131b9632c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package etlstat/0.4.5/etlstat-0.4.5.tar.gz

---
+++ packages/etlstat/etlstat-0.4.5.tar.gz/etlstat-0.4.5/docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index","modules"],objects:{},objnames:{},objtypes:{},terms:{content:0,etlstat:[],index:0,page:0,search:0},titles:["Welcome to pyaxis documentation!","etlstat"],titleterms:{document:0,etlstat:1,indice:0,modul:0,pyaxi:0,tabl:0,welcom:0}})
