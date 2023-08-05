29467b7a94cd63fe988080ae0cd21734f6746a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:25 +0000

    Add package quality-report/2.89.2/quality_report-2.89.2.tar.gz

---
+++ packages/quality-report/quality_report-2.89.2.tar.gz/quality_report-2.89.2/hqlib/metric_source/zap_scan_report.py
@@ -0,0 +1,107 @@
+"""
+Copyright 2012-2019 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import functools
+import logging
+import traceback
+
+import bs4
+
+from . import url_opener
+from .. import domain
+
+
+class ZAPScanReport(domain.MetricSource):
+    """ Class representing ZAP Scan reports. """
+    metric_source_name = 'ZAP Scan rapport'
+
+    def __init__(self, **kwargs) -> None:
+        self._url_opener = url_opener.UrlOpener(**kwargs)
+        super().__init__()
+
+    @functools.lru_cache(maxsize=1024)
+    def alerts(self, risk_level: str, *report_urls: str) -> int:
+        """ Return the number of alerts of the specified risk level. """
+        nr_alerts = 0
+        for url in report_urls:
+            try:
+                nr_alerts += self.__parse_alerts(self.__get_soup(url), risk_level)
+            except url_opener.UrlOpener.url_open_exceptions:
+                return -1
+            except IndexError as reason:
+                logging.warning("Couldn't parse alerts with %s risk level from %s: %s", risk_level, url, reason)
+                traceback.print_exc()
+                return -1
+        return nr_alerts
+
+    def get_warnings_info(self, risk_level: str, *report_urls: str) -> list:
+        """ Returns the information about the warnings from zap scan report of a certain risk level."""
+        warnings = []
+        for url in report_urls:
+            try:
+                warnings += self.__parse_warnings(self.__get_soup(url), risk_level)
+            except url_opener.UrlOpener.url_open_exceptions:
+                return []
+            except IndexError as reason:
+                logging.warning("Couldn't parse alert details with %s risk level from %s: %s", risk_level, url, reason)
+                traceback.print_exc()
+                return []
+        return warnings
+
+    @staticmethod
+    def __parse_warnings(soup, risk_level: str) -> list:
+        warnings = []
+        headers = soup.find_all('tr', attrs={"class": "risk-{level}".format(level=risk_level)})
+        for header in headers:
+            name = header.find_all('th')[1].string
+            description = header.find_next_sibling().find_all('td')[1]
+            while description.string is None:
+                description = description.contents[0]
+            warnings.append((name, description.string))
+        if not warnings:
+            logging.warning("Couldn't find any entries with %s risk level.", risk_level)
+        return warnings
+
+    @functools.lru_cache(maxsize=1024)
+    def __get_soup(self, url: str):
+        """ Return the HTML soup. """
+        return bs4.BeautifulSoup(self._url_opener.url_read(url), "lxml")
+
+    @staticmethod
+    def __parse_alerts(soup, risk_level: str) -> int:
+        """ Get the number of alerts from the HTML soup. """
+        summary_table = soup('table', {"class": "summary"})
+        # First try the new table format
+        # Find the row where the first td contains the specified risk level and get the number of alerts from
+        # the second td.
+        if summary_table:
+            for row in summary_table[0]('tr'):
+                if row('td') and row('td')[0].text == risk_level.capitalize():
+                    return int(row('td')[1].text)
+            logging.warning("Risk level %s could not be found in ZAP Scan report.", risk_level)
+            return -1
+        table_list = soup('table')
+        # Prevent IndexError in case of empty table
+        if table_list:
+            summary_table = table_list[0]
+            # Find the row where the first td contains the specified risk level and get the number of alerts from
+            # the second td. We use item(text=True)[0] to skip font and anchor tags and get the inner text node.
+            alert = [row('td')[1](text=True)[0] for row in summary_table('tr')
+                     if row('td')[0](text=True)[0] == risk_level.capitalize()][0]
+            return int(alert)
+        logging.warning("Summary table could not be found in ZAP Scan report.")
+        return -1
---
+++ packages/quality-report/quality_report-2.89.2.tar.gz/quality_report-2.89.2/tests/unittests/metric_source/zap_scan_report_tests.py
@@ -0,0 +1,149 @@
+"""
+Copyright 2012-2019 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import traceback
+import logging
+import unittest
+from unittest.mock import patch, call
+
+import urllib.error
+from hqlib.metric_source import ZAPScanReport, url_opener
+
+ZAP_REPORT = '''<html>
+    <body>
+    <table width="45%" class="summary">
+    <tr bgcolor="#666666">
+    <th width="45%" height="24">Risk
+          Level</th><th width="55%" align="center">Number
+          of Alerts</th>
+    </tr>
+    <tr bgcolor="#e8e8e8">
+    <td><a href="#high">High</a></td><td align="center">0</td>
+    </tr>
+    <tr bgcolor="#e8e8e8">
+    <td><a href="#medium">Medium</a></td><td align="center">1</td>
+    </tr>
+    <tr bgcolor="#e8e8e8">
+    <td><a href="#low">Low</a></td><td align="center">4</td>
+    </tr>
+    <tr bgcolor="#e8e8e8">
+    <td><a href="#info">Informational</a></td><td align="center">1</td>
+    </tr>
+    </table>
+    <div class="spacer-lg"></div>
+    <h3>Alert Detail</h3>
+    <div class="spacer"></div>
+    <table width="100%" class="results">
+        <tr height="24" class="risk-high">
+            <th width="20%"><a name="high"></a>High (Medium)</th>
+            <th width="80%">Warning Name</th>
+        </tr>
+        <tr bgcolor="#e8e8e8">
+            <td width="20%">Description</td>
+            <td width="80%">{description}</td>
+        </tr>
+    </table>
+    </body>
+    </html>
+    '''
+
+
+@patch.object(url_opener.UrlOpener, 'url_read')
+class ZAPScanReportTest(unittest.TestCase):
+    """ Unit tests for the ZAP Scan report class. """
+
+    def setUp(self):
+        ZAPScanReport.alerts.cache_clear()
+        ZAPScanReport._ZAPScanReport__get_soup.cache_clear()
+        self.__report = ZAPScanReport()
+
+    def test_get_warnings_info(self, mock_url_read):
+        """ Test that data about warnings are extracted correctly. """
+        mock_url_read.return_value = ZAP_REPORT.format(description="Be aware!")
+        result = self.__report.get_warnings_info('high', 'url')
+        self.assertEqual([("Warning Name", "Be aware!")], result)
+
+    def test_get_warnings_info_with_broad_description(self, mock_url_read):
+        """ Test that data about warnings are extracted correctly, when the description is folded in sub-elements. """
+        mock_url_read.return_value = ZAP_REPORT.format(
+            description="<div><p>The First Part</p><span>of a very long description</span></div>"
+        )
+        result = self.__report.get_warnings_info('high', 'url')
+        self.assertEqual([("Warning Name", "The First Part")], result)
+
+    def test_get_warnings_info_http_error(self, mock_url_read):
+        """ Test that data about warnings are empty when http error happens. """
+        mock_url_read.side_effect = urllib.error.HTTPError('raise', None, None, None, None)
+        result = self.__report.get_warnings_info('high', 'url')
+        self.assertEqual([], result)
+
+    @patch.object(logging, 'warning')
+    def test_get_warnings_info_attribute_error(self, mock_warning, mock_url_read):
+        """ Test that data about warnings are empty and appropriate logging is done no expected html tags are found. """
+        mock_url_read.return_value = 'no table at all!'
+        result = self.__report.get_warnings_info('high', 'url')
+        self.assertEqual([], result)
+        mock_warning.assert_called_once_with("Couldn't find any entries with %s risk level.", "high")
+
+    @patch.object(traceback, 'print_exc')
+    @patch.object(logging, 'warning')
+    def test_get_warnings_info_index_error(self, mock_warning, mock_traceback, mock_url_read):
+        """ Test that data about warnings are empty and appropriate logging is done no expected html tags are found. """
+        mock_url_read.return_value = '<table class="results"><tr class="risk-high"><th /></tr></table> '
+        result = self.__report.get_warnings_info('high', 'url')
+        self.assertEqual([], result)
+        mock_warning.assert_called_once()
+        self.assertEqual(mock_warning.call_args[0][0], "Couldn't parse alert details with %s risk level from %s: %s")
+        self.assertEqual(mock_warning.call_args[0][1], 'high')
+        self.assertEqual(mock_warning.call_args[0][2], 'url')
+        self.assertIsInstance(mock_warning.call_args[0][3], IndexError)
+        mock_traceback.assert_called_once()
+
+    def test_high_risk_alerts(self, mock_url_read):
+        """ Test the number of high risk alerts. """
+        mock_url_read.return_value = ZAP_REPORT
+        self.assertEqual(0, self.__report.alerts('high', 'url'))
+        mock_url_read.assert_called_once()
+
+    def test_medium_risk_alerts(self, mock_url_read):
+        """ Test the number of medium risk alerts. """
+        mock_url_read.return_value = ZAP_REPORT
+        self.assertEqual(1, self.__report.alerts('medium', 'url'))
+        mock_url_read.assert_called_once()
+
+    def test_low_risk_alerts(self, mock_url_read):
+        """ Test the number of low risk alerts. """
+        mock_url_read.return_value = ZAP_REPORT
+        self.assertEqual(4, self.__report.alerts('low', 'url'))
+        mock_url_read.assert_called_once()
+
+    def test_passed_raise(self, mock_url_read):
+        """ Test that the value is -1 when the report can't be opened. """
+        mock_url_read.side_effect = urllib.error.HTTPError('raise', None, None, None, None)
+        self.assertEqual(-1, self.__report.alerts('high', 'raise'))
+        mock_url_read.assert_called_once()
+
+    def test_multiple_urls(self, mock_url_read):
+        """ Test the number of alerts for multiple urls. """
+        mock_url_read.return_value = ZAP_REPORT
+        self.assertEqual(2, self.__report.alerts('medium', 'url1', 'url2'))
+        self.assertEqual(mock_url_read.call_args_list, [call('url1'), call('url2')])
+
+    def test_missing_table(self, mock_url_read):
+        """ Test that a missing table can be handled. """
+        mock_url_read.return_value = '<html></html>'
+        self.assertEqual(-1, self.__report.alerts('high', 'url'))
+        mock_url_read.assert_called_once()
