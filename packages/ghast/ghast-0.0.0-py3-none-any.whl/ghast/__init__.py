601757012af6db823741e8bb29f7c4d07e217bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package ghast/0.0.0/ghast-0.0.0-py3-none-any.whl

---
+++ packages/ghast/ghast-0.0.0-py3-none-any.whl/ghast/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Graylog HTTP Alert Script Triggerer (ghast)
+
+A simple server for triggering a script on a Graylog HTTP alert callback!
+"""
+
+__version__ = (0, 0, 0)
