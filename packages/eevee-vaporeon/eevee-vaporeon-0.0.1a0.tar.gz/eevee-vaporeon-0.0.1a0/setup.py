dc10d570ab0272aba204afa06b8f73545d97b13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-vaporeon/0.0.1a0/eevee-vaporeon-0.0.1a0.tar.gz

---
+++ packages/eevee-vaporeon/eevee-vaporeon-0.0.1a0.tar.gz/eevee-vaporeon-0.0.1a0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="eevee-vaporeon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Vaporeon Extension for the Eevee Discord Framework",
+    long_description_content_type="text/markdown",
+    packages=['eevee.ext.vaporeon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
