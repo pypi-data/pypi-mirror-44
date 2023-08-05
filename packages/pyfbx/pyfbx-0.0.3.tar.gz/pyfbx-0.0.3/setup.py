3946922807b095f10e1562c48ff4b5bcbc7cc897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package pyfbx/0.0.3/pyfbx-0.0.3.tar.gz

---
+++ packages/pyfbx/pyfbx-0.0.3.tar.gz/pyfbx-0.0.3/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfbx",
+    version="0.0.3",
+    description="Freebox thin client",
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    author_email="teebeenator@gmail.com",
+    url='https://framagit.org/sun/pyfbx',
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(),
+    install_requires=["zeroconf", "requests"],
+    include_package_data=True,
+)
