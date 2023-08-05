07432adb4dd8c0c688fc6d658c5a600b465b58ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package pytest-rerunfailures/7.0/pytest-rerunfailures-7.0.tar.gz

---
+++ packages/pytest-rerunfailures/pytest-rerunfailures-7.0.tar.gz/pytest-rerunfailures-7.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(name='pytest-rerunfailures',
+      version='7.0',
+      description='pytest plugin to re-run tests to eliminate flaky failures',
+      long_description=(
+          '.. contents::\n\n' +
+          open('README.rst').read() +
+          '\n\n' +
+          open('CHANGES.rst').read()),
+      author='Leah Klearman',
+      author_email='lklrmn@gmail.com',
+      url='https://github.com/pytest-dev/pytest-rerunfailures',
+      py_modules=['pytest_rerunfailures'],
+      entry_points={'pytest11': ['rerunfailures = pytest_rerunfailures']},
+      install_requires=['pytest >= 3.10'],
+      license='Mozilla Public License 2.0 (MPL 2.0)',
+      keywords='py.test pytest rerun failures flaky',
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Framework :: Pytest',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: POSIX',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Software Development :: Quality Assurance',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Utilities',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ])
