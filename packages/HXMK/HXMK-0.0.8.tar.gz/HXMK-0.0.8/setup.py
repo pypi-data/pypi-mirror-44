55c3243bc19ec26eb5677ad03df109de541ca228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package HXMK/0.0.8/HXMK-0.0.8.tar.gz

---
+++ packages/HXMK/HXMK-0.0.8.tar.gz/HXMK-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setup(
+	name				= "HXMK",
+	version				= "0.0.8",
+	url					= "https://github.com/Hexcell/HXMK.git",
+	author 				= "Hexcell",
+	author_email		= "fabian0010k@gmail.com",
+	description			= "A Python-based build system with a Makefile-like experience",
+	long_description	= long_description,
+	long_description_content_type = "text/markdown",
+	packages			= find_packages(),
+	install_requires	= ["colorama >= 0.4.1"],
+
+	entry_points = {
+		"console_scripts": [
+			"hxmk = HXMK.environment:main"
+		]
+	},
+	
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	]
+)
