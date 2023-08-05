1816b91a2005ef64be05b4e0b302d42d22386e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:17 +0000

    Add package pyleiades/0.0.0.dev7/pyleiades-0.0.0.dev7-py3-none-any.whl

---
+++ packages/pyleiades/pyleiades-0.0.0.dev7-py3-none-any.whl/pyleiades-0.0.0.dev7.data/scripts/pyleiades-demo.py
@@ -0,0 +1,12 @@
+import pyleiades as eia
+import os
+from matplotlib import pyplot as plt
+
+# Create a visual
+visual = eia.Visual(['coal', 'nuclear'])
+visual.include_energy('renewable')
+visual.linegraph(subject='totals', freq='yearly', start_date='1970')
+# Save and display the visual
+fig_path = os.path.join(os.path.dirname(eia.__file__), 'fig', 'demo-plot.png')
+plt.savefig(fig_path, dpi=300)
+plt.show()
