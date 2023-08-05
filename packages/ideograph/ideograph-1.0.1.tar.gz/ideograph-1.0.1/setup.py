0e5b8365b294b3f1f140bd6e493e8ca32c07dc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:51 +0000

    Add package ideograph/1.0.1/ideograph-1.0.1.tar.gz

---
+++ packages/ideograph/ideograph-1.0.1.tar.gz/ideograph-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ideograph",
+    version="1.0.1",
+    author="Ben Yang",
+    author_email="benayang@gmail.com",
+    description="Tool for finding ideographic (e.g. Han) characters from their components",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwsfutcmd/ideograph",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: Chinese (Traditional)",
+        "Natural Language :: Japanese",
+        "Natural Language :: Korean",
+        "Natural Language :: Vietnamese",
+        # "Natural Language :: Zhuang",
+    ],
+    scripts=["bin/ideograph"],
+    include_package_data=True,
+)
