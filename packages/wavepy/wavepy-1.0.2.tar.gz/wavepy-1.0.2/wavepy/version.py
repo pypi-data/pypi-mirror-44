f849a2a01d13e91cd44c6b087f5c0ce107f646a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:57 +0000

    Add package wavepy/1.0.2/wavepy-1.0.2.tar.gz

---
+++ packages/wavepy/wavepy-1.0.2.tar.gz/wavepy-1.0.2/wavepy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wavepy SETUP.PY
+short_version = '1.0.2'
+version = '1.0.2'
+full_version = '1.0.2'
+git_revision = '93aca353e9cac0459f3b0afed6b5bb706229d924'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
