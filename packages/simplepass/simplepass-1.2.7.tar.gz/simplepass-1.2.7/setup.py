2aba5d3a838cdd330eda6dbd8f38c1faa4290cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:11 +0000

    Add package simplepass/1.2.7/simplepass-1.2.7.tar.gz

---
+++ packages/simplepass/simplepass-1.2.7.tar.gz/simplepass-1.2.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ["pyperclip", "passlib", "cryptography"]
+
+setup(
+    name="simplepass",
+    version="1.2.7",
+    author="Jeff Moger",
+    author_email="jeff-simple@moger.com",
+    description="A simple password manager for the command line.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeffmoger/pypass",
+    packages=["simplepass"],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    entry_points={
+        'console_scripts': [
+            'simple = simplepass.__main__:main',
+        ],
+    }
+)
