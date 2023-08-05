76643249f9986e4cb350617411bc4902a482c78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package bitcoinpython/0.5.2.6/bitcoinpython-0.5.2.6-py3-none-any.whl

---
+++ packages/bitcoinpython/bitcoinpython-0.5.2.6-py3-none-any.whl/bitcoinpython/__init__.py
@@ -0,0 +1,7 @@
+from bitcoinpython.format import verify_sig
+from bitcoinpython.network.rates import SUPPORTED_CURRENCIES, set_rate_cache_time
+from bitcoinpython.network.services import set_service_timeout
+from bitcoinpython.wallet import Key, PrivateKey, PrivateKeyTestnet, wif_to_key
+
+name = 'bitcoinpython'
+__version__ = '0.5.2.6'
