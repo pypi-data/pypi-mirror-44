6ffb93dee7e71d92574874ad85a4dd5983df3794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:34 +0000

    Add package civilservant/0.0.6/civilservant-0.0.6.tar.gz

---
+++ packages/civilservant/civilservant-0.0.6.tar.gz/civilservant-0.0.6/civilservant/tasks/utils/print.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+COLORBLIND = bool(os.getenv("PIPENV_not COLORBLIND", 0))
+
+ANSI_BR_MAGENTA = "\u001b[35;1m" if not COLORBLIND else ""
+ANSI_BR_RED = "\u001b[31;1m" if not COLORBLIND else ""
+ANSI_BR_WHITE = "\u001b[37;1m" if not COLORBLIND else ""
+ANSI_BR_YELLOW = "\u001b[33;1m" if not COLORBLIND else ""
+ANSI_RESET = "\u001b[0m" if not COLORBLIND else ""
+
+
+def _colored_print(color, text, arg=None, file=sys.stdout):
+    print(f"{color}{text}", end="", file=file)
+    if arg:
+        print(f"{ANSI_BR_WHITE}{arg}", end="", file=file)
+    print(f"{ANSI_RESET}", file=file)
+
+
+def print_action(text, arg=None):
+    _colored_print(ANSI_BR_MAGENTA, f"{text}…", arg)
+
+
+def print_error(text, arg=None):
+    _colored_print(ANSI_BR_RED, f"Error: {text}", arg, file=sys.stderr)
+
+
+def print_warning(text, arg=None):
+    _colored_print(ANSI_BR_YELLOW, f"Warning: {text}", arg, file=sys.stderr)
+
