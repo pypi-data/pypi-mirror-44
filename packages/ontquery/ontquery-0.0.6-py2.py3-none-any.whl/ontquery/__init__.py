a3a56f60251722145cd011967048a5d1d5f73f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:56 +0000

    Add package ontquery/0.0.6/ontquery-0.0.6-py2.py3-none-any.whl

---
+++ packages/ontquery/ontquery-0.0.6-py2.py3-none-any.whl/ontquery/__init__.py
@@ -0,0 +1,5 @@
+from ontquery.query import OntQuery, OntQueryCli
+from ontquery.terms import OntCuries, OntId, OntTerm
+from ontquery import plugin
+
+__all__ = ['OntCuries', 'OntId', 'OntTerm', 'OntQuery', 'OntQueryCli']
