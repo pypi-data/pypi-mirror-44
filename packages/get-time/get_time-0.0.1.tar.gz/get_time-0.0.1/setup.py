2024d504ea8a2b15bf7a210308e426b93a30f2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package get-time/0.0.1/get_time-0.0.1.tar.gz

---
+++ packages/get-time/get_time-0.0.1.tar.gz/get_time-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='get_time',
+    version='0.0.1',
+    author='黄民航',
+    author_email='gmhesat@gmail.com',
+    url='https://github.com/Coxhuang/get_time',
+    description='获取任意时间',
+    packages=['get_time'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'get_time=get_time:get_time'
+        ]
+    }
+)
+
