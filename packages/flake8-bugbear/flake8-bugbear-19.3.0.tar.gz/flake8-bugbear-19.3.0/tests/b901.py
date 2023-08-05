d0fe8f63c6f092f954669fa64ba82501ca193c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package flake8-bugbear/19.3.0/flake8-bugbear-19.3.0.tar.gz

---
+++ packages/flake8-bugbear/flake8-bugbear-19.3.0.tar.gz/flake8-bugbear-19.3.0/tests/b901.py
@@ -0,0 +1,57 @@
+"""
+Should emit:
+B901 - on lines 9, 36
+"""
+
+
+def broken():
+    if True:
+        return [1, 2, 3]
+
+    yield 3
+    yield 2
+    yield 1
+
+
+def not_broken():
+    if True:
+        return
+
+    yield 3
+    yield 2
+    yield 1
+
+
+def not_broken2():
+    return not_broken()
+
+
+def not_broken3():
+    return
+
+    yield from not_broken()
+
+
+def broken2():
+    return [3, 2, 1]
+
+    yield from not_broken()
+
+
+async def not_broken4():
+    import asyncio
+
+    await asyncio.sleep(1)
+    return 1
+
+
+def actually_not_broken():
+    yield 2
+    return 1  # noqa
+
+
+def not_broken5():
+    def inner():
+        return 2
+
+    yield inner()
