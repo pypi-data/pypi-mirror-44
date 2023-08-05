f4a4b4252dd7d2076338f0885fdbae65249dbf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:23 +0000

    Add package profanity-filter/1.0.13/profanity-filter-1.0.13.tar.gz

---
+++ packages/profanity-filter/profanity-filter-1.0.13.tar.gz/profanity-filter-1.0.13/profanity_filter/__init__.py
@@ -0,0 +1,11 @@
+from .profanity_filter import (
+    Config,
+    DEEP_ANALYSIS_AVAILABLE,
+    MULTILINGUAL_AVAILABLE,
+    ProfanityFilter,
+    ProfanityFilterError,
+    Word,
+    __version__,
+    default_config,
+)
+from .console import main
