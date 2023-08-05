e000db1138d910ad13bad79ecf83d7890c395292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package tox-constraints/0.1/tox-constraints-0.1.tar.gz

---
+++ packages/tox-constraints/tox-constraints-0.1.tar.gz/tox-constraints-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("requirements/install_requires.txt") as fp:
+    install_requires = list(fp)
+
+setuptools.setup(
+    name="tox-constraints",
+    version="0.1",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=install_requires,
+    entry_points={"tox": ["constraints = tox_constraints"]},
+)
