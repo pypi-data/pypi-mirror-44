3de5ee3d20e17fb096be879f378f144b8575b244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package baseball/1.1/baseball-1.1.tar.gz

---
+++ packages/baseball/baseball-1.1.tar.gz/baseball-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='baseball',
+      version='1.1',
+      description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
+      url='https://github.com/benjamincrom/baseball',
+      author='Benjamin B. Crom',
+      author_email='benjamincrom@gmail.com',
+      include_package_data=True,
+      license='MIT',
+      packages=['baseball'],
+      zip_safe=False,
+      install_requires=['python-dateutil', 'pytz', 'requests'])
