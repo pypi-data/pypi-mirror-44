5a7799dadeb9493cc4e3bea42126fd40fe163a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package SplitMerge/0.0.1.post1/SplitMerge-0.0.1.post1.tar.gz

---
+++ packages/SplitMerge/SplitMerge-0.0.1.post1.tar.gz/SplitMerge-0.0.1.post1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SplitMerge",
+    version="0.0.1.post1",
+    author="Satyaki De",
+    author_email="satyaki.de@gmail.com",
+    description="Split & Merge utilities for large csv files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SatyakiDe2019/SplitMerge",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
