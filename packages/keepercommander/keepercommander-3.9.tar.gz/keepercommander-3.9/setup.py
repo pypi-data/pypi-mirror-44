54ba98f5db43c502363cbe6e8907a01d4fc46604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package keepercommander/3.9/keepercommander-3.9.tar.gz

---
+++ packages/keepercommander/keepercommander-3.9.tar.gz/keepercommander-3.9/setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    read_md = lambda f: open(f, 'r').read()
+from os import path
+
+import keepercommander
+
+LICENSE = open("LICENSE").read()
+
+# strip links from the descripton on the PyPI
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    'colorama',
+    'pycryptodomex>=3.7.2',
+    'libkeepass',
+    'requests',
+    'tabulate',
+    'prompt_toolkit>=2.0.4',
+    'asciitree',
+    'protobuf'
+]
+
+setup(name='keepercommander',
+      version=keepercommander.__version__,
+      description='Keeper Commander for Python 3',
+      long_description=read_md('README.md'),
+      author='Craig Lurey',
+      author_email='craig@keepersecurity.com',
+      url='https://github.com/Keeper-Security/Commander',
+      license='MIT',
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3.4",
+                   "Topic :: Security"],
+      keywords='security password',
+
+      packages=['keepercommander',
+                'keepercommander.commands',
+                'keepercommander.importer',
+                'keepercommander.importer.json',
+                'keepercommander.importer.csv',
+                'keepercommander.importer.keepass',
+                'keepercommander.plugins',
+                'keepercommander.plugins.adpasswd',
+                'keepercommander.plugins.awskey',
+                'keepercommander.plugins.mssql',
+                'keepercommander.plugins.mysql',
+                'keepercommander.plugins.oracle',
+                'keepercommander.plugins.postgresql',
+                'keepercommander.plugins.ssh',
+                'keepercommander.plugins.sshkey',
+                'keepercommander.plugins.unixpasswd',
+                'keepercommander.plugins.windows',
+                'keepercommander.plugins.pspasswd',
+                'keepercommander.yubikey',
+                ],
+
+      entry_points={
+          "console_scripts": [
+              "keeper=keepercommander.__main__:main",
+          ],
+      },
+      install_requires=install_requires
+      )
