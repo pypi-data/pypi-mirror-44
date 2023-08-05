1d684ff79fa1c1fbaa54c304eab9de88d0c03a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package autopaths/1.1.3/autopaths-1.1.3-py2-none-any.whl

---
+++ packages/autopaths/autopaths-1.1.3-py2-none-any.whl/autopaths/__init__.py
@@ -0,0 +1,12 @@
+# Special variables #
+__version__ = '1.1.3'
+
+# This is done to avoid circular import errors #
+from autopaths import common
+from autopaths import file_size
+from autopaths import file_permissions
+from autopaths import base_path
+from autopaths import file_path
+from autopaths import dir_path
+from autopaths import tmp_path
+from autopaths import auto_paths
