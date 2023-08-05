3923d85ef6c15277ed394c0b94e73112f0f5566e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package notechat/0.0.3/notechat-0.0.3.tar.gz

---
+++ packages/notechat/notechat-0.0.3.tar.gz/notechat-0.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/03/30 12:10
+# @Author  : niuliangtao
+# @Site    :
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup
+
+setup(name='notechat',
+      version='0.0.3',
+      description='notechat',
+      author='euler',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+      packages=['notechat', 'notechat.adv'],
+      )
