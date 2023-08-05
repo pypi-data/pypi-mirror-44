ee91aa4eed45d6a0d08d7fb46042d46b020861b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package simpledsapp/1.0.3/simpledsapp-1.0.3.tar.gz

---
+++ packages/simpledsapp/simpledsapp-1.0.3.tar.gz/simpledsapp-1.0.3/setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+# Make sure we are running python3.5+
+if 10 * sys.version_info[0]  + sys.version_info[1] < 35:
+    sys.exit("Sorry, only Python 3.5+ is supported.")
+
+from setuptools import setup
+
+
+def readme():
+    print("Current dir = %s" % os.getcwd())
+    print(os.listdir())
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+      name             =   'simpledsapp',
+      version          =   '1.0.3',
+      description      =   'A simple/dummy ChRIS DS app', 
+      long_description =   readme(),
+      author           =   'Rudolph Pienaar',
+      author_email     =   'rudolph.pienaar@gmail.com',
+      url              =   'https://github.com/FNNDSC/pl-simpledsapp',
+      packages         =   ['simpledsapp'],
+      install_requires =   ['chrisapp', 'pudb'],
+      test_suite       =   'nose.collector',
+      tests_require    =   ['nose'],
+      scripts          =   ['simpledsapp/simpledsapp.py'],
+      license          =   'MIT',
+      zip_safe         =   False
+     )
