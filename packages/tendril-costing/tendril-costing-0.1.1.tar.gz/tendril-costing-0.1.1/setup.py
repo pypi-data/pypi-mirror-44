f369c4623057019b1a4ac35ebbcca606ae885cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package tendril-costing/0.1.1/tendril-costing-0.1.1.tar.gz

---
+++ packages/tendril-costing/tendril-costing-0.1.1.tar.gz/tendril-costing-0.1.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+    find_packages = None
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'future',
+    'tendril-conventions-electronics>=0.1.1',
+    'tendril-utils-types>=0.1.2',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tendril-costing',
+    version='0.1.1',
+    description="Tendril Costing Primitives",
+    long_description=readme,
+    author="Chintalagiri Shashank",
+    author_email='shashank@chintal.in',
+    url='https://github.com/chintal/tendril-costing',
+    packages=find_packages(),
+    install_requires=requirements,
+    license="AGPLv3",
+    zip_safe=False,
+    keywords='tendril',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    # test_suite='tests',
+    # tests_require=test_requirements
+)
