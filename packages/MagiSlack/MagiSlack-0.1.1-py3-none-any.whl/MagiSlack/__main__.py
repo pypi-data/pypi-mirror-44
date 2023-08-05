3cbb02bc9f187eb3770f1660f7e89f224de8ae18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:42 +0000

    Add package MagiSlack/0.1.1/MagiSlack-0.1.1-py3-none-any.whl

---
+++ packages/MagiSlack/MagiSlack-0.1.1-py3-none-any.whl/MagiSlack/__main__.py
@@ -0,0 +1,33 @@
+from os import environ
+from random import randint
+
+from MagiSlack.io import MagiIO
+from MagiSlack.module import MagiModule
+
+
+def hello_world(*args, **kwargs):
+    return f"HELLO WORLD! user {kwargs['name']}, {kwargs['display_name']}"
+
+def random(*args, **kwargs):
+    for a in args:
+        print(a)
+    number = randint(int(args[0]), int(args[1]))
+    return f"You got {number}!"
+
+if __name__ == '__main__':
+    print('Magi Start!')
+    print('='*30)
+    print('MagiModule Initializing.')
+    module = MagiModule.MagiModule(environ['SLACK_API_TOKEN'])
+    print('Complete')
+
+    print('='*30)
+    print('MagiIO Initializing.')
+    io = MagiIO.MagiIO(module)
+    print('Complete')
+    print('='*30)
+
+    io.set_callback_func('hello', hello_world)
+    io.set_callback_func('random', random)
+
+    io.start()
