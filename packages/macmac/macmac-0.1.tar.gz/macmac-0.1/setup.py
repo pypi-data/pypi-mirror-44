67a6768ea5c8d0bea63ee5f1a355ebdd9dedc290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package macmac/0.1/macmac-0.1.tar.gz

---
+++ packages/macmac/macmac-0.1.tar.gz/macmac-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='macmac',
+      version='0.1',
+      description='Testing how python packages actually work',
+      url='http://github.com/dan/showme',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['macmac'],
+      zip_safe=False)
