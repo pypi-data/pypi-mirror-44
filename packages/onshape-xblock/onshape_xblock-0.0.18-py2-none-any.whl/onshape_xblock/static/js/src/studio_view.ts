f25bad08d14476b40df6494c80d23a33d82f40b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:15 +0000

    Add package onshape-xblock/0.0.18/onshape_xblock-0.0.18-py2-none-any.whl

---
+++ packages/onshape-xblock/onshape_xblock-0.0.18-py2-none-any.whl/onshape_xblock/static/js/src/studio_view.ts
@@ -0,0 +1,43 @@
+import * as $ from "jquery";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Form from "react-jsonschema-form";
+
+var $checkList;
+var formListItemHtml;
+var $form;
+
+$(function ($) {
+    setDOM();
+});
+
+function setDOM() {
+    $checkList = $("#xb-field-edit-check_list");
+    formListItemHtml = '<li className="field comp-setting-entry metadata_entry " data-field-name="check_list_form" >';
+
+    $checkList.before(formListItemHtml);
+    $form = $('li[data-field-name="check_list_form"]');
+
+    $checkList.attr("readonly", "");
+
+    var json = $.getJSON("/resource/onshape_xblock/public/json/check_list_form.json", function (schema) {
+        return loadForm(schema);
+    });
+}
+
+function loadForm(schema) {
+    var log = function log(type) {
+        return console.log.bind(console, type);
+    };
+
+    ReactDOM.render(React.createElement(Form, {
+        schema: schema,
+        onChange: onFormChange,
+        onSubmit: log("submitted"),
+        onError: log("errors")
+    }), $form[0]);
+}
+
+function onFormChange(data) {
+    $checkList.text(JSON.stringify(data.formData.this_array));
+}
