8f77951e22d2ebdabba54092e467af74defcbcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package pytraj/2.0.4/pytraj-2.0.4-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/pytraj/pytraj-2.0.4-cp36-cp36m-macosx_10_7_x86_64.whl/pytraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYTRAJ SETUP.PY
+short_version = '2.0.4'
+version = '2.0.4'
+full_version = '2.0.4'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
