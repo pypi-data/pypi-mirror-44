05b40da83b3b308ccd2aed519739cacb7da223aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package sekg/0.2.23/sekg-0.2.23.tar.gz

---
+++ packages/sekg/sekg-0.2.23.tar.gz/sekg-0.2.23/sekg/ir/preprocessor/base.py
@@ -0,0 +1,37 @@
+class Preprocessor:
+    """
+    a basic text preprocessor
+    """
+
+    def clean(self, text):
+        """
+        return a list of token from the text
+        :param text: the text need to preprocess
+        :return: list of str
+        """
+        return text.split()
+
+
+class SimplePreprocessor(Preprocessor):
+    remove_list = {"\n", "\t", "\r", "/", "*", ".", ";", "@", "{", "}", "<p>", "(", ")", "#", "=", ":", "+", "-",
+                   "!",
+                   "[", "]", ",", ":", "<", ">", "|", "\\", "&", "'", "?", "<", ">"}
+
+    def remove_special_char(self, str):
+        """
+        input;str
+        :return: str
+        """
+        new_str = str
+        for item in self.remove_list:
+            new_str = new_str.replace(item, " ").replace("  ", " ")
+        return new_str.strip()
+
+    def clean(self, text):
+        """
+        return a list of token from the text, only remove special char and lower the case
+        :param text: the text need to preprocess
+        :return: list of str
+        """
+        text = self.remove_special_char(text)
+        return text.lower().split()
