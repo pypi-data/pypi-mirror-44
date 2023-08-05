e8ead59c0d730ee1973e675258352f62b268b52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:55 +0000

    Add package fulcrum/1.9.0/fulcrum-1.9.0-py2.7.egg

---
+++ packages/fulcrum/fulcrum-1.9.0-py2.7.egg/fulcrum/__init__.py
@@ -0,0 +1,43 @@
+from fulcrum.api import Client
+from fulcrum.api.endpoints import (Forms, Records, Webhooks, Photos,
+                                   Memberships, Roles, ChoiceLists, Signatures,
+                                   ClassificationSets, Projects, Videos, Audio,
+                                   Changesets, ChildRecords, AuditLogs)
+
+__version__ = '1.9.0'
+
+
+def getUser(email, password):
+    return {'hey': 'yo'}
+
+class Fulcrum(object):
+    def __init__(self, key, uri='https://api.fulcrumapp.com'):
+        self.client = Client(key=key, uri=uri)
+
+        self.forms = Forms(client=self.client)
+        self.records = Records(client=self.client)
+        self.webhooks = Webhooks(client=self.client)
+        self.photos = Photos(client=self.client)
+        self.signatures = Signatures(client=self.client)
+        self.memberships = Memberships(client=self.client)
+        self.roles = Roles(client=self.client)
+        self.choice_lists = ChoiceLists(client=self.client)
+        self.classification_sets = ClassificationSets(client=self.client)
+        self.projects = Projects(client=self.client)
+        self.videos = Videos(client=self.client)
+        self.audio = Audio(client=self.client)
+        self.changesets = Changesets(client=self.client)
+        self.child_records = ChildRecords(client=self.client)
+        self.audit_logs = AuditLogs(client=self.client)
+
+    def query(self, sql, format = 'json'):
+        obj = {'q': sql, 'format': format}
+        kwargs = {
+            'data': obj,
+            'extra_headers': {'Content-Type': 'application/json'}
+        }
+
+        kwargs['json_content'] = False if format == 'csv' else True
+
+        api_resp = self.client.call('post', 'query', **kwargs)
+        return api_resp
