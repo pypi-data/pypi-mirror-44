afff538504a33f4c16a1dabcf4aa1e8adf904782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package snailwebs-seeyon-ops/0.0.2/snailwebs-seeyon-ops-0.0.2.tar.gz

---
+++ packages/snailwebs-seeyon-ops/snailwebs-seeyon-ops-0.0.2.tar.gz/snailwebs-seeyon-ops-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='snailwebs-seeyon-ops',
+    version='0.0.2',
+    description='致远协同办公系统运维工具',
+
+    author='snail',
+    author_email='snail_email@163.com',
+
+    packages=find_packages()
+)
+
+
+
+
