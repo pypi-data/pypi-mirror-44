185c37f795d544fc2bc10bd747a81d52f06332fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package jupyter-openbis-extension/0.2.1/jupyter-openbis-extension-0.2.1.tar.gz

---
+++ packages/jupyter-openbis-extension/jupyter-openbis-extension-0.2.1.tar.gz/jupyter-openbis-extension-0.2.1/jupyter-openbis-extension/static/state.js
@@ -0,0 +1,25 @@
+define([],
+    function () {
+        return {
+            // connection dialog
+            connection: {
+                name: null,
+                candidateName: null
+            },
+
+            // upload dialog
+            uploadDataSetType: null,
+            uploadDataSetTypes: {},
+            uploadEntityIdentifier: '',
+            datasetCheckboxes: [],
+            fileCheckboxes: [],
+            selectedFiles: [],
+            unselectedDatasets: [],
+
+            // download dialog
+            selectedDatasets: new Set([]),
+            entityIdentifier: '',
+            workingDirectory: ''
+        }
+    }
+)
