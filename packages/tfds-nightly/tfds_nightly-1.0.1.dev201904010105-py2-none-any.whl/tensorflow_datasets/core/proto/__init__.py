6adf986b60411b1d826a4cfc8bdf62756fe4c5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package tfds-nightly/1.0.1.dev201904010105/tfds_nightly-1.0.1.dev201904010105-py2-none-any.whl

---
+++ packages/tfds-nightly/tfds_nightly-1.0.1.dev201904010105-py2-none-any.whl/tensorflow_datasets/core/proto/__init__.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# Copyright 2019 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Public API of the proto package."""
+
+# pylint: disable=reimported,g-bad-import-order
+from tensorflow_datasets.core.proto import dataset_info_generated_pb2 as dataset_info_pb2
+from tensorflow_datasets.core.proto.dataset_info_generated_pb2 import SplitInfo
+# pylint: enable=reimported,g-bad-import-order
