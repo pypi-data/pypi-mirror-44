f23f908eb23782282fc89eac1775bf56e674b383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:15 +0000

    Add package gumo-task-emulator/0.0.3/gumo-task-emulator-0.0.3.tar.gz

---
+++ packages/gumo-task-emulator/gumo-task-emulator-0.0.3.tar.gz/gumo-task-emulator-0.0.3/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+
+name = 'gumo-task-emulator'
+version = '0.0.3'
+description = 'Gumo Task Emulator Library'
+dependencies = [
+    f'gumo-core >= {version}',
+    f'gumo-datastore >= {version}',
+    f'gumo-task >= {version}',
+    'Flask >= 1.0.2',
+    'flasgger >= 0.9.1',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = [
+    package for package in setuptools.find_packages()
+    if package.startswith('gumo')
+]
+
+namespaces = ['gumo', 'gumo.task_emulator']
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Gumo Project Team",
+    author_email="gumo-organizer@levii.co.jp",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gumo-py/gumo",
+    packages=packages,
+    namespaces=namespaces,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=dependencies,
+)
