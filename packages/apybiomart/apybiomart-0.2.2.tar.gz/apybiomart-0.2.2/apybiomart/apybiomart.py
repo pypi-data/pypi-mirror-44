e4ef6c93179df76dfe870aadac6e73c8f8bfe4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:17 +0000

    Add package apybiomart/0.2.2/apybiomart-0.2.2.tar.gz

---
+++ packages/apybiomart/apybiomart-0.2.2.tar.gz/apybiomart-0.2.2/apybiomart/apybiomart.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+from typing import Optional, List, Dict
+from .classes import MartServer, DatasetServer, AttributesServer, \
+    FiltersServer, SyncQuery, AsyncQuery
+
+
+def list_marts():
+    server = MartServer()
+    return server.list_marts()
+
+
+def list_datasets(mart: Optional[str] = "ENSEMBL_MART_ENSEMBL"):
+    server = DatasetServer(mart)
+    return server.list_datasets()
+
+
+def list_attributes(dataset: Optional[str] = "hsapiens_gene_ensembl"):
+    server = AttributesServer(dataset)
+    return server.list_attributes()
+
+
+def list_filters(dataset: Optional[str] = "hsapiens_gene_ensembl"):
+    server = FiltersServer(dataset)
+    return server.list_filters()
+
+
+def query(attributes: List[str],
+          filters: Dict[str, str],
+          dataset: Optional[str] = "hsapiens_gene_ensembl"):
+    server = SyncQuery(attributes, filters, dataset)
+    return server.query()
+
+
+async def aquery(attributes: List[str],
+                 filters: Dict[str, str],
+                 dataset: Optional[str] = "hsapiens_gene_ensembl"):
+    server = AsyncQuery(attributes, filters, dataset)
+    return await server.aquery()
