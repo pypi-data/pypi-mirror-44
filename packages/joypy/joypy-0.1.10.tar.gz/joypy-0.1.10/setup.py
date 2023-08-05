f1b521ea31eea566e54d8e1afdada497a91a048e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package joypy/0.1.10/joypy-0.1.10.tar.gz

---
+++ packages/joypy/joypy-0.1.10.tar.gz/joypy-0.1.10/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='joypy',
+      version='0.1.10',
+      description='Joyplots in python',
+      long_description='Joyplots in python.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Visualization',
+      ],
+      url='http://github.com/sbebo/joypy',
+      author='Leonardo Taccari',
+      author_email='leonardo.taccari@gmail.com',
+      license='MIT',
+      packages=['joypy'],
+      install_requires=[
+          'matplotlib',
+          'numpy',
+          'scipy>=0.11.0',
+          'pandas>=0.20.0'
+      ],
+      zip_safe=False)
