602c310af45b4cdf3601da1359e7464fca584e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package django-skivvy/0.1.12/django-skivvy-0.1.12.tar.gz

---
+++ packages/django-skivvy/django-skivvy-0.1.12.tar.gz/django-skivvy-0.1.12/README.rst
@@ -0,0 +1,45 @@
+django-skivvy
+-------------
+
+django-skivvy helps you write better and more readable tests for Django views.
+
+.. image:: https://travis-ci.org/oliverroick/django-skivvy.svg?branch=master
+    :target: https://travis-ci.org/oliverroick/django-skivvy
+
+Requirements
+~~~~~~~~~~~~
+- Python 3.4, 3.5, 3.6, 3.7
+- Django 1.11, 2.1, 2.2
+
+
+Installation
+~~~~~~~~~~~~
+
+.. code-block::
+
+    pip install django-skivvy
+
+Usage
+~~~~~
+Refer to the `documentation <https://oliverroick.de/django-skivvy/#django-skivvy>`_ for further details.
+
+
+Documentation
+~~~~~~~~~~~~~
+
+Docs live inside ``docs``, as a `DocBox <https://github.com/mapbox/docbox>`_ app. To change the content of the docs, edit the Markdown files in ``docs/content``.
+
+There's a script to help you build and deploy docs to the `gh-pages` branch. To deploy, run:
+
+.. code-block::
+
+    ./.deploy/deploy.sh
+
+Contributors
+~~~~~~~~~~~~~
+
+- `Adri <https://github.com/amplifi>`_
+- `Anthony Lukach <https://github.com/alukach>`_
+- `Bruno Alla <https://github.com/browniebroke>`_
+- `Parthvi Vala <https://github.com/valaparthvi>`_
+- `Oliver Roick <https://github.com/oliverroick>`_
---
+++ packages/django-skivvy/django-skivvy-0.1.12.tar.gz/django-skivvy-0.1.12/setup.py
@@ -0,0 +1,100 @@
+import sys
+import os
+import re
+import shutil
+from setuptools import setup
+
+name = 'django-skivvy'
+package = 'skivvy'
+description = ('Write faster integration tests for Django views – with less '
+               'code.')
+url = 'https://github.com/oliverroick/django-skivvy'
+author = 'Oliver Roick'
+author_email = 'oliver.roick@gmail.com'
+license = 'AGPL'
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(readme_file, 'r') as f:
+    long_description = f.readline().strip()
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
+                     init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version(package)
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    shutil.rmtree('dist', ignore_errors=True)
+    shutil.rmtree('build', ignore_errors=True)
+    os.system("python setup.py sdist")
+    os.system("python setup.py bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name=name,
+    version=version,
+    url=url,
+    license=license,
+    description=description,
+    long_description=long_description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Testing :: Mocking',
+    ]
+)
