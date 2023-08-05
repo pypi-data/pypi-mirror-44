aa41eb384c8e2198e616f0792083f8f987e39e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:19 +0000

    Add package pcapfilter/0.2.0/pcapfilter-0.2.0-py3-none-any.whl

---
+++ packages/pcapfilter/pcapfilter-0.2.0-py3-none-any.whl/pcapfilter/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PCap Filter."""
+
+__author__ = """Nahuel Defossé"""
+__email__ = "nahuel.defosse+pip@gmail.com"
+__version__ = "__version__ = '0.2.0'"
