784205ffb2e12481776d12c621d84dc3cb493915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:32 +0000

    Add package pyleiades/0.0.0.dev4/pyleiades-0.0.0.dev4-py3-none-any.whl

---
+++ packages/pyleiades/pyleiades-0.0.0.dev4-py3-none-any.whl/pyleiades/__init__.py
@@ -0,0 +1,28 @@
+# Licensed under the FreeBSD license
+
+"""
+pyleiades: Python Library for EIA Data Examination & Exhibition
+
+Tools to use with the EIA Monthly Energy Review datasets. This package provides
+an API for performing more sophisticated examination and visualization of the
+Energy Information Administration (EIA) Monthly Energy Review (MER) datasets.
+
+Data can be accessed directly at the EIA website:
+    https://www.eia.gov/totalenergy/data/browser/
+"""
+
+import os
+from pyleiades.energies import Energy
+from pyleiades.visuals import Visual
+
+package_dir = os.path.dirname(__file__)
+project_dir = os.path.join(package_dir, '..')
+
+# Read the contents of the VERSION file
+with open(os.path.join(project_dir, '_version')) as version_file:
+    __version__ = version_file.read().strip()
+
+DATA_DIR = os.path.join(package_dir, 'data')
+ARCHIVE_DIR = os.path.join(DATA_DIR, 'archive')
+
+
