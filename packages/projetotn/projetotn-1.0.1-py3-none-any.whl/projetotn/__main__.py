a7ceb8a80f0f37046c4ea3859c96bd6576177690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:10 +0000

    Add package projetotn/1.0.1/projetotn-1.0.1-py3-none-any.whl

---
+++ packages/projetotn/projetotn-1.0.1-py3-none-any.whl/projetotn/__main__.py
@@ -0,0 +1,9 @@
+#============ Nielsen ================
+# Created: 22/03/2019 / 
+# Created by Jeiso Silva
+#=====================================
+#--------------------------------------------------------------------------------------------------------------------------------------------- 
+from tnproject import test
+from tnproject import pilar2
+from tnproject import ap_promo
+from tnproject import funcao
