99b16a15dbb023e25468e4d7c46849d9ffed97ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:20 +0000

    Add package gumo-task/0.0.1/gumo-task-0.0.1.tar.gz

---
+++ packages/gumo-task/gumo-task-0.0.1.tar.gz/gumo-task-0.0.1/setup.py
@@ -0,0 +1,37 @@
+import os
+import setuptools
+
+
+name = 'gumo-task'
+with open(os.path.join('..', 'gumo_version.txt'), 'r') as fh:
+    version = fh.read().strip()
+
+description = 'Gumo Task Library'
+dependencies = [
+    f'gumo-core >= {version}',
+    f'gumo-datastore >= {version}',
+    'google-cloud-tasks >= 0.3.0',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = setuptools.find_packages()
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Gumo Project Team",
+    author_email="gumo-organizer@levii.co.jp",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gumo-py/gumo",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=dependencies,
+)
