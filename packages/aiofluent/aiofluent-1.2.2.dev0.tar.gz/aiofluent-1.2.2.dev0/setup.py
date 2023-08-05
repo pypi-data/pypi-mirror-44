698f9d6a9e0b5c384b1419567b31a56049eda855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:46 +0000

    Add package aiofluent/1.2.2.dev0/aiofluent-1.2.2.dev0.tar.gz

---
+++ packages/aiofluent/aiofluent-1.2.2.dev0.tar.gz/aiofluent-1.2.2.dev0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+README = path.abspath(path.join(path.dirname(__file__), 'README.rst'))
+CHANGELOG = path.abspath(path.join(path.dirname(__file__), 'CHANGELOG.rst'))
+desc = 'A Python logging handler for Fluentd event collector'
+
+setup(
+    name='aiofluent',
+    version='1.2.2.dev0',
+    description=desc,
+    long_description=open(README).read() + '\n\n' + open(CHANGELOG).read(),
+    package_dir={'aiofluent': 'aiofluent'},
+    packages=['aiofluent'],
+    install_requires=['msgpack-python'],
+    author='Nathan Van Gheem',
+    author_email='vangheem@gmail.com',
+    url='https://github.com/onna/aiofluent',
+    download_url='http://pypi.python.org/pypi/aiofluent/',
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+    ],
+    extras_require={
+        'test': [
+            'pytest<=3.1.0',
+            'pytest-asyncio>=0.8.0',
+            'pytest-aiohttp',
+            'pytest-cov',
+            'coverage'
+        ]
+    },
+    test_suite='tests'
+)
