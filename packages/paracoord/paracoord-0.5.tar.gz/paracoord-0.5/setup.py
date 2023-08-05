27e9b2545c46a2b1a7921fa0c09e37265a79dafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package paracoord/0.5/paracoord-0.5.tar.gz

---
+++ packages/paracoord/paracoord-0.5.tar.gz/paracoord-0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='paracoord',
+    version='0.5',
+    author='Manuel Capel',
+    author_email='manuel.capel82@gmail.com',
+    description='Parallel Coordinates plotting',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mancap314/paracoord',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
