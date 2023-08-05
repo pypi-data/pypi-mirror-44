d306c62ca38595631084ff3ebb1585ec10c638b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:15 +0000

    Add package saffy/0.1.1/saffy-0.1.1-py3-none-any.whl

---
+++ packages/saffy/saffy-0.1.1-py3-none-any.whl/saffy/plugins/PluginManager.py
@@ -0,0 +1,16 @@
+
+class PluginManager(object):
+	def __init__(self, *args, **kwargs):
+		self.fs = 0
+		self.num_channels = 0
+		self.channel_names = []
+		self.data = []
+		self.t = []
+		self.epochs = 0
+		self.tags = []
+		self.spectrum = []
+		self.spectrum_freqs = []
+		self.phase = []
+
+	def __repr__(self):
+		return 'Plugin'
