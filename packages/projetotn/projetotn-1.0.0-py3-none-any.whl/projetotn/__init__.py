6cb0b9e3524aeff54471a0394c5a0cb69f5b2c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:10 +0000

    Add package projetotn/1.0.0/projetotn-1.0.0-py3-none-any.whl

---
+++ packages/projetotn/projetotn-1.0.0-py3-none-any.whl/projetotn/__init__.py
@@ -0,0 +1,8 @@
+#============ Nielsen =================
+# Created: 09/03/2019 / Classe Pilar 2
+# Created by Jeiso Silva
+#======================================
+from tnproject import test
+from tnproject import pilar2
+from tnproject import appromo
+from tnproject import funcao
