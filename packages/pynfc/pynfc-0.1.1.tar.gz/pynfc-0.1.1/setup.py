4853b454f06c72395bfef662e1e9a74c914436a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package pynfc/0.1.1/pynfc-0.1.1.tar.gz

---
+++ packages/pynfc/pynfc-0.1.1.tar.gz/pynfc-0.1.1/setup.py
@@ -0,0 +1,28 @@
+""""""
+from setuptools import setup
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name="pynfc",
+    version="0.1.1",
+    description="`ctypeslib` converted libnfc and libfreefare",
+    long_description=long_description,
+    author="Barnaby Shearer",
+    author_email="b@Zi.iS",
+    url="https://github.com/BarnabyShearer/pynfc.git",
+    license='BSD',
+    keywords="RFID NFC Mifare Desfire",
+    packages=[
+        "pynfc"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
