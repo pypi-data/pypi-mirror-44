fbfc1d7a5c2852fd1889bf4538a11f02d01cb621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package reprobench/0.7.1.dev4/reprobench-0.7.1.dev4.tar.gz

---
+++ packages/reprobench/reprobench-0.7.1.dev4.tar.gz/reprobench-0.7.1.dev4/reprobench/core/events.py
@@ -0,0 +1,11 @@
+SERVER_PING = b"server:ping"
+
+WORKER_JOIN = b"worker:join"
+WORKER_REQUEST = b"worker:request"
+WORKER_DONE = b"worker:done"
+WORKER_LEAVE = b"worker:leave"
+
+RUN_START = b"run:start"
+RUN_STEP = b"run:step"
+RUN_INTERRUPT = b"run:interrupt"
+RUN_FINISH = b"run:finish"
