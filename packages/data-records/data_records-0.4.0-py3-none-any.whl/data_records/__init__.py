ed8ab749bdc41de91246b4596d86b902787bd4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:16 +0000

    Add package data-records/0.4.0/data_records-0.4.0-py3-none-any.whl

---
+++ packages/data-records/data_records-0.4.0-py3-none-any.whl/data_records/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+__version__ = "0.4.0"
+
+__all__ = [
+    'datarecord',
+    'CannotMutateRecordError',
+]
+
+from .record import datarecord
+from .exceptions import CannotMutateRecordError
