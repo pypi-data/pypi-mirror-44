6b999149c3bd0117d80486cbc4fdca6812bfb0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package toobuk/0.6/toobuk-0.6-py3-none-any.whl

---
+++ packages/toobuk/toobuk-0.6-py3-none-any.whl/toobuk/util.py
@@ -0,0 +1,14 @@
+
+# def replaceUrl (str, r) :
+# 	for key in r.keys() :
+# 		str = str.replace('#'+key+'#', r[key])
+
+# 	return str;
+
+# def toBool(str) :
+# 	return str.lower() is 'true'
+
+# typeSwitcher = {
+# 	"int" : int,
+# 	"float": float
+# }
