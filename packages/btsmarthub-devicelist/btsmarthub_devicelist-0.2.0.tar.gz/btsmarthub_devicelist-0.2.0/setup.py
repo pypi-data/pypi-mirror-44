5430d19ba1411d43a9027a8dd2a98cb4a7f80ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:01 +0000

    Add package btsmarthub-devicelist/0.2.0/btsmarthub_devicelist-0.2.0.tar.gz

---
+++ packages/btsmarthub-devicelist/btsmarthub_devicelist-0.2.0.tar.gz/btsmarthub_devicelist-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='btsmarthub_devicelist',
+    version='0.2.0',
+    packages=['btsmarthub_devicelist'],
+    url='https://github.com/jxwolstenholme/btsmarthub_devicelist',
+    license='MIT',
+    author='Jamie Wolstenholme',
+    author_email='jxwolstenholme@gmail.com',
+    description='Retrieve a list of devices from a bt smarthub or bt smarthub 2 on a local network'
+)
