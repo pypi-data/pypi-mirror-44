fbb3c61ddc5eff5bb37eba9e135e2aab4eb04fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package bbrc-bx/0.0.1/bbrc-bx-0.0.1.tar.gz

---
+++ packages/bbrc-bx/bbrc-bx-0.0.1.tar.gz/bbrc-bx-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+setup(
+  name = 'bbrc-bx',
+  install_requires=requirements,
+  scripts=['bin/bx'],
+  version = '0.0.1',
+  description = 'BarcelonaBeta + XNAT = bx',
+  author = 'Greg Operto',
+  author_email = 'goperto@barcelonabeta.org',
+  url = 'https://gitlab.com/xgrg/bx',
+  download_url = 'https://gitlab.com/xgrg/bx/-/archive/v0.0.1/bx-v0.0.1.tar.gz',
+  classifiers = ['Intended Audience :: Science/Research',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering',
+      'Operating System :: Unix',
+      'Programming Language :: Python :: 3.7' ]
+)
