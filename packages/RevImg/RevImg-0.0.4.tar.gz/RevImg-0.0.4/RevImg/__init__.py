b23f70c6a48cde85ff4173ac2f37154044d6a3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package RevImg/0.0.4/RevImg-0.0.4.tar.gz

---
+++ packages/RevImg/RevImg-0.0.4.tar.gz/RevImg-0.0.4/RevImg/__init__.py
@@ -0,0 +1,1 @@
+from .RevImg import RevImg
---
+++ packages/RevImg/RevImg-0.0.4.tar.gz/RevImg-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RevImg",
+    version="0.0.4",
+    author="Christophe Rimann",
+    author_email="infobiac1@gmail.com",
+    description="A package to interact with the google reverse image search service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/infobiac/RevImg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
