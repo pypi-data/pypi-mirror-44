3234b38aff6caadeacb8ee7c25db4323e9ad55a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:23 +0000

    Add package hapic/0.74/hapic-0.74.tar.gz

---
+++ packages/hapic/hapic-0.74.tar.gz/hapic-0.74/hapic/data.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import typing
+import urllib.parse
+
+
+class HapicData(object):
+    def __init__(self):
+        self.body = {}
+        self.path = {}
+        self.query = {}
+        self.headers = {}
+        self.forms = {}
+        self.files = {}
+
+
+class HapicFile(object):
+    def __init__(
+        self,
+        file_path: typing.Optional[str] = None,
+        file_object: typing.Any = None,
+        mimetype: typing.Any = None,
+        filename: str = None,
+        as_attachment: bool = False,
+    ):
+        self.file_path = file_path
+        self.file_object = file_object
+        self.filename = filename
+        self.mimetype = mimetype
+        self.as_attachment = as_attachment
+
+    def get_content_disposition_header_value(self) -> str:
+        disposition = "inline"
+        if self.as_attachment:
+            disposition = "attachment"
+        if self.filename:
+            # INFO - G.M - 2018-10-26 - deal correctly with unicode filename
+            # see rfc6266 for more info.
+            ascii_filename = self.filename.encode("ascii", "replace").decode()
+            # INFO - G.M - 2018-10-30 - Format correctly unicode.
+            # encoding is needed for correct unicode character support,
+            # Percent-encoding is best pratices, see also rfc5987.
+            urlencoded_unicode_filename = urllib.parse.quote(self.filename)
+
+            disposition = "{}; filename=\"{}\"; filename*=UTF-8''{};".format(
+                disposition, ascii_filename, urlencoded_unicode_filename
+            )
+        return disposition
---
+++ packages/hapic/hapic-0.74.tar.gz/hapic-0.74/tests/func/test_marshmallow_decoration.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+import json
+
+import marshmallow
+
+from hapic import Hapic
+from hapic import MarshmallowProcessor
+from tests.base import Base
+from tests.base import MyContext
+
+try:  # Python 3.5+
+    from http import HTTPStatus
+except ImportError:
+    from http import client as HTTPStatus
+
+
+class TestMarshmallowDecoration(Base):
+    def test_unit__input_files__ok__file_is_present(self):
+        hapic = Hapic(processor_class=MarshmallowProcessor)
+        hapic.set_context(
+            MyContext(
+                app=None, fake_files_parameters={"file_abc": "10101010101"}
+            )
+        )
+
+        class MySchema(marshmallow.Schema):
+            file_abc = marshmallow.fields.Raw(required=True)
+
+        @hapic.input_files(MySchema())
+        def my_controller(hapic_data=None):
+            assert hapic_data
+            assert hapic_data.files
+            return "OK"
+
+        result = my_controller()
+        assert "OK" == result
+
+    def test_unit__input_files__ok__file_is_not_present(self):
+        hapic = Hapic(processor_class=MarshmallowProcessor)
+        hapic.set_context(
+            MyContext(
+                app=None,
+                fake_files_parameters={
+                    # No file here
+                },
+            )
+        )
+
+        class MySchema(marshmallow.Schema):
+            file_abc = marshmallow.fields.Raw(required=True)
+
+        @hapic.input_files(MySchema())
+        def my_controller(hapic_data=None):
+            assert hapic_data
+            assert hapic_data.files
+            return "OK"
+
+        result = my_controller()
+        assert HTTPStatus.BAD_REQUEST == result.status_code
+        assert {
+            "http_code": 400,
+            "original_error": {
+                "details": {"file_abc": ["Missing data for required field"]},
+                "message": "Validation error of input data",
+            },
+        } == json.loads(result.body)
+
+    def test_unit__input_files__ok__file_is_empty_string(self):
+        hapic = Hapic(processor_class=MarshmallowProcessor)
+        hapic.set_context(
+            MyContext(app=None, fake_files_parameters={"file_abc": ""})
+        )
+
+        class MySchema(marshmallow.Schema):
+            file_abc = marshmallow.fields.Raw(required=True)
+
+        @hapic.input_files(MySchema())
+        def my_controller(hapic_data=None):
+            assert hapic_data
+            assert hapic_data.files
+            return "OK"
+
+        result = my_controller()
+        assert HTTPStatus.BAD_REQUEST == result.status_code
+        assert {
+            "http_code": 400,
+            "original_error": {
+                "details": {"file_abc": ["Missing data for required field"]},
+                "message": "Validation error of input data",
+            },
+        } == json.loads(result.body)
