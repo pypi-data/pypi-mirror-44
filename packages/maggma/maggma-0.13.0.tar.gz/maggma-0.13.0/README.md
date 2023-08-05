9ab84e58158a919a729f1b572a09fd33e038bc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package maggma/0.13.0/maggma-0.13.0.tar.gz

---
+++ packages/maggma/maggma-0.13.0.tar.gz/maggma-0.13.0/README.md
@@ -0,0 +1,4 @@
+
+# Maggma [![Build Status](https://travis-ci.org/materialsproject/maggma.svg?branch=master)](https://travis-ci.org/materialsproject/maggma)
+
+Workflow tools for MongoDB aggregation in Python
---
+++ packages/maggma/maggma-0.13.0.tar.gz/maggma-0.13.0/maggma/examples/builders.py
@@ -0,0 +1,10 @@
+"""
+Example builders for testing and general use.
+"""
+from maggma.builders import MapBuilder, CopyBuilder
+
+import warnings
+
+warnings.warn("maggma.examples.builder is now deprecated. "
+              "MapBuilder and CopyBuilder have been moved to the "
+              "main builders module")
