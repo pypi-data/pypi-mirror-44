016a7df64bf29cad35296bb224a02068d6948cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:50 +0000

    Add package pds4-tools/1.1/pds4_tools-1.1.zip

---
+++ packages/pds4-tools/pds4_tools-1.1.zip/pds4_tools-1.1/pds4_tools/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Lev Nagdimunov"
+__copyright__ = "University of Maryland"
+
+__version__ = "1.1"
+__email__ = "lnagdi1@astro.umd.edu"
+
+from .reader import pds4_read
+from .viewer import pds4_viewer
+
+from .reader import pds4_read as read
+from .viewer import pds4_viewer as view
