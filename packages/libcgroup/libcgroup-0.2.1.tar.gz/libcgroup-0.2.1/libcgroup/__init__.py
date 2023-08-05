131d9df12935bc660b745fe68f089c7431103828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package libcgroup/0.2.1/libcgroup-0.2.1.tar.gz

---
+++ packages/libcgroup/libcgroup-0.2.1.tar.gz/libcgroup-0.2.1/libcgroup/__init__.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+
+from libcgroup_bind.init import cgroup_init
+
+from .cgroup import CGroup
+# noinspection PyUnresolvedReferences
+from .mount_point import MountPoint
+from .tools import _raise_error
+
+ret = cgroup_init()
+if ret is not 0:
+    _raise_error(ret)
+
+CGroup.root = CGroup.from_existing('/')
