e830b3408999a0900112f903986d8c9474fb882c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package urmem/0.0.45/urmem-0.0.45.tar.gz

---
+++ packages/urmem/urmem-0.0.45.tar.gz/urmem-0.0.45/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name= 'urmem',
+    version= '0.0.45',
+    packages = ['urmem'],
+    entry_points = {
+        "console_scripts": [
+            "urmem = urmem.urmem:main",
+        ]
+    },
+    author='doomzhou',
+    author_email='zzepaigh@gmail.com',
+    license='Apache',
+    url='https://github.com/doomzhou/urmem',
+    description='Download a git repo raw file content in every server, sometimes for source bash_profile'
+)
