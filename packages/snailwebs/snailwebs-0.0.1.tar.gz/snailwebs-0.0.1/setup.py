45fe4d1253a3ada919f5686490394fd3a053ac24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:59 +0000

    Add package snailwebs/0.0.1/snailwebs-0.0.1.tar.gz

---
+++ packages/snailwebs/snailwebs-0.0.1.tar.gz/snailwebs-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='snailwebs',
+    version='0.0.1',
+    description='Python工具',
+
+    author='snail',
+    author_email='snail_email@163.com',
+
+    packages=['snailwebs']
+)
+
+
+
+
