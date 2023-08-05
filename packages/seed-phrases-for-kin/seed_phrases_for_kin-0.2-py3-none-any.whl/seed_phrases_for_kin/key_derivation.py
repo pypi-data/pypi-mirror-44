703b1158f26ca0a62aecbdf2090297b89e4f86d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:21 +0000

    Add package seed-phrases-for-kin/0.2/seed_phrases_for_kin-0.2-py3-none-any.whl

---
+++ packages/seed-phrases-for-kin/seed_phrases_for_kin-0.2-py3-none-any.whl/seed_phrases_for_kin/key_derivation.py
@@ -0,0 +1,124 @@
+# -*- python -*-
+# -*- coding: UTF-8 -*-
+
+# key_derivation
+# Copyright (C) 2017 chancity
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import struct
+import hashlib
+import hmac
+
+
+# BIP-0044 path format for Stellar keypair derivation, as specified in
+# https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0005.md
+ACCOUNT_PATH_FORMAT = "m/44'/2017'/%d'"
+
+
+# the index of the first hardened key, per BIP-0032 and SLIP-0010
+HARDENED = 0x80000000
+
+# as defined in https://github.com/satoshilabs/slips/blob/master/slip-0010.md
+CURVE = b'ed25519 seed'
+
+
+hmac_sha_512 = lambda key, data: hmac.new(key, data, hashlib.sha512).digest()
+
+
+def key(v):
+    """Return the private key part (the left half) of a 64-byte sequence v."""
+    return v[:32]
+
+
+def chain_code(v):
+    """Return the chain code part (the right half) of a 64-byte sequence v."""
+    return v[32:]
+
+
+def ser32(i):
+    """Serialize a 32-bit unsigned integer i as a 4-byte sequence.
+
+       The most significant byte of i appears first in the serialization.
+    """
+    return struct.pack('>L', i)
+    
+
+def new_master_key(seed):
+    """Return the extended master key derived from a 64-byte binary seed.
+    
+    BIP-0032 defines an extended key as a pair (private_key, chain_code).
+    The extended master key is the pair (master_private_key, master_chain_code)
+    specified by SLIP-0010.
+    """
+    h = hmac_sha_512(CURVE, seed);
+    return (key(h), chain_code(h))
+
+
+def derive(parent_key, parent_chain_code, i):
+    """Return the i-th extended child key from an extended parent key."""
+    assert len(parent_key) == 32
+    assert len(parent_chain_code) == 32    
+    assert i >= HARDENED, 'no public derivation for ed25519'
+    data = b'\x00' + parent_key + ser32(i)
+    h = hmac_sha_512(parent_chain_code, data)
+    return (key(h), chain_code(h))
+
+
+def derive_along_path(path, seed):
+    """Derive an extended key from a 64-byte binary seed and a BIP-0044 path.
+
+    Returns the extended key obtained by following the given derivation path, 
+    starting at the extended master key derived from the given binary seed.
+    """
+    elements = list(element.rstrip("'") for element in path.split('/'))[1:]
+    (key, chain_code) = new_master_key(seed)
+    for e in elements:
+        (key, chain_code) = derive(key, chain_code, int(e) | HARDENED)
+    return key
+
+
+def account_keypair(seed, account_number):
+    """Return the account keypair for a 64-byte binary seed and account_number."""
+    from stellar_base.keypair import Keypair
+    acc_seed = derive_along_path(ACCOUNT_PATH_FORMAT % account_number, seed);
+    return Keypair.from_raw_seed(acc_seed)
+
+def selftest():
+    from binascii import unhexlify
+    
+    seed = unhexlify(
+        'e4a5a632e70943ae7f07659df1332160937fad82587216a4c64315a0fb39497ee4a01f76ddab4cba68147977f3a147b6ad584c41808e8238a07f6cc4b582f186'
+    )
+    k = unhexlify(
+        'e0eec84fe165cd427cb7bc9b6cfdef0555aa1cb6f9043ff1fe986c3c8ddd22e3'
+    )
+    key = derive_along_path("m/44'/148'", seed)
+    assert key == k
+
+    kp = account_keypair(seed, 0)
+    assert kp.address().decode() == 'GDRXE2BQUC3AZNPVFSCEZ76NJ3WWL25FYFK6RGZGIEKWE4SOOHSUJUJ6'
+    assert kp.seed().decode() == 'SBGWSG6BTNCKCOB3DIFBGCVMUPQFYPA2G4O34RMTB343OYPXU5DJDVMN'
+
+    kp = account_keypair(seed, 9)
+    assert kp.address().decode() == 'GBTVYYDIYWGUQUTKX6ZMLGSZGMTESJYJKJWAATGZGITA25ZB6T5REF44'
+    assert kp.seed().decode() == 'SCJGVMJ66WAUHQHNLMWDFGY2E72QKSI3XGSBYV6BANDFUFE7VY4XNXXR'
---
+++ packages/seed-phrases-for-kin/seed_phrases_for_kin-0.2-py3-none-any.whl/seed_phrases_for_kin/seed_phrase_to_kin_keys.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python3
+# -*- python -*-
+# -*- coding: UTF-8 -*-
+
+# seed_phrase_to_kin_keys
+# Copyright (C) 2017 chancity
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+def to_binary_seed(seed_phrase, passphrase='', language='english'):
+    """Derive a 64-byte binary seed from a mnemonic seed phrase.
+
+    Return a pair (binary_seed, seed_phrase_type), where seed_phrase_type
+    is one of the following strings:
+    - 'BIP-0039'
+    - 'BIP-0039 and Electrum standard'
+    - 'BIP-0039 and Electrum segwit'
+    - 'BIP-0039 and Electrum 2FA'
+    - 'Old (pre 2.0) Electrum'
+    - 'Electrum standard'
+    - 'Electrum segwit'
+    - 'Electrum 2FA'
+    - 'UNKNOWN'
+
+    If seed_phrase is a BIP39-compliant phrase for the specfied language
+    (this condition covers the first four cases listed above), generate 
+    the binary seed as recommended by BIP39. Otherwise, if seed_phrase 
+    is an Electrum seed phrase (this condition covers the next four cases 
+    listed above), generate the binary seed by using Electrum's algorithm.
+    Otherwise (this is the last case listed above), generate the binary 
+    seed in a non-standard way.
+    
+    Keyword arguments:
+    passphrase -- an optional extension of the seed phrase (default: '')
+    language -- the language for a BIP-0039 seed phrase (default: 'english')
+    """
+    import hashlib
+    import hmac
+    from pbkdf2 import PBKDF2
+    from mnemonic import Mnemonic
+    from .electrum_mnemonic import normalize_text, \
+                                   is_new_electrum_seed_phrase, \
+                                   ELECTRUM_SEED_PREFIX_SW, \
+                                   ELECTRUM_SEED_PREFIX_2FA, \
+                                   electrum_seed_type
+    
+    seed_phrase = normalize_text(seed_phrase)
+    passphrase = normalize_text(passphrase)
+    
+    m = Mnemonic(language)
+    if m.check(seed_phrase):
+        seed_phrase_type = 'BIP-0039'
+        if is_new_electrum_seed_phrase(seed_phrase):
+            seed_phrase_type += ' and Electrum standard'
+        elif is_new_electrum_seed_phrase(seed_phrase, ELECTRUM_SEED_PREFIX_SW):
+            seed_phrase_type += ' and Electrum segwit'
+        elif is_new_electrum_seed_phrase(seed_phrase, ELECTRUM_SEED_PREFIX_2FA):
+            seed_phrase_type += ' and Electrum 2FA'
+        # salt for BIP-0039 seed
+        salt = 'mnemonic' + passphrase
+    else:
+        seed_phrase_type = electrum_seed_type(seed_phrase)
+        salt = ('electrum'
+                if seed_phrase_type != 'UNKNOWN'
+                else 'non-standard') + passphrase
+
+    PBKDF2_ROUNDS = 2048
+    binary_seed = PBKDF2(seed_phrase,
+                         salt,
+                         iterations = PBKDF2_ROUNDS,
+                         macmodule = hmac,
+                         digestmodule = hashlib.sha512).read(64)
+    return (binary_seed, seed_phrase_type)
+
+
+def digit_count(n):
+    return 1 if n < 10 else 1 + digit_count(n // 10)
+
+
+def account_message(i):
+    msg = '       account #%d:' % i
+    if i == 0:
+        msg += ' ------------- this is the primary account --------------'
+    return '\n' + msg[digit_count(i) - 1:]
+
+
+def interactive_function(language, n_accounts, print_binary_seed, force):
+    from .electrum_mnemonic import bin_to_hexstr
+    from .key_derivation import account_keypair
+    
+    seed_phrase = input('\nEnter the seed phrase:\n')
+    passphrase = input('\nEnter optional custom words (passphrase)'
+                       ' to extend the seed phrase:\n')
+    print('')
+
+    (binary_seed, seed_phrase_type) = to_binary_seed(seed_phrase,
+                                                     passphrase,
+                                                     language)
+    
+    print("      seed phrase: '" + seed_phrase + "'")
+    print("     custom words: '" + passphrase + "'")
+    print(' seed phrase type:', seed_phrase_type)
+    if seed_phrase_type != 'UNKNOWN' or force:
+        if print_binary_seed:
+            if seed_phrase_type.startswith('BIP-0039'):
+                print('    BIP-0039 seed:',  bin_to_hexstr(binary_seed))
+            elif seed_phrase_type != 'UNKNOWN':
+                print('    Electrum seed:',  bin_to_hexstr(binary_seed))
+            else:
+                print('non-standard seed:',  bin_to_hexstr(binary_seed))
+        if n_accounts > 1:
+            for i in range(n_accounts):
+                kp = account_keypair(binary_seed, i)
+                print(account_message(i))
+                print('       public key:',  kp.address().decode())
+                print('     private seed:',  kp.seed().decode())
+        else:
+            kp = account_keypair(binary_seed, 0)
+            print('\n  primary account:')
+            print('       public key:',  kp.address().decode())
+            print('     private seed:',  kp.seed().decode())
+    print()
+
+
+def main():
+    import argparse
+    import sys
+    import os
+    from mnemonic import Mnemonic
+    from .version import __version__
+
+    parser = argparse.ArgumentParser(
+        description='''Generate Kin account keys from BIP-0039/Electrum 
+                       seed phrases''',
+        epilog='''The default behavior of %(prog)s is to show just the keys 
+                  for one Kin account (the primary account). By default, 
+                  the binary seed derived from the seed phrase is not shown. 
+                  This behavior can be changed by the '-n N' and '-s' switches.
+               '''
+    )
+    parser.add_argument(
+        '-n',
+        '--n_accts',
+        dest='n',
+        type=int,
+        default=1,
+        help='show keys for multiple accounts (N > 0)')
+    parser.add_argument(
+        '-s',
+        '--show_seed', 
+        help='show the standard BIP-0039 or Electrum seed',
+        action='store_true')
+    parser.add_argument(
+        '-l',
+        '--list_languages', 
+        help='list available languages for BIP-0039 phrases and exit', 
+        action='store_true')
+    parser.add_argument(
+        '-L',
+        dest='lang',
+        type=str,
+        default='english',
+        help='language for BIP-0039 seed phrases (default: english)')
+    parser.add_argument(
+        '-v',
+        '--version', 
+        action='version',
+        version='%(prog)s version ' + __version__)
+    parser.add_argument(
+        '-F',
+        '--force', 
+        help='force keypair generation from phrase of unknown type', 
+        action='store_true')
+    args = parser.parse_args()
+    
+    if args.list_languages or args.lang != 'english':
+        lst = sorted(Mnemonic.list_languages())
+    if args.list_languages:
+        for language in lst:
+            print(language)
+    elif args.lang not in Mnemonic.list_languages():
+        print("language '%s' not available" % args.lang)
+        try:
+            m = Mnemonic(args.lang)
+        except FileNotFoundError as err:
+            msg = str(err)
+            # expected msg:
+            # "[Errno 2] No such file or directory: '...'"
+            if msg.startswith('[Errno 2] '):
+                msg = msg[len('[Errno 2] '):]
+            print(msg)
+    elif args.n < 0:
+        print('number of accounts must be greater than zero')
+    else:
+        interactive_function(args.lang, args.n, args.show_seed, args.force)
