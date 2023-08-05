2a249f0e21d47b4ce3600eb6a1ce6b290a19be16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package simple-NER/0.1.7/simple_NER-0.1.7.tar.gz

---
+++ packages/simple-NER/simple_NER-0.1.7.tar.gz/simple_NER-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='simple_NER',
+    version='0.1.7',
+    packages=['simple_NER', 'simple_NER.rules', 'simple_NER.annotators',
+              'simple_NER.annotators.utils'],
+    url='https://github.com/JarbasAl/simple_NER',
+    license='MIT',
+    author='jarbasAI',
+    install_requires=["padaos"],
+    author_email='jarbasai@mailfence.com',
+    description='rule based NER'
+)
