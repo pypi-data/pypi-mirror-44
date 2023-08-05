c3a69bdaeec0aee8360a2d4ca28a150d4c4d16b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package contraption/0.0.11/contraption-0.0.11.tar.gz

---
+++ packages/contraption/contraption-0.0.11.tar.gz/contraption-0.0.11/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="contraption",
+    version="0.0.11",
+    author="William Wyatt",
+    author_email="wwyatt@ucsc.edu",
+    description="pyvisa support for powersupplies DPO and DAQs (Aglient, Keithley, Tektronix, Lecroy)",
+    long_description="pyvisa support for powersupplies DPO and DAQs (Aglient, Keithley, Tektronix, Lecroy)",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Tsangares/Devices",
+    #scripts=['probecard/bin/probecard'],
+    install_requires=[
+        "PyVISA==1.9.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
