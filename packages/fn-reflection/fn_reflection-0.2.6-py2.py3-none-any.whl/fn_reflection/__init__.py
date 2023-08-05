ef3c1fd9bac3eadcc1f11c36ffa8c44c172ef811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package fn-reflection/0.2.6/fn_reflection-0.2.6-py2.py3-none-any.whl

---
+++ packages/fn-reflection/fn_reflection-0.2.6-py2.py3-none-any.whl/fn_reflection/__init__.py
@@ -0,0 +1,3 @@
+from . import decorator
+from . import func
+version = '0.2.6'
---
+++ packages/fn-reflection/fn_reflection-0.2.6-py2.py3-none-any.whl/fn_reflection/_external.py
@@ -0,0 +1,5 @@
+# pylint: disable=missing-docstring,invalid-name,unused-import
+import sys
+import os
+import typing
+import logging
