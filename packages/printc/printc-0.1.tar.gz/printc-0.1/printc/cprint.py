fb052ee178e2fbe4b227dd4784835d832565aeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package printc/0.1/printc-0.1.tar.gz

---
+++ packages/printc/printc-0.1.tar.gz/printc-0.1/printc/cprint.py
@@ -0,0 +1,18 @@
+def print_red(str):
+	# 绿色字体
+	print('\033[1;31m' + str + '\033[0m')
+def print_green(str):
+	# 绿色字体
+	print('\033[1;32m' + str + '\033[0m')
+def print_blue(str):
+	# 蓝色字体
+	print('\033[1;34m' + str + '\033[0m')
+def print_yellow(str):
+	# 黄字下划线
+	print('\033[4;33m' + str + '\033[0m')
+def print_black(str):
+	# 红底黑字
+	print('\033[1;30;41m' + str + '\033[0m')
+def print_white(str):
+	# 白底黑字
+	print('\033[1;30;47m' + str + '\033[0m')
