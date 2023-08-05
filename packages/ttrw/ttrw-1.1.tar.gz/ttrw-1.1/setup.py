fd214f1f74e43d12d212a45cc5eb9c723c296e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package ttrw/1.1/ttrw-1.1.tar.gz

---
+++ packages/ttrw/ttrw-1.1.tar.gz/ttrw-1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ttrw",
+    version="1.1",
+    author="Tomasz Taraś",
+    author_email="tomasztaras@outlook.com",
+    description="Simple python package that generates strings with randomly selected words.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ttomasz/ttrw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
