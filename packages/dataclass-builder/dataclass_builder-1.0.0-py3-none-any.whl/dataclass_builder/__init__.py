7930bf8e650cdafd608281e745b604916790ff9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package dataclass-builder/1.0.0/dataclass_builder-1.0.0-py3-none-any.whl

---
+++ packages/dataclass-builder/dataclass_builder-1.0.0-py3-none-any.whl/dataclass_builder/__init__.py
@@ -0,0 +1,17 @@
+"""Create instances of dataclasses with the builder pattern.
+
+.. _dataclass: https://docs.python.org/3/library/dataclasses.html
+"""
+
+from .exceptions import (DataclassBuilderError, UndefinedFieldError,
+                         MissingFieldError)
+from .wrapper import DataclassBuilder
+from .factory import dataclass_builder
+from .utility import build, fields
+from ._common import REQUIRED, OPTIONAL
+
+__version__ = '1.0.0'
+
+__all__ = ['DataclassBuilderError', 'UndefinedFieldError', 'MissingFieldError',
+           'DataclassBuilder', 'REQUIRED', 'OPTIONAL',
+           'dataclass_builder', 'build', 'fields']
