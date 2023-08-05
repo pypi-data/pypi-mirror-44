dcf90b10952c171ad56bc34c1276a805acd9b742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:58 +0000

    Add package django-basetestcase/0.0.10/django_basetestcase-0.0.10-py3-none-any.whl

---
+++ packages/django-basetestcase/django_basetestcase-0.0.10-py3-none-any.whl/basetestcase/base_ViewTestCase.py
@@ -0,0 +1,47 @@
+from basetestcase import UtilityTestCase
+from django.urls import resolve, reverse
+
+
+AJAX = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
+
+
+class ViewTestCase(TestCase):
+    
+    def assertTemplatesUsed(self, response, *templates):
+        for t in templates:
+            self.assertTemplateUsed(response, t)
+    
+    
+    def path_to_correct_view_test(self, view, url=None, name=None, args=None):
+        if name is None and if url is None:
+            self.fail('No name or url has been entered')
+        
+        if url is not None:
+            url = resolve(view_url)
+            self.assertEqual(url.func, view)
+        
+        if name is not None:
+            if args is not None:
+                name = resolve(reverse(view_name, args=[args]))
+            else:
+                name = resolve(reverse(view_name))
+            self.assertEqual(name.func, view)
+            
+
+
+    def view_required_field_error_test(self, data, required_fields=[]):
+        _data = dict(data)
+
+        for field, value in _data.items():
+            if field in required_fields:
+                original_value = value
+                _data[field] = ''
+                response = self.post(_data)
+            
+                self.assertContains(
+                    response,
+                    'This field is required.',
+                    msg_prefix=f'{field}'
+                )
+                    
+                _data[field] = original_value
