e034865edcc2132ffc874c9bc10a348031e72b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:05 +0000

    Add package clarusui/0.335/clarusui-0.335.tar.gz

---
+++ packages/clarusui/clarusui-0.335.tar.gz/clarusui-0.335/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+#vers='0.1.16'
+
+setup(name='clarusui',
+      version='0.335',
+      description='Clarus UI Library',
+      url='http://www.clarusft.com',
+      author='Clarus Financial Technology',
+      author_email='support@clarusft.com',
+      #packages=['clarusui'],
+      packages=find_packages(exclude=["test","clarus","clarus.services", "clarusx"]), # <- test is excluded
+      package_data={'clarusui': ['*.html', '*.css']},
+      install_requires=[
+          'Jinja2', 'plotly==2.5.0', 'pandas', 'premailer', 'clarus'
+      ],
+      zip_safe=False)
