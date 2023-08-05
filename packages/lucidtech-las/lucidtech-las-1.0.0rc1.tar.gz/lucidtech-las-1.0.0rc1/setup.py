c5693a8fb264a142fae7c084243075c301932903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:59 +0000

    Add package lucidtech-las/1.0.0rc1/lucidtech-las-1.0.0rc1.tar.gz

---
+++ packages/lucidtech-las/lucidtech-las-1.0.0rc1.tar.gz/lucidtech-las-1.0.0rc1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+with open('requirements.txt') as fp:
+    install_requires = fp.readlines()
+
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+
+setup(
+    name='lucidtech-las',
+    version='1.0.0rc1',
+    description='Python SDK for Lucidtech AI Services',
+    long_description=readme,
+    license='Apache 2.0',
+    platforms='Posix; MacOS X; Windows',
+    author='Lucidtech',
+    maintainer='August Kvernmo',
+    maintainer_email='august@lucidtech.ai',
+    url='https://github.com/LucidtechAI/las-sdk-python',
+    packages=['las'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet'
+    ]
+)
