e417278d9ab326d53fa0ed875801cb6cae646b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package cloudfile/0.0.1/cloudfile-0.0.1.tar.gz

---
+++ packages/cloudfile/cloudfile-0.0.1.tar.gz/cloudfile-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Cloudfile
+Will uploud from and download (large) files to the original position.
---
+++ packages/cloudfile/cloudfile-0.0.1.tar.gz/cloudfile-0.0.1/cloudfile/cloudfile.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+class CloudFile(object):
+    def __init__(self, clouldfile=".cloudfile", unit_test_link_dct=None):
+        self._cloudfile = ".cloudfile"
+        if os.path.isfile(self._cloudfile):
+            self.dct = json.load(open(clouldfile, "r"))
+        else:
+            self.dct = {} if unit_test_link_dct is None else unit_test_link_dct
+            self.save_json()
+
+    def save_json(self):
+        json.dump(self.dct, open(self._cloudfile, "w"))
+
+    def save_file(self, file):
+        assert os.path.isfile(file)
+        self.dct[file] = None
+        self.save_json()
+
+    def items(self) -> [(str, str)]:
+        return self.dct.items()
