372ed8646107b4a1cb48a0fbe1eb15e5e2c60b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:30 +0000

    Add package github-selective-clone-anishbadhri/1.0.1/github_selective_clone-anishbadhri-1.0.1.tar.gz

---
+++ packages/github-selective-clone-anishbadhri/github_selective_clone-anishbadhri-1.0.1.tar.gz/github_selective_clone-anishbadhri-1.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+	name = 	"github_selective_clone-anishbadhri",
+	version = "1.0.1",
+	scripts = ["cloneSelective.py"],
+	description = "Clones a particular folder or a file of a git repository"
+	)
