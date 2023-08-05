e167952afdaf28aae15f9d5feaeab124b8dd639a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package mysql-tracer/1.2.9/mysql_tracer-1.2.9.tar.gz

---
+++ packages/mysql-tracer/mysql_tracer-1.2.9.tar.gz/mysql_tracer-1.2.9/mysql_tracer/__init__.py
@@ -0,0 +1,6 @@
+# noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
+from mysql_tracer._configuration import auto_configure
+from mysql_tracer._query import Query
+# noinspection PyUnresolvedReferences
+from mysql_tracer._query import Result
