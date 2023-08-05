ca395d94cc4e2f5af8f52abb566be7aac6ee68b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package fastapi/0.10.2/fastapi-0.10.2.tar.gz

---
+++ packages/fastapi/fastapi-0.10.2.tar.gz/fastapi-0.10.2/docs/src/bigger_applications/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+from .routers import items, users
+
+app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(items.router, prefix="/items", tags=["items"])
