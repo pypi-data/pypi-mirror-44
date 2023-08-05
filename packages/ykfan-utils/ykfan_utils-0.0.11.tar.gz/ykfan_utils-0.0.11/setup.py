6f0ea92976f7b5694454d00ce9dafeba6e6f086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package ykfan-utils/0.0.11/ykfan_utils-0.0.11.tar.gz

---
+++ packages/ykfan-utils/ykfan_utils-0.0.11.tar.gz/ykfan_utils-0.0.11/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ykfan_utils",
+    version="0.0.11",
+    author="ykfan",
+    author_email="fanyongkang87@qq.com",
+    description="ykfan utils for deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fanyongkang87/ykfan_utils.git",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src', exclude='test'),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
