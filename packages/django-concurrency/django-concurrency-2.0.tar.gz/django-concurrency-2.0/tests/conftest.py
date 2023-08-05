35b3478c30b62ebafb9153a65e585255c76e370a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package django-concurrency/2.0/django-concurrency-2.0.tar.gz

---
+++ packages/django-concurrency/django-concurrency-2.0.tar.gz/django-concurrency-2.0/tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+import platform
+import sys
+
+import django
+
+import pytest
+
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+PYPY = py_impl() == 'PyPy'
+PURE_PYTHON = os.environ.get('PURE_PYTHON', False)
+
+windows = pytest.mark.skipif(sys.platform != 'win32', reason="requires windows")
+
+win32only = pytest.mark.skipif("sys.platform != 'win32'")
+
+skippypy = pytest.mark.skipif(PYPY, reason='skip on pypy')
+
+skipIfDjangoVersion = lambda v: pytest.mark.skipif("django.VERSION[:2]{}".format(v),
+                                                   reason="Skip if django{}".format(v))
+
+
+def pytest_configure():
+    from django.contrib.auth.models import Group
+    from django.conf import settings
+
+    settings.SILENCED_SYSTEM_CHECKS = ['concurrency.W001']
+    settings.CONCURRENCY_VERSION_FIELD_REQUIRED = False
+    if django.VERSION[:2] == (1.6):
+        from concurrency.api import apply_concurrency_check
+        from concurrency.fields import IntegerVersionField
+
+        apply_concurrency_check(Group, 'version', IntegerVersionField)
+
+
+@pytest.fixture(scope='session')
+def client(request):
+    import django_webtest
+
+    wtm = django_webtest.WebTestMixin()
+    wtm.csrf_checks = False
+    wtm._patch_settings()
+    request.addfinalizer(wtm._unpatch_settings)
+    app = django_webtest.DjangoTestApp()
+    return app
