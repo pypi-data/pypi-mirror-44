ecbe044496d4e0a0e4b867e8cf5164ad342c38da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package pandarallel/0.1.5/pandarallel-0.1.5.tar.gz

---
+++ packages/pandarallel/pandarallel-0.1.5.tar.gz/pandarallel-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'pandas',
+    'pyarrow >= 0.12.1',
+    'tqdm >= 4.31.1',
+]
+
+setup(
+    name='pandarallel',
+    version='0.1.5',
+    packages=find_packages(),
+    author='Manu NALEPA',
+    author_email='nalepae@gmail.com',
+    description='An easy to use library to speed up computation (by parallelizing on multi CPUs) with pandas.',
+    long_description=open('README.md').read(),
+    url='https://github.com/nalepae/pandarallel',
+    install_requires=install_requires,
+    license='BSD',
+)
