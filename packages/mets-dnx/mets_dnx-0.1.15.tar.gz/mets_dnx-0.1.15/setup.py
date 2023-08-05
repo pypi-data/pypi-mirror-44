b06f26541c981d0d196d0b4ea38232d5897d8d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package mets-dnx/0.1.15/mets_dnx-0.1.15.tar.gz

---
+++ packages/mets-dnx/mets_dnx-0.1.15.tar.gz/mets_dnx-0.1.15/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+VERSION='0.1.15'
+
+config = {
+	'name':'mets_dnx',
+	'version':VERSION,
+	'author':'Sean Mosely',
+	'author_email':'sean.mosely@gmail.com',
+	'packages':['mets_dnx',],
+	'description':'Python library for building Rosetta DNX/METS XML documents',
+	'install_requires':['lxml>=3.6.4', 'pymets','pydc', 'pydnx'],
+	'download_url': 'https://github.com/NLNZDigitalPreservation/mets_dnx/archive/v'+VERSION+'.tar.gz',
+	'license': 'MIT',
+	}
+
+setup(**config)
