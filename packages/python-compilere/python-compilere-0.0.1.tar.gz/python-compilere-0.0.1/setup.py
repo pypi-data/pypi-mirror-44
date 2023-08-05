f60ecd2ce23ae1b4febdf306058746ccb58cc87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:22 +0000

    Add package python-compilere/0.0.1/python-compilere-0.0.1.tar.gz

---
+++ packages/python-compilere/python-compilere-0.0.1.tar.gz/python-compilere-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-compilere',
+    version='0.0.1',
+    packages=["compilere"],
+    license='""',
+    author='umar moiz qurashi',
+    author_email='umar.maq@yandex.com',
+    install_requires=[''],
+    description='the compiler make in python'
+)
