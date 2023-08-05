a115929c31fccebc74d66e4724cd73687c02f521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:46 +0000

    Add package pspring/0.0.7/pspring-0.0.7.tar.gz

---
+++ packages/pspring/pspring-0.0.7.tar.gz/pspring-0.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pspring',
+    version='0.0.7',
+    license='TBD',
+    author='Vasudevan Palani',
+    author_email='vasudevan.palani@gmail.com',
+    url='https://github.com/vasudevan-palani/pspring',
+    long_description=long_description,
+    packages=['pspring','pspring.core'],
+    include_package_data=True,
+    description="A framework inspired by spring for python",
+)
