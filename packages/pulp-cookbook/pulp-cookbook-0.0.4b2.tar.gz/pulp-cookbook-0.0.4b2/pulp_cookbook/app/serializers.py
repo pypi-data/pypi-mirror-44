b62419980a2ce12b0e7bb21bc5906f69e1412e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package pulp-cookbook/0.0.4b2/pulp-cookbook-0.0.4b2.tar.gz

---
+++ packages/pulp-cookbook/pulp-cookbook-0.0.4b2.tar.gz/pulp-cookbook-0.0.4b2/pulp_cookbook/app/serializers.py
@@ -0,0 +1,87 @@
+# (C) Copyright 2018 Simon Baatz <gmbnomis@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from gettext import gettext as _
+
+from rest_framework import serializers
+
+from pulpcore.plugin.serializers import (
+    SingleArtifactContentSerializer,
+    RemoteSerializer,
+    PublisherSerializer,
+)
+
+from .models import CookbookPackageContent, CookbookRemote, CookbookPublisher
+
+
+class CookbookPackageContentSerializer(SingleArtifactContentSerializer):
+    """Serializer for the cookbook content."""
+
+    name = serializers.CharField(help_text=_("name of the cookbook"))
+    version = serializers.CharField(help_text=_("version of the cookbook"), required=False)
+    dependencies = serializers.JSONField(
+        help_text=_("dependencies of the cookbook"), read_only=True
+    )
+    content_id_type = serializers.HiddenField(default=CookbookPackageContent.SHA256)
+    content_id = serializers.CharField(
+        help_text=_(
+            "content_id of the cookbook (UUID (lazy download)/SHA256 (immediate download/import)"
+        ),
+        read_only=True,
+    )
+
+    def validate(self, data):
+        """Validate the CookbookPackageContent data."""
+        data = super().validate(data)
+        data["_relative_path"] = CookbookPackageContent.relative_path_from_data(data)
+        return data
+
+    def update(self, instance, validated_data):
+        raise serializers.ValidationError("content is immutable")
+
+    class Meta:
+        fields = tuple(set(SingleArtifactContentSerializer.Meta.fields) - {"_relative_path"}) + (
+            "name",
+            "version",
+            "dependencies",
+            "content_id_type",
+            "content_id",
+        )
+        model = CookbookPackageContent
+
+
+class CookbookRemoteSerializer(RemoteSerializer):
+    """Serializer for the remote pointing to a universe repo."""
+
+    cookbooks = serializers.JSONField(
+        help_text=_(
+            'An optional JSON object in the format {"<cookbook name>":'
+            ' "<version_string>" }. Used to limit the cookbooks to synchronize'
+            " from the remote"
+        ),
+        required=False,
+    )
+
+    class Meta:
+        fields = RemoteSerializer.Meta.fields + ("cookbooks",)
+        model = CookbookRemote
+
+    # TODO: No support for version specifiers yet, only cookbook names
+    def validate_cookbooks(self, value):
+        if value == "":  # blank value
+            return value
+        if isinstance(value, dict):
+            if all(value.keys()):
+                return value
+        raise serializers.ValidationError(
+            _('Format must be {"<cookbook_name>" : "version_string" }')
+        )
+
+
+class CookbookPublisherSerializer(PublisherSerializer):
+    """Serializer for the publisher."""
+
+    class Meta:
+        fields = PublisherSerializer.Meta.fields
+        model = CookbookPublisher
---
+++ packages/pulp-cookbook/pulp-cookbook-0.0.4b2.tar.gz/pulp-cookbook-0.0.4b2/pulp_cookbook/app/tasks/publishing.py
@@ -0,0 +1,125 @@
+# (C) Copyright 2018 Simon Baatz <gmbnomis@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import logging
+import os
+
+from gettext import gettext as _
+
+from django.core.files import File
+from django.db.models import Count
+
+from pulpcore.plugin.models import (
+    RepositoryVersion,
+    Publication,
+    PublishedArtifact,
+    PublishedMetadata,
+)
+from pulpcore.plugin.tasking import WorkingDirectory
+
+from pulp_cookbook.app.models import CookbookPackageContent, CookbookPublisher
+from pulp_cookbook.metadata import Entry, Universe
+
+log = logging.getLogger(__name__)
+
+BASE_PATH_MARKER = "{{ base_path }}"
+
+
+def path_template(artifact_path):
+    """Artifact path to write into published '__universe__' file."""
+    return BASE_PATH_MARKER + "/" + artifact_path
+
+
+def replace_all_paths(content, base_url):
+    """Replace BASE_PATH_MARKERs with base_url in the give '__universe__' content."""
+    return content.replace(BASE_PATH_MARKER, base_url)
+
+
+def publish(publisher_pk, repository_version_pk):
+    """
+    Use provided publisher to create a Publication based on a RepositoryVersion.
+
+    Args:
+        publisher_pk (str): Use the publish settings provided by this publisher.
+        repository_version_pk (str): Create a publication from this repository version.
+    """
+    publisher = CookbookPublisher.objects.get(pk=publisher_pk)
+    repository_version = RepositoryVersion.objects.get(pk=repository_version_pk)
+
+    log.info(
+        _("Publishing: repository=%(repository)s, version=%(version)d, publisher=%(publisher)s"),
+        {
+            "repository": repository_version.repository.name,
+            "version": repository_version.number,
+            "publisher": publisher.name,
+        },
+    )
+
+    with WorkingDirectory():
+        with Publication.create(repository_version, publisher) as publication:
+            check_repo_version_constraint(publication)
+            universe = Universe("__universe__")
+            universe.write(populate(publication))
+            metadata = PublishedMetadata(
+                relative_path=os.path.basename(universe.relative_path),
+                publication=publication,
+                file=File(open(universe.relative_path, "rb")),
+            )
+            metadata.save()
+
+    log.info(_("Publication: %(publication)s created"), {"publication": publication.pk})
+
+
+def check_repo_version_constraint(publication):
+    """
+    Ensure that repo version to publish fulfills repo_key_fields() uniqueness.
+
+    Raises:
+        ValueError: When constraint is violated
+
+    """
+    fields = CookbookPackageContent.repo_key_fields()
+    qs_content = CookbookPackageContent.objects.filter(
+        pk__in=publication.repository_version.content
+    )
+    qs = qs_content.values(*fields).annotate(num_cookbooks=Count("pk")).filter(num_cookbooks__gt=1)
+    duplicates = [f"{res['name']} {res['version']}" for res in qs]
+    if duplicates:
+        raise ValueError(
+            f"Publication would contain multiple versions of cookbooks: {', '.join(duplicates)}"
+        )
+
+
+def populate(publication):
+    """
+    Populate a publication.
+
+    Create published artifacts and yield a Universe Entry for each.
+
+    Args:
+        publication (pulpcore.plugin.models.Publication): A Publication to populate.
+
+    Yields:
+        Entry: Universe entry for each cookbook
+
+    """
+    for content in CookbookPackageContent.objects.filter(
+        pk__in=publication.repository_version.content
+    ).order_by("-_created"):
+        relative_path = "cookbook_files/{}/{}/".format(
+            content.name, content.version.replace(".", "_")
+        )
+        for content_artifact in content.contentartifact_set.all():
+            art_path = os.path.join(relative_path, content_artifact.relative_path)
+            published_artifact = PublishedArtifact(
+                relative_path=art_path, publication=publication, content_artifact=content_artifact
+            )
+            published_artifact.save()
+            entry = Entry(
+                name=content.name,
+                version=content.version,
+                download_url=path_template(art_path),
+                dependencies=content.dependencies,
+            )
+            yield entry
