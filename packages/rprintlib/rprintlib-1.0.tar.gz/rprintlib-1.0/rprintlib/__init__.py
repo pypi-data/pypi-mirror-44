feb66d4268c825734560cb2b490c33a9ed642e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package rprintlib/1.0/rprintlib-1.0.tar.gz

---
+++ packages/rprintlib/rprintlib-1.0.tar.gz/rprintlib-1.0/rprintlib/__init__.py
@@ -0,0 +1,8 @@
+from .rpmain import Rtdout, Rprint
+import sys
+rprint = Rprint() # predefined self-storage Rprint
+sprint = Rprint() # predefined stdout Rprint
+sprint.rtdout.stdout = sys.stdout
+
+__all__ = ['Rprint', 'Rtdout', 'rprint', 'sprint']
+__version__ = "1.0"
