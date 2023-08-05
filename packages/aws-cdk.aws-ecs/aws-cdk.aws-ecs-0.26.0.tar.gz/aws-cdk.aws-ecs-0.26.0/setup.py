68c9bd6bf70eeb6062c7509a98bc07917d62ff73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:52 +0000

    Add package aws-cdk.aws-ecs/0.26.0/aws-cdk.aws-ecs-0.26.0.tar.gz

---
+++ packages/aws-cdk.aws-ecs/aws-cdk.aws-ecs-0.26.0.tar.gz/aws-cdk.aws-ecs-0.26.0/setup.py
@@ -0,0 +1,57 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-ecs",
+    "version": "0.26.0",
+    "description": "The CDK Construct Library for AWS::ECS",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_ecs",
+        "aws_cdk.aws_ecs._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_ecs._jsii": [
+            "aws-ecs@0.26.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_ecs": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii",
+        "publication>=0.0.3",
+        "aws-cdk.assets-docker~=0.26.0",
+        "aws-cdk.aws-applicationautoscaling~=0.26.0",
+        "aws-cdk.aws-autoscaling~=0.26.0",
+        "aws-cdk.aws-certificatemanager~=0.26.0",
+        "aws-cdk.aws-cloudwatch~=0.26.0",
+        "aws-cdk.aws-ec2~=0.26.0",
+        "aws-cdk.aws-ecr~=0.26.0",
+        "aws-cdk.aws-elasticloadbalancing~=0.26.0",
+        "aws-cdk.aws-elasticloadbalancingv2~=0.26.0",
+        "aws-cdk.aws-events~=0.26.0",
+        "aws-cdk.aws-iam~=0.26.0",
+        "aws-cdk.aws-logs~=0.26.0",
+        "aws-cdk.aws-route53~=0.26.0",
+        "aws-cdk.aws-secretsmanager~=0.26.0",
+        "aws-cdk.cdk~=0.26.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
