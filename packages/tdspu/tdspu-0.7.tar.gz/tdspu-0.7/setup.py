cbb797323806fb7433c29b615718ebdfac665f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package tdspu/0.7/tdspu-0.7.tar.gz

---
+++ packages/tdspu/tdspu-0.7.tar.gz/tdspu-0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tdspu", # THREDDS Data Server Publication Utils
+    version="0.7",
+    packages=find_packages(),
+    package_data={
+        'src': ['data/*.j2'],
+    },
+    python_requires='>=3',
+    install_requires=['netCDF4', 'Jinja2'],
+
+    entry_points = {
+        "console_scripts": [
+            "ncml = tdspu.ncml:main",
+            "catalog = tdspu.catalog:main",
+        ],
+    },
+
+    author="zequihg50",
+    author_email="ezequiel.cimadevilla@unican.es",
+    description="Utils for NcML and TDS catalog generation",
+)
