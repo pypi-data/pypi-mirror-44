1d1ce26bd131ddb12725f086b366366656b0449e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package vid2frame/0.0.1/vid2frame-0.0.1.tar.gz

---
+++ packages/vid2frame/vid2frame-0.0.1.tar.gz/vid2frame-0.0.1/README.md
@@ -0,0 +1,15 @@
+# vid2img
+Convert video into images
+
+## Python version
+Python 3.6.4
+
+## Dependencies
+  1. opencv-python
+  2. numpy
+
+## How to install
+vid2img has been published on Python Package Index (PyPi). vid2img can be installed using the following command.
+```
+pip install vid2img
+```
---
+++ packages/vid2frame/vid2frame-0.0.1.tar.gz/vid2frame-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='vid2frame',
+	packages=find_packages(),
+	description='Video-to-Frames converter',
+	version='0.0.1',
+	url='https://github.com/dennis199441/vid2frame',
+	author='Dennis Cheung',
+	author_email='dennis199441@gmail.com',
+	install_requires=[
+		'opencv-python',
+		'numpy'
+    ],
+	keywords=['pip','dennis','vid2frame']
+)
