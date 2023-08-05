5a5aeb208bec6fd10cbaff2f02f1fde69b65d202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:43 +0000

    Add package svineruscheckpay/0.0.5/svineruscheckpay-0.0.5.tar.gz

---
+++ packages/svineruscheckpay/svineruscheckpay-0.0.5.tar.gz/svineruscheckpay-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="svineruscheckpay",
+    version="0.0.5",
+    author="svinerus",
+    author_email="svinerus@gmail.com",
+    description="A small checkpay module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
