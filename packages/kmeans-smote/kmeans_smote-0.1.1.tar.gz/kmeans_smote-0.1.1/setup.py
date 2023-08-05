1158c07e12c90845489886fafe917b7dcf66baf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package kmeans-smote/0.1.1/kmeans_smote-0.1.1.tar.gz

---
+++ packages/kmeans-smote/kmeans_smote-0.1.1.tar.gz/kmeans_smote-0.1.1/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+
+tests_require = [
+    'pytest>=4.2.1,<4.3',
+    'pytest-cov>=2.6.1,<2.7',
+    'codecov>=2.0.15,<2.1'
+],
+
+setup(
+    name='kmeans_smote',
+    version="0.1.1",
+    py_modules=['kmeans_smote'],
+    install_requires=[
+        'imbalanced-learn>=0.4.0,<0.5',
+        'scikit-learn>=0.19.0,<0.21',
+        'numpy>=1.13,<1.16'
+    ],
+    tests_require=tests_require,
+    extras_require={
+        'test':tests_require
+    },
+    author="Felix Last",
+    author_email="mail@felixlast.de",
+    url="https://github.com/felix-last/kmeans_smote",
+    description=("Oversampling for imbalanced learning based on k-means and SMOTE"),
+    long_description=read('README.rst'),
+    license="MIT",
+    keywords=[
+        'Class-imbalanced Learning',
+        'Oversampling',
+        'Classification',
+        'Clustering',
+        'Supervised Learning'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ]
+)
