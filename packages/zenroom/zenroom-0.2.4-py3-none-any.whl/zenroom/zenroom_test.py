bd3ce312279f4454de5fa270db8f45f1137b94d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package zenroom/0.2.4/zenroom-0.2.4-py3-none-any.whl

---
+++ packages/zenroom/zenroom-0.2.4-py3-none-any.whl/zenroom/zenroom_test.py
@@ -0,0 +1,55 @@
+import json
+
+import pytest
+
+from zenroom import zenroom
+from zenroom.zenroom import Error
+
+
+def test_basic():
+    script = "print('Hello world')"
+    output, errors = zenroom.execute(script)
+
+    assert output.decode() == "Hello world"
+
+
+def test_keygen():
+    script = """
+    -- generate a simple keyring
+    keyring = ECDH.new()
+    keyring:keygen()
+    
+    -- export the keypair to json
+    export = JSON.encode(
+       {
+          public  = keyring: public():base64(),
+          private = keyring:private():base64()
+       }
+    )
+    print(export)
+    """
+    output, _ = zenroom.execute(script, verbosity=3)
+    result = json.loads(output.decode())
+    assert 'public' in result
+    assert 'private' in result
+
+
+def test_zencode():
+    contract = """Scenario 'coconut': "To run over the mobile wallet the first time and store the output as keypair.keys"
+Given that I am known as 'identifier'
+When I create my new keypair
+Then print all data
+    """
+
+    result, _ = zenroom.zencode(contract)
+    assert result
+    assert b'public' in result
+    assert b'private' in result
+
+
+def test_broken_script():
+    with pytest.raises(Error) as e:
+        script = "print('"
+        result, errors = zenroom.execute(script)
+
+    assert e
