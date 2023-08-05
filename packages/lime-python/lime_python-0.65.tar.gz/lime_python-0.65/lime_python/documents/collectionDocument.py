646a645b940ba80bc8ab12cce8b91c762c599dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package lime-python/0.65/lime_python-0.65.tar.gz

---
+++ packages/lime-python/lime_python-0.65.tar.gz/lime_python-0.65/lime_python/documents/collectionDocument.py
@@ -0,0 +1,56 @@
+from lime_python.base.document import Document
+from lime_python.base.mediaType import MediaType
+
+
+class _CollectionDocument(Document):
+
+    MIME_TYPE = "application/vnd.lime.collection+json"
+
+    def __init__(self, itemType, items=[]):
+        super().__init__(MediaType.Parse(_CollectionDocument.MIME_TYPE))
+
+        self.ItemType = itemType
+        self.Items = items
+
+    @property
+    def ItemType(self):
+        return self.__ItemType
+
+    @ItemType.setter
+    def ItemType(self, itemType):
+        if itemType is not None and not isinstance(itemType, MediaType):
+            raise ValueError('"ItemType" must be a MediaType')
+        self.__ItemType = itemType
+
+    @property
+    def Items(self):
+        return self.__Items
+
+    @Items.setter
+    def Items(self, items):
+        if not isinstance(items, list):
+            raise ValueError(
+                '"Items" must be a list of Document')
+        for i in items:
+            if not isinstance(i, Document):
+                raise ValueError('All Items must be a Document')
+
+        self.__Items = items
+
+    @property
+    def Total(self):
+        return len(self.Items)
+
+    def GetDocumentsJson(self):
+        return [x.ToJson() for x in self.Items]
+
+    def ToJson(self):
+        return {
+            'itemType': str(self.ItemType),
+            'items': self.GetDocumentsJson()
+        }
+
+
+class CollectionDocument(_CollectionDocument):
+
+    Type = MediaType.Parse(_CollectionDocument.MIME_TYPE)
