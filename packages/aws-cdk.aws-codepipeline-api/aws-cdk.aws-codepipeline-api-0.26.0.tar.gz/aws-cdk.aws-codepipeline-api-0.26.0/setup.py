d04128cbf70e3ca39f8baf74f1fff44eaa1fc7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:51 +0000

    Add package aws-cdk.aws-codepipeline-api/0.26.0/aws-cdk.aws-codepipeline-api-0.26.0.tar.gz

---
+++ packages/aws-cdk.aws-codepipeline-api/aws-cdk.aws-codepipeline-api-0.26.0.tar.gz/aws-cdk.aws-codepipeline-api-0.26.0/setup.py
@@ -0,0 +1,45 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-codepipeline-api",
+    "version": "0.26.0",
+    "description": "Actions API for AWS Code Pipeline",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_codepipeline_api",
+        "aws_cdk.aws_codepipeline_api._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_codepipeline_api._jsii": [
+            "aws-codepipeline-api@0.26.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_codepipeline_api": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii",
+        "publication>=0.0.3",
+        "aws-cdk.aws-events~=0.26.0",
+        "aws-cdk.aws-iam~=0.26.0",
+        "aws-cdk.cdk~=0.26.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
