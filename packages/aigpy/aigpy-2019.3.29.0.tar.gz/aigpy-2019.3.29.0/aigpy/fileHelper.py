7d342db4220bc88391cb9d5459dbdeb88ae5099a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package aigpy/2019.3.29.0/aigpy-2019.3.29.0.tar.gz

---
+++ packages/aigpy/aigpy-2019.3.29.0.tar.gz/aigpy-2019.3.29.0/aigpy/fileHelper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   fileHelper.py
+@Time    :   2019/03/11
+@Author  :   Yaron Huang 
+@Version :   1.0
+@Contact :   yaronhuang@qq.com
+@Desc    :   
+'''
+
+import os
+
+def getFileSize(path):
+    try:
+        if os.path.isfile(path) == False:
+            return 0
+        return os.path.getsize(path)
+    except:
+        return 0
+
+def getFileContent(path, isBin = False):
+    mode = 'r'
+    if isBin:
+        mode = 'rb'
+    try:
+        size = getFileSize(path)
+        if size <= 0:
+            return ""
+        with open(path, mode) as fd:
+            content = fd.read(size) 
+        return content
+    except:
+        return ""
