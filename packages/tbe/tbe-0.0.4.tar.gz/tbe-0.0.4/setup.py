e2382501cf39a570d6239da53053f161c2a0630f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package tbe/0.0.4/tbe-0.0.4.tar.gz

---
+++ packages/tbe/tbe-0.0.4.tar.gz/tbe-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='tbe',
+      version='0.0.4',
+      description='',
+      packages = find_packages(),
+      url = "https://github.com/WangZhengzhe/Python---TBE",
+      author='ZhengzheWang',
+      author_email='archer.wang@daznet.cn',
+      license='MIT',
+      zip_safe=False)
