1cd3ddee2a5f674ec8652fa61c54609db469da7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package maubot/0.1.0.dev16/maubot-0.1.0.dev16.tar.gz

---
+++ packages/maubot/maubot-0.1.0.dev16.tar.gz/maubot-0.1.0.dev16/maubot/__init__.py
@@ -0,0 +1,2 @@
+from .plugin_base import Plugin
+from .matrix import MaubotMatrixClient as Client, MaubotMessageEvent as MessageEvent
---
+++ packages/maubot/maubot-0.1.0.dev16.tar.gz/maubot-0.1.0.dev16/maubot/management/frontend/build/precache-manifest.b71f09d2a06748443e9d156466d72a91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "./static/media/chevron-right.b6b6c4db.svg"
+  },
+  {
+    "revision": "aea3f2280044b009a147",
+    "url": "./static/css/main.707bdb7a.chunk.css"
+  },
+  {
+    "revision": "34ffe17d25ddc324a211",
+    "url": "./static/js/1.34ffe17d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1851431378ea0e1c6a63c7261ee65d9e",
+    "url": "./static/media/plus.18514313.svg"
+  },
+  {
+    "revision": "aea3f2280044b009a147",
+    "url": "./static/js/main.aea3f228.chunk.js"
+  },
+  {
+    "revision": "7e77da69cc71663ae51ce4b08b52e0d3",
+    "url": "./static/media/chevron-left.7e77da69.svg"
+  },
+  {
+    "revision": "ebe1c3a69ddb3b046edbb1f11f689d78",
+    "url": "./static/media/sort-down.ebe1c3a6.svg"
+  },
+  {
+    "revision": "4bff71677abd84948fb3335d9ecf4388",
+    "url": "./static/media/sort-up.4bff7167.svg"
+  },
+  {
+    "revision": "a57b766207fe6c113be29df03f171a6d",
+    "url": "./static/media/upload.a57b7662.svg"
+  },
+  {
+    "revision": "9f55d7d50537f57ba1d7b4b5d9af2898",
+    "url": "./index.html"
+  }
+];
