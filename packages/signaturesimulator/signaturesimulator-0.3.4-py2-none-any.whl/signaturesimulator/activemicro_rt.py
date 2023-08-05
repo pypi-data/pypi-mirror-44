778794457900ea39b20f9db2846022b376b0b85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package signaturesimulator/0.3.4/signaturesimulator-0.3.4-py2-none-any.whl

---
+++ packages/signaturesimulator/signaturesimulator-0.3.4-py2-none-any.whl/signaturesimulator/activemicro_rt.py
@@ -0,0 +1,117 @@
+# third party imports
+import numpy as np
+# signature simulator specific imports
+import state_vector as sv
+import backscatter as bs
+# import sense code
+from sense import model as sense_mod
+from sense import soil as sense_soil
+from sense import canopy as sense_canopy
+
+
+def active_microwave_rt(state, geom, freq=5.405, can_mod='turbid_rayleigh', surf_mod='Oh92', s=0.015, lai_coeff_kv=0.1,
+                        lai_coeff_kh=0.1, omega=0.1, clay=0.23, sand=0.27, bulk=1.65):
+    """
+    Function that simulates SAR backscattering, given surface biogeophysical variables and viewing geometries
+    :param state: instance of StateVector class
+    :type state: object
+    :param geom: instance of SensorGeometry class
+    :type geom: object
+    :param freq: frequency (GHz)
+    :type freq: float
+    :param s: surface rms height (m)
+    :type s: float
+    :param lai_coeff_kv: coefficient of lai for which to calculate extinction and volume scattering coefficients in V
+    polarisation
+    :type lai_coeff_kv: float
+    :param lai_coeff_kh: coefficient of lai for which to calculate extinction and volume scattering coefficients in H
+    polarisation
+    :type lai_coeff_kh: float
+    :param omega: coefficient for calculation of volume scattering coefficients
+    :type omega: float
+    :param clay: soil texture clay fraction
+    :type clay: float
+    :param sand: soil texture sand fraction
+    :type sand: float
+    :param bulk: soil bulk density (g cm-3)
+    :type bulk: float
+    :return: instance of BackScatter class
+    """
+    backscat = bs.BackScatter()
+    backscat.date_sat_ob = []
+    backscat.date_land_ob = []
+    backscat.soil_moisture = []
+    backscat.lai = []
+    backscat.can_height = []
+    backscat.hh = []
+    backscat.hv = []
+    backscat.vv = []
+    for date_utc in enumerate(geom.date_utc):
+        idx, timedelt = sv.find_nearest_date_idx(state.date_utc, date_utc[1])
+        SAR = run_sense(state.soil_moisture[idx], state.lai[idx], state.can_height[idx], geom.vza[date_utc[0]],
+                        freq=freq, stype=can_mod, surf=surf_mod, s=s, lai_coeff_kv=lai_coeff_kv,
+                        lai_coeff_kh=lai_coeff_kh, omega=omega, clay=clay, sand=sand, bulk=bulk)
+        backscat.date_sat_ob.append(date_utc[1])
+        backscat.date_land_ob.append(state.date_utc[idx])
+        backscat.soil_moisture.append(state.soil_moisture[idx])
+        backscat.lai.append(state.lai[idx])
+        backscat.can_height.append(state.can_height[idx])
+        backscat.hh.append(SAR.__dict__['stot']['hh'])  # backscatter in linear units (m2/m2) to convery to dB use 10*np.log10(backscat_arr)
+        backscat.hv.append(SAR.__dict__['stot']['hv'])
+        backscat.vv.append(SAR.__dict__['stot']['vv'])
+    return backscat
+
+
+
+def run_sense(soil_m, lai, can_height, vza, freq=5.405, stype='turbid_rayleigh', surf='Oh92', s=0.015, lai_coeff_kv=0.1,
+              lai_coeff_kh=0.1, omega=0.1, clay=0.23, sand=0.27, bulk=1.65):
+    """
+    Function that runs the SenSE SAR ScattEring model given some inputs
+    :param soil_m: soil moisture (m3 m-3)
+    :type soil_m: float
+    :param lai: leaf area index (m2 m-2)
+    :type lai: float
+    :param can_height: canopy height (m)
+    :type can_height: float
+    :param vza: view zenith angle of sensor [incidence angle] (degrees)
+    :type vza: float
+    :param freq: frequency (GHz)
+    :type freq: float
+    :param stype: canopy scattering model
+    :type stype: str
+    :param surf: surface scattering model
+    :type surf: str
+    :param s: surface rms height (m)
+    :type s: float
+    :param lai_coeff_kv: coefficient of lai for which to calculate extinction and volume scattering coefficients in V
+    polarisation
+    :type lai_coeff_kv: float
+    :param lai_coeff_kh: coefficient of lai for which to calculate extinction and volume scattering coefficients in H
+    polarisation
+    :type lai_coeff_kh: float
+    :param omega: coefficient for calculation of volume scattering coefficients
+    :type omega: float
+    :param clay: soil texture clay fraction
+    :type clay: float
+    :param sand: soil texture sand fraction
+    :type sand: float
+    :param bulk: soil bulk density (g cm-3)
+    :type bulk: float
+    :return: instance of sense SingleScatRT class
+    """
+    """Function that runs sense model, given surface biogeophysical variables and viewing geometries
+    """
+    models = {'surface': surf, 'canopy': stype}
+    SAR = sense_mod.SingleScatRT(
+          surface=sense_soil.Soil(mv=soil_m, f=freq, s=s, clay=clay, sand=sand, bulk=bulk),
+          # surface=sense_soil.Soil(eps=self.eps, f=self.freq, s=self.s),
+          canopy=sense_canopy.OneLayer(ke_h=lai_coeff_kh * lai,  # extinction coefficient
+                                       ke_v=lai_coeff_kv * lai,
+                                       d=can_height,
+                                       ks_h=omega * (lai_coeff_kh * lai),  # volume scattering coeff
+                                       ks_v=omega * (lai_coeff_kv * lai)),
+          models=models,
+          theta=np.deg2rad(vza),
+          freq=freq)
+    SAR.sigma0()
+    return SAR
---
+++ packages/signaturesimulator/signaturesimulator-0.3.4-py2-none-any.whl/signaturesimulator/jules.py
@@ -0,0 +1,140 @@
+# !/home/db903833//dataLand01/enthoughtDistros/epd-7.2-2-rh5-x86_64/bin/python
+# !/usr/bin/env python
+
+# core python modules:
+import subprocess
+import os
+# 3rd party modules:
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import netCDF4 as nc
+import f90nml
+import glob
+
+
+def crop_run_jules(year=2016, sow_date=110, run_id='crop_jules', output_dir='default',
+                   jules_exe='/home/if910917/jules/models/jules4.8/build/bin/jules.exe', print_jules=0):
+    """
+    Function running the JULES land surface model and updating the value of sow date for crops.
+    :param year: year to run JULES for, here only 2015 and 2016 available (str)
+    :param sow_date: crop sow date (int)
+    :param run_id: id for model run (str)
+    :param output_dir: directory to store output (str)
+    :return: 'run completed'
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    example_nml = dir_path + '/' + 'data/jules/example_nml'  # example nml directory bundled with simulator
+    if output_dir == 'default':
+        output_dir = dir_path + '/data/jules/output'  # if default use example directory to store model output
+    jules = Jules(nml_dir=example_nml, print_out=print_jules, jules_exe=jules_exe)
+    jules.nml_dic['timesteps']['jules_time']['main_run_start'] = str(year) + '-01-01 00:00:00'
+    jules.nml_dic['timesteps']['jules_time']['main_run_end'] = str(year) + '-12-31 21:00:00'
+    jules.nml_dic['ancillaries']['jules_crop_props']['const_val'][2] = sow_date  # set crop sow date
+    jules.nml_dic['output']['jules_output']['run_id'] = run_id  # set run id
+    jules.nml_dic['output']['jules_output']['output_dir'] = output_dir  # set directory to store model output
+    jules.run_jules()
+    return output_dir+'/'+run_id
+
+
+class Jules():
+    """Class to read in nml files and run JULES.
+
+    :param nml_dir: location of JULES nml file directory.
+    :type nml_dir: str
+    :param jules_exe: location of JULES executable.
+    :type jules_exe: str
+
+    .. note:: You must have JULES installed on local system with a version of 4.8 or higher.
+
+    """
+    def __init__(self, nml_dir, jules_exe='/home/if910917/jules/models/jules4.8/build/bin/jules.exe', print_out=0):
+        self.nml_dir = nml_dir
+        self.jules = jules_exe
+        self.nml_dic = self.read_nml(nml_dir)
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.example_nml = self.dir_path + '/' + 'data/jules/example_nml'
+        if print_out == 0:
+            self.run_jules = self.run_jules_pipe
+        elif print_out == 1:
+            self.run_jules = self.run_jules_print
+
+    def read_nml(self, nml_dir):
+        """
+        Reading all nml files in specified directory and stores them in a dictionary.
+        :param nml_dir: directory to read nml files from
+        :return: dictionary of nml files
+        """
+        nml_dic = {}
+        for f_nml in glob.glob(nml_dir + '/*.nml'):
+            nml_dic[f_nml.split('/')[-1][:-4]] = f90nml.read(f_nml)
+        return nml_dic
+
+    def write_nml(self):
+        """
+        Function to write dictionary of stored nml data to nml files in current working dir.
+        :return: n/a
+        """
+        for key in self.nml_dic.keys():
+            self.nml_dic[key].write(key + '.nml', force=True)
+
+    def run_jules_pipe(self):
+        """Write all NML files to disk. Run JULES in a subprocess. Check output for fatal errors.
+
+        :return: stdout and stderr output from JULES model run.
+        :rtype: tuple
+        """
+
+        # write all the nml files here so the
+        # user doesn't have to remember to...
+
+        # run JULES
+        cwd = os.getcwd()
+        os.chdir(self.nml_dir)
+        self.write_nml()
+        cmd = []
+        cmd.append(self.jules)
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out = p.stdout.readlines()
+        err = p.stderr.readlines()
+        p.wait()
+        os.chdir(cwd)
+
+        # catch "fatal" errors
+        for line in out:
+            if len(line.split()) == 0: continue
+            if line.split()[0] == "[FATAL":
+                print >> sys.stderr, "*** runJules: caught fatal error in JULES run:"
+                print >> sys.stderr, line,
+                sys.exit()
+
+        # catch anything in stderr
+        if len(err) > 0:
+            for line in err:
+                print >> sys.stderr, "*** runJules: caught output on stderr in JULES run:"
+                print >> sys.stderr, line,
+                # sys.exit()
+        return (out, err)
+
+    def run_jules_print(self):
+        """Write all NML files to disk. Run JULES in a subprocess. Check output for fatal errors.
+
+        :return: stdout and stderr output from JULES model run.
+        :rtype: str
+        """
+
+        # write all the nml files here so the
+        # user doesn't have to remember to...
+        cwd = os.getcwd()
+        os.chdir(self.nml_dir)
+        self.write_nml()
+
+        # run JULES
+        cmd = []
+        cmd.append(self.jules)
+        proc = subprocess.Popen(cmd, shell=False)
+        proc.communicate()
+        os.chdir(cwd)
+        return 'Done', 'Done'
