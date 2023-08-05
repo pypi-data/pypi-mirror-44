99b68bfa0351da3ffb34f827168535cf6d5565d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package bellows-homeassistant/0.7.2/bellows-homeassistant-0.7.2.tar.gz

---
+++ packages/bellows-homeassistant/bellows-homeassistant-0.7.2.tar.gz/bellows-homeassistant-0.7.2/setup.py
@@ -0,0 +1,30 @@
+"""Setup module for bellows"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="bellows-homeassistant",
+    version="0.7.2",
+    description="Library implementing EZSP",
+    url="http://github.com/zigpy/bellows",
+    author="Russell Cloran",
+    author_email="rcloran@gmail.com",
+    license="GPL-3.0",
+    packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'console_scripts': ['bellows=bellows.cli.main:main'],
+    },
+    install_requires=[
+        'Click',
+        'click-log==0.2.0',
+        'pure_pcapy3==1.0.1',
+        'pyserial-asyncio',
+        'zigpy-homeassistant',
+    ],
+    dependency_links=[
+        'https://github.com/rcloran/pure-pcapy-3/archive/e289c7d7566306dc02d8f4eb30c0358b41f40f26.zip#egg=pure_pcapy3-1.0.1',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
