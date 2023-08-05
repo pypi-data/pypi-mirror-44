385ff29249a0eda6c66350739db6090e6c7d799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:09 +0000

    Add package megaLib/0.9.3.1/megaLib-0.9.3.1.tar.gz

---
+++ packages/megaLib/megaLib-0.9.3.1.tar.gz/megaLib-0.9.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='megaLib',
+    version='0.9.3.1',
+    py_modules=['megalib'],
+    install_requires=['requests'],
+    url='https://github.com/jvdspeare/megaLib',
+    license='GPLv3',
+    author='james.speare',
+    author_email='info.megalib@gmail.com',
+    description='megaLib is a Python wrapper for the Megaport RESTful API'
+)
