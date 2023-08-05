d5fed4a11473a1e21849539fe8ab89c9b8260f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:19 +0000

    Add package RtmAPI/0.7.1/RtmAPI-0.7.1.tar.gz

---
+++ packages/RtmAPI/RtmAPI-0.7.1.tar.gz/RtmAPI-0.7.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='RtmAPI',
+    author='Michael Gruenewald',
+    author_email='mail@michaelgruenewald.eu',
+    description='API package for rememberthemilk.com',
+    long_description=open('README').read(),
+    license='License :: OSI Approved :: MIT License',
+    url='https://bitbucket.org/rtmapi/rtmapi',
+    version='0.7.1',
+    packages=['rtmapi', ],
+    requires=['httplib2 (>=0.6.0)'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+    ],
+    use_2to3=True,
+)
