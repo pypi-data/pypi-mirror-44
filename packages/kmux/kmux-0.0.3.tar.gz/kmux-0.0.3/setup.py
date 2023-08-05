2f53abdb7ba68c204f67c452876f1d948b60df96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:16 +0000

    Add package kmux/0.0.3/kmux-0.0.3.tar.gz

---
+++ packages/kmux/kmux-0.0.3.tar.gz/kmux-0.0.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="kmux",
+    version="0.0.3",
+    keywords=("restful",),
+    author="Mu Xiaofei",
+    author_email="me@muxiaofei.cn",
+    description="easy for restful development",
+    long_description="",
+    long_description_content_type="text/x-rst",
+    url="https://gitee.com/muxiaofei/kmux",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson"],
+)
