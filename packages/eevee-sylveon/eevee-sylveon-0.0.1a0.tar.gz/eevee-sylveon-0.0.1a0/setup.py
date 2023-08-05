79a0bf40c87d4a74b8468a2e4addf8cee302968a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-sylveon/0.0.1a0/eevee-sylveon-0.0.1a0.tar.gz

---
+++ packages/eevee-sylveon/eevee-sylveon-0.0.1a0.tar.gz/eevee-sylveon-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="eevee-sylveon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Sylveon Extension for the Eevee Discord Framework",
+    packages=['eevee.ext.sylveon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
