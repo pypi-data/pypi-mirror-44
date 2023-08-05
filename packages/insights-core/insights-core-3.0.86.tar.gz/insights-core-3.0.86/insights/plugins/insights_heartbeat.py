d843a544267cf9f8885f9b4b2d4b94eea815f51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:47 +0000

    Add package insights-core/3.0.86/insights-core-3.0.86.tar.gz

---
+++ packages/insights-core/insights-core-3.0.86.tar.gz/insights-core-3.0.86/insights/plugins/insights_heartbeat.py
@@ -0,0 +1,13 @@
+from insights.core.plugins import rule, make_response
+from insights.parsers.hostname import Hostname
+
+ERROR_KEY = "INSIGHTS_HEARTBEAT"
+HEARTBEAT_UUID = "9cd6f607-6b28-44ef-8481-62b0e7773614"
+HOST = "insights-heartbeat-" + HEARTBEAT_UUID
+
+
+@rule(Hostname)
+def is_insights_heartbeat(hostname):
+    hostname = hostname.hostname
+    if hostname == HOST:
+        return make_response(ERROR_KEY)
