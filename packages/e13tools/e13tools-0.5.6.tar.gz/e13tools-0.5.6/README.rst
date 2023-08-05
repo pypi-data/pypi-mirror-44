a17fa714f7e47f6f63f864dd8a11c397df97d87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:26 +0000

    Add package e13tools/0.5.6/e13tools-0.5.6.tar.gz

---
+++ packages/e13tools/e13tools-0.5.6.tar.gz/e13tools-0.5.6/README.rst
@@ -0,0 +1,42 @@
+|PyPI| |Python| |License| |Travis| |AppVeyor| |Coverage|
+
+Description
+===========
+This package contains a collection of functions that were created by 1313e.
+Most functions were created for personal use, but have been changed and optimized to work for others as well.
+
+How to install
+==============
+e13Tools can be found in the PyPI system, so ``pip install e13tools`` should suffice.
+
+Dependencies
+------------
+e13Tools requires:
+
+- ``python == 2.7`` or ``python >= 3.5``,
+- ``matplotlib >= 2.2.4``,
+- ``numpy >= 1.8``,
+- ``six >= 1.11.0``.
+
+The following packages are used by a few functions and therefore optional:
+
+- ``astropy >= 1.3``.
+
+.. |PyPI| image:: https://img.shields.io/pypi/v/e13Tools.svg
+   :target: https://pypi.python.org/pypi/e13Tools
+   :alt: PyPI - Release
+.. |Python| image:: https://img.shields.io/pypi/pyversions/e13Tools.svg
+   :target: https://pypi.python.org/pypi/e13Tools
+   :alt: PyPI - Python Versions
+.. |License| image:: https://img.shields.io/pypi/l/e13Tools.svg?colorB=blue
+   :target: https://github.com/1313e/e13Tools/raw/master/LICENSE
+   :alt: PyPI - License
+.. |Travis| image:: https://img.shields.io/travis/1313e/e13Tools/master.svg?logo=travis&label=Travis%20CI
+   :target: https://travis-ci.org/1313e/e13Tools
+   :alt: Travis CI - Build Status
+.. |AppVeyor| image:: https://img.shields.io/appveyor/ci/1313e/e13Tools/master.svg?logo=appveyor&label=AppVeyor
+   :target: https://ci.appveyor.com/project/1313e/e13Tools
+   :alt: AppVeyor - Build Status
+.. |Coverage| image:: https://img.shields.io/coveralls/github/1313e/e13Tools/master.svg
+   :target: https://coveralls.io/github/1313e/e13Tools?branch=master
+   :alt: Coveralls - Coverage Status
---
+++ packages/e13tools/e13tools-0.5.6.tar.gz/e13tools-0.5.6/e13tools/__init__.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+e13Tools
+========
+Provides a collection of functions that were created by **1313e**.
+Recommended usage::
+
+    import e13tools as e13
+
+Available modules
+-----------------
+:mod:`~core`
+    Provides a collection of functions that are core to **e13Tools** and are
+    imported automatically.
+
+:mod:`~dummyMPI`
+    Dummy module that emulates the functionality of the :mod:`mpi4py.MPI`
+    module. It is automatically imported as :mod:`e13tools.MPI` if
+    :mod:`mpi4py.MPI` is not available.
+    This is a specialized version of the `mpi_dummy` package available at
+    https://gitlab.mpcdf.mpg.de/ift/mpi_dummy
+
+:mod:`~MPI`
+    :mod:`mpi4py.MPI` if it is available, or :mod:`e13tools.dummyMPI` if it is
+    not.
+
+:mod:`~math`
+    Provides a collection of functions useful in various mathematical
+    calculations and data array manipulations.
+
+:mod:`~pyplot`
+    Provides a collection of functions useful in various plotting routines.
+
+:mod:`~sampling`
+    Provides a collection of functions and techniques useful in sampling
+    problems.
+
+:mod:`~utils`
+    Provides several useful utility functions.
+
+"""
+
+
+# %% IMPORTS AND DECLARATIONS
+from __future__ import absolute_import, division, print_function
+
+# Import package modules
+from .__version__ import version as __version__
+from . import core
+from .core import *
+from . import dummyMPI
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = dummyMPI
+from . import math
+from .math import *
+from . import pyplot
+from .pyplot import *
+from . import sampling
+from .sampling import *
+from . import utils
+from .utils import *
+
+__all__ = ['MPI', 'dummyMPI', 'math', 'pyplot', 'sampling', 'utils']
+__all__.extend(core.__all__)
+__all__.extend(math.__all__)
+__all__.extend(pyplot.__all__)
+__all__.extend(sampling.__all__)
+__all__.extend(utils.__all__)
