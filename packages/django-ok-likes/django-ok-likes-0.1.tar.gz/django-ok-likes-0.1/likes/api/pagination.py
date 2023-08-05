52ecc212f807a48d2380bcf9b92203823721d928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package django-ok-likes/0.1/django-ok-likes-0.1.tar.gz

---
+++ packages/django-ok-likes/django-ok-likes-0.1.tar.gz/django-ok-likes-0.1/likes/api/pagination.py
@@ -0,0 +1,16 @@
+from django.utils.module_loading import import_string
+
+from likes.settings import LIKES_REST_PAGINATION_CLASS
+
+__all__ = (
+    'get_pagination_class',
+)
+
+
+def get_pagination_class():
+    pagination_class = LIKES_REST_PAGINATION_CLASS
+    if pagination_class:
+        try:
+            return import_string(pagination_class)
+        except ImportError:
+            pass
