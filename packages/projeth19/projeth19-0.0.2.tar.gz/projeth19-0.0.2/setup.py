da3b8a92dc994127fff56f3e5de7e2eea2939c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:32 +0000

    Add package projeth19/0.0.2/projeth19-0.0.2.tar.gz

---
+++ packages/projeth19/projeth19-0.0.2.tar.gz/projeth19-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="projeth19",
+    version="0.0.2",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Projet GCB202 H19",
+    long_description="Projet GCB202 H19",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
