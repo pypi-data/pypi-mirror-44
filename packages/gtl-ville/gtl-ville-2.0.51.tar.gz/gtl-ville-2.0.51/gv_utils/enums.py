88b71448f3e4954e300726c8abf3f1cd6df836a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package gtl-ville/2.0.51/gtl-ville-2.0.51.tar.gz

---
+++ packages/gtl-ville/gtl-ville-2.0.51.tar.gz/gtl-ville-2.0.51/gv_utils/enums.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from enum import Enum, unique
+from itertools import chain
+
+
+class CsvData:
+    samples = 'samples'
+    timestamp = 'timestamp'
+
+    confidence = 'confidence'
+    density = 'density'
+    flow = 'flow'
+    fluidity = 'fluidity'
+    occupancy = 'occupancy'
+    speed = 'speed'
+    status = 'status'
+    traveltime = 'traveltime'
+
+
+class DataTypeId:
+    metropme = 'metropme'
+    roads = 'roads'
+    tomtomfcd = 'tomtomfcd'
+    zones = 'zones'
+
+    datapoints = 'datapoints'
+    mappingroadsdatapoints = 'mappingroadsdatapoints'
+
+
+class AttId:
+    att = 'att'
+    datapointseids = 'datapointseids'
+    datatypeeid = 'datatypeeid'
+    eid = 'eid'
+    ffspeed = 'ffspeed'
+    fow = 'fow'
+    frc = 'frc'
+    fromno = 'fromno'
+    geom = 'geom'
+    geomxy = 'geomxy'
+    length = 'length'
+    maxspeed = 'maxspeed'
+    name = 'name'
+    nlanes = 'nlanes'
+    no = 'no'
+    tono = 'tono'
+    webatt = 'webatt'
+
+    datapointeid = 'datapointeid'
+    roadeid = 'roadeid'
+    validfrom = 'validfrom'
+    validto = 'validto'
+    zoneeid = 'zoneeid'
+
+
+class NetworkObjId:
+    datapointsroadsmap = 'datapointsroadsmap'
+    frcroadsmap = 'frcroadsmap'
+    lonlatnodesmatrix = 'lonlatnodesmatrix'
+    newmappings = 'newmappings'
+    omiteddatapoints = 'omiteddatapoints'
+    roadsffspeedmap = 'roadsffspeedmap'
