a721e3904f561a57873392fdb9edda6d48580a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:45 +0000

    Add package spektral/0.0.5/spektral-0.0.5.tar.gz

---
+++ packages/spektral/spektral-0.0.5.tar.gz/spektral-0.0.5/spektral/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import division, print_function
+
+from . import brain
+from . import datasets
+from . import layers
+from . import session
+from . import utils
+
+__version__ = '0.0.4'
