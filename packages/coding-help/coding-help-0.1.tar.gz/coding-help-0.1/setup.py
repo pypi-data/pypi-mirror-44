63324c572a603f92ceab4865e80333c6407ddf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package coding-help/0.1/coding-help-0.1.tar.gz

---
+++ packages/coding-help/coding-help-0.1.tar.gz/coding-help-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'coding-help',         
+  packages = ['coding-help'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'some coding help.',   
+  author = 'Jashanpreet Singh',                   
+  author_email = 'Jashanpreetsingh2502@gmail.com',      
+  url = 'https://github.com/turing-20/coding-help',   
+  download_url = 'https://github.com/turing-20/coding-help/archive/v_0.01.tar.gz',    
+  keywords = ['coding', 'help', 'study'],   
+  install_requires=[            
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
