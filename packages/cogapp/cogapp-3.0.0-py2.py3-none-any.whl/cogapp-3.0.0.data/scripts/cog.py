34513ac50857572ccf584aabc9212d5bd457087b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package cogapp/3.0.0/cogapp-3.0.0-py2.py3-none-any.whl

---
+++ packages/cogapp/cogapp-3.0.0-py2.py3-none-any.whl/cogapp-3.0.0.data/scripts/cog.py
@@ -0,0 +1,11 @@
+#!python
+""" Cog content generation tool.
+    http://nedbatchelder.com/code/cog
+
+    Copyright 2004-2019, Ned Batchelder.
+"""
+
+import sys
+from cogapp import Cog
+
+sys.exit(Cog().main(sys.argv))
