2dd01a628df76b37a87fb9a9e19f0507a0048e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package conplyent/0.2.9/conplyent-0.2.9.tar.gz

---
+++ packages/conplyent/conplyent-0.2.9.tar.gz/conplyent-0.2.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name="conplyent",
+    version="0.2.9",
+    license="MIT",
+    author="Jayesh Joshi",
+    author_email="jayeshjo1@utexas.edu",
+    url="https://github.com/joshijayesh/conplyent/",
+    description="Local and Remote Console executor",
+    packages=["conplyent", "conplyent_scripts"],
+    install_requires=[
+        "pyzmq>=17.1.2",
+        "click>=7.0"],
+    entry_points={
+        "console_scripts": [
+            "conplyent=conplyent_scripts.cli:cli"
+        ],
+    },
+    python_requires=">=3.4.3",
+)
