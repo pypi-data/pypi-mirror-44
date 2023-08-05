bc51d66d1573e75a02525f8ca7130d73a9e0b09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package flask-core/1.0.0/flask-core-1.0.0.tar.gz

---
+++ packages/flask-core/flask-core-1.0.0.tar.gz/flask-core-1.0.0/flask_core/app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from flask import Flask, g
+from sqlalchemy import create_engine
+
+from .core import bp as core_bp, models as core_models
+from .middleware.handler import Handler
+
+
+def create_app(config=None):
+    """
+    Creates an Flask application instance.
+
+    Optionally takes in a configuration object OR fully-qualified object path. Config may also be provided through
+    a pyfile pointed by FLASK_CORE_CONFIG envvar.
+
+    If DB_CONNECTION_STRING is provided, a SQLAlchemy object will be instantiated and available at app.db
+
+    :param config: Configuration object or string
+    :return: Flask application instance
+    """
+    app = Flask(__name__)
+
+    if config:
+        app.config.from_object(config)
+
+    # Attempt to load config from pyfile as well, if it exists
+    app.config.from_envvar("FLASK_CORE_CONFIG", silent=True)
+
+    # setup our database connection
+    if "DB_CONNECTION_STRING" in app.config:
+        app.db = create_engine(
+            app.config["DB_CONNECTION_STRING"], pool_pre_ping=app.config.get("DB_AUTO_RECONNECT", True)
+        )
+
+    # Register core blueprints
+    app.register_blueprint(core_bp)
+
+    # Register all our middleware
+    app.wsgi_app = Handler(app.wsgi_app)
+
+    return app
