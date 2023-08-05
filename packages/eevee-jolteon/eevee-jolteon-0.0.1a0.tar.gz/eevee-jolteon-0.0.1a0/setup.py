62915a4cd236b8463dbd8d25f786411f78fab30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-jolteon/0.0.1a0/eevee-jolteon-0.0.1a0.tar.gz

---
+++ packages/eevee-jolteon/eevee-jolteon-0.0.1a0.tar.gz/eevee-jolteon-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="eevee-jolteon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Jolteon Extension for the Eevee Discord Framework",
+    packages=['eevee.ext.jolteon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
