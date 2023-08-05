640988c7bf3154471af46d7beb6e0f36f67959ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package pandas-ext/0.4.12/pandas_ext-0.4.12.tar.gz

---
+++ packages/pandas-ext/pandas_ext-0.4.12.tar.gz/pandas_ext-0.4.12/README.md
@@ -0,0 +1,30 @@
+# pandas_ext
+Python Pandas extensions for pandas dataframes
+
+
+# Usage
+
+```
+import pandas_ext as px
+
+px.read_csv
+px.to_csv
+
+px.read_gdrive
+px.to_gdrive
+
+px.read_parquet
+px.to_parquet
+
+px.to_spectrum
+
+px.read_sfdc
+px.sfdc_metadata
+px.patch_sfdc
+px.async_patch_sfdc
+
+px.read_sql
+px.list_backends
+
+px.to_xml
+```
---
+++ packages/pandas-ext/pandas_ext-0.4.12.tar.gz/pandas_ext-0.4.12/pandas_ext/__init__.py
@@ -0,0 +1,35 @@
+"""Versioning kept here."""
+__version__ = '0.4.12'
+__license__ = "MIT"
+
+__title__ = "pandas_ext"
+__description__ = "Python Pandas extensions for pandas dataframes"
+
+__author__ = "Rich Fernandez, Sean Massot, Brian Tenazas, Luke Orland"
+__email__ = "devs@newsela.com"
+
+__uri__ = "https://github.com/newsela/pandas_ext"
+
+import pandas as _pd
+
+from .amazon_spectrum import to_spectrum
+from .csv import to_csv
+from .gdrive import read_gdrive, to_gdrive
+from .parquet import read_parquet, to_parquet
+from .sfdc import (
+    read_sfdc, sfdc_metadata, patch_sfdc, async_patch_sfdc,
+)
+from .sql import read_sql, list_backends
+
+
+read_csv = _pd.read_csv
+
+# Don't pollute the namespace with the module names:
+del amazon_spectrum
+del common
+del csv
+del gdrive
+del parquet
+del sfdc
+del sql
+del _pd
