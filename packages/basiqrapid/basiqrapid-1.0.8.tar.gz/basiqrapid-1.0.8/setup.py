be7ac5385ca241aa88f890d24c2469db99667677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:27 +0000

    Add package basiqrapid/1.0.8/basiqrapid-1.0.8.tar.gz

---
+++ packages/basiqrapid/basiqrapid-1.0.8.tar.gz/basiqrapid-1.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name="basiqrapid",
+    packages=["basiqrapid"],
+    version="1.0.8",
+    description="A Python wrapper for Basiq.io's API",
+    author="Michael Gee",
+    python_requires=">=3",
+    install_requires=["requests","pandas"],
+    author_email="michael.gee2018@gmail.com",
+    url="https://github.com/mike-gee/basiqrapid",
+    download_url="https://github.com/mike-gee/basiqrapid/archive/v_01.tar.gz",
+    keywords=["basiq", "finance", "sdk", "api", "python", "wrapper"],
+    license = "MIT",
+    long_description="A Python wrapper for Basiq.io's API.",
+    classifiers=[
+        "Topic :: Utilities"]
+)
