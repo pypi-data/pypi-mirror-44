92aa6fd72e2b128643d84caeb3c361ebceadb173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package aio-swagger/0.1/aio_swagger-0.1.tar.gz

---
+++ packages/aio-swagger/aio_swagger-0.1.tar.gz/aio_swagger-0.1/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'degibenz'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="aio_swagger",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pyyaml', 'jinja2', 'aiohttp'],
+    author="Alexey Shkil",
+    author_email="aleksey.shkil@homecredit.ru",
+    url="https://gitlab.homecred.it/internal/aio-swagger"
+)
