95e75ec6b93a3bf7c34cc433a909f77f3be6ad0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package reprobench/0.6.0.dev6/reprobench-0.6.0.dev6-py3-none-any.whl

---
+++ packages/reprobench/reprobench-0.6.0.dev6-py3-none-any.whl/reprobench/core/schema.py
@@ -0,0 +1,44 @@
+from strictyaml import (
+    Map,
+    Regex,
+    Seq,
+    Str,
+    Int,
+    Optional,
+    Seq,
+    MapPattern,
+    Enum,
+    Bool,
+    Any,
+)
+
+limits_schema = Map(
+    {
+        "time": Int(),
+        Optional("memory", default=8192): Int(),
+        Optional("output"): Int(),
+        Optional("cores"): Str(),
+    }
+)
+
+module_schema = Regex(r"\.?\w+(\.\w+)*")
+
+step_schema = Seq(
+    Map({"step": module_schema, Optional("config"): MapPattern(Str(), Any())})
+)
+
+task_sources = Enum(["local", "url"])
+
+schema = Map(
+    {
+        "title": Str(),
+        Optional("description"): Str(),
+        "limits": limits_schema,
+        "steps": Map({"run": step_schema, Optional("compile"): step_schema}),
+        "tasks": MapPattern(Str(), MapPattern(Str(), Any())),
+        "tools": MapPattern(
+            Str(), Map({"module": module_schema, "parameters": Seq(Str())})
+        ),
+        "parameters": MapPattern(Str(), MapPattern(Str(), Any())),
+    }
+)
