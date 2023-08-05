967175ebe147a9dc5e793dd7a7211253beaebe79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:43 +0000

    Add package civicpy/0.0.3/civicpy-0.0.3.tar.gz

---
+++ packages/civicpy/civicpy-0.0.3.tar.gz/civicpy-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from civicpy.__version__ import __version__, __author__, __author_email__, __description__, __url__
+
+setup(
+    name='civicpy',
+    version=__version__,
+    packages=['civicpy'],
+    url=__url__,
+    license='MIT',
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'pytest',
+        'requests'
+    ],
+    python_requires='>=3.7',
+    entry_points={},
+)
