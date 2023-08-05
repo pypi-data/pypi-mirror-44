ddbdf8591b023442b34f95c0a076a210e9531ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package Keyness/0.25/Keyness-0.25.tar.gz

---
+++ packages/Keyness/Keyness-0.25.tar.gz/Keyness-0.25/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Keyness',
+    version='0.25',
+    packages=['keyness'],
+    url='https://github.com/mikesuhan/keyness',
+    license='MIT License',
+    author='Mike Suhan',
+    author_email='mikesuhan@gmail.com',
+    description='Rank orders log likelihood values for every token or ngram in a corpus.'
+)
