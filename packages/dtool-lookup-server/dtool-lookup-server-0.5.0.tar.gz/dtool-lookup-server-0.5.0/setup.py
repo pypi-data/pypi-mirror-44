472e9fc41935a651b407c536741352d2ec340145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:44 +0000

    Add package dtool-lookup-server/0.5.0/dtool-lookup-server-0.5.0.tar.gz

---
+++ packages/dtool-lookup-server/dtool-lookup-server-0.5.0.tar.gz/dtool-lookup-server-0.5.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-lookup-server"
+version = "0.5.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-lookup-server",
+    packages=["dtool_lookup_server"],
+    package_data={"dtool_lookup_server": ["templates/*"]},
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    version=version,
+    url=url,
+    entry_points={
+        'flask.commands': [
+            'base_uri=dtool_lookup_server.cli:base_uri_cli',
+            'user=dtool_lookup_server.cli:user_cli',
+        ],
+    },
+    install_requires=[
+        "flask",
+        "flask-sqlalchemy",
+        "flask-migrate",
+        "flask-pymongo",
+        "dtoolcore",
+        "dtool_irods",
+        "dtool_s3",
+        "dtool_ecs",
+        "flask-jwt-extended[asymmetric_crypto]",
+        "pyyaml",
+    ],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
