94719ef68e7ad69821c35dc54a2463df69509489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package vmi/0.0.10/vmi-0.0.10.tar.gz

---
+++ packages/vmi/vmi-0.0.10.tar.gz/vmi-0.0.10/vmi/__init__.py
@@ -0,0 +1,14 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from vtk import *
+
+from .vask import *
+from .vdcm import *
+from .vidget import *
+from .vio import *
+from .vprop import *
+
+name = 'vmi'
+version = '0.0.10'
