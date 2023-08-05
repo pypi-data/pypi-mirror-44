45893403554f95c5e2d76894123cf099d59a4630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:08 +0000

    Add package mdict-utils/1.0.6/mdict_utils-1.0.6-py3-none-any.whl

---
+++ packages/mdict-utils/mdict_utils-1.0.6-py3-none-any.whl/mdict_utils/about.py
@@ -0,0 +1,8 @@
+
+name = 'mdict-utils'
+version = '1.0.6'
+author = 'Yugang LIU'
+email = 'liuyug@gmail.com'
+url = 'https://github.com/liuyug/pymdict.git'
+license = 'MIT'
+description = 'MDict pack/unpack tool'
