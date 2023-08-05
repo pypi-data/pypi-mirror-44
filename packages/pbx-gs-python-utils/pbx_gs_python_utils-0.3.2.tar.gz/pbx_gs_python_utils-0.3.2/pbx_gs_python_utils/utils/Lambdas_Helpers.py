ca60e9fc4e81d71a4f5ee355362054aa0f183c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:07 +0000

    Add package pbx-gs-python-utils/0.3.2/pbx_gs_python_utils-0.3.2.tar.gz

---
+++ packages/pbx-gs-python-utils/pbx_gs_python_utils-0.3.2.tar.gz/pbx_gs_python_utils-0.3.2/pbx_gs_python_utils/utils/Lambdas_Helpers.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('.')
+from pbx_gs_python_utils.utils.aws.Lambdas import Lambdas
+
+def log_info(message, data = None, index = "gs_bot_logs",category = "API_GS_Bot"):
+    return log_to_elk(message=message, data=data, index=index, level='info', category=category)
+
+def log_debug(message, data=None, index="gs_bot_logs", category="API_GS_Bot"):
+    return log_to_elk(message=message, data=data, index=index, level='debug', category=category)
+
+def log_error(message, data = None, index = "gs_bot_logs", category = "API_GS_Bot"):
+    return log_to_elk(message=message, data=data, index=index, level='error', category=category)
+
+def log_to_elk(message, data = None, index = "gs_bot_logs", level = "debug", category = "API_GS_Bot"):
+    payload = {
+                "index"    : index    ,
+                "level"    : level    ,
+                "message"  : message  ,
+                "category" : category ,
+                "data"     : data
+              }
+
+    response = Lambdas('pbx_gs_python_utils.lambdas.utils.log_to_elk').invoke_async(payload)
+    return "{0}".format(response)
+
+def slack_message(text, attachments = [], channel = 'GDL2EC3EE', team_id='T7F3AUXGV'):  # GBMGMK88Z is the 'from-aws-lambda' channel in the GS-CST Slack workspace
+    payload = {
+                'text'        : text        ,
+                'attachments' : attachments ,
+                'channel'     : channel     ,
+                'team_id'     : team_id
+              }
+    Lambdas('pbx_gs_python_utils.lambdas.utils.slack_message').invoke_async(payload)
