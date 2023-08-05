dbd731fac3a123af651e296179078e801fe250d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:14 +0000

    Add package Flask-Logify/1.0.1/Flask-Logify-1.0.1.tar.gz

---
+++ packages/Flask-Logify/Flask-Logify-1.0.1.tar.gz/Flask-Logify-1.0.1/setup.py
@@ -0,0 +1,39 @@
+"""
+Flask-Logify
+-------------
+
+Logging configuration for flask application
+"""
+from setuptools import setup
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Flask-Logify',
+    version='1.0.1',
+    url='https://github.com/cs91chris/flask_logify',
+    license='MIT',
+    author='cs91chris',
+    author_email='cs91chris@voidbrain.me',
+    description='Logging configuration for flask application',
+    long_description=long_description,
+    packages=['flask_logify'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask==1.0.2',
+        'PyYAML==5.1'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
