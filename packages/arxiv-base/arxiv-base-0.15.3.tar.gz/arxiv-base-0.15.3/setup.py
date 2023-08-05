293e39524ae6c0ce7e8802697eda2d36dad5f1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:24 +0000

    Add package arxiv-base/0.15.3/arxiv-base-0.15.3.tar.gz

---
+++ packages/arxiv-base/arxiv-base-0.15.3.tar.gz/arxiv-base-0.15.3/setup.py
@@ -0,0 +1,21 @@
+"""Install arXiv-base as an importable package."""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='arxiv-base',
+    version='0.15.3',
+    packages=[f'arxiv.{package}' for package
+              in find_packages('arxiv', exclude=['*test*'])],
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'jsonschema',
+        'pytz',
+        'uwsgi',
+        'boto3',
+        'bleach==3.1.0'
+    ],
+    include_package_data=True
+)
