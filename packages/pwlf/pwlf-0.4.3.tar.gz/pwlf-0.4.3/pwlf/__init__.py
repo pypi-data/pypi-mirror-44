67def0c6d3f98dbfb6183b4c0603386348c72d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:40 +0000

    Add package pwlf/0.4.3/pwlf-0.4.3.tar.gz

---
+++ packages/pwlf/pwlf-0.4.3.tar.gz/pwlf-0.4.3/pwlf/__init__.py
@@ -0,0 +1,6 @@
+from .pwlf import PiecewiseLinFit
+import os
+
+# add rudimentary version tracking
+VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
+__version__ = open(VERSION_FILE).read().strip()
