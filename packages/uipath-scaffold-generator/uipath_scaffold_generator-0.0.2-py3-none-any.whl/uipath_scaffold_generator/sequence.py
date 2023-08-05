d8412d23ddfb0b919a5ed0aa4bf7123b93cb6095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:03 +0000

    Add package uipath-scaffold-generator/0.0.2/uipath_scaffold_generator-0.0.2-py3-none-any.whl

---
+++ packages/uipath-scaffold-generator/uipath_scaffold_generator-0.0.2-py3-none-any.whl/uipath_scaffold_generator/sequence.py
@@ -0,0 +1,7 @@
+class Sequence_To_Scaffold():
+	def __init__(self, name, location, parent, description):
+		self.name = name
+		self.location = location
+		self.parent = parent
+		self.description = description
+		self.path = ""
