3f0bd67b381d22a71b77292c0e5c6f40c61d51ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:30 +0000

    Add package pupy/2.4.8/pupy-2.4.8.tar.gz

---
+++ packages/pupy/pupy-2.4.8.tar.gz/pupy-2.4.8/pupy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+from pupy.savings_n_loads import ljson
+from pupy.savings_n_loads import load_jasm
+from pupy.savings_n_loads import loads
+from pupy.savings_n_loads import lpak
+from pupy.savings_n_loads import safepath
+from pupy.savings_n_loads import save_jasm
+from pupy.savings_n_loads import savings
+from pupy.savings_n_loads import sjson
+from pupy.savings_n_loads import spak
