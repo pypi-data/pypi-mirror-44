d4c3a11120fc48416c0599b3106d98dc4ed7372f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:42 +0000

    Add package MagiSlack/0.1.1/MagiSlack-0.1.1.tar.gz

---
+++ packages/MagiSlack/MagiSlack-0.1.1.tar.gz/MagiSlack-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+        setuptools.setup(
+                name="MagiSlack",
+                version="0.1.1",
+                author="Lee, Suho",
+                author_email="riemannulus@hitagi.moe",
+                description="For a make easy and quick command-respond chat bot",
+                long_description=long_description,
+                long_description_content_type="text/markdown",
+                url="https://github.com/riemannulus/MagiSlack",
+                packages=setuptools.find_packages(),
+                install_requires=[
+                        'slackclient',
+                ],
+                classifiers=[
+                            "Programming Language :: Python :: 3.7",
+                            "License :: OSI Approved :: MIT License",
+                            "Operating System :: OS Independent",
+                        ],
+        )
