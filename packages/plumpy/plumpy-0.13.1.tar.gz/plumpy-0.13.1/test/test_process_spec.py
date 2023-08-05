bcba53f8ebd637b58b6ee60176361f97a9f08415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:31 +0000

    Add package plumpy/0.13.1/plumpy-0.13.1.tar.gz

---
+++ packages/plumpy/plumpy-0.13.1.tar.gz/plumpy-0.13.1/test/test_process_spec.py
@@ -0,0 +1,92 @@
+from __future__ import absolute_import
+from plumpy.ports import PortNamespace, InputPort
+from plumpy import ProcessSpec
+from .utils import TestCase
+
+
+class StrSubtype(str):
+    pass
+
+
+class TestProcessSpec(TestCase):
+
+    def setUp(self):
+        self.spec = ProcessSpec()
+
+    def test_get_port_namespace_base(self):
+        """
+        Get the root, inputs and outputs port namespaces of the ProcessSpec
+        """
+        ports = self.spec.ports
+        input_ports = self.spec.inputs
+        output_ports = self.spec.outputs
+
+        self.assertTrue(input_ports.name, self.spec.NAME_INPUTS_PORT_NAMESPACE)
+        self.assertTrue(output_ports.name, self.spec.NAME_OUTPUTS_PORT_NAMESPACE)
+
+    def test_dynamic_output(self):
+        self.spec.outputs.dynamic = True
+        self.spec.outputs.valid_type = str
+        self.assertIsNone(self.spec.validate_outputs({'dummy': 'foo'}))
+        self.assertIsNone(self.spec.validate_outputs({'dummy': StrSubtype('bar')}))
+        self.assertIsNotNone(self.spec.validate_outputs({'dummy': 5}))
+
+        # Remove dynamic output
+        self.spec.outputs.dynamic = False
+        self.spec.outputs.valid_type = None
+
+        # Now add and check behaviour
+        self.spec.outputs.dynamic = True
+        self.spec.outputs.valid_type = str
+        self.assertIsNone(self.spec.validate_outputs({'dummy': 'foo'}))
+        self.assertIsNone(self.spec.validate_outputs({'dummy': StrSubtype('bar')}))
+        self.assertIsNotNone(self.spec.validate_outputs({'dummy': 5}))
+
+    def test_get_description(self):
+        spec = ProcessSpec()
+
+        # Adding an input should create some description
+        spec.input('test')
+        description = spec.get_description()
+        self.assertNotEqual(description, {})
+
+        # Similar with adding output
+        spec = ProcessSpec()
+        spec.output('test')
+        description = spec.get_description()
+        self.assertNotEqual(description, {})
+
+    def test_input_namespaced(self):
+        """
+        Test the creation of a namespaced input port
+        """
+        self.spec.input('some.name.space.a', valid_type=int)
+
+        self.assertTrue('some' in self.spec.inputs)
+        self.assertTrue('name' in self.spec.inputs['some'])
+        self.assertTrue('space' in self.spec.inputs['some']['name'])
+        self.assertTrue('a' in self.spec.inputs['some']['name']['space'])
+
+        self.assertTrue(isinstance(self.spec.inputs.get_port('some'), PortNamespace))
+        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name'), PortNamespace))
+        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name.space'), PortNamespace))
+        self.assertTrue(isinstance(self.spec.inputs.get_port('some.name.space.a'), InputPort))
+
+    def test_validate(self):
+        """
+        Test the global spec validator functionality.
+        """
+
+        def is_valid(_spec, inputs):
+            if not ('a' in inputs) ^ ('b' in inputs):
+                return 'Must have a OR b in inputs'
+            return
+
+        self.spec.input('a', required=False)
+        self.spec.input('b', required=False)
+        self.spec.inputs.validator = is_valid
+
+        self.assertIsNotNone(self.spec.validate_inputs(inputs={}))
+        self.assertIsNotNone(self.spec.validate_inputs(inputs={'a': 'a', 'b': 'b'}))
+        self.assertIsNone(self.spec.validate_inputs(inputs={'a': 'a'}))
+        self.assertIsNone(self.spec.validate_inputs(inputs={'b': 'b'}))
