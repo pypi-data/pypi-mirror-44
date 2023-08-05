1ee662386562ebb72b5758857f7f406103a8c3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package csirtgsdk/1.1.4/csirtgsdk-1.1.4.tar.gz

---
+++ packages/csirtgsdk/csirtgsdk-1.1.4.tar.gz/csirtgsdk-1.1.4/csirtgsdk/search.py
@@ -0,0 +1,30 @@
+import logging
+from csirtgsdk.client.http import HTTP as Client
+
+
+class Search(object):
+    """
+    Search Object class
+    """
+    def __init__(self, client=Client()):
+        """
+
+        :param client: client.Client object
+        :return: Search Object
+        """
+        self.logger = logging.getLogger(__name__)
+        self.client = client
+
+    def search(self, q, limit=None):
+        """
+        Performs a search against the /search endpoint
+
+        :param q: query to be searched for [STRING]
+        :param limit: limit the results [INT]
+        :return: list of dicts
+        """
+        uri = '{}/search?q={}&limit={}'.format(self.client.remote, q, limit)
+        self.logger.debug(uri)
+
+        body = self.client.get(uri)
+        return body
