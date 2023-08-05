92ef86796107775b4806ab692b4183d1c7b77eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package greynoise/0.1.2/greynoise-0.1.2.tar.gz

---
+++ packages/greynoise/greynoise-0.1.2.tar.gz/greynoise-0.1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='greynoise',
+    version='0.1.2',
+    description='Abstraction to interact with GreyNoise API.',
+    url="https://greynoise.io/",
+    author="Brandon Dixon",
+    author_email="brandon@9bplus.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=['dict2xml', 'requests'],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ],
+    entry_points={
+        'console_scripts': [
+            'greynoise = greynoise.cli.manage:main'
+        ]
+    },
+    zip_safe=False,
+    keywords=['internet', 'scanning', 'threat intelligence', 'security'],
+    download_url='https://github.com/GreyNoise-Intelligence/pygreynoise'
+)
