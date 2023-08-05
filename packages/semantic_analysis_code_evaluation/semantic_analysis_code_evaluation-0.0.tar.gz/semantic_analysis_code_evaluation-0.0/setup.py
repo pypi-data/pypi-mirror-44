9765a3213f093f64f5768f8b0707283e5ce2bb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package semantic_analysis_code_evaluation/0.0/semantic_analysis_code_evaluation-0.0.tar.gz

---
+++ packages/semantic_analysis_code_evaluation/semantic_analysis_code_evaluation-0.0.tar.gz/semantic_analysis_code_evaluation-0.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='semantic_analysis_code_evaluation',
+    version=0.0,
+    description=(
+        'it is uncomplete, do not use it'
+    ),
+    #long_description=open('README.rst').read(),
+    author='ZixuanCao',
+    author_email='1240292104@qq.com',
+    maintainer='ZixuanCao',
+    maintainer_email='1240292104@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://blog.csdn.net/caozixuan98724',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
