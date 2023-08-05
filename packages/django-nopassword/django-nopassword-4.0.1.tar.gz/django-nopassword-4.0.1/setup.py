57ad7b7387ea680a7134c8790090a012e507c84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:31 +0000

    Add package django-nopassword/4.0.1/django-nopassword-4.0.1.tar.gz

---
+++ packages/django-nopassword/django-nopassword-4.0.1.tar.gz/django-nopassword-4.0.1/setup.py
@@ -0,0 +1,65 @@
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+
+def _read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except ImportError:
+        return None
+
+
+with open('nopassword/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+try:
+    from semantic_release import setup_hook
+
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+setup(
+    name="django-nopassword",
+    version=version,
+    url='http://github.com/relekang/django-nopassword',
+    author='Rolf Erik Lekang',
+    author_email='me@rolflekang.com',
+    description='Authentication backend for django that uses a one time code instead of passwords',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        'django>=1.11',
+    ],
+    extras_require={
+        'rest': ['djangorestframework>=3.1.3'],
+    },
+    tests_require=[
+        'django>=1.11',
+        'twilio==4.4.0',
+        'mock>=1.0'
+    ],
+    license='MIT',
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ]
+)
