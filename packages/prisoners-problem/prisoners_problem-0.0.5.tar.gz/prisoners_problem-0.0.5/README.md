5552f193db96a16889b6ce2f27029ff715063ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:02 +0000

    Add package prisoners-problem/0.0.5/prisoners_problem-0.0.5.tar.gz

---
+++ packages/prisoners-problem/prisoners_problem-0.0.5.tar.gz/prisoners_problem-0.0.5/README.md
@@ -0,0 +1,5 @@
+To build the C extension, run `python setup.py build`.
+
+Then run `python src/viz.py`.
+
+You'll need NumPy, Matplotlib.
---
+++ packages/prisoners-problem/prisoners_problem-0.0.5.tar.gz/prisoners_problem-0.0.5/prisoners_problem/simulate.py
@@ -0,0 +1,45 @@
+import random
+
+
+def seed_random(i):
+    random.seed(i)
+
+
+def try_find_self(boxes, start, limit):
+    next_box = boxes[start]
+    num_opened = 1
+    while next_box != start and num_opened < limit:
+        next_box = boxes[next_box]
+        num_opened += 1
+    return next_box == start
+
+
+def sample(n=100, limit=50, strategy=try_find_self):
+    """Takes a number boxes, n, the total number of attempts per person
+    allowed, limit, and a strategy function, strategy.
+
+    strategy(boxes: list, person: int, limit: int) -> bool
+
+    It takes a list of boxes (in the room), a person's ID (also the number on
+    their dollar), and the total number of moves allowed. Currently, this is on
+    the honor system."""
+    # boxes = random.sample(range(n), k=n)
+    boxes = list(range(n))
+    random.shuffle(boxes)
+    return sum(strategy(boxes, person, limit) for person in range(n))
+
+
+def nsamples(n, strategy=try_find_self):
+    """Takes a number of samples, n, and a strategy function, strategy.
+
+    strategy(boxes: list, person: int, limit: int) -> bool
+
+    It takes a list of boxes (in the room), a person's ID (also the number on
+    their dollar), and the total number of moves allowed. Currently, this is on
+    the honor system."""
+    return sum([sample(100, 50, strategy) == 100 for _ in range(n)]) / n
+
+
+if __name__ == "__main__":
+    seed_random(5)
+    print(nsamples(1000))
