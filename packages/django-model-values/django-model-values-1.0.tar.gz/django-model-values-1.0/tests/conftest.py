dafbbc7ae0c0735e94e62887aa1ba523ad4077a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:39 +0000

    Add package django-model-values/1.0/django-model-values-1.0.tar.gz

---
+++ packages/django-model-values/django-model-values-1.0.tar.gz/django-model-values-1.0/tests/conftest.py
@@ -0,0 +1,13 @@
+import django
+from django.conf import settings
+
+
+def pytest_report_header(config):
+    return 'Django ' + django.__version__
+
+
+pytest_plugins = 'django',
+settings.configure(
+    INSTALLED_APPS=['tests'],
+    DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+)
