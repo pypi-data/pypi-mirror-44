2728e9c84fd1aced8600fdf0d48b05c1ee0369b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package fifteenrock/0.1.3/fifteenrock-0.1.3.tar.gz

---
+++ packages/fifteenrock/fifteenrock-0.1.3.tar.gz/fifteenrock-0.1.3/fifteenrock/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fifteenrock."""
+
+__author__ = """Rajiv Abraham"""
+__email__ = 'rajiv.abraham@15rock.com'
+__version__ = '0.1.0'
+
+from .fifteenrock import get_database_client
