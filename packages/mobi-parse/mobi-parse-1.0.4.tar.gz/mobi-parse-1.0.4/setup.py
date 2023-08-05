1f1f73c5db4ad4dd9ea6a2d132b0f5886c3b2ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:47 +0000

    Add package mobi-parse/1.0.4/mobi-parse-1.0.4.tar.gz

---
+++ packages/mobi-parse/mobi-parse-1.0.4.tar.gz/mobi-parse-1.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mobi-parse',
+    version='1.0.4',
+    description=(
+        'mobi parse'
+    ),
+    long_description=open('README.rst').read(),
+    author='eric_zyh',
+    author_email='8040334@qq.com',
+    license='BSD License',
+    packages=['mobiparse'],
+    platforms=["all"],
+    url='https://github.com/ericzyh/mobi-parse',
+    install_requires=[
+        'bitarray==0.8.1'
+    ],
+    classifiers=[
+    ],
+)
