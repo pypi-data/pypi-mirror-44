31f528d9d5702d9d37baf3fc45ece6669eff5cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package webplatform-cli/1.0.14/webplatform-cli-1.0.14.tar.gz

---
+++ packages/webplatform-cli/webplatform-cli-1.0.14.tar.gz/webplatform-cli-1.0.14/setup.py
@@ -0,0 +1,32 @@
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name = "webplatform-cli",
+   version = "1.0.14",
+   description = "CLI used for a webplatform",
+   author = "Matthew Owens",
+   author_email = "mowens@redhat.com",
+   url = "https://github.com/lost-osiris/webplatform-cli",
+   # packages = ['lib', 'controller'],
+   packages = find_packages(exclude=("db", "settings")),
+   package_data={
+      '': [
+         'settings/*',
+         'db/docker/*',
+         'db/data/README.md'
+      ]
+   },
+   include_package_data = True,
+   install_requires = [
+      'docker',
+      'pymongo'
+   ],
+   license='MIT',
+   scripts = ["webplatform_cli/webplatform-cli"],
+   long_description = """TODO""",
+   classifiers = [
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+)
