4a6225cd3e828336d087454ae8901cafcd7e954e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:24 +0000

    Add package androidtv/0.0.14/androidtv-0.0.14.tar.gz

---
+++ packages/androidtv/androidtv-0.0.14.tar.gz/androidtv-0.0.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='androidtv',
+    version='0.0.14',
+    description='Communicate with an Android TV or Fire TV device via ADB over a network.',
+    url='https://github.com/JeffLIrion/python-androidtv/',
+    license='MIT',
+    author='Jeff Irion',
+    author_email='jefflirion@users.noreply.github.com',
+    packages=['androidtv'],
+    install_requires=['pycryptodome', 'rsa', 'adb-homeassistant', 'pure-python-adb-homeassistant'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
