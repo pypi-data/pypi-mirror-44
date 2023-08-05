f070da476388bc66e28b152b0e1be21efd0e0583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:54 +0000

    Add package company-matcher/0.8/company_matcher-0.8.tar.gz

---
+++ packages/company-matcher/company_matcher-0.8.tar.gz/company_matcher-0.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="company_matcher",
+    version="0.8",
+    author="Hudge",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    py_modules=["company_matcher"],
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
