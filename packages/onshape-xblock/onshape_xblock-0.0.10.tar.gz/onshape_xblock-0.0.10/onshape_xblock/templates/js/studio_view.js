ddfcbcaa2b9a3fd2b050064653d8f262fed39f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package onshape-xblock/0.0.10/onshape_xblock-0.0.10.tar.gz

---
+++ packages/onshape-xblock/onshape_xblock-0.0.10.tar.gz/onshape_xblock-0.0.10/onshape_xblock/templates/js/studio_view.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var Form = JSONSchemaForm.default;
+var $checkList = $("#xb-field-edit-check_list");
+$checkList.attr("readonly", "");
+var formListItemHtml = '<li className="field comp-setting-entry metadata_entry " data-field-name="check_list_form" >';
+$checkList.before(formListItemHtml);
+var $form = $('li[data-field-name="check_list_form"]');
+$(function ($) {
+  var json = $.getJSON("{{ check_list_form }}", function (schema) {
+    return loadForm(schema);
+  });
+});
+
+function loadForm(schema) {
+  var log = function log(type) {
+    return console.log.bind(console, type);
+  };
+
+  ReactDOM.render(React.createElement(Form, {
+    schema: schema,
+    onChange: onFormChange,
+    onSubmit: log("submitted"),
+    onError: log("errors")
+  }), $form[0]);
+}
+
+function onFormChange(data) {
+  $checkList.text(JSON.stringify(data.formData.this_array));
+}
