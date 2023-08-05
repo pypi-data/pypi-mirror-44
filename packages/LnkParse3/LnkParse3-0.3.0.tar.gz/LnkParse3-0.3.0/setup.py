13d96b63e72bf5442b78655ce48171b4e5ac39cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:14 +0000

    Add package LnkParse3/0.3.0/LnkParse3-0.3.0.tar.gz

---
+++ packages/LnkParse3/LnkParse3-0.3.0.tar.gz/LnkParse3-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+	name='LnkParse3',
+    version='0.3.0',
+    description='Windows Shortcut file (LNK) parser',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Matmaus/LnkParse',
+    author='Matúš Jasnický',
+    author_email='matusjas.work@gmail.com',
+    license='MIT',
+    packages=['src'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
