918c6f3fecd633f4e91767905d78977b1749c4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package mkdocs-datosgcba/1.0.1/mkdocs-datosgcba-1.0.1.tar.gz

---
+++ packages/mkdocs-datosgcba/mkdocs-datosgcba-1.0.1.tar.gz/mkdocs-datosgcba-1.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+
+
+setup(
+    name="mkdocs-datosgcba",
+    version=VERSION,
+    url='http://www.mkdocs.org',
+    license='BSD',
+    description='Temas de datosgcba para documentación con MkDocs',
+    author='Buenos Aires Data',
+    author_email='gobiernoabierto@buenosaires.gob.ar',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'datosgcba_docs=mkdocs_datosgcba.datosgcba_docs'
+        ]
+    },
+    zip_safe=False
+)
