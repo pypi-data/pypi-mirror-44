270a262819d929c0138fe966da7a502ea957efb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package returns-decorator/1.1/returns-decorator-1.1.tar.gz

---
+++ packages/returns-decorator/returns-decorator-1.1.tar.gz/returns-decorator-1.1/returns.py
@@ -0,0 +1,20 @@
+from functools import wraps as _wraps
+
+__author__ = 'Michael'
+__version__ = '1.1'
+
+
+class returns(object):
+    def __init__(self, type_, allow_none=False):
+        self.__type = type_
+        self.__allow_none = allow_none
+
+    def __call__(self, func):
+        @_wraps(func)
+        def __new_func(*args, **kwargs):
+            val = func(*args, **kwargs)
+            if self.__allow_none and val is None:
+                return None
+            return self.__type(val)
+
+        return __new_func
