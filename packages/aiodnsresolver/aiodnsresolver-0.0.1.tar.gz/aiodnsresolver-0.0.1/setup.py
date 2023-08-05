fef2eb92fbd49c66081afcf347fe4c91f4531914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package aiodnsresolver/0.0.1/aiodnsresolver-0.0.1.tar.gz

---
+++ packages/aiodnsresolver/aiodnsresolver-0.0.1.tar.gz/aiodnsresolver-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='aiodnsresolver',
+    version='0.0.1',
+    description='aiodnsresolver',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/aiodnsresolver',
+    author='Gerald',
+    author_email='i@gerald.top',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='async dns asyncio',
+    test_suite='test',
+    py_modules=[
+        'aiodnsresolver',
+    ],
+)
