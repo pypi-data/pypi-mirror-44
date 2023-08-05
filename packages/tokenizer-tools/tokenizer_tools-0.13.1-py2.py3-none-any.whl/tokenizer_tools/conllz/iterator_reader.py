387fa2e7f988186e4d8bb8645cdacbb52d31b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:40 +0000

    Add package tokenizer-tools/0.13.1/tokenizer_tools-0.13.1-py2.py3-none-any.whl

---
+++ packages/tokenizer-tools/tokenizer_tools-0.13.1-py2.py3-none-any.whl/tokenizer_tools/conllz/iterator_reader.py
@@ -0,0 +1,14 @@
+from tokenizer_tools.conllz.reader import read_conllz_from_string
+
+
+def read_conllz_iterator(conll_file):
+    with open(conll_file) as fd:
+        content = fd.read()
+        for sentence in read_conllz_from_string(content):
+            yield sentence
+
+
+def iterator_reader(input_file_list):
+    for input_file in input_file_list:
+        for i in read_conllz_iterator(input_file):
+            yield i
