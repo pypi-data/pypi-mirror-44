c322c7aeadb3a12752fd39814db562df4fceefc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package easy-workflow-manager/0.0.11/easy_workflow_manager-0.0.11-py3-none-any.whl

---
+++ packages/easy-workflow-manager/easy_workflow_manager-0.0.11-py3-none-any.whl/easy_workflow_manager/scripts/show_branches.py
@@ -0,0 +1,27 @@
+import click
+import input_helper as ih
+import easy_workflow_manager as ewm
+
+
+@click.command()
+@click.option(
+    '--all', '-a', 'all_branches', is_flag=True, default=False,
+    help='Show all remote branches (including QA)'
+)
+@click.option(
+    '--local', '-l', 'local', is_flag=True, default=False,
+    help='Also show local branches'
+)
+@click.argument('grep', nargs=1, default='')
+def main(grep, all_branches, local):
+    """Show branches that match specified grep pattern"""
+    if local:
+        print('\nRemote:')
+    ewm.show_remote_branches(grep=grep, all_branches=all_branches)
+    if local:
+        print('\nLocal:')
+        ewm.show_local_branches(grep=grep)
+
+
+if __name__ == '__main__':
+    main()
