bed0ec34a54a2989ef8d7b41bc4c7ab0facdc046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:06 +0000

    Add package git-annex-gui/0.2.3/git_annex_gui-0.2.3-py3-none-any.whl

---
+++ packages/git-annex-gui/git_annex_gui-0.2.3-py3-none-any.whl/gitannexgui/app.py
@@ -0,0 +1,29 @@
+import sys
+
+import pkg_resources
+
+from PySide2.QtCore import Qt
+from PySide2.QtCore import QResource
+from PySide2.QtCore import QCoreApplication
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
+
+from gitannexgui.gitannex import GitAnnex
+
+
+def main():
+    
+    qml_filepath = pkg_resources.resource_filename('gitannexgui', 'main.qml')
+    
+    # prepare gui
+    QResource.registerResource("qml.rcc")
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine(qml_filepath)
+
+    # setup gitannex handler and make available in QML
+    git_annex = GitAnnex()
+    engine.rootContext().setContextProperty("gitannex", git_annex)
+
+    # start the app
+    sys.exit(app.exec_())
