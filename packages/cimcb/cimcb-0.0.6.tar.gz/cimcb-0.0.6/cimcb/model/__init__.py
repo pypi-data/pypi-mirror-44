46dc0c7e0bf2e46389dbddcc6616d5e4f706d3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package cimcb/0.0.6/cimcb-0.0.6.tar.gz

---
+++ packages/cimcb/cimcb-0.0.6.tar.gz/cimcb-0.0.6/cimcb/model/__init__.py
@@ -0,0 +1,10 @@
+from .NN_LinearLinear import NN_LinearLinear
+from .NN_LinearLogit import NN_LinearLogit
+from .NN_LogitLogit import NN_LogitLogit
+from .PCLR import PCLR
+from .PCR import PCR
+from .PLS_SIMPLS import PLS_SIMPLS
+from .RF import RF
+from .SVM import SVM
+
+__all__ = ["NN_LinearLinear", "NN_LinearLogit", "NN_LogitLogit", "PCLR", "PCR", "PLS_SIMPLS", "RF", "SVM"]
