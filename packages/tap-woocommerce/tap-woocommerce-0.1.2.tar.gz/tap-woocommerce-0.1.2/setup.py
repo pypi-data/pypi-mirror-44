37e638fdf16e3e7f596ea629ef5ec157338bf017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:07 +0000

    Add package tap-woocommerce/0.1.2/tap-woocommerce-0.1.2.tar.gz

---
+++ packages/tap-woocommerce/tap-woocommerce-0.1.2.tar.gz/tap-woocommerce-0.1.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-woocommerce",
+    version="0.1.2",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_woocommerce"],
+    install_requires=[
+        'attrs==18.1.0',
+        'backoff==1.3.2',
+        'python-dateutil==2.7.3',
+        'requests==2.20.0',
+        'singer-python==5.0.15',
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-woocommerce=tap_woocommerce:main
+    """,
+    packages=["tap_woocommerce"],
+    package_data = {
+        "schemas": ["tap_woocommerce/schemas/*.json"]
+    },
+    include_package_data=True,
+)
