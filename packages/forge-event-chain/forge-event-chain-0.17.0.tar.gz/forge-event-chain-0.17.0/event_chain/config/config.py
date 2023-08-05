28392216bc6122bdb27785ebf62f93210faa3d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package forge-event-chain/0.17.0/forge-event-chain-0.17.0.tar.gz

---
+++ packages/forge-event-chain/forge-event-chain-0.17.0.tar.gz/forge-event-chain-0.17.0/event_chain/config/config.py
@@ -0,0 +1,20 @@
+import logging
+import os
+import os.path as path
+
+from forge import ForgeConfig
+
+logger = logging.getLogger('ec-config')
+
+app_config = path.join(path.dirname(__file__), "forge.toml")
+
+forge_config = ForgeConfig(app_config)
+
+app_path = forge_config.get_app_path()
+
+app_host = forge_config.get_app_host()
+app_port = forge_config.get_app_port()
+
+db_path = path.join(app_path, "ec.db")
+
+googlemaps_key = os.environ.get('GOOGLEMAPS_KEY')
