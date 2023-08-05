72d3afc9eb0142a134dd382ef26af60a7773a314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package hello-python-package/0.2/hello_python_package-0.2.tar.gz

---
+++ packages/hello-python-package/hello_python_package-0.2.tar.gz/hello_python_package-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hello_python_package',
+      version='0.2',
+      description='Test Hello World package',
+      url='http://pypi.python.org/pypi/hello_python_package/0.1',
+      author='Daniel Keighley',
+      author_email='daniel.keighley@wpengine.com',
+      license='WP',
+      packages=['hello_python_package'],
+      zip_safe=False)
