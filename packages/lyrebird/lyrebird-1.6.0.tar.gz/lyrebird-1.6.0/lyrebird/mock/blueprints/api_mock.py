41642cdf6c27562788a2435adb227e9035828d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package lyrebird/1.6.0/lyrebird-1.6.0.tar.gz

---
+++ packages/lyrebird/lyrebird-1.6.0.tar.gz/lyrebird-1.6.0/lyrebird/mock/blueprints/api_mock.py
@@ -0,0 +1,72 @@
+import codecs
+import json
+import os
+import traceback
+from types import FunctionType
+from flask import Blueprint, request, abort
+
+from ..handlers.handler_context import HandlerContext
+from .. import plugin_manager
+from .. import context
+from lyrebird import log
+
+
+logger = log.get_logger()
+
+
+api_mock = Blueprint('mock', __name__, url_prefix='/mock')
+
+
+@api_mock.route('/', methods=['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS'])
+@api_mock.route('/<path:path>', methods=['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS'])
+def index(path=''):
+    # todo add request handlers
+    resp = None
+    req_context = HandlerContext(request, path)
+    req_context.update_client_req_time()
+
+    for handler_name in plugin_manager.inner_handler:
+        handler = plugin_manager.inner_handler[handler_name]
+        try:
+            handler.handle(req_context)
+            if req_context.response:
+                resp = req_context.response
+        except Exception:
+            logger.error(traceback.format_exc())
+
+    for plugin_name in plugin_manager.data_handler_plugins:
+        try:
+            plugin = plugin_manager.data_handler_plugins[plugin_name]
+            plugin.handle(req_context)
+            if hasattr(plugin, 'change_response'):
+                if isinstance(plugin.change_response, bool) and plugin.change_response:
+                    resp = req_context.response
+                elif isinstance(plugin.change_response, FunctionType) and plugin.change_response():
+                    resp = req_context.response
+                else:
+                    logger.error(f'Plugin {plugin_name} has attr change_response, but its not bool or function')
+        except Exception:
+            logger.error(f'plugin error {plugin_name}\n{traceback.format_exc()}')
+
+    if not resp:
+        resp = abort(404, 'Not handle this request')
+    
+    req_context.update_client_resp_time()
+    
+    context.emit('action', 'add flow log')
+
+    return resp
+
+
+def create_json_schema(response_json_file_path):
+    json_obj = json.loads(codecs.open(response_json_file_path, 'r', 'utf-8').read())
+    schema = genson.Schema()
+    schema.add_object(json_obj)
+    schema_file_path = os.path.join(os.path.dirname(response_json_file_path), 'schema.json')
+    schema_file = codecs.open(schema_file_path, 'w', 'utf-8')
+    schema_file.write(schema.to_json(ensure_ascii=False, indent=4))
+    schema_file.close()
+
+
+
+
---
+++ packages/lyrebird/lyrebird-1.6.0.tar.gz/lyrebird-1.6.0/setup.py
@@ -0,0 +1,47 @@
+import runpy
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = runpy.run_path(
+        os.path.join(here, "lyrebird", "version.py")
+    )["VERSION"]
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lyrebird',
+    version=VERSION,
+    packages=['lyrebird'],
+    url='https://github.com/meituan/lyrebird',
+    author='HBQA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ),
+    entry_points={
+        'console_scripts': [
+            'lyrebird = lyrebird.manager:main'
+        ]
+    },
+    install_requires=[
+        'mitmproxy==4.0.4',
+        'flask==1.0.2',
+        'requests==2.21.0',
+        'colorama==0.4.1',
+        'flask-socketio==3.3.1',
+        'flask-restful==0.3.7',
+        'beautifulsoup4==4.7.1',
+        'portpicker==1.3.0',
+        'colorama==0.4.1',
+        'packaging==19.0',
+        'SQLAlchemy==1.2.18'
+    ])
