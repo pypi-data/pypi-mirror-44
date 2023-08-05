625e36df8ad36ade701f45dd96994c5c45c43516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:20 +0000

    Add package webplatform-cli/1.0.22/webplatform-cli-1.0.22.tar.gz

---
+++ packages/webplatform-cli/webplatform-cli-1.0.22.tar.gz/webplatform-cli-1.0.22/webplatform_cli/__init__.py
@@ -0,0 +1,26 @@
+# __all__ = [
+#    "containers",
+#    "dependencies",
+#    "lib",
+#    "tasks",
+#    "cli",
+#    "commands_parser",
+#    "handler",
+# ]
+from webplatform_cli.containers import *
+from webplatform_cli.lib import *
+from webplatform_cli.dependencies import *
+from webplatform_cli.tasks import *
+from webplatform_cli import cli
+from webplatform_cli import handler
+
+import os, sys
+
+controller_path = os.path.dirname(os.path.realpath(__file__))
+base_path = os.path.abspath(os.path.join(controller_path, '..'))
+
+if base_path not in sys.path:
+   sys.path.append(base_path)
+
+if controller_path not in sys.path:
+   sys.path.append(controller_path)
