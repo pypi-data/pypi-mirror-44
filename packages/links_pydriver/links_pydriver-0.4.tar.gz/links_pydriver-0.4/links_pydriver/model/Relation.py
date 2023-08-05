5a14c1a99637132e971100cff3ed038805e89491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package links_pydriver/0.4/links_pydriver-0.4.tar.gz

---
+++ packages/links_pydriver/links_pydriver-0.4.tar.gz/links_pydriver-0.4/links_pydriver/model/Relation.py
@@ -0,0 +1,47 @@
+class Relation:
+    def __init__(self, relationID, nodeAid, nodeBid, isOriented, type):
+        self.__relationID = relationID
+        self.__nodeAid = nodeAid
+        self.__nodeBid = nodeBid
+        self.__isOriented = isOriented
+        self.__type = type
+
+    @property
+    def relationID(self):
+        return self.__relationID
+
+    @relationID.setter
+    def relationID(self, relationID):
+        self.__relationID = relationID
+
+    @property
+    def nodeAid(self):
+        return self.__nodeAid
+
+    @nodeAid.setter
+    def nodeAid(self, nodeAid):
+        self.__nodeAid = nodeAid
+
+    @property
+    def nodeBid(self):
+        return self.__nodeBid
+
+    @nodeBid.setter
+    def nodeBid(self, nodeBid):
+        self.__nodeBid = nodeBid
+
+    @property
+    def isOriented(self):
+        return self.__isOriented
+
+    @isOriented.setter
+    def isOriented(self, isOriented):
+        self.__isOriented = isOriented
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, type):
+        self.__type = type
---
+++ packages/links_pydriver/links_pydriver-0.4.tar.gz/links_pydriver-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='links_pydriver',
+      version='0.4',
+      description='A python driver for Links',
+      url='https://github.com/tanguyesteoule/links_pydriver',
+      author='Tanguy Esteoule',
+      author_email='tanguy.esteoule@gmail.com',
+      license='MIT',
+      packages = find_packages(),
+      zip_safe=False)
