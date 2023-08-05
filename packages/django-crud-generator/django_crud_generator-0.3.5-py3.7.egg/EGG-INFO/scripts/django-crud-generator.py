8883daf027a40ec4dd1cfcf95d6ec5a7e5692649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package django-crud-generator/0.3.5/django_crud_generator-0.3.5-py3.7.egg

---
+++ packages/django-crud-generator/django_crud_generator-0.3.5-py3.7.egg/EGG-INFO/scripts/django-crud-generator.py
@@ -0,0 +1,5 @@
+#!/Users/ma0/miniconda3/envs/django_crud_generator/bin/python
+from django_crud_generator import execute_from_command_line
+
+if __name__ == "__main__":
+    execute_from_command_line()
