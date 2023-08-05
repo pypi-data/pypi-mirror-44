4f0b4fa1a101f79b4a46630ee5fa633059c5109a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:21 +0000

    Add package smif/1.0.8/smif-1.0.8.tar.gz

---
+++ packages/smif/smif-1.0.8.tar.gz/smif-1.0.8/src/smif/data_layer/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Data access modules for loading system-of-systems model configuration
+"""
+
+# import classes for access like ::
+#         from smif.data_layer import DataHandle`
+from smif.data_layer.data_array import DataArray
+from smif.data_layer.data_handle import DataHandle
+from smif.data_layer.store import Store
+
+# Define what should be imported as * ::
+#         from smif.data_layer import *
+__all__ = ['DataArray', 'DataHandle', 'Store']
