ac1136034e755a10472e70351b584c36a5f8f5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package getip4/1.0.0/getip4-1.0.0.tar.gz

---
+++ packages/getip4/getip4-1.0.0.tar.gz/getip4-1.0.0/README.md
@@ -0,0 +1,20 @@
+# getip2
+
+Gets your public IP Address
+
+
+# Usage
+
+``` Python
+>>> import getip4
+>>> ip = getip4.get()
+>>> ip
+'1.2.3.4'
+```
+
+
+# Installation
+
+```
+-$ pip install getip2
+```
---
+++ packages/getip4/getip4-1.0.0.tar.gz/getip4-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='getip4',
+    version='1.0.0',
+    description='Gets your public IP4 Address',
+    author='thanhson.rf@gmail.com',
+    author_email='thanhson.rf@gmail.com',
+    url='https://github.com/sonnt85',
+    py_modules=['getip4'])
