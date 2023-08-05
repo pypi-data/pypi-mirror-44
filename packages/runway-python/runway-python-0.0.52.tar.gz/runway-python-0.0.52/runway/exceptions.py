48c63590759f29c0f5b27810d4c18f5cb8294bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package runway-python/0.0.52/runway-python-0.0.52.tar.gz

---
+++ packages/runway-python/runway-python-0.0.52.tar.gz/runway-python-0.0.52/runway/exceptions.py
@@ -0,0 +1,55 @@
+import sys
+import traceback
+
+class RunwayError(Exception):
+    def __init__(self):
+        super(RunwayError, self).__init__()
+        self.message = 'An unknown error occurred.'
+        self.code = 500
+
+    def to_response(self):
+        _, _, tb = sys.exc_info()
+        formatted_tb = ''.join(traceback.format_tb(tb))
+        return {'error': self.message, 'traceback': formatted_tb}
+
+
+class MissingOptionException(RunwayError):
+    def __init__(self, name):
+        super(MissingOptionException, self).__init__()
+        self.message = 'Missing option: %s.' % name
+        self.code = 400
+
+
+class MissingInputException(RunwayError):
+    def __init__(self, name):
+        super(MissingInputException, self).__init__()
+        self.message = 'Missing input: %s.' % name
+        self.code = 400
+
+
+class InferenceError(RunwayError):
+    def __init__(self, message):
+        super(InferenceError, self).__init__()
+        self.message = 'Inference error: %s' % message
+        self.code = 500
+
+
+class UnknownCommandError(RunwayError):
+    def __init__(self, name):
+        super(UnknownCommandError, self).__init__()
+        self.message = 'Unknown command: %s.' % name
+        self.code = 404
+
+
+class SetupError(RunwayError):
+    def __init__(self, message):
+        super(SetupError, self).__init__()
+        self.message = 'Setup error: %s' % message
+        self.code = 500
+
+
+class MissingArgumentError(RunwayError):
+    def __init__(self, arg):
+        super(SetupError, self).__init__()
+        self.message = 'Missing argument: %s' % arg
+        self.code = 500
---
+++ packages/runway-python/runway-python-0.0.52.tar.gz/runway-python-0.0.52/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='runway-python',
+    version='0.0.52',
+    description='Helper library for creating Runway models',
+    author='Runway AI, Inc.',
+    author_email='anastasis@runwayml.com',
+    url='https://github.com/runwayml/python-sdk',
+    packages=['runway'],
+    scripts=[],
+    install_requires=[
+        'Flask>=0.12.2',
+        'Flask-Cors>=3.0.2',
+        'numpy>=1.15.0',
+        'Pillow>=4.3.0',
+        'gevent>=1.4.0',
+        'wget>=3.2'
+    ],
+    license="MIT"
+)
