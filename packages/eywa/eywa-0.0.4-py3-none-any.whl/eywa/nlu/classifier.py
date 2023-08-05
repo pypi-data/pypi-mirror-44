728d9256b029367503448e50d3268273a73f9c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package eywa/0.0.4/eywa-0.0.4-py3-none-any.whl

---
+++ packages/eywa/eywa-0.0.4-py3-none-any.whl/eywa/nlu/classifier.py
@@ -0,0 +1,122 @@
+from ..lang import Document
+from ..math import vector_sequence_similarity, euclid_similarity, softmax
+from collections import defaultdict
+import tensorflow as tf
+import numpy as np
+
+
+class Classifier(object):
+
+    def __init__(self):
+        self.X = []
+        self.Y = []
+        self.data = {}
+        self.weights = [tf.Variable(w, dtype='float32') for w in self.__class__.default_weights()]
+        pass
+
+    @staticmethod
+    def default_weights():
+        return [0.5, .1, .1, 1., .05]
+
+    @property
+    def classes(self):
+        return list(self.data.keys())
+
+    def fit(self, X, Y):
+        if type(X) in (str, Document):
+            X = [X]
+            Y = [Y]
+        if type(Y) not in (list, tuple):
+            Y = [Y] * len(X)
+        assert len(X) == len(Y), "Different number of samples in X and Y."
+        X_app = self.X.append
+        Y_app = self.Y.append
+        data = self.data
+        for x, y in zip(X, Y):
+            x = Document(x)
+            # need these for quick eval
+            X_app(x)
+            Y_app(y)
+            if y in data:
+                data[y].append(x)
+            else:
+                data[y] = [x]
+
+    def forward(self, x):
+        assert isinstance(x, Document)
+        scores = []
+        f = self._similarity
+        for i, v in enumerate(self.data.values()):
+            score = f(x, v[0])
+            for x2 in v[1:]:
+                score = max(score, f(x, x2))
+            scores.append(score)
+        scores = tf.stack(scores)
+        return scores
+
+    def predict(self, x, return_scores=False):
+        if type(x) in (list, tuple):
+            return type(x)([self.predict(i, return_scores) for i in x])
+        if type(x) is not Document:
+            x = Document(x)
+        classes = list(self.data.keys())
+        scores = self.forward(x)
+        if return_scores:
+            return {z[0]: float(z[1]) for z in zip(classes, scores)}
+        return classes[np.argmax(scores.numpy())]
+
+    def _similarity(self, x1, x2):
+        # if x1 == x2:
+        #    return 1
+        if len(x1) == 0 or len(x2) == 0:
+            return 0.
+        weights = self.weights
+        w0 = weights[0]
+        score1 = lambda: euclid_similarity(x1.embedding, x2.embedding)
+        score2 = lambda: tf.tensordot(x1.embedding, x2.embedding, (0, 0))
+        score3 = lambda: vector_sequence_similarity(x1.embeddings, x2.embeddings, w0, 'dot')
+        score4 = lambda: vector_sequence_similarity(x1.embeddings, x2.embeddings, w0, 'euclid')
+        scores = [score1, score2, score3, score4]
+        score_weights = weights[1:5]
+        score = 0.
+        for s, w in zip(scores, score_weights):
+            if w > 0.05:
+                score += s()
+        return score * 0.25
+
+    def evaluate(self, X=None, Y=None):
+        if X is None:
+            X = self.X
+            Y = self.Y
+        acc = 0.
+        err = 0.
+        Y_pred = self.predict(X, True)
+        for y, y_pred in zip(Y, Y_pred):
+            if y == max(y_pred, key=y_pred.__getitem__):
+                acc += 1.
+            err -= np.log(y_pred[y])
+        acc /= len(X)
+        return err, acc
+
+    def serialize(self):
+        config = {}
+        config['X'] = [str(x) for x in self.X]
+        config['Y'] = self.Y[:]
+        config['weights'] = [w.tolist() for w in self.get_weights()]
+        return config
+
+    @classmethod
+    def deserialize(cls, config):
+        clf = cls()
+        clf.set_weights(config['weights'])
+        clf.fit(config['X'], config['Y'])
+        return clf
+
+    def set_weights(self, weights):
+        assert isinstance(weights, list)
+        assert len(weights) == len(self.weights)
+        for (w_in, w_curr) in zip(weights, self.weights):
+                w_curr.assign(w_in)
+
+    def get_weights(self):
+        return [w.numpy() for w in self.weights]
---
+++ packages/eywa/eywa-0.0.4-py3-none-any.whl/eywa/nlu/entity_extractor.py
@@ -0,0 +1,212 @@
+from ..math import batch_vector_sequence_similarity, euclid_similarity
+from ..math import should_pick, get_token_score
+from ..lang import Document, Token, tokenize_by_stop_words
+import tensorflow as tf
+import numpy as np
+
+
+
+class EntityExtractor(object):
+
+    def __init__(self):
+        self.X = []
+        self.Y = []
+        self.keys = {}
+        self._changed = False
+        self.weights = [tf.Variable(w, dtype='float32') for w in self.__class__.default_weights()]
+
+    @staticmethod
+    def default_weights():
+        return [0.5, 0.5, 0.5, 0.5]
+
+    @property
+    def entities(self):
+        return list(self.keys.keys())
+
+    def fit(self, X, Y):
+        x_app = self.X.append
+        y_app = self.Y.append
+        for x, y in zip(X, Y):
+            x = Document(x)
+            x_app(x)
+            y_app(y)
+        self._changed = True
+
+    def compile(self):
+        # create a profile for each 'key'
+        keys = set()
+        for y in self.Y:
+            for k in y:
+                keys.add(k)
+        self.keys = {k: {'picks': [], 'lefts': [], 'rights': [], 'values': [], 'consts': {}, 'types': set()} for k in
+                     keys}
+        keys = self.keys
+        for i, (x, y) in enumerate(zip(self.X, self.Y)):
+            for k in keys:
+                if k in y:
+                    kk = keys[k]
+                    types = kk['types']
+                    v = y[k]
+                    indices = []
+                    types_add = types.add
+                    indices_app = indices.append
+                    for j, t in enumerate(x):
+                        if t.text == v:
+                            indices_app(j)
+                            types_add(t.type)
+                    if indices:
+                        kk['picks'].append(i)
+                        lefts_app = kk['lefts'].append
+                        rights_app = kk['rights'].append
+                        values_app = kk['values'].append
+                        for ind in indices:
+                            left = x[:ind]
+                            right = x[ind:]
+                            lefts_app(left)
+                            rights_app(right)
+                            values_app(Token(v))
+                    else:
+                        consts = kk['consts']
+                        if v in consts:
+                            consts[v].append(i)
+                        else:
+                            consts[v] = [i]
+                else:
+                    consts = kk['consts']
+                    if None in consts:
+                        consts[None].append(i)
+                    else:
+                        consts[None] = [i]
+
+    def predict(self, x, keys=None, return_scores=False):
+        if type(x) in (list, tuple):
+            return type(x)(map(self.predict, x))
+        if self._changed:
+            self.compile()
+            self._changed = False
+        if keys is None:
+            keys = self.keys.keys()
+        x = tokenize_by_stop_words(x)
+        y_scores = self.forward(x)
+        y = {}
+        self_keys = self.keys
+        if return_scores:
+            for k in keys:
+                kk = self_keys[k]
+                should_pick, pick_scores, const_scores = y_scores[k]
+                should_pick = 1 if should_pick > 0 else 0
+                vals = [self.Y[i][k] for i in kk['picks']] + list(kk['consts'].keys())
+                scores = (pick_scores.numpy() * should_pick).tolist()
+                scores += (const_scores.numpy() * (1 - should_pick)).tolist()
+                return sorted(list(zip(vals,scores)), key=lambda x: x[1], reverse=True)
+        else:            
+            for k in keys:
+                kk = self_keys[k]
+                should_pick, pick_scores, const_scores = y_scores[k]
+                if should_pick > 0:
+                    vals = [w.text for w in x]
+                    y[k] = vals[int(tf.argmax(pick_scores))]
+                else:
+                    y[k] = list(kk['consts'].keys())[int(tf.argmax(const_scores))]
+        return y
+
+    def forward(self, x):
+        assert isinstance(x, Document)
+        x_embs = x.embeddings
+        X = self.X
+        self_keys = self.keys
+        weights = self.weights
+        keys = self.keys.keys()
+        y = {}
+        for k in keys:
+            kk = self_keys[k]
+            pick_scores = tf.zeros(len(x))
+            consts = kk['consts']
+            const_scores = tf.zeros(len(consts))
+            kk = self_keys[k]
+            types = kk['types']
+            if len(types) == 1:
+                entity_type = list(types)[0]
+            else:
+                entity_type = None
+            pick_idxs = kk['picks']
+            picks = []
+            non_picks = []
+            for i in range(len(X)):
+                if i in pick_idxs:
+                    picks.append(i)
+                else:
+                    non_picks.append(i)
+            if not picks:
+                pick = tf.zeros(1)
+            else:
+                if not consts:
+                    pick = tf.ones(1)
+                else:
+                    pick_embs = [X[i].embeddings for i in picks]
+                    non_pick_embs = [X[i].embeddings for i in non_picks]
+                    vals = [self.Y[i][k] for i in pick_idxs]
+                    n = len(vals)
+                    c = len(set(vals))
+                    variance = float(c) / n
+                    pick = should_pick(x_embs, pick_embs, non_pick_embs, variance, weights)
+            if pick > 0:
+                pick_scores = []
+                lefts_embs = [d.embeddings for d in kk['lefts']]
+                rights_embs = [d.embeddings for d in kk['rights']]
+                vals_embs = [v.embedding for v in kk['values']]
+                for i, t in enumerate(x):
+                    left = x[:i]
+                    right = x[i:]
+                    token_score = get_token_score(t.embedding, left.embeddings, right.embeddings,
+                                                  lefts_embs, rights_embs, vals_embs, bool(entity_type), weights)
+                    pick_scores.append(token_score)
+                pick_scores = tf.stack(pick_scores)
+            else:
+                const_scores = []
+                for ck in consts:
+                    docs = [X[i] for i in consts[ck]]
+                    embs = [doc.embeddings for doc in docs]
+                    score = tf.reduce_max(batch_vector_sequence_similarity(embs, x_embs))
+                    const_scores.append(score)
+                const_scores = tf.stack(const_scores)
+            y[k] = pick, pick_scores, const_scores
+        return y
+
+    def evaluate(self, X=None, Y=None):
+        if X is None:
+            X = self.X
+            Y = self.Y
+        errors = 0
+        n = 0
+        for x, y in zip(X, Y):
+            y_pred = self.predict(x)
+            for k in y:
+                n += 1
+                if y[k] != y_pred[k]:
+                    errors += 1
+        accuracy = 1. - float(errors) / n
+        return errors, accuracy
+
+    def serialize(self):
+        config = {}
+        config['X'] = [str(x) for x in self.X]
+        config['Y'] = self.Y[:]
+        config['weights'] = [w.tolist() for w in self.get_weights()]
+        return config
+
+    @classmethod
+    def deserialize(cls, config):
+        ee = cls()
+        ee.fit(config['X'], config['Y'])
+        ee.set_weights(config['weights'])
+        return ee
+
+    def set_weights(self, weights):
+        assert isinstance(weights, list)
+        assert len(weights) == len(self.weights)
+        for (w_in, w_curr) in zip(weights, self.weights):
+                w_curr.assign(w_in)
+
+    def get_weights(self):
+        return [w.numpy() for w in self.weights]
