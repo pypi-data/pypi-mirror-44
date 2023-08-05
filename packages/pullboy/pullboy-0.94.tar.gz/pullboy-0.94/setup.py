5e6d7a830d1d62b2b7a51cc751b96cc6b903c8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:37 +0000

    Add package pullboy/0.94/pullboy-0.94.tar.gz

---
+++ packages/pullboy/pullboy-0.94.tar.gz/pullboy-0.94/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+__version__ = ['0.94']
+with open('README.md', 'r') as fl:
+    long_desc = fl.read()
+
+setup(name='pullboy',
+      version='.'.join(__version__),
+      description='Super Simple Auto-Deployment server',
+      long_description=long_desc,
+      long_description_content_type='text/markdown',
+      url='http://github.com/theSage21/pullboy',
+      author='Arjoonn Sharma',
+      author_email='arjoonn.94@gmail.com',
+      packages=['pullboy'],
+      install_requires=['bottle', 'pyyaml'],
+      entry_points={'console_scripts': ['pullboy=pullboy:main']},
+      keywords=['pullboy', 'auto deploy server'],
+      zip_safe=False)
