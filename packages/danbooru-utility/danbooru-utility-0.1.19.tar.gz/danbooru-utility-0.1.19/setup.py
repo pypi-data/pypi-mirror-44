05a6514ce73a4587fbba45e551ee907c5b63e54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package danbooru-utility/0.1.19/danbooru-utility-0.1.19.tar.gz

---
+++ packages/danbooru-utility/danbooru-utility-0.1.19.tar.gz/danbooru-utility-0.1.19/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'danbooru-utility',
+    version = '0.1.19',
+    url = 'https://github.com/reidsanders/danbooru-utility.git',
+    author = 'Reid Sanders',
+    author_email = 'reid@reidsanders.net',
+    description = 'Utility for working with danbooru2018 dataset',
+    long_description= long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    #data_files=[('', ['lbpcascade_animeface.xml'])],
+    package_data={'': ['danbooru-utility/lbpcascade_animeface.xml']},
+    packages = find_packages(),    
+    py_modules=['danbooru-utility/danbooru_utility'],
+    install_requires = [
+        'numpy >= 1.15.4',
+        'opencv_python >= 3.4.3.18',
+        'python_resize_image >= 1.1.18',
+        'Pillow >= 5.4.1',
+        ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'danbooru-utility=danbooru.danbooru_utility:main',
+        ],
+    },
+)
