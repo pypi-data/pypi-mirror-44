46355ea59149afdc6d0b25a46e77c2f8c0735f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package macmac/0.1/macmac-0.1-py2-none-any.whl

---
+++ packages/macmac/macmac-0.1-py2-none-any.whl/macmac/__init__.py
@@ -0,0 +1,15 @@
+def quote():
+    return("""
+        Success is never final, failure is never final.It's courage that counts.
+    """)
+
+
+def snake():
+    return(
+        """\
+        --..,_                     _,.--.
+           `'.'.                .'`__ o  `;__.
+              '.'.            .'.'`  '---'`  `
+                '.`'--....--'`.'
+                  `'--....--'`
+    """)
