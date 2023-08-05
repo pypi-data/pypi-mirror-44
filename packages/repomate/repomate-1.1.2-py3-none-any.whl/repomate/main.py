879c71737332ed7c7d22909ab79911b302e12d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package repomate/1.1.2/repomate-1.1.2-py3-none-any.whl

---
+++ packages/repomate/repomate-1.1.2-py3-none-any.whl/repomate/main.py
@@ -0,0 +1,89 @@
+"""Main entrypoint for the repomate CLI application.
+
+.. module:: main
+    :synopsis: Main entrypoint for the repomate CLI application.
+
+.. moduleauthor:: Simon Larsén
+"""
+
+import sys
+from typing import List
+
+import daiquiri
+
+from repomate import cli
+from repomate import plugin
+from repomate import exception
+
+LOGGER = daiquiri.getLogger(__file__)
+
+_PRE_INIT_ERROR_MESSAGE = """exception was raised before pre-initialization was
+complete. This is usually due to incorrect settings.
+Try running the `verify-settings` command and see if
+the problem can be resolved. If all fails, please open
+an issue at https://github.com/slarse/repomate/issues/new
+and supply the stack trace below.""".replace(
+    "\n", " "
+)
+
+
+def _separate_args(args: List[str]) -> (List[str], List[str]):
+    """Separate args into plugin args and repomate args."""
+    plugin_args = []
+    if args and (args[0].startswith("-p") or "plug" in args[0]):
+        cur = 0
+        while cur < len(args) and args[cur].startswith("-"):
+            if args[cur].startswith("-p"):
+                plugin_args += args[cur : cur + 2]
+                cur += 2
+            elif args[cur] == "--no-plugins":
+                plugin_args.append(args[cur])
+                cur += 1
+            else:
+                break
+    return plugin_args, args[len(plugin_args) :]
+
+
+def main(sys_args: List[str]):
+    """Start the repomate CLI."""
+    LOGGER.warning(
+        "The repomate package is being renamed repobee. "
+        "This is the final version of repomate, but all "
+        "functionality carries over to repobee. Please see "
+        "https://github.com/repobee/repobee for details."
+    )
+
+    args = sys_args[1:]  # drop the name of the program
+    traceback = False
+    pre_init = True
+    try:
+        plugin_args, app_args = _separate_args(args)
+
+        if plugin_args:
+            parsed_plugin_args = cli.parse_plugins(plugin_args)
+            if parsed_plugin_args.no_plugins:
+                LOGGER.info("plugins disabled")
+            else:
+                plugin.initialize_plugins(parsed_plugin_args.plug)
+        else:
+            plugin.initialize_plugins()
+        parsed_args, api = cli.parse_args(app_args)
+        traceback = parsed_args.traceback
+        pre_init = False
+        cli.dispatch_command(parsed_args, api)
+    except Exception as exc:
+        # FileErrors can occur during pre-init because of reading the config
+        # and we don't want tracebacks for those (afaik at this time)
+        if traceback or (
+            pre_init and not isinstance(exc, exception.FileError)
+        ):
+            LOGGER.error(str(exc))
+            if pre_init:
+                LOGGER.info(_PRE_INIT_ERROR_MESSAGE)
+            LOGGER.exception("critical exception")
+        else:
+            LOGGER.error("{.__class__.__name__}: {}".format(exc, str(exc)))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ packages/repomate/repomate-1.1.2-py3-none-any.whl/repomate/tuples.py
@@ -0,0 +1,58 @@
+"""Tuples module.
+
+This module contains various namedtuple containers used throughout repomate.
+There are still a few namedtuples floating about in their own modules, but
+the goal is to collect all container types in this module.
+
+.. module:: tuples
+    :synopsis: Module containing various namedtuple containers used throughout
+        repomate.
+
+.. moduleauthor:: Simon Larsén
+"""
+from collections import namedtuple
+
+
+class Issue(
+    namedtuple("Issue", ("title", "body", "number", "created_at", "author"))
+):
+    def __new__(cls, title, body, number=None, created_at=None, author=None):
+        return super().__new__(cls, title, body, number, created_at, author)
+
+
+Args = namedtuple(
+    "Args",
+    (
+        "subparser",
+        "org_name",
+        "github_base_url",
+        "user",
+        "master_repo_urls",
+        "master_repo_names",
+        "students",
+        "issue",
+        "title_regex",
+        "traceback",
+        "state",
+        "show_body",
+        "author",
+        "num_reviews",
+        "master_org_name",
+        "token",
+    ),
+)
+Args.__new__.__defaults__ = (None,) * len(Args._fields)
+
+Team = namedtuple("Team", ("name", "members", "id"))
+
+
+class Repo(
+    namedtuple("Repo", ("name", "description", "private", "team_id", "url"))
+):
+    def __new__(cls, name, description, private, team_id=None, url=None):
+        return super().__new__(cls, name, description, private, team_id, url)
+
+
+Review = namedtuple("Review", ["repo", "done"])
+
+Deprecation = namedtuple("Deprecation", ["replacement", "remove_by"])
