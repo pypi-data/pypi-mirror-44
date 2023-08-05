62530381f3ba1733aa2a8bc4acdec04e01e961e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:01 +0000

    Add package libmediainfo-cffi/1.1.0/libmediainfo_cffi-1.1.0.tar.gz

---
+++ packages/libmediainfo-cffi/libmediainfo_cffi-1.1.0.tar.gz/libmediainfo_cffi-1.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='libmediainfo_cffi',
+    version='1.1.0',
+    author='Alessandro Cerruti',
+    author_email='thereap3r97@gmail.com',
+    description='CFFI interface for libmediainfo',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/strychnide/libmediainfo_cffi',
+    license='MIT',
+    project_urls={
+        'Source': 'https://github.com/strychnide/libmediainfo_cffi',
+        'Issues': 'https://github.com/strychnide/libmediainfo_cffi/issues'
+    },
+    python_requires='>=3.6',
+    setup_requires=['cffi'],
+    cffi_modules=['libmediainfo_cffi/_cffi.py:ffibuilder'],
+    install_requires=[
+        'cffi'
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'flake8-sorted-keys',
+            'flake8-import-order',
+            'flake8-quotes',
+            'flake8-bugbear',
+            'coverage',
+        ]
+    },
+    packages=find_packages(exclude=['*_cffi.py'])
+)
