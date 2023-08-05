a2e5a07dd16d0cdb083552688d61dc653b956a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package keyword2cmdline/1.2.0/keyword2cmdline-1.2.0.tar.gz

---
+++ packages/keyword2cmdline/keyword2cmdline-1.2.0.tar.gz/keyword2cmdline-1.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='keyword2cmdline',
+      description='Keyword arguments of a function as command-line arguments',
+      author='Vikas Dhiman',
+      url='https://github.com/wecacuee/keyword2cmdline',
+      author_email='dhiman@umich.edu',
+      version='1.2.0',
+      license='MIT',
+      classifiers=(
+          'Development Status :: 3 - Alpha',
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ),
+      python_requires='>=3.5',
+      py_modules=['keyword2cmdline'],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"])
