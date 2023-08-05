283dc5b20340948784c7f8ac8e95e4e56f254ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package gumo-core/0.0.1/gumo-core-0.0.1.tar.gz

---
+++ packages/gumo-core/gumo-core-0.0.1.tar.gz/gumo-core-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import os
+import setuptools
+
+
+name = 'gumo-core'
+with open(os.path.join('..', 'gumo_version.txt'), 'r') as fh:
+    version = fh.read().strip()
+
+description = 'Gumo Core Library'
+dependencies = [
+    'pyyaml >= 5.1',
+    'injector >= 0.13.1',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = setuptools.find_packages()
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Gumo Project Team",
+    author_email="gumo-organizer@levii.co.jp",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gumo-py/gumo",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=dependencies,
+)
