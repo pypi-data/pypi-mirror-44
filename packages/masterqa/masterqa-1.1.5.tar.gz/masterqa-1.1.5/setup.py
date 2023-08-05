fe111e09b313ea563975a1d918a146b77c89bc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:26 +0000

    Add package masterqa/1.1.5/masterqa-1.1.5.tar.gz

---
+++ packages/masterqa/masterqa-1.1.5.tar.gz/masterqa-1.1.5/setup.py
@@ -0,0 +1,36 @@
+"""
+The setup package to install MasterQA requirements
+"""
+
+from setuptools import setup, find_packages  # noqa
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+long_description = None
+try:
+    with open(path.join(this_directory, 'README.md'), 'rb') as f:
+        long_description = f.read().decode('utf-8')
+except IOError:
+    long_description = (
+        'Automation-Assisted Manual Testing - http://masterqa.com')
+
+setup(
+    name='masterqa',
+    version='1.1.5',
+    description='Automation-Assisted Manual Testing - http://masterqa.com',
+    long_description=long_description,
+    platforms='Mac * Windows * Linux',
+    url='http://masterqa.com',
+    author='Michael Mintz',
+    author_email='mdmintz@gmail.com',
+    maintainer='Michael Mintz',
+    license='The MIT License',
+    install_requires=[
+        'seleniumbase',
+        ],
+    packages=['masterqa'],
+    entry_points={
+        'nose.plugins': []
+        }
+    )
