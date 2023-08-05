cbf69d2daa89ff501e67c3aab72c6d09b7ee78d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package georss-tfs-incidents-client/0.1/georss_tfs_incidents_client-0.1.tar.gz

---
+++ packages/georss-tfs-incidents-client/georss_tfs_incidents_client-0.1.tar.gz/georss_tfs_incidents_client-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    'georss_client>=0.8',
+]
+
+setup(
+    name="georss_tfs_incidents_client",
+    version="0.1",
+    author="Malte Franken",
+    author_email="coding@subspace.de",
+    description="A GeoRSS client library for the Tasmania Fire Service Incidents feed.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exxamalte/python-georss-tfs-incidents-client",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
