a25c283e6a164372428a2fa4d5aa13a17a0b04b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package invokust/0.5/invokust-0.5.tar.gz

---
+++ packages/invokust/invokust-0.5.tar.gz/invokust-0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name = 'invokust',
+    version = '0.5',
+    author = 'Max Williams',
+    author_email = 'futuresharks@gmail.com',
+    url = 'https://github.com/FutureSharks/invokust',
+    download_url  =  'https://github.com/FutureSharks/invokust/archive/0.41.tar.gz',
+    license = 'GPLv2',
+    description = 'A small wrapper for locust to allow load testing on AWS Lambda',
+    scripts = ['invokr.py'],
+    packages = [
+        'invokust',
+        'invokust.aws_lambda',
+    ],
+    install_requires = [
+        'locustio==0.9.0',
+        'boto3',
+        'pyzmq',
+        'numpy'
+    ],
+    keywords = ['testing', 'loadtest', 'lamba', 'locust'],
+    classifiers = [
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Testing :: Traffic Generation',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
