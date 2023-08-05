4195e964d1f3b1d0df9356e909da2c0bd5d03fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:53 +0000

    Add package kirlent-sphinx/0.3.0/kirlent_sphinx-0.3.0-py3-none-any.whl

---
+++ packages/kirlent-sphinx/kirlent_sphinx-0.3.0-py3-none-any.whl/kirlent_sphinx/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2019 H. Turgut Uyar <uyar@tekir.org>
+
+from pathlib import Path
+
+from . import slides, tables
+
+
+__version__ = "0.3.0"  # sig: str
+
+
+_template_path = Path(Path(__file__).parent, "templates")
+
+
+def get_path():
+    """Get the base path for templates."""
+    return str(_template_path)
+
+
+def setup(app):
+    """Register this extension with Sphinx."""
+    tables.setup(app)
+    slides.setup(app)
+    app.add_html_theme("kirlent_revealjs", str(Path(_template_path, "kirlent_revealjs")))
+    app.add_html_theme("kirlent_impressjs", str(Path(_template_path, "kirlent_impressjs")))
+    return {"version": __version__}
