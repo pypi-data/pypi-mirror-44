541c18e9d20be7deb8e52b699bf6b42b7f8810cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package hdx-python-api/3.5.8/hdx-python-api-3.5.8.tar.gz

---
+++ packages/hdx-python-api/hdx-python-api-3.5.8.tar.gz/hdx-python-api-3.5.8/tests/hdx/facades/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+from hdx.hdx_configuration import Configuration
+
+
+class TestResult:
+    actual_result = None
+
+
+testresult = TestResult()
+
+
+def my_testfn():
+    testresult.actual_result = Configuration.read().get_hdx_site_url()
+
+
+def my_testkeyfn():
+    testresult.actual_result = Configuration.read().get_api_key()
+
+
+def my_testuafn():
+    testresult.actual_result = Configuration.read().user_agent
+
+
+def my_excfn():
+    testresult.actual_result = Configuration.read().get_hdx_site_url()
+    raise ValueError('Some failure!')
