8cd9b4590be417d462de648e4a4206b9e65fea00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:01 +0000

    Add package eevee/0.0.1a0/eevee-0.0.1a0.tar.gz

---
+++ packages/eevee/eevee-0.0.1a0.tar.gz/eevee-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="eevee",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Eevee Discord Bot Framework",
+    packages=['eevee'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
