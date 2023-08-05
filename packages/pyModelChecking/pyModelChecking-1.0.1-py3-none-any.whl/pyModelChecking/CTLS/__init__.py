dde3391ca36a0d20690cc3f8d857405c8cceaba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:39 +0000

    Add package pyModelChecking/1.0.1/pyModelChecking-1.0.1-py3-none-any.whl

---
+++ packages/pyModelChecking/pyModelChecking-1.0.1-py3-none-any.whl/pyModelChecking/CTLS/__init__.py
@@ -0,0 +1,12 @@
+"""
+.. module:: CTLS
+   :synopsis: Represents the CTL* language and provides model
+              checking methods for it.
+
+.. moduleauthor:: Alberto Casagrande <acasagrande@units.it>
+"""
+
+from .language import *
+from .model_checking import modelcheck
+
+from ..language import LNot
