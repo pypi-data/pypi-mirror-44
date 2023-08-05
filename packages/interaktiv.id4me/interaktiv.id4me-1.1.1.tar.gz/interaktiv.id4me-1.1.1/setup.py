423f5a257c3f73ff3474db9525fcf7bfd03337e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package interaktiv.id4me/1.1.1/interaktiv.id4me-1.1.1.tar.gz

---
+++ packages/interaktiv.id4me/interaktiv.id4me-1.1.1.tar.gz/interaktiv.id4me-1.1.1/setup.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+version = '1.1.1'
+
+long_description = (
+    open('README.rst').read()
+)
+
+setup(
+    name='interaktiv.id4me',
+    version=version,
+    description="ID4Me Plugin for Plone 5.x",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        'Framework :: Plone :: 5.1',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Thomas Kastenholz',
+    author_email='kastenholz@interaktiv.de',
+    url='https://github.com/interaktivgmbh/interaktiv.id4me',
+    license='GPL version 2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['interaktiv', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'unittest2',
+        'plone.testing',
+        'plone.app.testing',
+        'Products.BeakerSessionDataManager',
+        'id4me-rp-client'
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+        ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """
+)
