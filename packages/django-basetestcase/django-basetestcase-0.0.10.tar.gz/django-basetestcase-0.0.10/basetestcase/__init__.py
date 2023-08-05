6e28cb48c74c8342f761e0845b9eb0b7a3f445fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:58 +0000

    Add package django-basetestcase/0.0.10/django-basetestcase-0.0.10.tar.gz

---
+++ packages/django-basetestcase/django-basetestcase-0.0.10.tar.gz/django-basetestcase-0.0.10/basetestcase/__init__.py
@@ -0,0 +1,9 @@
+name = 'basetestcase'
+
+__version__ = '0.0.10'
+
+from .base_FormTestCase import *
+from .base_FunctionalTestCase import *
+from .base_ModelTestCase import *
+from .base_ViewTestCase import *
+from .base_UtilityTestCase import *
