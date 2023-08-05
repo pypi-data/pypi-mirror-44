48489990a7d0f54122871b365c359a7c3337cea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package qqmusicbox/0.1/qqmusicbox-0.1.tar.gz

---
+++ packages/qqmusicbox/qqmusicbox-0.1.tar.gz/qqmusicbox-0.1/qqmusicbox/__version__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+qqmusicbox/__version__.py was created on 2019/03/31.
+file in :relativeFile
+Author: Charles_Lai
+Email: lai.bluejay@gmail.com
+"""
+__title__ = "QQMUSIC-MusicBox"
+__version__ = "0.1"
+
