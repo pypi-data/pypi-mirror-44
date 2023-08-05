c0b4e8408237c854d8b66f6fc498a900ab83f9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-glaceon/0.0.1a0/eevee-glaceon-0.0.1a0.tar.gz

---
+++ packages/eevee-glaceon/eevee-glaceon-0.0.1a0.tar.gz/eevee-glaceon-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="eevee-glaceon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Glaceon Extension for the Eevee Discord Framework",
+    packages=['eevee.ext.glaceon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
