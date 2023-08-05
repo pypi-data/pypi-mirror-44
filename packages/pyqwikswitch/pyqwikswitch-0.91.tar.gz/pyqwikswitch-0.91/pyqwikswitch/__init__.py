9061432f279d7d4c6041eeeb562b174799bf6805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package pyqwikswitch/0.91/pyqwikswitch-0.91.tar.gz

---
+++ packages/pyqwikswitch/pyqwikswitch-0.91.tar.gz/pyqwikswitch-0.91/pyqwikswitch/__init__.py
@@ -0,0 +1,12 @@
+"""QwikSwitch USB Modem library for Python.
+
+See: http://www.qwikswitch.co.za/qs-usb.php
+
+Currently supports relays, buttons and LED dimmers
+
+Source: http://www.github.com/kellerza/pyqwikswitch
+"""
+
+from pyqwikswitch.qwikswitch import async_, threaded  # pylint: disable=wildcard-import
+
+__all__ = ["async_", "threaded"]
