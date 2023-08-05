5fc563b2fc439fde05649d2d21928459c08d87b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package polyaxon-cli/0.4.3rc0/polyaxon_cli-0.4.3rc0-py2.py3-none-any.whl

---
+++ packages/polyaxon-cli/polyaxon_cli-0.4.3rc0-py2.py3-none-any.whl/polyaxon_cli/schemas/experiment.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from polyaxon_schemas.api.experiment import ExperimentConfig  # noqa
+from polyaxon_schemas.api.experiment import ExperimentJobConfig  # noqa
---
+++ packages/polyaxon-cli/polyaxon_cli-0.4.3rc0-py2.py3-none-any.whl/polyaxon_cli/schemas/global_configuration.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from marshmallow import fields
+
+from polyaxon_cli.schemas.base import BaseConfig, BaseSchema
+
+
+class GlobalConfigurationSchema(BaseSchema):
+    verbose = fields.Bool(allow_none=True)
+    host = fields.Str(allow_none=True)
+    http_port = fields.Str(allow_none=True)
+    ws_port = fields.Str(allow_none=True)
+    use_https = fields.Bool(allow_none=True)
+    verify_ssl = fields.Bool(allow_none=True)
+
+    @staticmethod
+    def schema_config():
+        return GlobalConfigurationConfig
+
+
+class GlobalConfigurationConfig(BaseConfig):
+    SCHEMA = GlobalConfigurationSchema
+    IDENTIFIER = 'global'
+
+    def __init__(self,
+                 verbose=False,
+                 host='localhost',
+                 http_port=80,
+                 ws_port=80,
+                 use_https=False,
+                 verify_ssl=None):
+        self.verbose = verbose
+        self.host = host
+        self.http_port = str(http_port)
+        self.ws_port = str(ws_port)
+        self.use_https = use_https
+        self.verify_ssl = verify_ssl
