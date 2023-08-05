92f634a86264db6e2cbc3a3b388bf1bf17fa04e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:41 +0000

    Add package edenchain-client-sdk/0.1.4/edenchain-client-sdk-0.1.4.tar.gz

---
+++ packages/edenchain-client-sdk/edenchain-client-sdk-0.1.4.tar.gz/edenchain-client-sdk-0.1.4/eden_client_api/config.py
@@ -0,0 +1,22 @@
+"""
+    Edenchain configuration related
+"""
+import os
+
+class EdenConfig:
+    def __init__(self):
+        self.apis=[
+            {
+            },
+            {
+                'api_key': 'AIzaSyCxBr_r3Q7d2letGIezVoO0cah0TtdZSeA',
+                'api_end_point': 'https://api-ep-br.edenchain.io/api'
+            }
+        ]
+
+
+    def getConfig(self, network):
+        if len(self.apis) <= network or network < 0:
+            return False,{}
+            
+        return True, self.apis[network] 
