ecfcce1bad32b09c73b2e114ea32405b0617df70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package elit/0.1.29.dev1553713736/elit-0.1.29.dev1553713736.tar.gz

---
+++ packages/elit/elit-0.1.29.dev1553713736.tar.gz/elit-0.1.29.dev1553713736/elit/tests/conftest.py
@@ -0,0 +1,50 @@
+# ========================================================================
+# Copyright 2018 ELIT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+import os
+
+import pytest
+
+from elit.util.reader import tsv_reader, json_reader
+from elit.tokenizer import Tokenizer, SpaceTokenizer, EnglishTokenizer
+
+__author__ = "Gary Lai"
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+@pytest.fixture()
+def tsv_reader():
+    return tsv_reader
+
+
+@pytest.fixture()
+def json_reader():
+    return json_reader
+
+
+@pytest.fixture()
+def tokenizer():
+    return Tokenizer()
+
+
+@pytest.fixture()
+def space_tokenizer():
+    return SpaceTokenizer()
+
+
+@pytest.fixture()
+def english_tokenizer():
+    return EnglishTokenizer()
