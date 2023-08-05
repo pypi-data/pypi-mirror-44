666e6201926f2e2fc0b3aafcad473046a6669d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package kirjava/0.1.1/kirjava-0.1.1-py3-none-any.whl

---
+++ packages/kirjava/kirjava-0.1.1-py3-none-any.whl/kirjava.py
@@ -0,0 +1,74 @@
+"""The kirjava library."""
+
+__version__ = "0.1.1"
+__author__ = "Sam Ireland"
+
+import requests
+import json
+
+class Client:
+    """A GraphQL client. This is the object which sends requests to the GraphQL
+    server.
+
+    The URL that serves the GraphQL content is given on creating the Client.
+
+    :param str url: The URL of the GraphQL server to interact with."""
+
+    def __init__(self, url):
+        self._url = url
+        self._headers = {
+         "Accept": "application/json", "Content-Type": "application/json"
+        }
+        self._history = []
+
+
+    def __repr__(self):
+        return f"<Client (URL: {self._url})>"
+
+
+    @property
+    def url(self):
+        """The URL of the GraphQL server to interact with.
+
+        :rtype: ``str``"""
+
+        return self._url
+
+
+    @property
+    def headers(self):
+        """The HTTP headers that will be sent with every request.
+
+        :rtype: ``dict``"""
+
+        return self._headers
+
+
+    @property
+    def history(self):
+        """The queries sent, most recent first.
+
+        :rtype: ``tuple``"""
+
+        return tuple(self._history)
+
+
+    def execute(self, message, method="POST", variables=None):
+        """Sends a request to the GraphQL server.
+
+        :param str message: The query to make.
+        :param str method: By default, POST requests are sent, but this can be\
+        overriden here.
+        :param dict variables: Any GraphQL variables can be passed here.
+        :rtype: ``dict``"""
+
+        data = {"query": message}
+        if variables: data["variables"] = variables
+        resp = requests.request(
+         method, self._url, headers=self._headers, data=json.dumps(data)
+        )
+        result = resp.json()
+        self._history.insert(0, (
+         {"string": message, "variables": variables or {}}, result
+        ))
+        return result
