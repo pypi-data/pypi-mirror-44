ecfb6398a451033b88ad030584828ce94dc52392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package edc-metadata/0.2.33/edc_metadata-0.2.33-py3-none-any.whl

---
+++ packages/edc-metadata/edc_metadata-0.2.33-py3-none-any.whl/edc_metadata/views/home_view.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic.base import TemplateView
+
+from edc_base.view_mixins import EdcBaseViewMixin
+
+
+class HomeView(EdcBaseViewMixin, TemplateView):
+
+    template_name = f"edc_metadata/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
