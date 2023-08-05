21ea75a157a6ec2e421ebad8d3a971f3787c5a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package bert-deid/0.1.1/bert-deid-0.1.1.tar.gz

---
+++ packages/bert-deid/bert-deid-0.1.1.tar.gz/bert-deid-0.1.1/README.md
@@ -0,0 +1,3 @@
+# bert-deid is a package for finding personal identifiers
+
+bert-deid is a Python package for removing personal identifiers from a clinical database. Available mid-2019.
---
+++ packages/bert-deid/bert-deid-0.1.1.tar.gz/bert-deid-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='bert-deid',
+      version='0.1.1',
+      description='Remove identifiers from data with BERT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/MIT-LCP/pydeid',
+      keywords='deid pydeid deidentification de-identification EHR',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[]
+    )
