d07be8556ba278304d7b57bdc3f16844a00d5520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:47 +0000

    Add package termlink/0.2.1/termlink-0.2.1-py3-none-any.whl

---
+++ packages/termlink/termlink-0.2.1-py3-none-any.whl/termlink/__init__.py
@@ -0,0 +1,10 @@
+"""Configures project for PyPI publication"""
+
+import json
+
+_PACKAGE_FILE = '../package.json'
+
+with open(_PACKAGE_FILE, 'r') as file:
+    package = json.load(file)
+
+name = package['name']
