eb71eb2de1699a76561debec324a3ad274488b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package ais-dom/0.90.5/ais-dom-0.90.5.tar.gz

---
+++ packages/ais-dom/ais-dom-0.90.5.tar.gz/ais-dom-0.90.5/homeassistant/components/ais_host/__init__.py
@@ -0,0 +1,16 @@
+"""Support for the definition of AIS hostname."""
+
+
+async def async_setup(hass, config):
+    """Set up if necessary."""
+    return True
+
+
+async def async_setup_entry(hass, config_entry):
+    """Set up ais host as config entry."""
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    return True
