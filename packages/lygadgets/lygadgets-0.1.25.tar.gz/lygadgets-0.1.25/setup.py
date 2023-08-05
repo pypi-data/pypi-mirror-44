1f9498b1bb98d1a7d7f3e96ce11360a11a1a3ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package lygadgets/0.1.25/lygadgets-0.1.25.tar.gz

---
+++ packages/lygadgets/lygadgets-0.1.25.tar.gz/lygadgets-0.1.25/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', 'r') as fx:
+      return fx.read()
+
+
+setup(name='lygadgets',
+      version='0.1.25',
+      description='Tools to make klayout, the standalone, and python environments work better together',
+      long_description=readme(),
+      author='Alex Tait',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lygadgets'],
+      install_requires=['future', 'xmltodict'],
+      package_data={'': ['*.lym']},
+      include_package_data=True,
+      entry_points={'console_scripts': ['lygadgets_link=lygadgets.command_line:cm_link_any']},
+      )
