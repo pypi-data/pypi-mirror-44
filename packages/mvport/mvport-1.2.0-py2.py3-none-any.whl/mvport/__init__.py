8cf6c60a551c3ff7e9c03a64a1390be8ee6fcb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:23 +0000

    Add package mvport/1.2.0/mvport-1.2.0-py2.py3-none-any.whl

---
+++ packages/mvport/mvport-1.2.0-py2.py3-none-any.whl/mvport/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Mean Variance Portfolio."""
+
+__author__ = """Rafael Lopes Conde dos Reis"""
+__email__ = 'rafael.lcreis@gmail.com'
+__version__ = '1.2.0'
+
+from .stock import Stock
+from .portfolio import Portfolio
