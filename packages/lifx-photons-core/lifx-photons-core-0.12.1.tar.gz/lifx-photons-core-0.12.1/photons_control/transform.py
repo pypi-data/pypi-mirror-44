7b6b69493bc519993ba2bfcced8ffa9f4c01cd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:01 +0000

    Add package lifx-photons-core/0.12.1/lifx-photons-core-0.12.1.tar.gz

---
+++ packages/lifx-photons-core/lifx-photons-core-0.12.1.tar.gz/lifx-photons-core-0.12.1/photons_control/transform.py
@@ -0,0 +1,145 @@
+"""
+.. autoclass:: photons_control.transform.Transformer
+"""
+from photons_control.script import Decider, Pipeline
+
+from photons_app.errors import PhotonsAppError
+from photons_app.actions import an_action
+
+from photons_messages import LightMessages, DeviceMessages
+from photons_colour import Parser
+
+from input_algorithms import spec_base as sb
+
+class Transformer(object):
+    """
+    This is responsible for creating the messages to send to a device for a
+    transformation
+
+    .. code-block:: python
+
+        msg = Transformer.using({"power": "on", "color": "red"}, keep_brightness=False)
+        async for info in target.script(msg).run_with(references):
+            ...
+
+    If there are no color related attributes specified then we just generate a
+    message to set the power on or off as specified.
+
+    If we have ``keep_brightness=True`` or ``power=on``; and ``color`` options
+    then we first ask the devices what their current colour is and use that to
+    make the transition to the new state smooth. Essentially when turning a
+    light on from off we first set it's brightness to 0 in the new color, then
+    turn the light on, then turn the brightness up to the desired level. The
+    ``keep_brightness`` option will ensure the light stays the same brightness
+    regardless of the ``color`` options.
+
+    For the color options we use ``photons_colour.Parser`` to create the
+    ``SetWaveformOptional`` message that is needed to change the device. This
+    means that we support an ``effect`` option for setting different waveforms.
+    """
+    @classmethod
+    def using(kls, state, keep_brightness=False):
+        transformer = kls()
+        has_color_options = transformer.has_color_options(state)
+
+        if "power" not in state and not has_color_options:
+            return []
+
+        if keep_brightness or (state.get("power") == "on" and has_color_options):
+            return transformer.transition(state, keep_brightness=keep_brightness)
+
+        msgs = []
+        if "power" in state:
+            msgs.append(transformer.power_message(state))
+
+        if has_color_options:
+            msgs.append(transformer.color_message(state))
+
+        if len(msgs) == 1:
+            return msgs[0]
+
+        return Pipeline(*msgs)
+
+    def has_color_options(self, state):
+        return any(k in state for k in ["hue", "brightness", "saturation", "kelvin", "color"])
+
+    def power_message(self, state):
+        power_level = 65535 if state["power"] == "on" else 0
+
+        if state.get("duration") in (sb.NotSpecified, "", 0, None):
+            return DeviceMessages.SetPower(level=power_level, res_required=False)
+        else:
+            return LightMessages.SetLightPower(level=power_level, duration=state["duration"], res_required=False)
+
+    def color_message(self, state):
+        msg = Parser.color_to_msg(state.get("color", None), overrides=state)
+        msg.res_required = False
+        return msg
+
+    def transition(self, state, keep_brightness=False):
+        def receiver(reference, *states):
+            if not states:
+                return
+
+            current_state = states[0].as_dict()["payload"]
+            power_message = None if "power" not in state else self.power_message(state)
+
+            msg_dict = dict(state)
+            h, s, b, k = Parser.hsbk(state.get("color", None), overrides=state)
+            msg_dict.update({"hue": h, "saturation": s, "brightness": b, "kelvin": k})
+
+            final_overrides = dict(msg_dict)
+
+            pipeline = []
+
+            reset = False
+
+            now_off = current_state["power"] in (None, 0)
+
+            if now_off:
+                overrides = dict(msg_dict)
+                overrides["brightness"] = 0
+                if "duration" in overrides:
+                    del overrides["duration"]
+                msg = Parser.color_to_msg(None, overrides=overrides)
+                msg.target = reference
+                msg.ack_required = True
+                msg.res_required = False
+                reset = True
+                pipeline.append(msg)
+
+            if power_message is not None:
+                want_off = power_message.level == 0
+
+                if now_off ^ want_off:
+                    power_message.target = reference
+                    pipeline.append(power_message)
+
+            if keep_brightness or (reset and "brightness" not in state):
+                final_overrides["brightness"] = current_state["brightness"]
+
+            msg = Parser.color_to_msg(None, overrides=final_overrides)
+            msg.target = reference
+            msg.ack_required = True
+            msg.res_required = False
+            pipeline.append(msg)
+
+            yield Pipeline(*pipeline)
+
+        getter = LightMessages.GetColor(ack_required=False, res_required=True)
+        return Decider(getter, receiver, [LightMessages.LightState])
+
+@an_action(needs_target=True, special_reference=True)
+async def transform(collector, target, reference, **kwargs):
+    """
+    Do a http-api like transformation over whatever target you specify
+
+    ``target:transform d073d5000000 -- '{"color": "red", "effect": "pulse"}'``
+
+    It takes in ``color``, ``effect``, ``power`` and valid options for a
+    ``SetWaveformOptional``.
+    """
+    msg = Transformer.using(collector.configuration["photons_app"].extra_as_json)
+    if not msg:
+        raise PhotonsAppError('Please specify valid options after --. For example ``transform -- \'{"power": "on", "color": "red"}\'``')
+    await target.script(msg).run_with_all(reference)
---
+++ packages/lifx-photons-core/lifx-photons-core-0.12.1.tar.gz/lifx-photons-core-0.12.1/photons_socket/fake.py
@@ -0,0 +1,222 @@
+"""
+Test helpers for creating fake devices that talk over the lan
+
+Usage looks like:
+
+.. code-block:: python
+
+    from photons_socket.fake import FakeDevice, MemorySocketTarget
+    from photons_messages import protocol_register, DeviceMessages
+
+    import asyncio
+
+    class MyDevice(FakeDevice):
+        def make_response(self, pkt, protocol):
+            if pkt | DeviceMessages.GetGroup:
+                return DeviceMessages.StateGroup(group="123", label="one", updated_at=1)
+
+    final_future = asyncio.Future()
+    options = {
+          "final_future": final_future
+        , "protocol_register": protocol_register
+        }
+    target = MemorySocketTarget.create(options)
+
+    device1 = MyDevice("d073d5000001", protocol_register)
+    await device1.start()
+    target.add_device(device1)
+
+    try:
+        async for pkt, _, _ in target.script(DeviceMessages.GetGroup()).run_with("d073d5000001"):
+            # pkt should be that StateGroup
+            print(pkt)
+    finally:
+        for device in target.devices.values():
+            await device.finish()
+
+If you want a device to not appear when the target finds devices then
+set ``device.online = False``.
+"""
+from photons_socket.target import SocketTarget, SocketBridge
+
+from photons_app.errors import FoundNoDevices
+from photons_app import helpers as hp
+
+from photons_transport.target.retry_options import RetryOptions
+from photons_messages import Services, CoreMessages
+from photons_protocol.messages import Messages
+
+from contextlib import contextmanager
+import binascii
+import logging
+import asyncio
+import socket
+
+log = logging.getLogger("photons_socket.fake")
+
+class MemorySocketBridge(SocketBridge):
+    class RetryOptions(RetryOptions):
+        timeouts = [(0.2, 0.2)]
+
+    async def _find_specific_serials(self, serials, ignore_lost=False, raise_on_none=False, timeout=60, broadcast=True, **kwargs):
+        res = {}
+
+        broadcast_address = (
+              self.default_broadcast if broadcast is True else broadcast
+            ) or self.default_broadcast
+
+        for target, device in self.transport_target.devices.items():
+            if device.is_reachable(broadcast_address):
+                res[target[:6]] = device.services
+
+        if not ignore_lost:
+            for target in list(self.found):
+                if target not in res:
+                    del self.found[target]
+
+        if raise_on_none and not res:
+            raise FoundNoDevices()
+
+        self.found.update(res)
+        return self.found
+
+class WithDevices(object):
+    def __init__(self, target, devices):
+        self.target = target
+        self.devices = devices
+
+    async def __aenter__(self):
+        for device in self.devices:
+            self.target.add_device(device)
+            await device.start()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        for device in self.devices:
+            await device.finish()
+
+class MemorySocketTarget(SocketTarget):
+    bridge_kls = lambda s: MemorySocketBridge
+
+    def setup(self, *args, **kwargs):
+        super(MemorySocketTarget, self).setup(*args, **kwargs)
+        self.devices = {}
+
+    def add_device(self, device):
+        target = binascii.unhexlify(device.serial)
+        self.devices[target] = device
+
+    def with_devices(self, *devices):
+        return WithDevices(self, devices)
+
+class FakeDevice(object):
+    def __init__(self, serial, protocol_register, port=None):
+        self.serial = serial
+        self.online = False
+        self.chosen_port = port
+        self.protocol_register = protocol_register
+
+    async def start(self):
+        await self.start_services()
+        self.online = True
+
+    async def start_services(self):
+        await self.start_udp()
+        self.services = (
+              set(
+                [ (Services.UDP, ("127.0.0.1", self.port))
+                ]
+              )
+            , "255.255.255.255"
+            )
+
+    async def start_udp(self):
+        class ServerProtocol(asyncio.Protocol):
+            def connection_made(sp, transport):
+                self.udp_transport = transport
+
+            def datagram_received(sp, data, addr):
+                if not self.online:
+                    return
+
+                log.debug(hp.lc("RECV", bts=binascii.hexlify(data).decode(), protocol="udp", serial=self.serial))
+
+                pkt = Messages.unpack(data, self.protocol_register, unknown_ok=True)
+                if pkt.serial not in ("000000000000", self.serial):
+                    return
+
+                ack = self.ack_for(pkt, "udp")
+                if ack:
+                    ack.sequence = pkt.sequence
+                    ack.source = pkt.source
+                    ack.target = self.serial
+                    self.udp_transport.sendto(ack.tobytes(serial=self.serial), addr)
+
+                for res in self.response_for(pkt, "udp"):
+                    res.sequence = pkt.sequence
+                    res.source = pkt.source
+                    res.target = self.serial
+                    self.udp_transport.sendto(res.tobytes(serial=self.serial), addr)
+
+        remote = None
+
+        for i in range(3):
+            port = self.make_port()
+
+            try:
+                remote, _ = await asyncio.get_event_loop().create_datagram_endpoint(ServerProtocol, local_addr=("0.0.0.0", port))
+                break
+            except OSError:
+                if i == 2:
+                    raise
+                await asyncio.sleep(0.1)
+
+        if remote is None:
+            raise Exception("Failed to bind to a udp socket for fake device")
+
+        self.udp_remote = remote
+        self.port = port
+
+    async def finish(self):
+        if hasattr(self, "udp_remote"):
+            self.udp_remote.close()
+
+    def ack_for(self, pkt, protocol):
+        if pkt.ack_required:
+            return CoreMessages.Acknowledgement()
+
+    def do_send_response(self, pkt):
+        return pkt.res_required
+
+    def response_for(self, pkt, protocol):
+        res = self.make_response(pkt, protocol)
+        if res is None or not self.do_send_response(pkt):
+            return
+
+        if type(res) is list:
+            for r in res:
+                yield r
+        else:
+            yield res
+
+    def make_port(self):
+        """Return the port to listen to"""
+        if self.chosen_port is not None:
+            return self.chosen_port
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('0.0.0.0', 0))
+            return s.getsockname()[1]
+
+    def make_response(self, pkt, protocol):
+        raise NotImplementedError()
+
+    def is_reachable(self, broadcast_address):
+        return self.online
+
+    @contextmanager
+    def offline(self):
+        try:
+            self.online = False
+            yield
+        finally:
+            self.online = True
