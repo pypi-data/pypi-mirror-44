6a70bb0f3b082f0a803e5f8e447edda594de43f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package eevee-umbreon/0.0.1a0/eevee-umbreon-0.0.1a0.tar.gz

---
+++ packages/eevee-umbreon/eevee-umbreon-0.0.1a0.tar.gz/eevee-umbreon-0.0.1a0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="eevee-umbreon",
+    version="0.0.1a0",
+    author="Scragly",
+    description="Umbreon Extension for the Eevee Discord Framework",
+    long_description_content_type="text/markdown",
+    packages=['eevee.ext.umbreon'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
