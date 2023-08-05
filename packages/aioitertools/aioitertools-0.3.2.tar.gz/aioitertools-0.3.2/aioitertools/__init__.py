93cbd56eb07855102c1c0abe2445bb6ff279e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package aioitertools/0.3.2/aioitertools-0.3.2.tar.gz

---
+++ packages/aioitertools/aioitertools-0.3.2.tar.gz/aioitertools-0.3.2/aioitertools/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2018 John Reese
+# Licensed under the MIT license
+
+"""
+itertools for AsyncIO and mixed iterables
+"""
+
+__author__ = "John Reese"
+__version__ = "0.3.2"
+
+from .builtins import iter, next, list, set, enumerate, map, sum, zip
+from .itertools import (
+    accumulate,
+    chain,
+    combinations,
+    combinations_with_replacement,
+    compress,
+    count,
+    cycle,
+    dropwhile,
+    filterfalse,
+    groupby,
+    islice,
+    permutations,
+    product,
+    repeat,
+    starmap,
+    takewhile,
+    tee,
+    zip_longest,
+)
---
+++ packages/aioitertools/aioitertools-0.3.2.tar.gz/aioitertools-0.3.2/aioitertools/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2018 John Reese
+# Licensed under the MIT license
+
+from .builtins import BuiltinsTest
+from .itertools import ItertoolsTest
+from .helpers import HelpersTest
