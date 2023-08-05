e11a5df3cad392529c38584cbbbcfb7e0e5e9ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:15 +0000

    Add package jet-bridge/0.2.1/jet_bridge-0.2.1.tar.gz

---
+++ packages/jet-bridge/jet_bridge-0.2.1.tar.gz/jet_bridge-0.2.1/jet_bridge/models/data_types.py
@@ -0,0 +1,9 @@
+
+TEXT = 'CharField'
+BOOLEAN = 'BooleanField'
+INTEGER = 'IntegerField'
+FLOAT = 'FloatField'
+DATE_TIME = 'DateTimeField'
+TIMESTAMP = 'TimeStampField'
+FOREIGN_KEY = 'ForeignKey'
+JSON = 'JSONField'
