8d68c11c298281ff8d9c2cc584dbc2ae0d23e84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package repomate/1.1.2/repomate-1.1.2.tar.gz

---
+++ packages/repomate/repomate-1.1.2.tar.gz/repomate-1.1.2/README.md
@@ -0,0 +1,99 @@
+# Repomate
+[![Build Status](https://travis-ci.com/slarse/repomate.svg)](https://travis-ci.com/slarse/repomate)
+[![Code Coverage](https://codecov.io/gh/slarse/repomate/branch/master/graph/badge.svg)](https://codecov.io/gh/slarse/repomate)
+[![Documentation Status](https://readthedocs.org/projects/repomate/badge/?version=latest)](http://repomate.readthedocs.io/en/latest/)
+[![PyPi Version](https://badge.fury.io/py/repomate.svg)](https://badge.fury.io/py/repomate)
+![Supported Python Versions](https://img.shields.io/badge/python-3.5%2C%203.6%2C%203.7-blue.svg)
+![Supported Platforms](https://img.shields.io/badge/platforms-Linux%2C%20macOS-blue.svg)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![Code Style: Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+## Overview
+Repomate is A CLI tool for administrating large amounts of GitHub
+repositories, geared towards teachers and GitHub Enterprise. Repomate is
+currently being used for the introductory courses in computer science at
+[KTH Royal Technical Institute of Technology](https://www.kth.se/en/eecs). The
+courses have roughly 200 students and several thousands of repositories,
+allowing us to test Repomate at quite a large scale.
+
+### Install
+Repomate is on PyPi, so `python3 -m pip install repomate` should do the trick. See the
+[install instructions](https://repomate.readthedocs.io/en/latest/install.html)
+for more elaborate instructions.
+
+### Getting started
+The best way to get started with Repomate is to head over to the
+[Docs](https://repomate.readthedocs.io/en/latest/), where you (among other
+things) will find the
+[user guide](https://repomate.readthedocs.io/en/latest/userguide.html).
+It covers the use of Repomate's varous commands by way of practical example,
+and should set you on the right path with little effort.
+
+## Why Repomate?
+Repomate was developed at KTH Royal Technical Institute of Technology to help
+teachers and teaching assistants administrate GitHub repositories. It is
+inspired by the old
+[`teachers_pet` tool](https://github.com/education/teachers_pet), with added
+features and a user experience more to our liking. Features range from creating
+student repositories based on master (template) repos, to opening and closing
+issues in bulk, to assigning peer reviews and cloning repos in bulk. Some parts
+of Repomate can be customized using a simple but powerful [plugin
+system](https://github.com/slarse/repomate-plug). For example, the
+[`repomate-junit4` plugin](https://github.com/slarse/repomate-junit4) allows for
+automatically running `JUnit4` test classes on production code in student repos.
+Below is the output from running `repomate -h`, giving brief descriptions of
+each of the main features:
+
+```
+$ repomate -h
+usage: repomate [-h] [-v]
+                {show-config,setup,update,migrate,clone,open-issues,
+                 close-issues,list-issues,assign-reviews,
+                 purge-review-teams,check-reviews,verify-settings}
+                ...
+
+A CLI tool for administering large amounts of git repositories on GitHub
+instances. See the full documentation at https://repomate.readthedocs.io
+
+positional arguments:
+  {show-config,setup,update,migrate,clone,open-issues,
+   close-issues,list-issues,assign-reviews,
+   purge-review-teams,check-reviews,verify-settings}
+    show-config         Show the configuration file
+    setup               Setup student repos.
+    update              Update existing student repos.
+    migrate             Migrate master repositories into the target
+                        organization.
+    clone               Clone student repos.
+    open-issues         Open issues in student repos.
+    close-issues        Close issues in student repos.
+    list-issues         List issues in student repos.
+    assign-reviews      Randomly assign students to peer review each others'
+                        repos.
+    purge-review-teams  Remove all review teams associated with the specified
+                        students and master repos.
+    check-reviews       Fetch all peer review teams for the specified student
+                        repos, and check which assigned reviews have been done
+                        (i.e. which issues have been opened).
+    verify-settings     Verify your settings, such as the base url and the
+                        OAUTH token.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         Display version info
+
+```
+
+## Roadmap
+As of December 17th 2018, Repomate's CLI is a stable release and adheres to
+[Semantic Versioning 2.0.0](https://semver.org/spec/v2.0.0.html). The internals
+of Repomate _do not_ adhere to this versioning, so using Repomate as a library
+is not recommended.
+
+The plugin system is still to be considered in the beta phase, as it has seen
+much less live action use than the rest of the CLI. Features are highly
+unlikely to be cut, but hooks may be modified as new use-cases arise.
+
+## License
+This software is licensed under the MIT License. See the [LICENSE](LICENSE)
+file for specifics.
---
+++ packages/repomate/repomate-1.1.2.tar.gz/repomate-1.1.2/repomate/util.py
@@ -0,0 +1,163 @@
+"""Some general utility functions.
+
+.. module:: util
+    :synopsis: Miscellaneous utility functions that don't really belong
+        anywhere else.
+
+.. moduleauthor:: Simon Larsén
+"""
+import os
+import sys
+import pathlib
+from typing import Iterable, Generator, Union
+from repomate import tuples
+
+
+def validate_non_empty(**kwargs) -> None:
+    r"""Validate that arguments are not empty. Raise ValueError if any argument
+    is empty.
+
+    Args:
+        **kwargs: Mapping on the form {param_name: argument} where param_name
+        is the name of the parameter and argument is the value passed in.
+    """
+    for param_name, argument in kwargs.items():
+        if not argument:
+            raise ValueError("{} must not be empty".format(param_name))
+
+
+def validate_types(**kwargs) -> None:
+    r"""Validate argument types. Raise TypeError if there is a mismatch.
+
+    Args:
+        **kwargs: Mapping on the form {param_name: (argument, expected_type)},
+        where param_name is the name of the parameter, argument is the passed
+        in value and expected type is either a single type, or a tuple of
+        types.
+    """
+    for param_name, (argument, expected_types) in kwargs.items():
+        if not isinstance(argument, expected_types):
+            if isinstance(expected_types, tuple):
+                exp_type_str = " or ".join(
+                    [t.__name__ for t in expected_types]
+                )
+            else:
+                exp_type_str = expected_types.__name__
+            raise TypeError(
+                "{} is of type {.__class__.__name__}, expected {}".format(
+                    param_name, argument, exp_type_str
+                )
+            )
+
+
+def read_issue(issue_path: str) -> tuples.Issue:
+    """Attempt to read an issue from a textfile. The first line of the file
+    is interpreted as the issue's title.
+
+    Args:
+        issue_path: Local path to textfile with an issue.
+    """
+    if not os.path.isfile(issue_path):
+        raise ValueError("{} is not a file".format(issue_path))
+    with open(issue_path, "r", encoding=sys.getdefaultencoding()) as file:
+        return tuples.Issue(file.readline().strip(), file.read())
+
+
+def generate_repo_name(team_name: str, master_repo_name: str) -> str:
+    """Construct a repo name for a team.
+
+    Args:
+        team_name: Name of the associated team.
+        master_repo_name: Name of the template repository.
+    """
+    validate_non_empty(team_name=team_name, master_repo_name=master_repo_name)
+    return "{}-{}".format(team_name, master_repo_name)
+
+
+def generate_repo_names(
+    team_names: Iterable[str], master_repo_names: Iterable[str]
+) -> Iterable[str]:
+    """Construct all combinations of generate_repo_name(team_name,
+    master_repo_name) for the provided team names and master repo names.
+
+    Args:
+        team_names: One or more names of teams.
+        master_repo_names: One or more names of master repositories.
+
+    Returns:
+        a list of repo names for all combinations of team and master repo.
+    """
+    validate_non_empty(
+        team_names=team_names, master_repo_names=master_repo_names
+    )
+    master_repo_names = list(
+        master_repo_names
+    )  # needs to be traversed multiple times
+    return [
+        generate_repo_name(team_name, master_name)
+        for master_name in master_repo_names
+        for team_name in team_names
+    ]
+
+
+def generate_review_team_name(student: str, master_repo_name: str) -> str:
+    """Generate a review team name.
+
+    Args:
+        student: A student username.
+        master_repo_name: Name of a master repository.
+
+    Returns:
+        a review team name for the student repo associated with this master
+        repo and student.
+    """
+    return generate_repo_name(student, master_repo_name) + "-review"
+
+
+def repo_name(repo_url: str) -> str:
+    """Extract the name of the repo from its url.
+
+    Args:
+        repo_url: A url to a repo.
+    """
+    repo_name = repo_url.split("/")[-1]
+    if repo_name.endswith(".git"):
+        return repo_name[:-4]
+    return repo_name
+
+
+def is_git_repo(path: str) -> bool:
+    """Check if a directory has a .git subdirectory.
+
+    Args:
+        path: Path to a local directory.
+    Returns:
+        True if there is a .git subdirectory in the given directory.
+    """
+    return os.path.isdir(path) and ".git" in os.listdir(path)
+
+
+def _ends_with_ext(
+    path: Union[str, pathlib.Path], extensions: Iterable[str]
+) -> bool:
+    _, ext = os.path.splitext(str(path))
+    return ext in extensions
+
+
+def find_files_by_extension(
+    root: Union[str, pathlib.Path], *extensions: str
+) -> Generator[pathlib.Path, None, None]:
+    """Find all files with the given file extensions, starting from root.
+
+    Args:
+        root: The directory to start searching.
+        extensions: One or more file extensions to look for.
+    Returns:
+        a generator that yields a Path objects to the files.
+    """
+    if not extensions:
+        raise ValueError("must provide at least one extension")
+    for cwd, _, files in os.walk(root):
+        for file in files:
+            if _ends_with_ext(file, extensions):
+                yield pathlib.Path(cwd) / file
