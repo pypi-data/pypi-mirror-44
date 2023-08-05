c8c73e60e0ed1418d04d915fc1fcd01b1f08e4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package pyUSIrest/0.2.2/pyUSIrest-0.2.2-py2.py3-none-any.whl

---
+++ packages/pyUSIrest/pyUSIrest-0.2.2-py2.py3-none-any.whl/pyUSIrest/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python EBI submission REST API."""
+
+__author__ = """Paolo Cozzi"""
+__email__ = 'cozzi@ibba.cnr.it'
+__version__ = '0.2.2'
+
+from . import auth
+from . import client
+
+__all__ = ["auth", "client"]
