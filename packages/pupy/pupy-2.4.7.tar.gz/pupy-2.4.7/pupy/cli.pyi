42e232ee6d2ead3d39b6adbe87e42fe15f7dbd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:29 +0000

    Add package pupy/2.4.7/pupy-2.4.7.tar.gz

---
+++ packages/pupy/pupy-2.4.7.tar.gz/pupy-2.4.7/pupy/cli.pyi
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+from typing import Any, Optional
+
+def unescaped_str(arg_str: Any): ...
+
+parser: Any
+
+def main(args: Optional[Any] = ...) -> None: ...
