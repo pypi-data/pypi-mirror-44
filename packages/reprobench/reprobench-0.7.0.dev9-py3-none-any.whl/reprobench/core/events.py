2bf6e0e7c1aa7e67e2012e7d10c7a7502c9c5283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:30 +0000

    Add package reprobench/0.7.0.dev9/reprobench-0.7.0.dev9-py3-none-any.whl

---
+++ packages/reprobench/reprobench-0.7.0.dev9-py3-none-any.whl/reprobench/core/events.py
@@ -0,0 +1,8 @@
+WORKER_JOIN = b"worker:register"
+WORKER_REQUEST = b"worker:request"
+WORKER_DONE = b"worker:done"
+WORKER_LEAVE = b"worker:leave"
+
+RUN_START = b"run:start"
+RUN_STEP = b"run:step"
+RUN_FINISH = b"run:finish"
