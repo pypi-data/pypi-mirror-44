c652f6d0dad6139bef94c8d37f70edb53858a77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:11 +0000

    Add package mavnbclient/0.2/mavnbclient-0.2.tar.gz

---
+++ packages/mavnbclient/mavnbclient-0.2.tar.gz/mavnbclient-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setuptools
+
+setup(name='mavnbclient',
+      version='0.2',
+      description='NB API wrapper for Bot development',
+      url='https://github.com/Manoor00/MavNBClient',
+      author='Sunil Manoor',
+      author_email='sunil.manoor@mavenir.com',
+      license='LGPL',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
