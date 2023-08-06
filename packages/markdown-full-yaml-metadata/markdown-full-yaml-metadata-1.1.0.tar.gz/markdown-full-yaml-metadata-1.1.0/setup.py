ce73843715f976fc37101807f7b816c8978c86af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:29 +0000

    Add package markdown-full-yaml-metadata/1.1.0/markdown-full-yaml-metadata-1.1.0.tar.gz

---
+++ packages/markdown-full-yaml-metadata/markdown-full-yaml-metadata-1.1.0.tar.gz/markdown-full-yaml-metadata-1.1.0/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as _f:
+        return _f.read()
+
+
+setup(
+    author="Nikita Sivakov",
+    author_email="sivakov512@gmail.com",
+    description="YAML metadata extension for Python-Markdown",
+    install_requires=["Markdown~=3.0", "PyYAML>=3.13"],
+    keywords="markdown yaml meta metadata",
+    license="MIT",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    name="markdown-full-yaml-metadata",
+    py_modules=["full_yaml_metadata"],
+    python_requires=">=3.6",
+    version="1.1.0",
+    url="https://github.com/sivakov512/python-markdown-full-yaml-metadata",
+)
