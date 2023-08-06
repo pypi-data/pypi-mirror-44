5b28df9bed0abccc6171eae0e3eb33ff25723a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:17 +0000

    Add package shapeplotter/0.2.1/shapeplotter-0.2.1.tar.gz

---
+++ packages/shapeplotter/shapeplotter-0.2.1.tar.gz/shapeplotter-0.2.1/shapeplotter/shapeplotter.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+
+
+class Plotter:
+    def __init__(self, grid=(1, 1, 1)):
+        self.grid = grid
+        self.wire = []
+        self.fig = 0
+        self.ax = 0
+
+    @staticmethod
+    def __clearplot():
+        plt.clf()
+        plt.cla()
+        plt.close()
+
+    def addwire(self, geom):
+        if geom.geom_type == "Point":
+            raise Exception('Cannot wire plot a Point')
+        elif geom.geom_type == "LineString":
+            x = []
+            y = []
+            for points in list(geom.coords):
+                x.append(points[0])
+                y.append(points[1])
+            self.wire.append([x, y])
+        elif geom.geom_type == "LinearRing":
+            x = []
+            y = []
+            for points in list(geom.coords):
+                x.append(points[0])
+                y.append(points[1])
+            self.wire.append([x, y])
+        elif geom.geom_type == "Polygon":
+            x, y = geom.exterior.xy
+            self.wire.append([x, y])
+        else:
+            for g in geom:
+                self.addwire(g)
+
+    def clear(self):
+        self.wire = []
+
+    def plot(self, restart=True):
+        if restart:
+            self.__clearplot()
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(*self.grid)
+        for element in self.wire:
+            self.ax.plot(element[0], element[1])
+        plt.show()
+        if restart:
+            self.__clearplot()
