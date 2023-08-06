1b2fb1885982903f0d8ed8df8ab71fa678e6ff15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:21 +0000

    Add package analyticalmachine/0.0/analyticalmachine-0.0.tar.gz

---
+++ packages/analyticalmachine/analyticalmachine-0.0.tar.gz/analyticalmachine-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='analyticalmachine',
+      version='0.0',
+      description='Python package for AnalyticalMachine.com',
+      author='Prithvi Kambhampati & Sai Veena Katta',
+      author_email='kambhampati.prithvi@gmail.com',
+      license='MIT',
+      packages=['analyticalmachine'],
+      zip_safe=False)
+
