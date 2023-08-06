08ecf51fb76b6812bb1c68dbc200ed16b1a3f467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:21 +0000

    Add package django-l10n-extensions/1.0.5/django-l10n-extensions-1.0.5.tar.gz

---
+++ packages/django-l10n-extensions/django-l10n-extensions-1.0.5.tar.gz/django-l10n-extensions-1.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-l10n-extensions',
+    version='1.0.5',
+    author=u'Cees van Wieringen',
+    author_email='ceesvw@gmail.com',
+    package_dir={'': 'src'},
+    packages=find_packages(where='./src'),
+    include_package_data=True,
+    install_requires=['GitPython==1.0.1', 'Django>=2', 'polib>=1.0'],
+    url='https://github.com/ceasaro/django-l10n-extensions',
+    license='',
+    description=open('DESCRIPTION').read(),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    key_words=['django', 'l10n', ]
+)
