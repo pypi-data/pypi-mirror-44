4706c4c75f2047fd7af1f0793cf5ebd38fce83c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:25 +0000

    Add package apiron/2.4.0/apiron-2.4.0-py3-none-any.whl

---
+++ packages/apiron/apiron-2.4.0-py3-none-any.whl/tests/service/test_base.py
@@ -0,0 +1,30 @@
+import pytest
+
+from apiron import Service
+
+
+@pytest.fixture(scope='class')
+def service():
+    class SomeService(Service):
+        domain = 'http://foo.com'
+    return SomeService
+
+
+class TestService:
+    def test_get_hosts_returns_domain(self, service):
+        assert ['http://foo.com'] == service.get_hosts()
+
+    def test_str_method_on_class(self, service):
+        assert 'http://foo.com' == str(service)
+
+    def test_str_method_on_instance(self, service):
+        assert 'http://foo.com' == str(service())
+
+    def test_repr_method_on_class(self, service):
+        assert 'SomeService(domain=http://foo.com)' == repr(service)
+
+    def test_repr_method_on_instance(self, service):
+        assert 'SomeService(domain=http://foo.com)' == repr(service())
+
+    def test_required_hosts_returns_dictionary(self, service):
+        assert {} == service.required_headers
