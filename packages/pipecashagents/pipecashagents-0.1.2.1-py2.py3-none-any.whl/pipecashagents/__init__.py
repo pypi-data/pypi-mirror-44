1ae0b562a8f17bca0965409b758deff7feda2b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:30 +0000

    Add package pipecashagents/0.1.2.1/pipecashagents-0.1.2.1-py2.py3-none-any.whl

---
+++ packages/pipecashagents/pipecashagents-0.1.2.1-py2.py3-none-any.whl/pipecashagents/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pipecashagents."""
+
+__author__ = """Aleksandar Nikolaev Dinkov"""
+__email__ = 'alexander.n.dinkov@gmail.com'
+__version__ = '0.1.2.1'
+
+from pipecashagents.Email.Email import *
+from pipecashagents.WalletAgents.WalletAgents import *
+from pipecashagents.EventGates.DifferenceDetection import *
+from pipecashagents.EventGates.EventTimingAgents import *
+from pipecashagents.DataFormats.RSS import *
