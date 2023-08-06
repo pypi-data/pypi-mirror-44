7f007703885d6e5627258225bf2a1b9de7e67571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:42 +0000

    Add package metalearn/0.5.3/metalearn-0.5.3-py3-none-any.whl

---
+++ packages/metalearn/metalearn-0.5.3-py3-none-any.whl/test/config.py
@@ -0,0 +1,9 @@
+"""
+This file is used to configure any shared or global parameters for the test
+suite.
+"""
+
+CORRECTNESS_SEED = 0
+METADATA_PATH = "./test/data/test_dataset_metadata.json"
+METAFEATURES_DIR = "./test/data/dataset_metafeatures/"
+OPENML_COMPARE_RESULTS_DIR = './openmlComparisonResults/'
