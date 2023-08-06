3118b5d6a095fe7229b60d9540aa958e728a11cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:54 +0000

    Add package aud/0.1.2/aud-0.1.2.tar.gz

---
+++ packages/aud/aud-0.1.2.tar.gz/aud-0.1.2/aud/AudLib.py
@@ -0,0 +1,24 @@
+import colorlog
+from os import makedirs
+from os.path import exists
+
+def setupLogger():
+    '''
+    Config and setup the logger
+    '''
+    ### SETUP LOGGER ###
+    logger = colorlog.getLogger()
+    # DEBUG INFO WARNING ERROR CRITICAL #
+    logger.setLevel(colorlog.colorlog.logging.DEBUG)
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter())
+    logger.addHandler(handler)
+    return logger
+
+def createFolder(path):
+    # Create folder if it does not already exist
+    try:
+        if not exists(path):
+            makedirs(path)
+    except OSError:
+        logger.error("Creating directory failed")
