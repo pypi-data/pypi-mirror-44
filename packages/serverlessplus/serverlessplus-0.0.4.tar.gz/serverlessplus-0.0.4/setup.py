43f1c6fb6744dded14e9a74eff604fadd3e9465c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:01 +0000

    Add package serverlessplus/0.0.4/serverlessplus-0.0.4.tar.gz

---
+++ packages/serverlessplus/serverlessplus-0.0.4.tar.gz/serverlessplus-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = 'serverlessplus',
+    packages = ['serverlessplus'],
+    version = '0.0.4',
+    license='Apache-2.0',
+    description = 'serverless your django/flask apps',
+    author = 'chenhengqi',
+    author_email = 'ritchiechen@tencent.com',
+    url = 'https://github.com/serverlessplus/py',
+    keywords = ['serverless', 'scf', 'tencent-cloud', 'django', 'flask'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
