4b3ad34f12432cdaade62e889bf8831c636b3b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:10 +0000

    Add package influxdbpusher/0.1.3/influxdbpusher-0.1.3.tar.gz

---
+++ packages/influxdbpusher/influxdbpusher-0.1.3.tar.gz/influxdbpusher-0.1.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="influxdbpusher",
+    version="0.1.3",
+    url="https://github.com/GambitResearch/influxdbpusher",
+
+    author="Gustavo Carneiro",
+    author_email="gjcarneiro@gmail.com",
+
+    description="Minimal and smart pusher of samples to InfluxDB for asyncio programs",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['aiohttp>=2.0'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
