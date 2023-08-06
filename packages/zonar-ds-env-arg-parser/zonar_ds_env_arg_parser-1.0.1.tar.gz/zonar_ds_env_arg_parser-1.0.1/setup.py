9a0e7078afc64fb0d1699bab20c816197a172c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:28 +0000

    Add package zonar-ds-env-arg-parser/1.0.1/zonar_ds_env_arg_parser-1.0.1.tar.gz

---
+++ packages/zonar-ds-env-arg-parser/zonar_ds_env_arg_parser-1.0.1.tar.gz/zonar_ds_env_arg_parser-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zonar_ds_env_arg_parser",
+    version="1.0.1",
+    author="Zonar Systems - Data Services Pod",
+    author_email="DSPod@zonarsystems.com",
+    keywords="zonar_ds_env_arg_parser",
+    description="An argument helper used to validate environment variables",
+    long_description=long_description,
+    url="https://github.com/evcallia/zonar_ds_env_arg_parser.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
