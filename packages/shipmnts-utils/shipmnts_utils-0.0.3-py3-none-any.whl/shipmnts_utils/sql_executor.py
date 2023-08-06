a5b532a81066a4a9daff965d11aa46b224f7112d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:17 +0000

    Add package shipmnts-utils/0.0.3/shipmnts_utils-0.0.3-py3-none-any.whl

---
+++ packages/shipmnts-utils/shipmnts_utils-0.0.3-py3-none-any.whl/shipmnts_utils/sql_executor.py
@@ -0,0 +1,18 @@
+from django.db import connection
+
+
+def execute_select_query(query, fetch):
+    data = None
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        if fetch == 'one':
+            data = cursor.fetchone()
+        else:
+            data = cursor.fetchall()
+
+    return data
+
+
+def execute_update_query(query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
