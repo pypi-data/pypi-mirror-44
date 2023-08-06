b280c1e7532831f3d0fe345b5b7a837c8d5d3f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:14 +0000

    Add package luckydonald-utils/0.21/luckydonald_utils-0.21-py3.4.egg

---
+++ packages/luckydonald-utils/luckydonald_utils-0.21-py3.4.egg/luckydonaldUtils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'luckydonald'
+
+VERSION = "0.21"  # had a leading zero before version "0.20" (i.e. "0.019").  PyPI always removed that.
+
+
+from sys import version as python_version
+
+py2 = True if python_version < '3' else False
+py3 = not py2
