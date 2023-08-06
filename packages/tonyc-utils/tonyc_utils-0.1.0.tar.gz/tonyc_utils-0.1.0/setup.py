f361c33696a1d0a6ab0ea201e33027440734f232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:28 +0000

    Add package tonyc-utils/0.1.0/tonyc_utils-0.1.0.tar.gz

---
+++ packages/tonyc-utils/tonyc_utils-0.1.0.tar.gz/tonyc_utils-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = "tonyc_utils"
+setup(name=name,
+      version='0.1.0',
+      description='Various utilities -- most notably logging helpers',
+      author='Tony Cebzanov',
+      author_email='tonycpsu@gmail.com',
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers'
+      ],
+      packages=find_packages()
+)
