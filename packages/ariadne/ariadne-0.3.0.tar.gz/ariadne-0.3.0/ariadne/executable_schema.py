0e50f5f0ba07c02f03e0adf3e23982df053a68ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:41 +0000

    Add package ariadne/0.3.0/ariadne-0.3.0.tar.gz

---
+++ packages/ariadne/ariadne-0.3.0.tar.gz/ariadne-0.3.0/ariadne/executable_schema.py
@@ -0,0 +1,27 @@
+from typing import List, Union
+
+from graphql import GraphQLSchema, build_schema
+
+from .types import SchemaBindable
+
+
+def make_executable_schema(
+    type_defs: Union[str, List[str]],
+    bindables: Union[SchemaBindable, List[SchemaBindable], None] = None,
+) -> GraphQLSchema:
+    if isinstance(type_defs, list):
+        type_defs = join_type_defs(type_defs)
+
+    schema = build_schema(type_defs)
+
+    if isinstance(bindables, list):
+        for obj in bindables:
+            obj.bind_to_schema(schema)
+    elif bindables:
+        bindables.bind_to_schema(schema)
+
+    return schema
+
+
+def join_type_defs(type_defs: List[str]) -> str:
+    return "\n\n".join(t.strip() for t in type_defs)
