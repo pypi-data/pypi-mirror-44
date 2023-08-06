ae4aaac2a2da977c0b092e8947f4bbdd4e87f9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package stmpy/0.7.1/stmpy-0.7.1.tar.gz

---
+++ packages/stmpy/stmpy-0.7.1.tar.gz/stmpy-0.7.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name='stmpy',
+  packages=['stmpy'],
+  version='0.7.1',
+  description='Support for simple state machines',
+  author='Frank Alexander Kraemer',
+  author_email='kraemer.frank@gmail.com',
+  license='MIT',
+  url='https://github.com/falkr/stmpy',
+  download_url='https://github.com/falkr/stmpy/archive/0.2.tar.gz',
+  keywords=['state machines', 'stm', 'automata'],
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5']
+)
