b806ecb8b707e851e1d1b87a3f859e8761dbd524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package ckipparser/0.0.0/ckipparser-0.0.0.tar.gz

---
+++ packages/ckipparser/ckipparser-0.0.0.tar.gz/ckipparser-0.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+	name='ckipparser',
+	description='CKIP Parser',
+	long_description='Use `pyckip <https://pypi.org/project/pyckip/>`_ instead.',
+	url='https://pypi.org/project/pyckip/',
+	install_requires=['pyckip'],
+)
