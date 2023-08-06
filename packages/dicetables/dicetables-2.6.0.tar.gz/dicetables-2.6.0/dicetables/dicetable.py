b0e0d0f22e545fe0d09407779ed0697f356a9dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:11 +0000

    Add package dicetables/2.6.0/dicetables-2.6.0.tar.gz

---
+++ packages/dicetables/dicetables-2.6.0.tar.gz/dicetables-2.6.0/dicetables/dicetable.py
@@ -0,0 +1,105 @@
+"""
+DiceTable and DetailedDiceTable compute combination of Die events
+"""
+from __future__ import absolute_import
+
+from dicetables.additiveevents import AdditiveEvents, EventsDictCreator
+from dicetables.eventsinfo import EventsCalculations
+from dicetables.factory.eventsfactory import EventsFactory
+
+
+class DiceTable(AdditiveEvents):
+    def __init__(self, events_dict, dice_record):
+        self._record = dice_record
+        super(DiceTable, self).__init__(events_dict)
+
+    def dice_data(self):
+        return self._record
+
+    def get_list(self):
+        """
+
+        :return: sorted copy of dice list: [(die, number of dice), ...]
+        """
+        return sorted(self._record.get_dict().items())
+
+    def number_of_dice(self, query_die):
+        return self._record.get_number(query_die)
+
+    def weights_info(self):
+        """
+
+        :return: str: complete info for all dice
+        """
+        output = ''
+        for die, number in self.get_list():
+            output += format_die_info(die, number)
+        return output.rstrip('\n')
+
+    def __str__(self):
+        str_list = [die.multiply_str(number) for die, number in self.get_list()]
+        return '\n'.join(str_list)
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        dice = str(self).replace('\n', ', ')
+        return '<{} containing [{}]>'.format(class_name, dice)
+
+    def add_die(self, die, times=1):
+        """
+
+        :param die: any subclass of ProtoDie: Die, ModDie, WeightedDie, ModWeightedDie, Modifier,
+            StrongDie, Exploding, ExplodingOn
+        :param times: int>= 0
+        """
+        dice_data = self._record.add_die(die, times)
+        dictionary = EventsDictCreator(self, die).create_using_combine_by_fastest(times)
+        return EventsFactory.from_dictionary_and_dice(self, dictionary, dice_data)
+
+    def remove_die(self, die, times=1):
+        """
+
+        :param die: any subclass of ProtoDie: Die, ModDie, WeightedDie, ModWeightedDie, Modifier,
+            StrongDie, Exploding, ExplodingOn
+        :param times: 0 <= int <= number of "die" in table
+        """
+        dice_data = self._record.remove_die(die, times)
+        dictionary = EventsDictCreator(self, die).create_using_remove_by_tuple_list(times)
+        return EventsFactory.from_dictionary_and_dice(self, dictionary, dice_data)
+
+    def __eq__(self, other):
+        return super(DiceTable, self).__eq__(other) and self.dice_data() == other.dice_data()
+
+
+def format_die_info(die, number):
+    weight_info = die.weight_info()
+    adjusted_info = weight_info.replace(str(die), die.multiply_str(number)) + '\n\n'
+    return adjusted_info
+
+
+class DetailedDiceTable(DiceTable):
+    def __init__(self, events_dict, dice_record, calc_includes_zeroes=True):
+        super(DetailedDiceTable, self).__init__(events_dict, dice_record)
+        self._calc = EventsCalculations(self, calc_includes_zeroes)
+
+    @property
+    def info(self):
+        return self._calc.info
+
+    @property
+    def calc(self):
+        return self._calc
+
+    @property
+    def calc_includes_zeroes(self):
+        return self._calc.include_zeroes
+
+    def switch_boolean(self):
+        """
+
+        :return: a new DetailedDiceTable with the `calc_includes_zeroes` boolean switched
+        """
+        return EventsFactory.from_params(self, {'calc_includes_zeroes': not self.calc_includes_zeroes})
+
+    def __eq__(self, other):
+        return super(DetailedDiceTable, self).__eq__(other) and self.calc_includes_zeroes == other.calc_includes_zeroes
