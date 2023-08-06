c1f736e92d07a02872c70b1c88bb231a98a16617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package mitopipeline/0.1/MitoPipeline-0.1.tar.gz

---
+++ packages/mitopipeline/MitoPipeline-0.1.tar.gz/MitoPipeline-0.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name="MitoPipeline",
+    version="0.1",
+    packages=['mitopipeline'],
+
+    #add samtools, bwa, etc. here
+    install_requires=[
+        'jinja2'
+    ],
+
+    package_data={},
+
+    #metadata
+    author="Timothy Kuo",
+    author_email="timykuo@gmail.com",
+    description="Mitochondrial Genome Pipeline",
+    license="MIT",
+    keywords="genetics research data pipeline dependency management mitochondrial genome",
+    url="https://github.com/timmykuo/mitopipeline",
+    download_url="",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    project_urls={
+        "Bug Tracker": "",
+        "Documentation": "",
+        "Source Code": "",
+    }
+)
