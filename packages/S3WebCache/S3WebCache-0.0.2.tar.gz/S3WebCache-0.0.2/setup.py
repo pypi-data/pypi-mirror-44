7de068a96ce3ddae0cadaf798c00cc1d8a3e937c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:23 +0000

    Add package S3WebCache/0.0.2/S3WebCache-0.0.2.tar.gz

---
+++ packages/S3WebCache/S3WebCache-0.0.2.tar.gz/S3WebCache-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='S3WebCache',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    author_email="zamechek@wharton.upenn.edu",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
