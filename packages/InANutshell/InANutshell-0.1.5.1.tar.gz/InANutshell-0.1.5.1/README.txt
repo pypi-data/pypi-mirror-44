4b4df93fb9299f07e50a82eb89163f3f175f0feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:09 +0000

    Add package InANutshell/0.1.5.1/InANutshell-0.1.5.1.tar.gz

---
+++ packages/InANutshell/InANutshell-0.1.5.1.tar.gz/InANutshell-0.1.5.1/README.txt
@@ -0,0 +1,10 @@
+=============	
+In a Nutshell	
+=============	
+
+
+In a Nutshell is a quick simple script made to 	
+format your text into a sarcastic spongebob format	
+ie: iN a NuTsHeLl.
+
+to use run the  `nutshell` command in your terminal.
---
+++ packages/InANutshell/InANutshell-0.1.5.1.tar.gz/InANutshell-0.1.5.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+VERSION = '0.1.5.1'
+
+setup(
+    name='InANutshell',
+    version=VERSION,
+    author='Josh Prakke',
+    author_email='joshprakke@gmail.com',
+    url='https://github.com/JPrakke/in-a-nutshell',
+    install_requires=['pyperclip == 1.7.0',],
+    packages=['nutshell',],
+    license='MIT License',
+    description='makes meme text',
+    long_description=open('README.txt').read(),
+    entry_points = {
+        'console_scripts':[
+            'nutshell=nutshell.nutshell:run'
+        ]
+    }
+)
+
+
+# Entry point format is terminal_command_name=python_script_name:main_method_name
+
