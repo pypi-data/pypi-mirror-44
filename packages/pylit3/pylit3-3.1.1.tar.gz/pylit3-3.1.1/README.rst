34761d4fea05555f7a5d79f943d02b954f9b0cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:50 +0000

    Add package pylit3/3.1.1/pylit3-3.1.1.tar.gz

---
+++ packages/pylit3/pylit3-3.1.1.tar.gz/pylit3-3.1.1/README.rst
@@ -0,0 +1,19 @@
+############
+PyLit 3.1
+############
+
+
+See http://slott56.github.io/PyLit-3/index.html for the complete documentation.
+
+The ``Makefile`` is used via ``make html`` to rebuild the code and documentation.
+
+The ``docs`` directory contains the documentation, plus many examples.
+
+The ``pylit.py`` is the module.  Once you've installed it, use ``python -m pylit`` to run it.
+
+The ``test`` directory contains the unit test suite.
+
+The ``contribs`` directory are old contributed modules which have not yet been
+converted to Python3.
+
+The code repository is: https://github.com/slott56/PyLit-3
---
+++ packages/pylit3/pylit3-3.1.1.tar.gz/pylit3-3.1.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ********
+# setup.py
+# ********
+#
+# Literate programming with reStructuredText
+#
+# ::
+
+"""pylit: bidirectional text <-> code converter
+
+Convert between a *text source* with embedded computer code and a *code source*
+with embedded documentation.
+"""
+
+# Requirements for installation::
+
+from setuptools import setup, find_packages
+import pathlib
+
+# Module Definition::
+
+setup(
+      name='pylit3',
+      version='3.1.1',
+      description='Python Literate Programming',
+      long_description=pathlib.Path('README.rst').read_text(),
+      author='S.Lott',
+      author_email='slott56@gmail.com',
+      python_requires=">=3.5",
+      url='https://github.com/slott56/PyLit-3',
+      project_urls={
+            "Documentation": 'https://slott56.github.io/PyLit-3/index.html',
+            "Source Code": 'https://github.com/slott56/PyLit-3',
+            "Bug Tracker": 'https://github.com/slott56/PyLit-3/issues',
+      },
+      py_modules=['pylit'],
+      classifiers=[
+            "Development Status :: 6 - Mature",
+            "Intended Audience :: Developers",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Software Development :: Build Tools",
+      ],
+      license='GNU General Public License (v. 2 or later)',
+     )
+
+# And that's it. This will be installed into the site-packages directory.
+
+# Technically, this isn't necessary.  A simple copy will do.
+# Further, the :envvar:`PYTHONPATH` environment variable can name a directory
+# that has :file:`pylit.py` in it.
