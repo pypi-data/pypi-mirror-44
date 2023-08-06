011bebe8b17d40fc1e492d75948b0944486910c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:36 +0000

    Add package mip/1.0.29/mip-1.0.29.tar.gz

---
+++ packages/mip/mip-1.0.29.tar.gz/mip-1.0.29/mip/constants.py
@@ -0,0 +1,42 @@
+VERSION = '1.0.29'
+
+# epsilon number (practical zero)
+EPS = 10e-6
+
+# infinity representation
+INF = float("inf")
+
+# optimization status
+ERROR = -1
+OPTIMAL = 0
+INFEASIBLE = 1
+UNBOUNDED = 2
+FEASIBLE = 3
+INT_INFEASIBLE = 4
+NO_SOLUTION_FOUND = 5
+LOADED = 6
+CUTOFF = 7
+
+# constraint senses
+EQUAL = "="
+LESS_OR_EQUAL = "<"
+GREATER_OR_EQUAL = ">"
+
+# optimization directions
+MINIMIZE = "MIN"
+MAXIMIZE = "MAX"
+
+# Search emphasis
+FEASIBILITY = 1
+OPTIMALITY = 2
+
+# variable types
+BINARY = "B"
+CONTINUOUS = "C"
+INTEGER = "I"
+
+# solvers
+CBC = "CBC"
+CPLEX = "CPX"
+GUROBI = "GRB"
+SCIP = "SCIP"
