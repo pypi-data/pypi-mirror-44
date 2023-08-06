38a931693b9b288c4aabc81075fce94f3c172d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:30 +0000

    Add package vistir/0.4.0/vistir-0.4.0.tar.gz

---
+++ packages/vistir/vistir-0.4.0.tar.gz/vistir-0.4.0/src/vistir/backports/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from .functools import partialmethod
+from .tempfile import NamedTemporaryFile
+
+__all__ = ["NamedTemporaryFile", "partialmethod"]
