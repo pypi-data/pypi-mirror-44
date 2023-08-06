82df60055c3a30f1d0af0ecd0632c05b329849d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:22 +0000

    Add package openpipe/0.0.114/openpipe-0.0.114-py3-none-any.whl

---
+++ packages/openpipe/openpipe-0.0.114-py3-none-any.whl/openpipe/client/loaders.py
@@ -0,0 +1,15 @@
+from openpipe.client import PipelineLoader
+from os.path import normpath
+
+
+class PipelineFileLoader(PipelineLoader):
+    def get(self, pipeline_name):
+        pipeline_name = normpath(pipeline_name)
+        with open(pipeline_name) as document_file:
+            return document_file.read()
+
+
+class PipelineStringLoader(PipelineLoader):
+    def get(self, pipeline_string):
+        pipeline_string = "string_loader"
+        return pipeline_string
