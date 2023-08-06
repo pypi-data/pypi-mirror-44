9313c7887161fbeda4fad14f68f3db072d885c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:00 +0000

    Add package coc.py/0.0.3/coc.py-0.0.3.tar.gz

---
+++ packages/coc.py/coc.py-0.0.3.tar.gz/coc.py-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+requirements = []
+with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+version = '0.0.3'
+
+readme = ''
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='coc.py',
+      author='mathsman5133',
+      url='https://github.com/mathsman5133/coc.py',
+      packages=['coc'],
+      version=version,
+      license='MIT',
+      description='A python wrapper for the Clash of Clans API',
+      long_description=readme,
+      long_description_content_type="text/x-rst",
+      python_requires='>=3.5.3',
+      install_requires=requirements
+      )
