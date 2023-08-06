a640db5f83dd4a0b19351574d5fbe151c51ca152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:58 +0000

    Add package Homevee-Cloud/0.0.0.15/Homevee-Cloud-0.0.0.15.tar.gz

---
+++ packages/Homevee-Cloud/Homevee-Cloud-0.0.0.15.tar.gz/Homevee-Cloud-0.0.0.15/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Homevee-Cloud",
+    version="0.0.0.15",
+    author="Homevee-Cloud",
+    author_email="support@homevee.de",
+    description="Dein neues Smarthome-System!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/homevee/homevee-cloud",
+    entry_points = {
+        #'console_scripts': ['HomeveeCloud=HomeveeCloud.command_line:main'],
+        'console_scripts': ['HomeveeCloudLegacy=HomeveeCloudLegacy:main'],
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyfcm',
+    ]
+)
+
+#Commands to build
+#sudo apt-get install python3-pip python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev -y
+#pip install --upgrade setuptools wheel twine
