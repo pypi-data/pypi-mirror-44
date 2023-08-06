9696d2291a7485e9e7e12304e3a41cccf57b5ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:05 +0000

    Add package osrs-net/0.0.2/osrs_net-0.0.2.tar.gz

---
+++ packages/osrs-net/osrs_net-0.0.2.tar.gz/osrs_net-0.0.2/osrs_net/util/file_io.py
@@ -0,0 +1,26 @@
+import os
+
+
+def write(string, filepath):
+    try:
+        with open(filepath, 'w') as f:
+            f.write(string)
+        return True
+
+    except Exception as e:
+        print('Failed to write data to file: {}, error: {}'.format(filepath, e))
+
+
+def read(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            return f.read()
+    except Exception as e:
+        print('Failed to read from file: {}, error: {}'.format(filepath, e))
+
+
+def create_directory(directory_path):
+    try:
+        os.makedirs(os.path.dirname(directory_path), exist_ok=True)
+    except Exception as e:
+        print('Failed to create directory: {}, error: {}'.format(directory_path, e))
