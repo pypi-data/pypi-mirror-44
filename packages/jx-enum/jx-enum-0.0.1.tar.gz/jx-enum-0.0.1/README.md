90b33eb9f7d4b7f52b0b0f533bd7ff3c68b18c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:06 +0000

    Add package jx-enum/0.0.1/jx-enum-0.0.1.tar.gz

---
+++ packages/jx-enum/jx-enum-0.0.1.tar.gz/jx-enum-0.0.1/README.md
@@ -0,0 +1,10 @@
+# enum
+
+Python enum implementation
+
+
+## TODO
+
+* Added setup.py
+* Unittest
+* CI
---
+++ packages/jx-enum/jx-enum-0.0.1.tar.gz/jx-enum-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jx-enum",
+    version="0.0.1",
+    author="Xue Jiao",
+    author_email="jiao.xuejiao@gmail.com",
+    description="A small enum package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jiao.xue.libs/enum",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['six'],
+)
