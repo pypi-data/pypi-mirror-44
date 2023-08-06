a3583af91adc593e9636ab6646e2e016142966bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:59 +0000

    Add package snippet-manager/1.0.0/snippet_manager-1.0.0-py3-none-any.whl

---
+++ packages/snippet-manager/snippet_manager-1.0.0-py3-none-any.whl/snippet/meta.py
@@ -0,0 +1,42 @@
+import json
+
+from snippet.serializer import DateTimeDecoder
+from snippet.serializer import DateTimeEncoder
+
+_GREEN = "\033[92m"
+_ENDC = "\033[0m"
+
+
+class Meta:
+    def __init__(self, date, hash, message, count):
+        self.date = date
+        self.hash = hash
+        self.message = message
+        self.count = count
+
+    def update(self, message, count):
+        self.message = message
+        self.count = count
+
+    def __str__(self):
+        return (_GREEN + f"Commit: {self.hash}\n" + _ENDC +
+                f"Number of files: {self.count}\n"
+                f"Date: {self.date:%a %b %d %H:%M:%S %Y}\n\n"
+                f"\t{self.message}\n")
+
+    def to_json(self):
+        return save({
+            "date": self.date,
+            "message": self.message,
+            "count": self.count,
+            "hash": self.hash,
+        })
+
+
+def load(content):
+    meta = json.loads(content, cls=DateTimeDecoder)
+    return Meta(**meta)
+
+
+def save(dictionary):
+    return json.dumps(dictionary, sort_keys=True, cls=DateTimeEncoder)
