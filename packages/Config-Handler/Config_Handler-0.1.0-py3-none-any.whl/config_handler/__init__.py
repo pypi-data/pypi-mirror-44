f5bb28a6b95f18761fd6ca93bf89d8b872ccab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:46 +0000

    Add package Config-Handler/0.1.0/Config_Handler-0.1.0-py3-none-any.whl

---
+++ packages/Config-Handler/Config_Handler-0.1.0-py3-none-any.whl/config_handler/__init__.py
@@ -0,0 +1,4 @@
+from config_handler.constants import ConfigKeys
+from config_handler.handler import ConfigHandler
+from config_handler.exceptions import (ConfigHandlerException,
+                                       ConfigHandlerFileReadException)
---
+++ packages/Config-Handler/Config_Handler-0.1.0-py3-none-any.whl/config_handler/exceptions.py
@@ -0,0 +1,10 @@
+class ConfigHandlerException(Exception):
+    """Main Config Handler Exception class"""
+
+
+class ConfigHandlerFileReadException(ConfigHandlerException):
+    """Config Handler ConfigFile Read Exception class"""
+
+
+class ConfigHandlerNamingException(ConfigHandlerException):
+    """Config Handler Naming Exception class"""
