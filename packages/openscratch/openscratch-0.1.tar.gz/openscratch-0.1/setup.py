e520357577f67fb1d11a9643dae7480cd8ce7fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:25 +0000

    Add package openscratch/0.1/openscratch-0.1.tar.gz

---
+++ packages/openscratch/openscratch-0.1.tar.gz/openscratch-0.1/setup.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/env python
+ 
+from setuptools import setup
+setup(name='openscratch',
+version='0.1',
+description='openscratch',
+author='shaolizhi',
+author_email='53733522@qq.com',
+url='http://www.openscratch.com/',
+packages=['ywdblogmath', 'ywdblogmath.adv'],
+)
