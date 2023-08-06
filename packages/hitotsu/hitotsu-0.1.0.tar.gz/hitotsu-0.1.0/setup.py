2c72a87e063f56e57e1fcd429f92e1b8f38a5e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:19 +0000

    Add package hitotsu/0.1.0/hitotsu-0.1.0.tar.gz

---
+++ packages/hitotsu/hitotsu-0.1.0.tar.gz/hitotsu-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'hitotsu',
+    version = '0.1.0',
+    keywords='hitotsu',
+    description = 'a library for hitotsu',
+    license = 'BSD License',
+    author = 'd',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
