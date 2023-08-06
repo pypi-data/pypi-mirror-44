54784e1dece540f645339ca0336c066463104d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package kwnpeb/0.1.11/kwnpeb-0.1.11.tar.gz

---
+++ packages/kwnpeb/kwnpeb-0.1.11.tar.gz/kwnpeb-0.1.11/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def get_requirements(path):
+    requirements = []
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+            requirements.append(line)
+
+    return requirements
+
+
+setuptools.setup(
+    name='kwnpeb',
+    version='0.1.11',
+    author="Sile Tao, Li Zhang, Guanqi Huang",
+    author_email="sile@ualberta.ca, lzhang2@ualberta.ca, frank.huangguanqi@gmail.com",
+    description="Compute the Kiefer-Wolfowitz nonparametric maximum likelihood estimator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sit836/KW_NPEB",
+    packages=setuptools.find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
