f297e4232146f9dfec3e06b4042669843294eed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:53 +0000

    Add package petal/0.0.0/petal-0.0.0.tar.gz

---
+++ packages/petal/petal-0.0.0.tar.gz/petal-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='petal',
+    version='',
+    packages=['petal'],
+    url='',
+    license='',
+    author='Tom Forbes',
+    author_email='',
+    description=''
+)
