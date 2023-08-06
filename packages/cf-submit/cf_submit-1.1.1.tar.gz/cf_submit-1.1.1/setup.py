1dc496124dd24c5b15bac82b7912e8fc28108762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:56 +0000

    Add package cf-submit/1.1.1/cf_submit-1.1.1.tar.gz

---
+++ packages/cf-submit/cf_submit-1.1.1.tar.gz/cf_submit-1.1.1/setup.py
@@ -0,0 +1,54 @@
+from setuptools.command.install import install
+from setuptools import setup, dist
+
+
+def post_install():
+    import os
+    print("Copying cf_ckecher to /usr/bin")
+    os.system('cp cf_submit/cf_checker /usr/bin')
+    print("Copying auto_complete_cf to /etc/bash_completion.d")
+    os.system('cp cf_submit/auto_complete_cf /etc/bash_completion.d')
+    os.system('source /etc/bash_completion.d/auto_complete_cf')
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        post_install()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cf_submit",
+    version="1.1.1",
+    scripts=['cf'],
+    author="Nasreddine Bac Ali",
+    author_email="nasreddine.bacali95@gmail.com",
+    description="Submit Codeforces codes via terminal and other coll stuff",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bacali95/cf_submit",
+    packages=['cf_submit'],
+    package_data={
+        'cf_submit': [
+            'cf_checker',
+            'auto_complete_cf',
+            'hack_prob.sh'
+        ]
+    },
+    install_requires=[
+        'lxml',
+        'robobrowser',
+        'prettytable',
+        'javalang'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], cmdclass={
+        'install': CustomInstallCommand
+    }
+)
