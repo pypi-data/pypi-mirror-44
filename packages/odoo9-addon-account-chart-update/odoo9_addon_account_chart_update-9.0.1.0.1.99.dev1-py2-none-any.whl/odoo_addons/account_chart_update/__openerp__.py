94815eca8ac0138488f9342ddc3605af17ab7157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:06 +0000

    Add package odoo9-addon-account-chart-update/9.0.1.0.1.99.dev1/odoo9_addon_account_chart_update-9.0.1.0.1.99.dev1-py2-none-any.whl

---
+++ packages/odoo9-addon-account-chart-update/odoo9_addon_account_chart_update-9.0.1.0.1.99.dev1-py2-none-any.whl/odoo_addons/account_chart_update/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2016 Jacques-Etienne Baudoux <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Detect changes and update the Account Chart from a template",
+    "summary": "Wizard to update a company's account chart from a template",
+    'version': "9.0.1.0.1",
+    'author': "Tecnativa, "
+              "BCIM,"
+              "Odoo Community Association (OCA)",
+    'website': "http://odoo-community.org",
+    'depends': ["account"],
+    'category': "Accounting & Finance",
+    'contributors': [
+        'Pedro M. Baeza',
+        'Jairo Llopis',
+        'Jacques-Etienne Baudoux',
+        'Nacho Muñoz',
+    ],
+    'license': "AGPL-3",
+    "data": [
+        'wizard/wizard_chart_update_view.xml',
+        'views/account_config_settings_view.xml',
+    ],
+}
