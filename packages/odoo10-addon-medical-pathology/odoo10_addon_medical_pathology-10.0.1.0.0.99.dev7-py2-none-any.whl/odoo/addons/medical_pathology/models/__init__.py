6bab6723af208682f6919e61decf68f0313c5d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package odoo10-addon-medical-pathology/10.0.1.0.0.99.dev7/odoo10_addon_medical_pathology-10.0.1.0.0.99.dev7-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-pathology/odoo10_addon_medical_pathology-10.0.1.0.0.99.dev7-py2-none-any.whl/odoo/addons/medical_pathology/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from . import medical_pathology
+from . import medical_pathology_category
+from . import medical_pathology_code_type
---
+++ packages/odoo10-addon-medical-pathology/odoo10_addon_medical_pathology-10.0.1.0.0.99.dev7-py2-none-any.whl/odoo/addons/medical_pathology/models/medical_pathology_code_type.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2008 Luis Falcon <lfalcon@gnusolidario.org>
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from odoo import models, fields
+
+
+class MedicalPathologyCodeType(models.Model):
+    _name = 'medical.pathology.code.type'
+    _description = 'Medical Pathology Code Type'
+
+    name = fields.Char(
+        required=True,
+    )
