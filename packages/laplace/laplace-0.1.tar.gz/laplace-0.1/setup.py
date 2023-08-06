5e2ed1d6f4d3db196e9fd058e76041a72afb680d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:26 +0000

    Add package laplace/0.1/laplace-0.1.tar.gz

---
+++ packages/laplace/laplace-0.1.tar.gz/laplace-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='laplace',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='https://github.com/gastrodia/laplace',
+      author='gastrodia',
+      author_email='ipv6.elata@gmail.com',
+      license='MIT',
+      packages=['laplace','z3'],
+      zip_safe=False)
