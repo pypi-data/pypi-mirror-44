072c64818a2d1a49a95a117ba553a8f84c162608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:25 +0000

    Add package elasticroute/1.0.0/elasticroute-1.0.0-py3-none-any.whl

---
+++ packages/elasticroute/elasticroute-1.0.0-py3-none-any.whl/elasticroute/__init__.py
@@ -0,0 +1,8 @@
+from .exceptions import BadFieldError
+from .data_models import Depot, Stop, Vehicle
+from .defaults import *
+from .client_models import Plan, Solution
+
+import json
+
+name = "elasticroute"
