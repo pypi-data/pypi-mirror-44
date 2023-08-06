3ff40e994325b4f1b1ae6f6391491b29d803c679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:22 +0000

    Add package febs/0.0.6/febs-0.0.6.tar.gz

---
+++ packages/febs/febs-0.0.6.tar.gz/febs-0.0.6/febs/__init__.py
@@ -0,0 +1,18 @@
+
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 BP Ltd. All rights reserved.
+#
+#   Author      : brainpoint
+#   Created date: 2019-04-04 17:33
+#   Description : 
+#
+#================================================================
+
+from .file import file
+
+__all__ = [
+    'file',
+    ]
+
