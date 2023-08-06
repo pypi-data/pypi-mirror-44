eacec125571715a533b60b32d511a10c8a9288d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:29 +0000

    Add package logs-analyzer/0.5.1/logs-analyzer-0.5.1.tar.gz

---
+++ packages/logs-analyzer/logs-analyzer-0.5.1.tar.gz/logs-analyzer-0.5.1/README.rst
@@ -0,0 +1,3 @@
+Logs-analyzer is a library containing functions that can help you extract usable data from logs.
+
+For a better description please visit : https://github.com/ddalu5/logs-analyzer
---
+++ packages/logs-analyzer/logs-analyzer-0.5.1.tar.gz/logs-analyzer-0.5.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='logs-analyzer',
+      version='0.5.1',
+      description='Logs-analyzer is a library containing functions that can help you extract usable data from logs.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/ddalu5/logs-analyzer',
+      author='Salah OSFOR',
+      author_email='osfor.salah@gmail.com',
+      license='Apache V2',
+      packages=find_packages(exclude=['docs', 'tests']),
+      test_suite='tests',
+      tests_require=['unittest'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'Topic :: System :: Logging',
+          'Topic :: System :: Monitoring',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      zip_safe=False)
