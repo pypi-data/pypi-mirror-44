fa211dc7992505b354db449f969b4d2b32654018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:58 +0000

    Add package Config-Handler/0.1.0.dev0/Config_Handler-0.1.0.dev0-py3-none-any.whl

---
+++ packages/Config-Handler/Config_Handler-0.1.0.dev0-py3-none-any.whl/config_handler/__init__.py
@@ -0,0 +1,2 @@
+from config_handler.handler import ConfigHandler
+from config_handler.exceptions import ConfigHandlerFileReadException
---
+++ packages/Config-Handler/Config_Handler-0.1.0.dev0-py3-none-any.whl/config_handler/constants.py
@@ -0,0 +1,5 @@
+from collections import namedtuple
+
+
+ConfigKeys = namedtuple('ConfigKeys', ['DEFAULT'])\
+    ('DEFAULT')
