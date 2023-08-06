30571ddd21466237ab532a38fe873bd6ea0fb795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package mercury-ml/0.2.1.dev0/mercury-ml-0.2.1.dev0.tar.gz

---
+++ packages/mercury-ml/mercury-ml-0.2.1.dev0.tar.gz/mercury-ml-0.2.1.dev0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(name="mercury-ml",
+      version="0.2.1.dev",
+      description="A library for managing Machine Learning workflows",
+      url="https://github.com/mercury-ml-team/mercury-ml",
+      author="Karl Schriek",
+      author_email="kschriek@gmail.com",
+      license="MIT",
+      packages=find_packages(
+          exclude=["*.tests", "*.tests.*", "tests.*", "tests",
+                   "*.examples", "*.examples.*", "examples.*", "examples"]),
+      include_package_data=True,
+      install_requires=["numpy", "pandas", "sklearn", "jsonref", "json-tricks"],
+      extras_require={
+            "tensorflow": ["tensorflow", "pillow"],
+            "tensorflow-gpu": ["tensorflow-gpu", "pillow"],
+            "h2o": ["h2o"],
+            "h2o-sparkling": ["h2o", "pyspark", "h2o-pysparkling"],
+            "s3": ["boto3"],
+            "gcs": ["google-cloud-storage"],
+            "mongo": ["pymongo"]
+            },
+      python_requires=">=3.5",
+      zip_safe=False)
+
+
+
