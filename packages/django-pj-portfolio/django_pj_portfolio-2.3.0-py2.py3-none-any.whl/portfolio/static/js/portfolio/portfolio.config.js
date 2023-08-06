e2f83987ff3f2257742fa0bf7c8793e3f1486ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:31 +0000

    Add package django-pj-portfolio/2.3.0/django_pj_portfolio-2.3.0-py2.py3-none-any.whl

---
+++ packages/django-pj-portfolio/django_pj_portfolio-2.3.0-py2.py3-none-any.whl/portfolio/static/js/portfolio/portfolio.config.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('portfolio.config')
+        .config(config);
+
+    config.$inject = ['$locationProvider'];
+
+    /**
+     * @name config
+     * @desc Enable HTML5 mode
+     */
+
+    function config($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+})();
