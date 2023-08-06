a7d9acd02a798f672ca2e3eb7973f4fd165bf65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:55 +0000

    Add package choropleth-geojson/0.0.7/choropleth_geojson-0.0.7.tar.gz

---
+++ packages/choropleth-geojson/choropleth_geojson-0.0.7.tar.gz/choropleth_geojson-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="choropleth_geojson",
+    version="0.0.7",
+    author="Wei Ruen Leong",
+    author_email="wei.leong2@uqconnect.edu.au",
+    description="Plot a choropleth map with a geojson file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/QuantStats/choropleth_geojson",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+	"Programming Language :: Python :: 3",
+	"Operating System :: OS Independent",
+	"Intended Audience :: Science/Research",       
+	"Topic :: Scientific/Engineering :: Visualization"	
+    ]
+)
