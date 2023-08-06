5faa5f892330c69c23acec38af347a801d6f8f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package validators/0.12.5/validators-0.12.5.tar.gz

---
+++ packages/validators/validators-0.12.5.tar.gz/validators-0.12.5/tests/test_domain.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from validators import domain, ValidationFailure
+
+
+@pytest.mark.parametrize('value', [
+    'example.com',
+    'xn----gtbspbbmkef.xn--p1ai',
+    'underscore_subdomain.example.com',
+    'something.versicherung',
+    '11.com',
+    'somerandomexample.xn--fiqs8s'
+])
+def test_returns_true_on_valid_domain(value):
+    assert domain(value)
+
+
+@pytest.mark.parametrize('value', [
+    'example.com/',
+    'example.com:4444',
+    'example.-com',
+    'example.',
+    '-example.com',
+    'example',
+    'a......b.com'
+])
+def test_returns_failed_validation_on_invalid_domain(value):
+    assert isinstance(domain(value), ValidationFailure)
