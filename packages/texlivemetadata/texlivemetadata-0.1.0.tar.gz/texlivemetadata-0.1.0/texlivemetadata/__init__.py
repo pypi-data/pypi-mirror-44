5377db1f168eb3aaf1da80a05681854d0b93ff65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package texlivemetadata/0.1.0/texlivemetadata-0.1.0.tar.gz

---
+++ packages/texlivemetadata/texlivemetadata-0.1.0.tar.gz/texlivemetadata-0.1.0/texlivemetadata/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Get TexLive installation metadata from Python.
+Wrap and parse tlmgr output.
+
+Documentation at: https://github.com/YtoTech/python-texlivemetadata
+
+Copyright (c) 2019 Yoan Tournade <yoan@ytotech.com>
+Released under MIT License. See LICENSE file.
+"""
+from .packages import (
+    list_packages,
+    list_installed_packages,
+    get_package_info,
+    get_ctan_link,
+)
