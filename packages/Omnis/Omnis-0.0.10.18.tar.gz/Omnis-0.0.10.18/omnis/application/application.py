70d4cad6dd88dfd5ac6df2996817c198f7ee01c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:59 +0000

    Add package Omnis/0.0.10.18/Omnis-0.0.10.18.tar.gz

---
+++ packages/Omnis/Omnis-0.0.10.18.tar.gz/Omnis-0.0.10.18/omnis/application/application.py
@@ -0,0 +1,24 @@
+from keras.models import load_model
+
+import numpy
+import cv2
+
+import os
+import platform
+
+class Application(object):
+    
+    def __init__(self):
+        try:
+            self.os_type = platform.system()
+        except Exception as e:
+            print("Application initialize failed")
+            raise e
+        # default setting for using 1gpu and gpu_ids for 0
+        self.gpu_num = 1
+        self.gpu_ids = [0]
+
+    def use_specific_gpus(self, gpuids):
+        self.gpu_ids = gpuids
+        self.gpu_num = len(gpuids)
+        
