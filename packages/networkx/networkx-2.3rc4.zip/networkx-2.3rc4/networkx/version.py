4d59bcf3a386076593eba1055f600023583ced24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:01 +0000

    Add package networkx/2.3rc4/networkx-2.3rc4.zip

---
+++ packages/networkx/networkx-2.3rc4.zip/networkx-2.3rc4/networkx/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '2.3rc4'
+date = 'Thu Apr 11 20:48:39 2019'
+
+# Was NetworkX built from a development version? If so, remember that the major
+# and minor versions reference the "target" (rather than "current") release.
+dev = False
+
+# Format: (name, major, min, revision)
+version_info = ('networkx', '2', '3rc4', None)
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2019, 4, 11, 20, 48, 39)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = (None, (None, None))
+
