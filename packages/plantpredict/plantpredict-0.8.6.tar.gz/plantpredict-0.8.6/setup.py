b3532ebc230bb3937f63df2122dc8d8db18161b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package plantpredict/0.8.6/plantpredict-0.8.6.tar.gz

---
+++ packages/plantpredict/plantpredict-0.8.6.tar.gz/plantpredict-0.8.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='plantpredict',
+    version='0.8.6',
+    description='Python 2.7 SDK for PlantPredict (https://ui.plantpredict.com).',
+    url='http://github.com/storborg/funniest',
+    author='Stephen Kaplan, Performance & Prediction Engineer at First Solar, Inc.',
+    author_email='stephen.kaplan@firstsolar.com',
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    packages=['plantpredict'],
+    install_requires=[
+        'requests==2.21.0',
+        'pandas==0.24.2',
+        'certifi==2019.3.9',
+        'chardet==3.0.4',
+        'idna==2.8',
+        'numpy==1.16.2',
+        'python-dateutil==2.8.0',
+        'pytz==2018.9',
+        'six==1.12.0',
+        'urllib3==1.24.1'
+    ]
+)
