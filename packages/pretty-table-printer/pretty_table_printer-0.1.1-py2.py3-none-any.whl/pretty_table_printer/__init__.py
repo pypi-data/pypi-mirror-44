a2ea453652d5c9d1d56456772d2f8a28f9df9d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:16 +0000

    Add package pretty-table-printer/0.1.1/pretty_table_printer-0.1.1-py2.py3-none-any.whl

---
+++ packages/pretty-table-printer/pretty_table_printer-0.1.1-py2.py3-none-any.whl/pretty_table_printer/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pretty-table-printer."""
+
+__author__ = """Collin Meyers"""
+__email__ = 'cfmeyers@gmail.com'
+__version__ = '0.1.1'
+
+from .pretty_table_printer import (
+    ColumnSpec,
+    RowCollection,
+    pretty_date,
+    pretty_money,
+    pretty_generic_decimal,
+    pretty_int,
+    guess_row_collection,
+    clean_headers,
+)
