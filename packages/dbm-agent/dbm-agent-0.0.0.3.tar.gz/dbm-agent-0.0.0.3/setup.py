ccc92e16671b9bac9fb233c512e8361522fc16f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package dbm-agent/0.0.0.3/dbm-agent-0.0.0.3.tar.gz

---
+++ packages/dbm-agent/dbm-agent-0.0.0.3.tar.gz/dbm-agent-0.0.0.3/setup.py
@@ -0,0 +1,25 @@
+
+from distutils.core import setup
+from dbma import config
+
+setup(name='dbm-agent',
+      version=config.__version,
+      description='dbm-agent 数据库管理中心客户端程序',
+      author="Neeky",
+      author_email="neeky@live.com",
+      maintainer='Neeky',
+      maintainer_email='neeky@live.com',
+      scripts=['bin/dbm-agent'],
+      packages=['dbma','dbma.native','dbma.utils'],
+      package_data={'dbma':['static/cnfs/*']},
+      url='https://github.com/Neeky/dbm-agent',
+      install_requires=['Jinja2>=2.10.1','mysql-connector-python=8.0.15'],
+      python_requires='>=3.6.*,',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7']
+      )
