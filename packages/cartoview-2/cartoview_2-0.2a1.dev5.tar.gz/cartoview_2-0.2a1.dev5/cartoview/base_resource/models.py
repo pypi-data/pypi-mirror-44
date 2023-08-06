b1c041b4c25e2720ac55f29efcf5b7004bd16986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:53 +0000

    Add package cartoview-2/0.2a1.dev5/cartoview_2-0.2a1.dev5.tar.gz

---
+++ packages/cartoview-2/cartoview_2-0.2a1.dev5.tar.gz/cartoview_2-0.2a1.dev5/cartoview/base_resource/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    title = models.CharField(max_length=255, default=_("No title Provided"))
+    description = models.TextField(
+        null=True, blank=True, default=_("No Description Provided"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    thumbnail = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('title', '-created_at', '-updated_at')
