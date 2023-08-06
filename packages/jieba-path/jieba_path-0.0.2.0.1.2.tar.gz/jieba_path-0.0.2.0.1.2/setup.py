c1491d52c603b4a7986a9f96add4681d1439163c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:25 +0000

    Add package jieba-path/0.0.2.0.1.2/jieba_path-0.0.2.0.1.2.tar.gz

---
+++ packages/jieba-path/jieba_path-0.0.2.0.1.2.tar.gz/jieba_path-0.0.2.0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+setup(
+    name='jieba_path',
+    version='0.0.2.0.1.02',
+    description='结巴分词预处理目录下的文档',
+    author='author',  # 作者
+    author_email='napoler2008@gmail.com',
+    url='https://www.terrychan.org/p/977',
+    # packages=find_packages(),
+    packages=['jieba_path'],  # 这里是所有代码所在的文件夹名称
+    install_requires=['jieba'])
+
+# python setup.py sdist
+# #python setup.py install
+# python setup.py sdist upload
