e45c3384d313b2e07be94eaeaf7299d900883e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:55 +0000

    Add package sfmc-to-json/0.1/sfmc-to-json-0.1.tar.gz

---
+++ packages/sfmc-to-json/sfmc-to-json-0.1.tar.gz/sfmc-to-json-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='sfmc-to-json',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['salesforce_to_json']                  # The name of your scipt, and also the command you'll be using for calling it
+)
