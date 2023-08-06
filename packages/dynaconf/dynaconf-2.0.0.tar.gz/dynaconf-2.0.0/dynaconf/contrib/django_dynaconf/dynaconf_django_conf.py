330f0303267624e57821a2a55bc37233a7bc464d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:33 +0000

    Add package dynaconf/2.0.0/dynaconf-2.0.0.tar.gz

---
+++ packages/dynaconf/dynaconf-2.0.0.tar.gz/dynaconf-2.0.0/dynaconf/contrib/django_dynaconf/dynaconf_django_conf.py
@@ -0,0 +1,27 @@
+# WARNING: THIS EXTENSION IS DEPRECATED
+# Read more on how to integrate with django on
+# https://dynaconf.readthedocs.io/en/latest/guides/django.html
+import os
+from dynaconf import LazySettings
+from django.conf import settings as django_settings
+
+
+dj = {}
+for key in dir(django_settings):
+    if key.isupper() and key != 'SETTINGS_MODULE':
+        dj[key] = getattr(django_settings, key, None)
+    dj['ORIGINAL_SETTINGS_MODULE'] = django_settings.SETTINGS_MODULE
+
+
+dj.setdefault('GLOBAL_ENV_FOR_DYNACONF', 'DJANGO')
+
+env_prefix = '{0}_ENV'.format(
+    dj['GLOBAL_ENV_FOR_DYNACONF']
+)  # DJANGO_ENV
+
+dj.setdefault(
+    'ENV_FOR_DYNACONF',
+    os.environ.get(env_prefix, 'DEVELOPMENT').upper()
+)
+
+settings = LazySettings(**dj)
