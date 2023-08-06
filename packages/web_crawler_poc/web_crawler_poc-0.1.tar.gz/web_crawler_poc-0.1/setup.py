0b51eebc6bbdf6f6a28f44d763bdffb7621f130c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:17 +0000

    Add package web_crawler_poc/0.1/web_crawler_poc-0.1.tar.gz

---
+++ packages/web_crawler_poc/web_crawler_poc-0.1.tar.gz/web_crawler_poc-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='web_crawler_poc',
+      version='0.1',
+      description='Crawler to print web links',
+      url='http://github.com/storborg/funniest',
+      author='Aneesh Nair',
+      author_email='aneeshrnair@gmail.com',
+      license='MIT',
+      packages=['web_crawler'],
+      install_requires=[
+          'lxml',
+          'beautifulsoup4',
+          'requests'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
