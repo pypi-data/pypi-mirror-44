a8a5f0505ba35561c5dc7794edf36086c41c001f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:06 +0000

    Add package overdisp/0.0.1/overdisp-0.0.1.tar.gz

---
+++ packages/overdisp/overdisp-0.0.1.tar.gz/overdisp-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='overdisp',
+    version='0.0.1',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='Estimation of overdispersion parameters',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/overdisp.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[]
+)
