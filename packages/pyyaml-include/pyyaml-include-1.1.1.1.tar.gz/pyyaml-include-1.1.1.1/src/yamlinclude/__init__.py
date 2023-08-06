a576c9bbe42432d3052bf72846e4868eef0dc1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:53 +0000

    Add package pyyaml-include/1.1.1.1/pyyaml-include-1.1.1.1.tar.gz

---
+++ packages/pyyaml-include/pyyaml-include-1.1.1.1.tar.gz/pyyaml-include-1.1.1.1/src/yamlinclude/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Include YAML files within YAML
+"""
+
+
+from __future__ import absolute_import
+
+from .constructor import YamlIncludeConstructor
+from .version import version as __version__
