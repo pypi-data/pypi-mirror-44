72ad35de47c623408351924fb5ebe9042d9aeecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package isacalc/v1.1.0/isacalc-v1.1.0.tar.gz

---
+++ packages/isacalc/isacalc-v1.1.0.tar.gz/isacalc-v1.1.0/isacalc/main_executable.py
@@ -0,0 +1,26 @@
+from isacalc.src import Atmosphere
+
+def get_atmosphere() -> Atmosphere:
+    """
+    Function to obtain the atmosphere model
+    :return: Atmospheric Model
+    """
+    return Atmosphere()
+
+
+def calculate_at_h(h: float, atmosphere_model: Atmosphere = get_atmosphere()) -> list:
+    """
+    Function to calculate Temperature, Pressure and Density at h
+    :param h:                   Height in [m]
+    :param atmosphere_model:    Atmosphere Object
+    :return:                    [h, T, P, D]
+    """
+    return atmosphere_model.calculate(h)
+
+
+if __name__ == "__main__":
+
+    atmosphere = get_atmosphere()
+    h = 50000.0
+
+    h, T, P, d = calculate_at_h(h, atmosphere)
