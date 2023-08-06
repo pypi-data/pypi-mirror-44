400dd6dba611a3335b59492b4a8d03af4e82615a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:39 +0000

    Add package lib-platform/1.0.1/lib_platform-1.0.1.tar.gz

---
+++ packages/lib-platform/lib_platform-1.0.1.tar.gz/lib_platform-1.0.1/setup.py
@@ -0,0 +1,43 @@
+"""Setuptools entry point."""
+import codecs
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+dirname = os.path.dirname(__file__)
+
+long_description = (
+    codecs.open(os.path.join(dirname, 'README.rst'), encoding='utf-8').read() + '\n' +
+    codecs.open(os.path.join(dirname, 'CHANGES.rst'), encoding='utf-8').read()
+)
+
+setup(
+    name='lib_platform',
+    version='1.0.1',
+    description='some small helpers used in many scripts',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Robert Nowotny',
+    author_email='rnowotny1966@gmail.com',
+    url='https://github.com/bitranox/lib_platform',
+    packages=['lib_platform'],
+    install_requires=['pytest',
+                      'typing',
+                      'lib_registry'],
+    classifiers=CLASSIFIERS,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'dill'])
