b7af4afc6238b5565e1eb9dd082b3d4f5a563828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:12 +0000

    Add package customjson/2.10.0/customjson-2.10.0.tar.gz

---
+++ packages/customjson/customjson-2.10.0.tar.gz/customjson-2.10.0/customjson/cli.py
@@ -0,0 +1,26 @@
+"""Enable CLI."""
+import click
+
+
+@click.command()
+@click.option('--token', '-T', default=None, help='GitHub access_token.')
+@click.option('--push', '-P', is_flag=True, help="Publish a release.")
+@click.option('--mode', '-M', default=None, help='Repos.')
+@click.option('--version', '-V', is_flag=True, help='Print version.')
+def cli(token, push, mode, version):
+    """CLI for this package."""
+    if version:
+        from customjson.version import __version__
+        print(__version__)
+    else:
+        from customjson.custom import CreateJson
+        create_json = CreateJson(token, push)
+        if mode == 'card':
+            create_json.card()
+        elif mode == 'component':
+            create_json.component()
+        else:
+            print("--mode must be 'card' or 'component'")
+
+
+cli()  # pylint: disable=E1120
