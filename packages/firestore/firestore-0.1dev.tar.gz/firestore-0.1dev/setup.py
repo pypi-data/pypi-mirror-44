05c15fd46c44bc41824f3ed077b427941897eccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:18 +0000

    Add package firestore/0.1dev/firestore-0.1dev.tar.gz

---
+++ packages/firestore/firestore-0.1dev.tar.gz/firestore-0.1dev/setup.py
@@ -0,0 +1,11 @@
+
+
+from distutils.core import setup
+
+setup(
+    name="firestore",
+    version="0.1dev",
+    packages=['firestore'],
+    license='MIT',
+    long_description=open('README.md').read()
+)
