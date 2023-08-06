56cc6f61f58c612504d58a7b88dc5ec9b9169a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:43 +0000

    Add package pyqubole/0.1.7/pyqubole-0.1.7.tar.gz

---
+++ packages/pyqubole/pyqubole-0.1.7.tar.gz/pyqubole-0.1.7/qubole/__init__.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+"""Basic Qubole Functionality
+"""
+import json
+import os
+import pprint
+
+import requests
+
+__version__ = "0.1.7"
+
+class Qubole:
+    """Basic qubole wrapper"""
+    def __init__(self):
+        try:
+            self.token = os.environ["QUBOLE_TOKEN"]
+        except KeyError:
+            print("Qubole Token: MISSING")
+        self.api = "https://us.qubole.com/api/v1.3/clusters/{CLUSTERID}/state"
+        self.active = None
+
+    def info(self, cluster):
+        """Finds info of the cluster"""
+        response = requests.get(
+            url=self.api.format(CLUSTERID=cluster),
+            headers={'X-AUTH-TOKEN': self.token}
+        )
+        values = json.loads(response.content)
+        pprint.pprint(values)
+
+    def state(self, cluster, full=False):
+        """Finds out the state of the cluster"""
+        response = requests.get(
+            url=self.api.format(CLUSTERID=cluster),
+            headers={'X-AUTH-TOKEN': self.token}
+        )
+
+        values = json.loads(response.content)
+        try:
+            if full:
+                pprint.pprint(values)
+            else:
+                if values["state"] == 'UP':
+                    print(f"Cluster {cluster} UP")
+                    self.active = True
+                    print('Ganglia: https://us.qubole.com/ganglia-metrics-{clusterid}/'.format(clusterid = values["cluster_id"]))
+                elif values["state"] == 'DOWN':
+                    print(f"Cluster {cluster} DOWN")
+                    self.active = False
+                elif values["state"] == "PENDING":
+                    print(f"Cluster {cluster} PENDING")
+                    self.active = False
+                else:
+                    print(f"Cluster {cluster} UNKNOWN")
+                    self.active = False
+        except:
+            print(values)
+
+
+    def toggle(self, cluster):
+        """Toggles the cluster on and off"""
+        self.state(cluster)
+        state = "start" if not self.active else "terminate"
+        response = requests.put(
+            url=self.api.format(CLUSTERID=cluster),
+            headers={
+                'X-AUTH-TOKEN': self.token,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            data=json.dumps({"state": state})
+        )
+        try:
+            pprint.pprint(json.loads(response.content))
+        except TypeError as e:
+            print(f"Error: {e}")
---
+++ packages/pyqubole/pyqubole-0.1.7.tar.gz/pyqubole-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name='pyqubole',
+        version='0.1.7',
+        author='Wesley Goi',
+        author_email='picy2k@gmail.com',
+        packages=['qubole'],
+        scripts=['bin/qubole'],
+        description='Managing qubole clusters',
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+)
