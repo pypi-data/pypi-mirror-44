c14ead51a02b2cba5718088a978dbbdd5d0aaac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:51 +0000

    Add package invana-bot/0.1.2/invana-bot-0.1.2.tar.gz

---
+++ packages/invana-bot/invana-bot-0.1.2.tar.gz/invana-bot-0.1.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, __version__ as setuptools_version
+
+requirements = open('./requirements.txt', 'r').readlines()
+
+setup(name='invana-bot',
+      version='0.1.2',
+      description='A web crawler framework that can transform websites into datasets'
+                  ' with Crawl, Transform and Index workflow.',
+      author='Ravi Raja Merugu',
+      author_email='ravi@invanalabs.ai',
+      url='https://github.com/invanalabs/invana-bot',
+      packages=find_packages(
+          exclude=("bin", "dist", "docs", "example", "tests",)
+      ),
+      install_requires=[requirement for requirement in requirements],
+      entry_points={
+          'console_scripts': ['invana-bot = invana_bot.cmd.run:invana_bot_run']
+      },
+      )
