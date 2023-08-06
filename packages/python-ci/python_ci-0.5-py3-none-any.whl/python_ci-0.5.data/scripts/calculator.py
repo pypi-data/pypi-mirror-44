401b17281577c8ecd879c261a49d5390268a1667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:20 +0000

    Add package python-ci/0.5/python_ci-0.5-py3-none-any.whl

---
+++ packages/python-ci/python_ci-0.5-py3-none-any.whl/python_ci-0.5.data/scripts/calculator.py
@@ -0,0 +1,16 @@
+#!python
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
