1a5d092de0c4b965228cc3fc724c6a419a342d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:57 +0000

    Add package effective/0.1.23/effective-0.1.23.tar.gz

---
+++ packages/effective/effective-0.1.23.tar.gz/effective-0.1.23/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    File Name: setup.py
+    Author: HuHao
+    Mail: whohow20094702@163.com
+    Created Time:  '2019/2/28 20:48:00'
+    Info: A effective style to operate sql
+          Support for HA-connection, retry-support, high-tolerance params, self-batch execution
+    Licence: GPL Licence
+    Url: https://github.com/GitHuHao/effective_sql.git
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="effective",
+    version="0.1.23",
+    keywords=("pip", "upsert","crud", "mysql", "sql", "logging"),
+    description="A effective style to play in python",
+    long_description="Support for HA-connection, retry-support, high-tolerance params, self-batch execution",
+    license="GPL Licence",
+    url="https://github.com/GitHuHao/effective.git",
+    author="HuHao",
+    author_email="whohow20094702@163.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['pymysql', 'DBUtils', 'gevent', 'errno', 'logging']
+)
+
