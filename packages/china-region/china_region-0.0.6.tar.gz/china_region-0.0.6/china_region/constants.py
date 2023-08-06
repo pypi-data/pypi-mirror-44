f4b64f1e28134a5b28899d178a4dd0f37d58f2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:52 +0000

    Add package china-region/0.0.6/china_region-0.0.6.tar.gz

---
+++ packages/china-region/china_region-0.0.6.tar.gz/china_region-0.0.6/china_region/constants.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+import os
+
+# current_dir = os.path.abspath(os.path.dirname(__file__))
+# csv_name = os.path.join(current_dir, 'region.csv')
+
+super_dir = os.path.dirname(os.path.dirname(__file__))
+csv_name = os.path.join(super_dir, 'resource', 'region.csv')
+
+CITY_DF = pd.read_csv(csv_name, engine='python', encoding='utf-8')
+
+
+
