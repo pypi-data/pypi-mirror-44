293742aacc41907698c1b61c5ae5db6d4fd196e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package Calkulate/2.0.22/Calkulate-2.0.22.tar.gz

---
+++ packages/Calkulate/Calkulate-2.0.22.tar.gz/Calkulate-2.0.22/calkulate/constants.py
@@ -0,0 +1,14 @@
+# Calkulate: seawater total alkalinity from titration data
+# Copyright (C) 2019  Matthew Paul Humphreys  (GNU GPLv3)
+
+# Universal physical constants
+R = 8.3144621 # Gas constant in J / (K * mol)
+F = 96.4853365 # Faraday constant in kC / mol
+Tzero = 273.15 # 0 degC in K
+
+# Salinity to chlorinity ratio [WLD69]
+psal2Cl = 1.80655
+
+# Relative molecular masses in g / mol
+RMM_B = 10.811 # Boron
+RMM_F = 18.998 # Fluoride
