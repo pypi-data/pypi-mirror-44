fc595bb125c7f8b6e1c1206788f43c8a58b6f0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:49 +0000

    Add package Git-Clean-Commit/0.1/Git%20Clean%20Commit-0.1.tar.gz

---
+++ packages/Git-Clean-Commit/Git%20Clean%20Commit-0.1.tar.gz/Git Clean Commit-0.1/README.md
@@ -0,0 +1,20 @@
+# treeview
+
+Easily clean commit history
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install git-clean-commit.
+
+```bash
+pip install git-clean-commit
+```
+
+## Usage
+
+```bash
+git-clean-commit [GIT PATH] [COMMIT TEXT]
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/Git-Clean-Commit/Git%20Clean%20Commit-0.1.tar.gz/Git Clean Commit-0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Git Clean Commit',  
+    version='0.1',
+    scripts=['git-clean-commit'] ,
+    author="Low Yiyiu",
+    author_email="lowyiyiu@gmail.com",
+    description="Clean commit history",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lowyiyiu/git-clean-commit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
