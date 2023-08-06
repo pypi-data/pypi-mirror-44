965b0c8f293f51c7e6f4abdbe1d2ae8af0c5c2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:02 +0000

    Add package cellular-automaton/0.1.4/cellular_automaton-0.1.4.tar.gz

---
+++ packages/cellular-automaton/cellular_automaton-0.1.4.tar.gz/cellular_automaton-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="cellular_automaton",
+    version="0.1.4",
+    author="Richard Feistenauer",
+    author_email="r.feistenauer@web.de",
+    packages=["cellular_automaton"],
+    url="https://gitlab.com/DamKoVosh/cellular_automaton",
+    license="Apache License 2.0",
+    description="N dimensional cellular automaton with multi processing capability.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    requires=["pygame"]
+)
