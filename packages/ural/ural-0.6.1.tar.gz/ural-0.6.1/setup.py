6e087a79cbaeb99b0cc7272b78d49c5d9db3cc66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package ural/0.6.1/ural-0.6.1.tar.gz

---
+++ packages/ural/ural-0.6.1.tar.gz/ural-0.6.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='ural',
+      version='0.6.1',
+      description='A helper library full of URL-related heuristics.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/medialab/ural',
+      license='MIT',
+      author='Guillaume Plique',
+      author_email='kropotkinepiotr@gmail.com',
+      keywords='url',
+      python_requires='>=2.7',
+      packages=find_packages(exclude=['test']),
+      package_data={'docs': ['README.md']},
+      install_requires=[
+          'phylactery==0.2.2'
+      ],
+      entry_points={
+          'console_scripts': ['ural=ural.cli.__main__:main']
+      },
+      zip_safe=True)
