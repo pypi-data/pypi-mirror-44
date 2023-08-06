a25d0f7e116989febf583f10c54bcd792e6c01fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package dvc/0.34.1/dvc-0.34.1.tar.gz

---
+++ packages/dvc/dvc-0.34.1.tar.gz/dvc-0.34.1/dvc/remote/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+from dvc.remote.azure import RemoteAzure
+from dvc.remote.gs import RemoteGS
+from dvc.remote.hdfs import RemoteHDFS
+from dvc.remote.local import RemoteLOCAL
+from dvc.remote.s3 import RemoteS3
+from dvc.remote.ssh import RemoteSSH
+from dvc.remote.http import RemoteHTTP
+
+
+REMOTES = [
+    RemoteAzure,
+    RemoteGS,
+    RemoteHDFS,
+    RemoteHTTP,
+    RemoteS3,
+    RemoteSSH,
+    # NOTE: RemoteLOCAL is the default
+]
+
+
+def _get(config):
+    for remote in REMOTES:
+        if remote.supported(config):
+            return remote
+    return RemoteLOCAL
+
+
+def Remote(repo, config):
+    return _get(config)(repo, config)
