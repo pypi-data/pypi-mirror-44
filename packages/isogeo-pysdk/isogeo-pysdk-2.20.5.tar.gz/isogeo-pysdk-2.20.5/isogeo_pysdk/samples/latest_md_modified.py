71c27d570f6249b7d2718523c9c9ad7aae36ace8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:28 +0000

    Add package isogeo-pysdk/2.20.5/isogeo-pysdk-2.20.5.tar.gz

---
+++ packages/isogeo-pysdk/isogeo-pysdk-2.20.5.tar.gz/isogeo-pysdk-2.20.5/isogeo_pysdk/samples/latest_md_modified.py
@@ -0,0 +1,78 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import (absolute_import, print_function, unicode_literals)
+# ------------------------------------------------------------------------------
+# Name:         Isogeo sample - Latest modified datasets
+# Purpose:      Get the latest modified datasets from an Isogeo share, using
+#               the Isogeo API Python minimalist SDK.
+# Author:       Julien Moura (@geojulien)
+#
+# Python:       2.7.x
+# Created:      14/02/2016
+# Updated:      18/02/2016
+# ------------------------------------------------------------------------------
+
+# ##############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import configparser     # to manage options.ini
+from os import path
+
+# Isogeo
+from isogeo_pysdk import Isogeo
+
+# ############################################################################
+# ######### Main program ###########
+# ##################################
+
+if __name__ == '__main__':
+    """Standalone execution"""
+    # specific import
+    from dateutil.parser import parse as dtparse
+    # storing application parameters into an ini file
+    settings_file = r"../isogeo_params.ini"
+
+    # testing ini file
+    if not path.isfile(path.realpath(settings_file)):
+        print("ERROR: to execute this script as standalone,"
+              " you need to store your Isogeo application settings"
+              " in a isogeo_params.ini file."
+              " You can use the template to set your own.")
+        import sys
+        sys.exit()
+    else:
+        pass
+
+    # reading ini file
+    config = configparser.SafeConfigParser()
+    config.read(settings_file)
+
+    share_id = config.get('auth', 'app_id')
+    share_token = config.get('auth', 'app_secret')
+
+    # ------------ Real start ----------------
+    # instanciating the class
+    isogeo = Isogeo(client_id=share_id,
+                    client_secret=share_token,
+                    lang="fr")
+
+    token = isogeo.connect()
+
+    # ------------ REAL START ----------------------------
+    latest_data_modified = isogeo.search(token,
+                                         page_size=10,
+                                         order_by="modified",
+                                         whole_share=0,
+                                         include=["events"]
+                                         )
+
+    print("Last 10 data updated \nTitle | datetime\n\t description")
+    for md in latest_data_modified.get("results"):
+        title = md.get('title')
+        evt_description = md.get("events")[0].get("description")
+        print(str("___________________\n\n{} | {} \n\t {}").
+              format(title.encode("utf8"),
+                     dtparse(md.get("modified")[:19]).strftime("%a %d %B %Y"),
+                     evt_description.encode("utf8")))
---
+++ packages/isogeo-pysdk/isogeo-pysdk-2.20.5.tar.gz/isogeo-pysdk-2.20.5/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Setup script to package Isogeo PySDK Python module
+
+    see: https://github.com/isogeo/isogeo-api-py-minsdk/
+"""
+
+# ############################################################################
+# ########## Libraries #############
+# ##################################
+
+# standard library
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+# package (to get version)
+import isogeo_pysdk
+
+# SETUP ######################################################################
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# intentionally *not* adding an encoding option to open, See:
+#   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+# setup metadata
+setup(
+    # meta
+    name="isogeo-pysdk",
+    version=isogeo_pysdk.__version__,
+    author="Isogeo",
+    author_email="support@isogeo.com",
+    description="Python package to make it easy to use Isogeo REST API",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    keywords="GIS metadata INSPIRE Isogeo API REST geographical data ISO19139",
+    license='LGPL3',
+    url="https://github.com/isogeo/isogeo-api-py-minsdk",
+    project_urls={
+        'Docs': "http://isogeo-api-pysdk.rtfd.io/",
+        'Bug Reports': 'https://github.com/isogeo/isogeo-api-py-minsdk/issues',
+        'Source': 'https://github.com/isogeo/isogeo-api-py-minsdk',
+        'Isogeo API': 'https://www.gitbook.com/book/isogeo/api/details',
+    },
+    # dependencies
+    install_requires=["requests>=2.9.1",
+                      "future",
+                      "six"
+                      ],
+    extras_require={
+        "dev": ["configparser"],
+        "test": ["coverage", "pycodestyle", "python-dateutil"],
+    },
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    # packaging
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
