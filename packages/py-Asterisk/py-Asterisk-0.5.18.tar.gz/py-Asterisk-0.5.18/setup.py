3c17f479d21aa836c07d43821dfc67b528c5fb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package py-Asterisk/0.5.18/py-Asterisk-0.5.18.tar.gz

---
+++ packages/py-Asterisk/py-Asterisk-0.5.18.tar.gz/py-Asterisk-0.5.18/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+'''
+py-Asterisk distutils script.
+'''
+
+from setuptools import setup
+
+__author__ = 'David Wilson'
+__id__ = '$Id$'
+
+
+setup(name='py-Asterisk',
+      version='0.5.18',
+      description='Asterisk Manager API Python interface.',
+      author='David Wilson',
+      author_email='dw@botanicus.net',
+      license='MIT',
+      url='https://github.com/jeansch/py-asterisk/',
+      packages=['Asterisk'],
+      scripts=['asterisk-dump', 'py-asterisk'])
