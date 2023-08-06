d58d81831ca6b377f736703ec343fef18dc283a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:07 +0000

    Add package noms/0.1.5/noms-0.1.5-py3-none-any.whl

---
+++ packages/noms/noms-0.1.5-py3-none-any.whl/noms/objects/food.py
@@ -0,0 +1,27 @@
+import copy
+import operator
+from .nutrient_dict import nutrient_dict, index_from_name
+
+class Food:
+    def __init__(self, data):
+        self.data = data
+        self.desc = data["food"]["desc"]
+        self.nutrients = data["food"]["nutrients"]
+
+class Meal:
+    def __init__(self, foods):
+        self.foods = foods
+        self.nutrients = []
+        for nutrient in foods[0].nutrients:
+            to_app = nutrient.copy()
+            to_app["value"] = 0
+            self.nutrients.append(to_app)
+        for food in foods:
+            n = 0
+            for nutrient in food.nutrients:
+                self.nutrients[n]["value"] += nutrient["value"]
+                n += 1
+    def sort_by_top(self, n):
+        ni = index_from_name(n)
+        self.foods.sort(key=lambda f: f.nutrients[ni]["value"], reverse=True)
+        
