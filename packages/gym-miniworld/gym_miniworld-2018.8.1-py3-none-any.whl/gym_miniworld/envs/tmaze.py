dffb87a4b676c75cde17c0e41f36d2c22f766d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:11 +0000

    Add package gym-miniworld/2018.8.1/gym_miniworld-2018.8.1-py3-none-any.whl

---
+++ packages/gym-miniworld/gym_miniworld-2018.8.1-py3-none-any.whl/gym_miniworld/envs/tmaze.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+from ..miniworld import MiniWorldEnv, Room
+from ..entity import Box
+from gym import spaces
+
+class TMaze(MiniWorldEnv):
+    """
+    Two hallways connected in a T-junction
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            max_episode_steps=140,
+            **kwargs
+        )
+
+        # Allow only the movement actions
+        self.action_space = spaces.Discrete(self.actions.move_forward+1)
+
+    def _gen_world(self):
+        room1 = self.add_rect_room(
+            min_x=-1, max_x=8,
+            min_z=-2, max_z=2
+        )
+        room2 = self.add_rect_room(
+            min_x=8, max_x=12,
+            min_z=-8, max_z=8
+        )
+        self.connect_rooms(room1, room2, min_z=-2, max_z=2)
+
+        # Add a box at a random end of the hallway
+        self.box = Box(color='red')
+        if self.rand.bool():
+            self.place_entity(self.box, room=room2, min_z=room2.max_z - 2)
+        else:
+            self.place_entity(self.box, room=room2, max_z=room2.min_z + 2)
+
+        # Choose a random room and position to spawn at
+        self.place_agent(
+            dir=self.rand.float(-math.pi/4, math.pi/4),
+            room=room1
+        )
+
+    def step(self, action):
+        obs, reward, done, info = super().step(action)
+
+        if self.near(self.box):
+            reward += self._reward()
+            done = True
+
+        return obs, reward, done, info
