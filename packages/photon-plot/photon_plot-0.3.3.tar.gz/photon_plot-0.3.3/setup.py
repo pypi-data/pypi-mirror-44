17d9b27e403a197447a78c84a75d01464fa0afa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:02 +0000

    Add package photon-plot/0.3.3/photon_plot-0.3.3.tar.gz

---
+++ packages/photon-plot/photon_plot-0.3.3.tar.gz/photon_plot-0.3.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup  # Always prefer setuptools over distutils
+
+__author__ = "Romain Thomas"
+__credits__ = "Romain Thomas"
+__license__ = "GNU GPL v3"
+__version__ = "0.3.3"
+__maintainer__ = "Romain Thomas"
+__email__ = "the.spartan.proj@gmail.com"
+__status__ = "Development"
+__website__ = "https://astrom-tom.github.io/Photon/build/html/index.html" 
+
+setup(
+   name = 'photon_plot',
+   version = __version__,
+   author = __author__,
+   author_email = __email__,
+   packages = ['photon'],
+   entry_points = {'gui_scripts': ['photon = photon.__main__:main',],},
+   url = __website__,
+   license = __license__,
+   description = 'Python tool for easy data plotting',
+   python_requires = '>=3.6',
+   install_requires = [
+      "PyQt5 >= v5.10.1",
+      "scipy >= 1.0.1",
+      "numpy >=1.14.2",
+      "matplotlib >= 2.2.2",
+      "astropy >= 3.0.2",
+      "Pillow >=5.1.0",
+   ],
+   include_package_data=True,
+)
