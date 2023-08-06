71784864f431dda0025a522a780cb642483a252e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:53:15 +0000

    Add package progimage/0.0.9/progimage-0.0.9-py3-none-any.whl

---
+++ packages/progimage/progimage-0.0.9-py3-none-any.whl/progimage/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+name = "progimage"
+__title__ = 'progimage'
+__author__ = 'Nasko Grozdanov'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019, Nasko Grozdanov'
+
+from .image import Image
