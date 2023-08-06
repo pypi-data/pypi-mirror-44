7707a835385f9479df8a2490dc0f9ad64a3db403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:06 +0000

    Add package odoo10-addon-medical-agpl/10.0.1.0.0.99.dev2/odoo10_addon_medical_agpl-10.0.1.0.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-agpl/odoo10_addon_medical_agpl-10.0.1.0.0.99.dev2-py2-none-any.whl/odoo/addons/medical_agpl/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
---
+++ packages/odoo10-addon-medical-agpl/odoo10_addon_medical_agpl-10.0.1.0.0.99.dev2-py2-none-any.whl/odoo/addons/medical_agpl/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+{
+    'name': 'Odoo Medical - GPL',
+    'version': '10.0.1.0.0',
+    'category': 'Medical',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
+    'license': 'GPL-3',
+    'depends': [
+        'medical',
+        'partner_contact_birthdate',
+        'partner_contact_weight',
+        'partner_firstname',
+        'partner_identification',
+        'partner_contact_gender',
+    ],
+    'installable': True,
+}
