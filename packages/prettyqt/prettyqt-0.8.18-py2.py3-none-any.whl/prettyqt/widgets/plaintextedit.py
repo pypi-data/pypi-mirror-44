61ac21d953e7be9f21a807fd1efe0f342746260f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:06 +0000

    Add package prettyqt/0.8.18/prettyqt-0.8.18-py2.py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-0.8.18-py2.py3-none-any.whl/prettyqt/widgets/plaintextedit.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtWidgets, QtGui
+
+
+class PlainTextEdit(QtWidgets.QTextEdit):
+
+    def set_enabled(self):
+        self.setEnabled(True)
+
+    def set_disabled(self):
+        self.setEnabled(False)
+
+    def set_font(self, font: QtGui.QFont):
+        self.setFont(font)
+
+    def append(self, text: str):
+        self.appendPlainText(text)
+
+    def set_text(self, text: str):
+        self.setPlainText(text)
+
+    def text(self) -> str:
+        return self.toPlainText()
+
+    def set_read_only(self, value: bool):
+        """set test to read only
+
+        Args:
+            value: True, for read-only, otherwise False
+        """
+        self.setReadOnly(value)
+
+    def scroll_to_end(self):
+        """scroll to the end of the text
+        """
+        self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    widget = PlainTextEdit("This is a test")
+    widget.show()
+    app.exec_()
