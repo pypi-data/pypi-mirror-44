006b923ce17eeb9e1d2f63a7708a928cbf51e134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:34 +0000

    Add package ovation/1.25.0/ovation-1.25.0.tar.gz

---
+++ packages/ovation/ovation-1.25.0.tar.gz/ovation-1.25.0/ovation/lab/constants.py
@@ -0,0 +1,18 @@
+# Entities
+REQUISITION = 'requisition'
+RESOURCE_GROUP = 'resource_group'
+RESOURCE = 'resource'
+WORKFLOW = 'workflow'
+SAMPLE = 'sample'
+
+# Paths
+REPORTS = 'reports'
+WORKFLOW_SAMPLE_RESULTS = 'workflow_sample_results'
+REQUISITIONS = 'requisitions'
+SAMPLES = 'samples'
+RESOURCES = 'resources'
+
+# Other
+FILE_ASSAY = 'file'
+WSR_RESULT_KEY = 'result'
+WSR_RESULT_RECORDS_KEY = 'records'
