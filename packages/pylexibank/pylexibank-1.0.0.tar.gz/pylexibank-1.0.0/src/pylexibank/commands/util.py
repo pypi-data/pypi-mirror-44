b0f544b1fc98bfe6d5bafa0d4c59b3155f10f92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:49 +0000

    Add package pylexibank/1.0.0/pylexibank-1.0.0.tar.gz

---
+++ packages/pylexibank/pylexibank-1.0.0.tar.gz/pylexibank-1.0.0/src/pylexibank/commands/util.py
@@ -0,0 +1,37 @@
+from time import time
+
+from clldutils.path import Path
+from clldutils.clilib import ParserError
+
+from pylexibank.db import Database
+
+
+def get_dataset(args, name=None):
+    id_ = Path(name or args.args[0]).name
+    for dataset in args.cfg.datasets:
+        if dataset.id == id_:
+            return dataset
+    raise ParserError('invalid dataset spec')  # pragma: no cover
+
+
+def with_dataset(args, func, default_to_all=False):
+    for dataset in args.cfg.datasets:
+        if (dataset.id in args.args) or (default_to_all and not args.args):
+            s = time()
+            args.log.info('processing %s ...' % dataset.id)
+            func(get_dataset(args, dataset.id), **vars(args))
+            args.log.info('... done %s [%.1f secs]' % (dataset.id, time() - s))
+
+
+def _load(ds, **kw):
+    db = Database(kw['db'])
+    db.create(exists_ok=True)
+    db.load(ds)
+    db.load_concepticon_data(ds.concepticon)
+    db.load_glottolog_data(ds.glottolog)
+
+
+def _unload(ds, **kw):
+    db = Database(kw['db'])
+    db.create(exists_ok=True)
+    db.unload(ds)
