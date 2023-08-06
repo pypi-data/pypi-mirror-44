14045fa1217d86d09cd06aae3ba3605e240ecf25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:50 +0000

    Add package ri-distributions/1.0/ri_distributions-1.0.tar.gz

---
+++ packages/ri-distributions/ri_distributions-1.0.tar.gz/ri_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ri_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ri_distributions'],
+      autor = 'Ralph I',
+      zip_safe=False)
