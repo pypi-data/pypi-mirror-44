9ec8d4ea8fa0bbea6b64261fa4fe5fbc7999b147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:44 +0000

    Add package energyweb/0.4.3/energyweb-0.4.3.tar.gz

---
+++ packages/energyweb/energyweb-0.4.3.tar.gz/energyweb-0.4.3/energyweb/__init__.py
@@ -0,0 +1,32 @@
+"""
+ _
+| |                  |
+| |   __   _  _    __|
+|/ \_/  \_/ |/ |  /  |
+ \_/ \__/   |  |_/\_/|_/
+
+Bond - Your favorite library for logging energy data on the blockchain
+"""
+
+import energyweb.config as config
+import energyweb.database.dao as dao
+import energyweb.smart_contract.usn.rent_v1 as iotlayer
+import energyweb.smart_contract.origin_v1 as origin
+
+from energyweb.interfaces import Serializable, ExternalData, IntegrationPoint, BlockchainClient
+from energyweb.log import Logger
+from energyweb.dispatcher import App, Task
+from energyweb.carbonemission import CarbonEmissionData
+from energyweb.eds.interfaces import EnergyUnit, EnergyData, EnergyDevice
+from energyweb.smart_contract.interfaces import EVMSmartContractClient
+from energyweb.storage import OnDiskChain
+from energyweb.database.memorydao import MemoryDAO, MemoryDAOFactory
+from energyweb.database.elasticdao import ElasticSearchDAO, ElasticSearchDAOFactory
+
+
+__name__ = 'energyweb'
+__author__ = 'Paul Depraz <github.com/cerealkill>'
+__repository__ = 'github.com/energywebfoundation/ew-link-bond'
+__status__ = "pre-alpha"
+__version__ = "0.4.3"
+__date__ = "14 December 2018"
