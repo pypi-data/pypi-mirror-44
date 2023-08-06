69afc0ce0843f7faba37d26d5f6107279034ea1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:38 +0000

    Add package thunderfit/1.0.7.6/thunderfit-1.0.7.6.tar.gz

---
+++ packages/thunderfit/thunderfit-1.0.7.6.tar.gz/thunderfit-1.0.7.6/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='thunderfit',
+    python_requires='>3.6',
+    version='1.0.7.6',
+    description='Thunderfit fitting code',
+    long_description=readme(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='raman analysis fitting',
+    url='https://github.com/Mitchwatts93/thunderfit',
+    author='https://github.com/Mitchwatts93',
+    author_email='',
+    license='MIT',
+    packages=['thunderfit'],
+    install_requires = ['jsonschema>=2.6.0',
+        'dill>=0.2.9',
+        'scipy>=1.2.1',
+        'numpy>=1.16.2',
+        'matplotlib>=2.2.3',
+        'pandas>=0.23.4',
+        'lmfit>=0.9.11'],
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    entry_points={
+        "console_scripts": ['ramananalyse = thunderfit.thunderfit:main']},
+    )
+
