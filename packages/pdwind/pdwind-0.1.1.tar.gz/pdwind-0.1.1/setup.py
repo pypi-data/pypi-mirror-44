7eba1c8933f7b88e535c3cd51a6d82c84e93b052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:55 +0000

    Add package pdwind/0.1.1/pdwind-0.1.1.tar.gz

---
+++ packages/pdwind/pdwind-0.1.1.tar.gz/pdwind-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pdwind',
+    version='0.1.1',
+    author='fuchen',
+    author_email='ink.pad@163.com',
+    url='https://github.com/fuchen/pdwind',
+    description=u'Pandas adapter for WindPy',
+    packages=['pdwind'],
+    install_requires=[],
+    entry_points={
+    }
+)
