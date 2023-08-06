0a82ed4d0b883150babb5a5a44537638c179887e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:57 +0000

    Add package py2utils/0.3.0/py2utils-0.3.0.tar.gz

---
+++ packages/py2utils/py2utils-0.3.0.tar.gz/py2utils-0.3.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: zhangkai
+Email: kai.zhang1@nio.com
+Last modified: 2019-04-07 00:07:43
+'''
+from setuptools import setup
+
+
+setup(
+    name="py2utils",
+    version="0.3.0",
+    description="digua python utils",
+    author="zhangkai",
+    author_email="zkdfbb@qq.com",
+    url="http://www.ishield.cn",
+    license="MIT",
+    install_requires=['tornado==5.0.0', 'numpy'],
+    include_package_data=True,
+    py_modules=[
+        'utils',
+    ],
+    classifiers=[
+        # 发展时期
+        'Development Status :: 4 - Beta',
+        # 开发的目标用户
+        'Intended Audience :: Developers',
+        # 属于什么类型
+        'Topic :: Software Development :: Build Tools',
+        # 许可证信息
+        'License :: OSI Approved :: MIT License',
+        # 目标 Python 版本
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
