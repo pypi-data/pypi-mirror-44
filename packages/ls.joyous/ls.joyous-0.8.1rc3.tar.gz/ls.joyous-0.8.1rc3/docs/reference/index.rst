6b2b562006e929eead70ef4836b517a5ae55a8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:47 +0000

    Add package ls.joyous/0.8.1rc3/ls.joyous-0.8.1rc3.tar.gz

---
+++ packages/ls.joyous/ls.joyous-0.8.1rc3.tar.gz/ls.joyous-0.8.1rc3/docs/reference/index.rst
@@ -0,0 +1,15 @@
+Reference
+=========
+
+
+.. toctree::
+    :maxdepth: 2
+
+    models/index
+    templatetags
+    formats
+    utils
+    admin_ui
+    fields
+    middleware
+
---
+++ packages/ls.joyous/ls.joyous-0.8.1rc3.tar.gz/ls.joyous-0.8.1rc3/runtests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os
+import sys
+from coverage import Coverage
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+def run():
+    verbosity = 1
+    if "-v" in sys.argv or "--verbose" in sys.argv:
+        verbosity = 2
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'ls.joyous.tests.settings'
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(top_level="ls/joyous",
+                             verbosity=verbosity,
+                             keepdb=False)
+    labels = ["ls.joyous.tests."+arg for arg in sys.argv[1:]
+              if not arg.startswith("-")]
+    if not labels:
+        labels = ["ls.joyous.tests"]
+    failures = test_runner.run_tests(labels)
+    return failures
+
+def coverage():
+    if "--coverage" in sys.argv:
+        cover = Coverage(source=["ls.joyous"], omit=["*/tests*"])
+        cover.start()
+        failures = run()
+        cover.stop()
+        cover.save()
+        cover.html_report()
+    else:
+        failures = run()
+    return failures
+
+if __name__ == "__main__":
+    failures = coverage()
+    sys.exit(bool(failures))
