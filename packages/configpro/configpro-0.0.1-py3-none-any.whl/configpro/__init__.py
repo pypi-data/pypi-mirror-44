6b432edfbe475e297aac08344eecb0bf6e784c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:42 +0000

    Add package configpro/0.0.1/configpro-0.0.1-py3-none-any.whl

---
+++ packages/configpro/configpro-0.0.1-py3-none-any.whl/configpro/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+
+def main():
+    print("Hi there!")
+
+
+if __name__ == '__main__':
+    main()
