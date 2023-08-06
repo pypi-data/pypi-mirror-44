58536c4574e541ad4a9d59b29ebc835c4c276c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:11 +0000

    Add package fzutils/0.2.8.9/fzutils-0.2.8.9-py3-none-any.whl

---
+++ packages/fzutils/fzutils-0.2.8.9-py3-none-any.whl/fzutils/spider/app_utils.py
@@ -0,0 +1,65 @@
+# coding:utf-8
+
+'''
+@author = super_fazai
+@File    : app_utils.py
+@connect : superonesfazai@gmail.com
+'''
+
+"""
+app utils
+"""
+
+from ..spider.async_always import async_sleep
+
+__all__ = [
+    # atx
+    'u2_page_back',                     # u2的页面返回
+    'u2_get_device_display_h_and_w',    # u2获取设备的高跟宽
+    'u2_get_some_ele_height',           # u2得到某一个ele块的height
+    'u2_up_swipe_some_height',          # u2上滑某个高度
+]
+
+async def u2_page_back(d, back_num=1):
+    """
+    u2的页面返回
+    :param d: eg: u2 d
+    :param back_num:
+    :return:
+    """
+    while back_num > 0:
+        d.press('back')
+        back_num -= 1
+        await async_sleep(.3)
+
+    return
+
+async def u2_get_device_display_h_and_w(d) -> tuple:
+    """
+    u2获取设备的高跟宽
+    :param d: eg: u2 d
+    :return:
+    """
+    device_height = d.device_info.get('display', {}).get('height')
+    device_width = d.device_info.get('display', {}).get('width')
+
+    return device_height, device_width
+
+async def u2_get_some_ele_height(ele):
+    """
+    u2得到某一个ele块的height
+    :param ele: eg: d(resourceId="com.taobao.taobao:id/topLayout")
+    :return:
+    """
+    return ele.info.get('bounds', {}).get('bottom') \
+           - ele.info.get('bounds', {}).get('top')
+
+async def u2_up_swipe_some_height(d, swipe_height, base_height=.1) -> None:
+    """
+    u2 上滑某个高度
+    :param d:
+    :param height:
+    :param base_height:
+    :return:
+    """
+    d.swipe(0., base_height + swipe_height, 0., base_height)
