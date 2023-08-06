525ee34470885ae128639f424c816dce0b5a87cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:01 +0000

    Add package inequality-coefficients/1.2.3/inequality_coefficients-1.2.3.tar.gz

---
+++ packages/inequality-coefficients/inequality_coefficients-1.2.3.tar.gz/inequality_coefficients-1.2.3/inequality_coefficients/__init__.py
@@ -0,0 +1,2 @@
+from .gini import *
+from .ratios_top import ratio_top_rest, ratio_top10_rest
---
+++ packages/inequality-coefficients/inequality_coefficients-1.2.3.tar.gz/inequality_coefficients-1.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='inequality_coefficients',
+    packages=['inequality_coefficients'],
+    version='1.2.3',
+    description='Coefficients to measure inequality.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Abel 'Akronix' Serrano Juste",
+    author_email='akronix5@gmail.com',
+    url='https://github.com/Grasia/inequality_coefficients',
+    python_requires='>=3',
+    install_requires=['numpy'],
+    license='MIT',
+    keywords='inequality coefficient index gini ratio top',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research'
+    ]
+)
