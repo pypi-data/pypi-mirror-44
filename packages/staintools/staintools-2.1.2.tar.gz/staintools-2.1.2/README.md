c5d1ae6b877a47dfa26fffca9cd2599adc45be05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:30 +0000

    Add package staintools/2.1.2/staintools-2.1.2.tar.gz

---
+++ packages/staintools/staintools-2.1.2.tar.gz/staintools-2.1.2/README.md
@@ -0,0 +1,5 @@
+# StainTools
+
+Tools for tissue image stain normalization and augmentation in Python 3.
+
+Find the README [here](https://hackmd.io/s/SJ7o7pk1N).
---
+++ packages/staintools/staintools-2.1.2.tar.gz/staintools-2.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='staintools',
+    version='2.1.2',
+    description='A package for tissue image stain normalization, augmentation and more.',
+    author='Peter Byfield',
+    author_email='byfield554@gmail.com',
+    url='https://github.com/Peter554/StainTools',
+    packages=find_packages(exclude=('tests')),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'opencv-python'
+        # 'spams'
+    ]
+)
