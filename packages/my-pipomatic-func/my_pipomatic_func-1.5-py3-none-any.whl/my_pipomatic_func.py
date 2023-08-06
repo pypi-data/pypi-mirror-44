201dab101bcea78247ae285c479a54e15b3aa1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:50:38 +0000

    Add package my-pipomatic-func/1.5/my_pipomatic_func-1.5-py3-none-any.whl

---
+++ packages/my-pipomatic-func/my_pipomatic_func-1.5-py3-none-any.whl/my_pipomatic_func.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Sample Pipomatic module
+#
+# This notebook is a sample notebook that you can use to try out Pipomatic.
+#
+# What you'll do is:
+#
+# 1. Register at PyPI.org if you do not already have a PyPI account.
+# 2. Save this notebook to a folder on your machine.
+# 3. Install Pipomatic using _pip install pipomatic_.
+# 4. Run the command pipomatic _the_name_of_your_notebook_.
+# 5. Login to your PyPI account to see your pip-installable module
+#
+# You can now pip-install your module by typing pip install _the_name_of_your_notebook_ in any Python environment.
+
+# In[3]:
+
+
+def square_it(number_to_square):
+    return number_to_square * number_to_square
+
+
+# In[1]:
+
+
+import os
+
+os.name
+
+
+# In[ ]:
