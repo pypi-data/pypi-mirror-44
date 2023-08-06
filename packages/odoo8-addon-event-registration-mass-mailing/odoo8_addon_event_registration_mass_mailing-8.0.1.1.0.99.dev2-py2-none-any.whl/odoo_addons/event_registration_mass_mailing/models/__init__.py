b6d58923c545cc5c344fbb26d2d0d680e8c436ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:02 +0000

    Add package odoo8-addon-event-registration-mass-mailing/8.0.1.1.0.99.dev2/odoo8_addon_event_registration_mass_mailing-8.0.1.1.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo8-addon-event-registration-mass-mailing/odoo8_addon_event_registration_mass_mailing-8.0.1.1.0.99.dev2-py2-none-any.whl/odoo_addons/event_registration_mass_mailing/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
+# See README.rst file on addon root folder for more details
+
+from . import email_template
+from . import event_registration
+from . import mass_mailing
