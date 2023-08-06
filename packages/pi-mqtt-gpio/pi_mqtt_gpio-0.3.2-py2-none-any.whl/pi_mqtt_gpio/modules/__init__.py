432298a48985f30ba96df91dd23443e5f2df3ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package pi-mqtt-gpio/0.3.2/pi_mqtt_gpio-0.3.2-py2-none-any.whl

---
+++ packages/pi-mqtt-gpio/pi_mqtt_gpio-0.3.2-py2-none-any.whl/pi_mqtt_gpio/modules/__init__.py
@@ -0,0 +1,71 @@
+import abc
+
+from enum import Enum
+
+
+BASE_SCHEMA = {
+    "name": {"required": True, "empty": False},
+    "module": {"required": True, "empty": False},
+    "cleanup": {"required": False, "type": "boolean", "default": True},
+}
+
+
+class PinDirection(Enum):
+    INPUT = 0
+    OUTPUT = 1
+
+
+class PinPullup(Enum):
+    OFF = 0
+    UP = 1
+    DOWN = 2
+
+
+class GenericGPIO(object):
+    """
+    Abstracts a generic GPIO interface to be implemented by the modules in this
+    directory.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def setup_pin(self, pin, direction, pullup, pin_config):
+        pass
+
+    @abc.abstractmethod
+    def set_pin(self, pin, value):
+        pass
+
+    @abc.abstractmethod
+    def get_pin(self, pin):
+        pass
+
+    def cleanup(self):
+        """
+        Called when closing the program to handle any cleanup operations.
+        """
+        pass
+
+
+class GenericSensor(object):
+    """
+    Abstracts a generic sensor interface to be implemented
+    by the modules in this directory.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def setup_sensor(self, config):
+        pass
+
+    @abc.abstractmethod
+    def get_value(self, sensor):
+        pass
+
+    def cleanup(self):
+        """
+        Called when closing the program to handle any cleanup operations.
+        """
+        pass
