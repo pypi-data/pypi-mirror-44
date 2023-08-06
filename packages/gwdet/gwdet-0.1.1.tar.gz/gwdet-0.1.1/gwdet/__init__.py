854fad9c813d541d2e2f7570e3101593313cd08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:41 +0000

    Add package gwdet/0.1.1/gwdet-0.1.1.tar.gz

---
+++ packages/gwdet/gwdet-0.1.1.tar.gz/gwdet-0.1.1/gwdet/__init__.py
@@ -0,0 +1,10 @@
+from .gwdet import __author__
+from .gwdet import __email__
+from .gwdet import __license__
+from .gwdet import __version__
+from .gwdet import __doc__
+
+from .gwdet import averageangles
+from .gwdet import detectability
+from .gwdet import compare_Pw
+from .gwdet import compare_Psnr
