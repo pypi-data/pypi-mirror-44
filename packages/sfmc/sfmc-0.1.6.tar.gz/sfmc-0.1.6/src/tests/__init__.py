ebe0e66f5df7a279694d3e58533d471e1c2cced7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:24 +0000

    Add package sfmc/0.1.6/sfmc-0.1.6.tar.gz

---
+++ packages/sfmc/sfmc-0.1.6.tar.gz/sfmc-0.1.6/src/tests/__init__.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+import json
+
+INCLUDE_LONG_TESTS = bool(os.getenv('LONG_TESTS', False))
+
+
+class TestCase(unittest.TestCase):
+    """
+    Base class for any sfmc unit tests
+    """
+
+    mc_config: dict = None
+
+    @classmethod
+    def get_mc_credentials(cls):
+        if cls.mc_config is None:
+            path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.json'))
+            with open(path, 'r') as f:
+                cls.mc_config = json.load(f)
+
+        return cls.mc_config
