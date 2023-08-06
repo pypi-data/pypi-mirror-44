26fff20c52ee3c6afd6fa55656713bd88679038a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:43 +0000

    Add package annotated-images/0.1.2/annotated_images-0.1.2.tar.gz

---
+++ packages/annotated-images/annotated_images-0.1.2.tar.gz/annotated_images-0.1.2/annotated_images/__init__.py
@@ -0,0 +1,2 @@
+from .split import split
+from .parse_tags import findKeysJson, findTagsJson, findKeysXml, findTagsXml
---
+++ packages/annotated-images/annotated_images-0.1.2.tar.gz/annotated_images-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+classifiers = [
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Utilities']
+
+setup(name='annotated_images',
+      version='0.1.2',
+      description='Split training data images into training, validation and test (dataset) folders.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/saberd/annotated-images',
+      author='saberd',
+      author_email='mail@saberd.com',
+      license='MIT',
+      packages=['annotated_images'],
+      classifiers=classifiers)
