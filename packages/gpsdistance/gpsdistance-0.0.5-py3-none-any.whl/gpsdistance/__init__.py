017f7d333f6d72096e2686db49decc6ecb452048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package gpsdistance/0.0.5/gpsdistance-0.0.5-py3-none-any.whl

---
+++ packages/gpsdistance/gpsdistance-0.0.5-py3-none-any.whl/gpsdistance/__init__.py
@@ -0,0 +1,41 @@
+from math import sin, cos, sqrt, atan2, radians
+
+
+class gpsdistance():
+
+
+    def get_2distance(self, lat1, lon1, lat2, lon2):
+
+        R = 6373.0
+
+        lat1 = radians(lat1)
+        lon1 = radians(lon1)
+        lat2 = radians(lat2)
+        lon2 = radians(lon2)
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = R * c
+
+        # output km
+        return distance
+
+
+    def get_3distance(self,lat1,lon1,alt1,lat2,lon2,alt2):
+
+
+        # 畢達哥拉斯
+        alt1 = float(alt1)
+        alt2 = float(alt2)
+        alt_diff = alt2 - alt1
+        _2distance = self.get_2distance(lat1,lon1,lat2,lon2)
+        distance = sqrt((_2distance * _2distance) + (alt_diff * alt_diff))
+
+        return distance
+
+if __name__ == '__main__':
+    pass
