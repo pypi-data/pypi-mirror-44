2e5fecd2932a40499bb75fce085864ff328f8858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:21 +0000

    Add package hellobalazs/0.0.2/hellobalazs-0.0.2.tar.gz

---
+++ packages/hellobalazs/hellobalazs-0.0.2.tar.gz/hellobalazs-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='hellobalazs',
+    version='0.0.2',
+    description='Say hello!',
+    py_modules=["hellobalazs"],
+    package_dir={'': 'src'},
+)
