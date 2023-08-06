8ad94562598acb409612aa50ef87c2d880482bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:58 +0000

    Add package django-chroniker/1.0.12/django-chroniker-1.0.12.tar.gz

---
+++ packages/django-chroniker/django-chroniker-1.0.12.tar.gz/django-chroniker-1.0.12/chroniker/management/commands/cron_clean.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+import sys
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from chroniker.models import Log
+
+class Command(BaseCommand):
+    help = 'Deletes old job logs.'
+
+    def handle(self, *args, **options):
+
+        if len(args) != 2:
+            sys.stderr.write(
+                'Command requires two arguments. '
+                'Unit (weeks, days, hours or minutes) and interval.\n')
+            return
+
+        unit = str(args[0])
+        if unit not in ['weeks', 'days', 'hours', 'minutes']:
+            sys.stderr.write('Valid units are weeks, days, hours or minutes.\n')
+            return
+        try:
+            amount = int(args[1])
+        except ValueError:
+            sys.stderr.write('Interval must be an integer.\n')
+            return
+
+        kwargs = {unit: amount}
+        time_ago = timezone.now() - timedelta(**kwargs)
+        Log.cleanup(time_ago)
