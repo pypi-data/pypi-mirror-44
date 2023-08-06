9a17ef9116f182d6936d7ca30ba982cab49c590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:21 +0000

    Add package meander/0.0.1/meander-0.0.1.tar.gz

---
+++ packages/meander/meander-0.0.1.tar.gz/meander-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='meander',
+        version='0.0.1',
+        description='Tools for computing contours on 2d surfaces.',
+        url='https://github.com/austinschneider/meander',
+        author='Austin Schneider',
+        author_email='austin.schneider@icecube.wisc.edu',
+        license='LGPL-3.0',
+        packages=['meander'],
+        zip_safe=True
+    )
+
