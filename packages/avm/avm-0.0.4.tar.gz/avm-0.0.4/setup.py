04e4c179033f4a5f90db4a287a78986d092ead92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package avm/0.0.4/avm-0.0.4.tar.gz

---
+++ packages/avm/avm-0.0.4.tar.gz/avm-0.0.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools  # noqa
+from os import environ
+
+setup(name='avm',
+      version='0.0.4',
+      description='AWS profile switching tool',
+      author='Gregory Reshetniak',
+      author_email='ror6ax@gmail.com',
+      install_requires=[
+          'inquirer',
+      ],
+      packages=['avm'],
+      scripts=['avm/bin/avm'])
