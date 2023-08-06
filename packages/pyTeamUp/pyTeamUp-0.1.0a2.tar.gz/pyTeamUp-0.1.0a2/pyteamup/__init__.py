23f6f501548f8e3372e1b6be4e0fe7837c4420b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:42 +0000

    Add package pyTeamUp/0.1.0a2/pyTeamUp-0.1.0a2.tar.gz

---
+++ packages/pyTeamUp/pyTeamUp-0.1.0a2.tar.gz/pyTeamUp-0.1.0a2/pyteamup/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+pyteamup
+
+Python Library for interfacing with the TeamUp api
+
+"""
+
+__version__ = '0.0.1a'
+__author__ = 'David Jones'
+
+name = 'pyTeamUp'
+
+from pyteamup.calendar import Calendar
+from pyteamup.event import Event
+
