269f69f702da0cf01c222c4e417114817cc1f4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package FigCLI/0.1.0/FigCLI-0.1.0-py3-none-any.whl

---
+++ packages/FigCLI/FigCLI-0.1.0-py3-none-any.whl/fig_cli/fig_project_template/hooks/post_gen_project.py
@@ -0,0 +1,25 @@
+import sys
+import pathlib
+import subprocess
+
+HERE = pathlib.Path('.')
+REQUIREMENTS_TXT = (HERE / "requirements.txt")
+
+
+def pip_install_required_elements():
+    # TODO: Create Virtual Env for pip install
+    subprocess.check_call([sys.executable, "-m", "pip",
+                           "install", "-r", REQUIREMENTS_TXT])
+
+
+def git_init():
+    subprocess.check_call(["git", "init"])
+
+
+def post_gen():
+    print("Installing project depedencies")
+    pip_install_required_elements()
+    git_init()
+
+
+post_gen()
