19a06b3b65df91d1d118b6feb409e4c9c7d3a46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package OpenVisus/1.3.15/OpenVisus-1.3.15-cp37-none-manylinux1_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.15-cp37-none-manylinux1_x86_64.whl/OpenVisus/setup.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import shutil
+import platform
+import glob
+import atexit
+import setuptools
+from configure import *
+	
+PROJECT_NAME="OpenVisus"
+
+# use a number like 1.0.xxxx for travis testing
+# otherwise use a number greater than the one uploaded in pip
+# PROJECT_VERSION="1.0.1000"
+PROJECT_VERSION="1.3.15"
+
+# ////////////////////////////////////////////////////////////////////
+def findFilesInCurrentDirectory():
+
+	# this are cached directories that should not be part of OpenVisus distribution
+	shutil.rmtree('./build', ignore_errors=True)
+	shutil.rmtree('./__pycache__', ignore_errors=True)	
+	shutil.rmtree('./.git', ignore_errors=True)	
+	shutil.rmtree('./%s.egg-info' % (PROJECT_NAME,), ignore_errors=True)
+	
+	files=[]	
+	for dirpath, __dirnames__, filenames in os.walk("."):
+		for it in filenames:
+
+			filename= os.path.abspath(os.path.join(dirpath, it))
+
+			if filename.startswith(os.path.abspath('./dist')): continue
+			if "__pycache__" in filename: continue	    							
+
+			files.append(filename)
+	return files
+				
+
+# ////////////////////////////////////////////////////////////////////
+if __name__ == "__main__":
+	setuptools.setup(
+	  name = PROJECT_NAME,
+	  description = "ViSUS multiresolution I/O, analysis, and visualization system",
+	  version=PROJECT_VERSION,
+	  url="https://github.com/sci-visus/OpenVisus",
+	  author="visus.net",
+	  author_email="support@visus.net",
+	  packages=[PROJECT_NAME],
+	  package_dir={PROJECT_NAME:'.'},
+	  package_data={PROJECT_NAME: findFilesInCurrentDirectory()},
+	  platforms=['Linux', 'OS-X', 'Windows'],
+	  license = "BSD",
+	  install_requires=["numpy"],
+	)
+
+
+
