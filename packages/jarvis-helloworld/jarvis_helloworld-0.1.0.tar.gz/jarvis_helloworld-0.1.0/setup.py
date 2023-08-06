c27a0fd1063a11bb0cc77bfa8c8270077df520ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:34 +0000

    Add package jarvis-helloworld/0.1.0/jarvis_helloworld-0.1.0.tar.gz

---
+++ packages/jarvis-helloworld/jarvis_helloworld-0.1.0.tar.gz/jarvis_helloworld-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "jarvis_helloworld",
+    version = "0.1.0",
+    description = " jarvis test project",
+    author = "Jarvis Wang",
+    author_email = "172762619@qq.com",
+    license = "MIT",
+    packages = ["jarvis_helloworld"]
+)
