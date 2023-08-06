c318b30c502451f290fd6da6d119c6bb221b7045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:56 +0000

    Add package no-headers-target-csv/0.0.2/no-headers-target-csv-0.0.2.tar.gz

---
+++ packages/no-headers-target-csv/no-headers-target-csv-0.0.2.tar.gz/no-headers-target-csv-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='no-headers-target-csv',
+      version='0.0.2',
+      description='Singer.io target for writing CSV files with or without headers.',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=[' no_headers_target_csv'],
+      install_requires=[
+          'jsonschema==2.6.0',
+          'singer-python==2.1.4',
+      ],
+      entry_points='''
+          [console_scripts]
+          no-headers-target-csv=target_csv:main
+      ''',
+)
