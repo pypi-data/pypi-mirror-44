17523e290faeb570b10084bf3723ef674aa45914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:31 +0000

    Add package squad-df/0.61/squad_df-0.61.tar.gz

---
+++ packages/squad-df/squad_df-0.61.tar.gz/squad_df-0.61/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name='squad_df',
+      version='0.61',
+      packages=['squad_df'],
+      install_requires=['requests'],
+      python_requires=">=3.6",
+      zip_safe=False)
