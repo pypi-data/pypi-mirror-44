79e6afb0041d020b6486b33705f55557b33a0a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:01 +0000

    Add package mech.m/0.0.0/mech.m-0.0.0.tar.gz

---
+++ packages/mech.m/mech.m-0.0.0.tar.gz/mech.m-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mech.m',
+    version='',
+    url='',
+    license='',
+    author='gaochao; wangkun',
+    author_email='gaochao@iga.ac.cn',
+    py_modules =['__init__','mechanism'],
+    description='mechanism for  air quality model'
+)
