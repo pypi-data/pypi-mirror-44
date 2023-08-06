dd4c00973f3a9b29a00e0ca1bf95178a55161145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:01 +0000

    Add package tendril-extensions-qtpl/0.1.2/tendril-extensions-qtpl-0.1.2.tar.gz

---
+++ packages/tendril-extensions-qtpl/tendril-extensions-qtpl-0.1.2.tar.gz/tendril-extensions-qtpl-0.1.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+    find_packages = None
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tendril-extensions-qtpl',
+    version='0.1.2',
+    description="Tendril Extensions for the QTPL Instance",
+    long_description=readme,
+    author="Chintalagiri Shashank",
+    author_email='shashank@chintal.in',
+    url='https://gitlab.com/qtpl/tendril-extensions-qtpl',
+    packages=['tendril'],
+    install_requires=requirements,
+    license="AGPLv3",
+    zip_safe=False,
+    keywords='tendril',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    # test_suite='tests',
+    # tests_require=test_requirements
+)
