5a641998de5acb3c6f44cd90f93fd9b48affe31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:57 +0000

    Add package wofry/1.0.20/wofry-1.0.20.tar.gz

---
+++ packages/wofry/wofry-1.0.20.tar.gz/wofry-1.0.20/README.txt
@@ -0,0 +1,17 @@
+WOFRY (Wave Optics) library
+======
+
+Kernel classes for WOFRY in python
+
+syned package requires Python 3.3 or newer.
+
+
+Installing
+----------
+
+syned is pip installable (https://pip.pypa.io/), simply run::
+
+    pip install wofry
+
+to install it.
+
---
+++ packages/wofry/wofry-1.0.20.tar.gz/wofry-1.0.20/wofry/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofry SETUP.PY
+short_version = '1.0.20'
+version = '1.0.20'
+full_version = '1.0.20'
+git_revision = 'db7be6382a2e1117748d181b4d7d587b45f2c273'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
