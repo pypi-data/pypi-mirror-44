1b8c6992e89717f01e14c434fa9652a3e70aaa03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:32 +0000

    Add package BrowserAutomator/1.0.10/BrowserAutomator-1.0.10.tar.gz

---
+++ packages/BrowserAutomator/BrowserAutomator-1.0.10.tar.gz/BrowserAutomator-1.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="BrowserAutomator",
+    version="1.0.10",
+    author="EDEKA DIGITAL",
+    description="A package to simplify browser automation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edekadigital/BrowserAutomator",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "ruamel.yaml",
+        "requests",
+        "pycryptodome",
+    ],
+    python_requires='>=3.3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    exclude=["config"]
+)
