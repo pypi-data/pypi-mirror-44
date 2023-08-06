7a27d9823b40088164931b21658fffedb89a3e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:09 +0000

    Add package mattes-allegro-pl/0.4.1/mattes_allegro_pl-0.4.1-py3-none-any.whl

---
+++ packages/mattes-allegro-pl/mattes_allegro_pl-0.4.1-py3-none-any.whl/allegro_pl/__init__.py
@@ -0,0 +1,5 @@
+from .allegro import Allegro
+from .oauth import AllegroAuth, ClientCredentialsAuth, URL_TOKEN, TokenStore, PassTokenStore, ClientCodeStore
+
+__name__ = 'mattes-allegro-pl'
+__version__ = '0.4.0'
