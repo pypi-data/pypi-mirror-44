e8fe640a66899f95ee1d59ebf5ecbec00d911973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package ffx/1.3.10/ffx-1.3.10.tar.gz

---
+++ packages/ffx/ffx-1.3.10.tar.gz/ffx-1.3.10/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+setup(
+    name = "ffx",
+    version = "1.3.10",
+    author = "Trent McConaghy",
+    author_email = "gtrent@gmail.com",
+    maintainer = "Nate Kupp",
+    maintainer_email ="nathan.kupp@gmail.com",
+    description = ("Fast Function Extraction: A fast, scalable, and deterministic symbolic regression tool."),
+    license = "See readme",
+    keywords = "symbolic regression machine learning",
+    url = "https://github.com/natekupp/ffx",
+    packages=['ffx', 'tests'],
+    scripts=['ffx/bin/runffx'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+        "License :: Other/Proprietary License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
