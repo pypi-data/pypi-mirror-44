63103499b7b155cab18468dcdb9f08730f4bafaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:15 +0000

    Add package hdx-data-freshness/1.3.3/hdx_data_freshness-1.3.3-py2.py3-none-any.whl

---
+++ packages/hdx-data-freshness/hdx_data_freshness-1.3.3-py2.py3-none-any.whl/hdx/freshness/database/dbdataset.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from hdx.utilities.database import Base
+from sqlalchemy import Column, DateTime, Boolean, String
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+
+from hdx.freshness.database.dbinfodataset import DBInfoDataset
+from hdx.freshness.database.dbrun import DBRun
+
+
+class DBDataset(Base):
+    run_number = Column(Integer, ForeignKey(DBRun.run_number), primary_key=True)
+    id = Column(String, ForeignKey(DBInfoDataset.id), primary_key=True)
+    dataset_date = Column(String)
+    update_frequency = Column(Integer)
+    metadata_modified = Column(DateTime, nullable=False)
+    last_modified = Column(DateTime, nullable=False)
+    what_updated = Column(String, nullable=False)
+    last_resource_updated = Column(String, nullable=False)
+    last_resource_modified = Column(DateTime, nullable=False)
+    fresh = Column(Integer)
+    error = Column(Boolean, nullable=False)
+
+    def __repr__(self):
+        output = '<Dataset(run number=%d, id=%s, ' % (self.run_number, self.id)
+        output += 'dataset date=%s, ' % str(self.dataset_date)
+        output += 'update frequency=%s,\nlast_modified=%s' % (self.update_frequency, str(self.last_modified))
+        output += 'what updated=%s, metadata_modified=%s,\n' % (str(self.what_updated), str(self.metadata_modified))
+        output += 'Resource %s: last modified=%s,\n' % (str(self.last_resource_updated), str(self.last_resource_modified))
+        output += 'Dataset fresh=%s' % str(self.fresh)
+        return output
---
+++ packages/hdx-data-freshness/hdx_data_freshness-1.3.3-py2.py3-none-any.whl/hdx/freshness/database/dbresource.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from hdx.utilities.database import Base
+from sqlalchemy import Boolean
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+
+from hdx.freshness.database.dbinfodataset import DBInfoDataset
+from hdx.freshness.database.dbrun import DBRun
+
+
+class DBResource(Base):
+    run_number = Column(Integer, ForeignKey(DBRun.run_number), primary_key=True)
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    dataset_id = Column(String, ForeignKey(DBInfoDataset.id), nullable=False)
+    url = Column(String, nullable=False)
+    error = Column(String)
+    last_modified = Column(DateTime, nullable=False)
+    what_updated = Column(String, nullable=False)
+    revision_last_updated = Column(DateTime, default=None)
+    http_last_modified = Column(DateTime, default=None)
+    md5_hash = Column(String, default=None)
+    when_hashed = Column(DateTime, default=None)
+    when_checked = Column(DateTime, default=None)
+    api = Column(Boolean)
+
+    def __repr__(self):
+        output = '<Resource(run number=%d, id=%s, name=%s, ' % (self.run_number, self.id, self.name)
+        output += 'dataset id=%s,\nurl=%s,\n' % (self.dataset_id, self.url)
+        output += 'error=%s, last modified=%s, what updated=%s,\n' % (self.error, str(self.last_modified),
+                                                                     str(self.what_updated))
+        output += 'revision last updated=%s, http last modified=%s, ' % (str(self.revision_last_updated),
+                                                                         str(self.http_last_modified))
+        output += 'MD5 hash=%s, when hashed=%s, ' % (str(self.http_last_modified), str(self.when_hashed))
+        output += 'when checked=%s, ' % str(self.when_checked)
+        output += 'api=%s)>' % str(self.api)
+        return output
