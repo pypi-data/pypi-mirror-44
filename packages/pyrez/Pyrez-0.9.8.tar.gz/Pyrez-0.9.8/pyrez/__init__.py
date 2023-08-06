c58e7eec01ad373bb9338ffa996016a35740fae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:48 +0000

    Add package pyrez/0.9.8/Pyrez-0.9.8.tar.gz

---
+++ packages/pyrez/Pyrez-0.9.8.tar.gz/Pyrez-0.9.8/pyrez/__init__.py
@@ -0,0 +1,20 @@
+
+import sys
+from datetime import datetime
+if sys.version_info[:2] < (3, 4) and datetime.utcnow().year >= 2020:
+    raise RuntimeError("Unsupported Python version")
+
+__author__ = "Luis (Lugg) Gustavo"
+__author_email__ = "luis.silva.1044894@gmail.com"
+__copyright__ = "Copyright (c) 2018 Luis (Lugg) Gustavo"
+__build__ = 0x000908
+__description__ = "An open-source wrapper for Hi-Rez Studios' API (Paladins, Realm Royale, and Smite), written in Python"
+__license__ = "MIT"
+__package_name__ = "Pyrez"
+__url__ = "https://discord.gg/XkydRPS"
+__version__ = "0.9.8"
+__title__ = "{}-{}".format(__package_name__, __version__)
+version = __version__
+
+from collections import namedtuple
+version_info = namedtuple("VersionInfo", "major minor micro releaselevel serial")(major=0, minor=9, micro=9, releaselevel="beta", serial=0)
