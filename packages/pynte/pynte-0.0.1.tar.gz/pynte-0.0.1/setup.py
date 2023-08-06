6dafb05cdfe5b2832c7e2e793f2abf2e9b5a2652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:56 +0000

    Add package pynte/0.0.1/pynte-0.0.1.tar.gz

---
+++ packages/pynte/pynte-0.0.1.tar.gz/pynte-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="pynte",
+		version="0.0.1",
+		url="https://github.com/polambert/pynte.git",
+		license="MIT",
+		author="Parker Lambert",
+		author_email="parkerthelamb@gmail.com",
+		description="Pint-sized networking package for python.",
+		packages=find_packages(),
+		zip_safe=False)
