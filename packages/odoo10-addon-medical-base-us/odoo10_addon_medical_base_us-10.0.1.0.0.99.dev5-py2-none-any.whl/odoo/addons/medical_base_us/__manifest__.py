b52c01c5d05541db7801211935f0e4789a27c8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:05 +0000

    Add package odoo10-addon-medical-base-us/10.0.1.0.0.99.dev5/odoo10_addon_medical_base_us-10.0.1.0.0.99.dev5-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-base-us/odoo10_addon_medical_base_us-10.0.1.0.0.99.dev5-py2-none-any.whl/odoo/addons/medical_base_us/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+{
+    'name': 'Medical Base - US',
+    'summary': 'Provides some helper mixins for US locale',
+    'version': '10.0.1.0.0',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'category': 'Medical',
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'GPL-3',
+    'installable': True,
+    'depends': [
+        'medical_agpl',
+    ],
+    'data': [
+        'data/res_partner_id_category.xml',
+    ],
+}
