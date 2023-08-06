ef213530d48382f6f38a4978c1985dd39197fbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:07 +0000

    Add package mass-renamer/0.3.2/mass_renamer-0.3.2.tar.gz

---
+++ packages/mass-renamer/mass_renamer-0.3.2.tar.gz/mass_renamer-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'mass_renamer',
+  packages = ['mass_renamer'],
+  version = '0.3.2',
+  license='MIT',
+  description = 'A Python script to mass rename files in a directory.',
+  author = 'Matthew Kleiner',
+  url = 'https://github.com/mrniceguy127/mass-renamer',
+  download_url = 'https://github.com/mrniceguy127/mass-renamer/archive/v_0.3.2.tar.gz',
+  keywords = ['mass', 'renamer', 'renamer', 'files', 'directories'],
+  entry_points = {
+    "console_scripts": [
+      "mass-renamer=mass_renamer.mass_renamer:run",
+    ]
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
