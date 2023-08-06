3cf6d7caa8339e7bdf93a2e6cbd6c39264154c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package darksky_weather/1.1.3/darksky_weather-1.1.3.tar.gz

---
+++ packages/darksky_weather/darksky_weather-1.1.3.tar.gz/darksky_weather-1.1.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+__version__ = '1.1.3'
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+    README = f.read()
+
+
+setup(
+    version=__version__,
+    name='darksky_weather',
+    packages=find_packages(),
+
+    install_requires=[
+        'requests==2.21.0'
+    ],
+
+    description='The Dark Sky API wrapper',
+    long_description=README,
+
+    author='Detrous',
+    author_email='detrous@protonmail.com',
+
+    url='https://github.com/Detrous/darksky_weather',
+    download_url='https://github.com/Detrous/darksky_weather/archive/%s.tar.gz' % __version__,
+
+    license='MIT License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
