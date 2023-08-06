57341af2d4756c402bb72946ee1718cfbc82e51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:55 +0000

    Add package fast-arrow/0.3.1/fast_arrow-0.3.1.tar.gz

---
+++ packages/fast-arrow/fast_arrow-0.3.1.tar.gz/fast_arrow-0.3.1/fast_arrow/exceptions.py
@@ -0,0 +1,14 @@
+class AuthenticationError(Exception):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+class NotImplementedError(Exception):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+class TradeExecutionError(Exception):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
