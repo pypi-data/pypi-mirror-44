db2a212002941d805977a0575c13c7a27f1ce480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:12 +0000

    Add package hanamusume/0.0.dev1/hanamusume-0.0.dev1.tar.gz

---
+++ packages/hanamusume/hanamusume-0.0.dev1.tar.gz/hanamusume-0.0.dev1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hanamusume",
+    version="0.0.dev1",
+    author="boyu",
+    author_email="chuboyu.tw@gmail.com",
+    description="A small package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
