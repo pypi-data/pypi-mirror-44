4509538a8ed0dde942a18fdcbdab46e120eec98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:29 +0000

    Add package citrus/0.0.2/citrus-0.0.2-py2.7.egg

---
+++ packages/citrus/citrus-0.0.2-py2.7.egg/citrus/errors.py
@@ -0,0 +1,16 @@
+import pulp
+
+class CitrusError(pulp.PulpError):
+    pass
+
+class NonBinaryVariableError(CitrusError):
+    pass
+
+def assert_binary(var):
+    if not var.isBinary():
+        raise NonBinaryVariableError(var.name)
+
+def assert_same_problem(x, y):
+    if x._problem is not y._problem:
+        raise CitrusError('Variables must be associated with the same problem.')
+    return x # return for use in reduce(assert_same_problem, (x, y, z))
