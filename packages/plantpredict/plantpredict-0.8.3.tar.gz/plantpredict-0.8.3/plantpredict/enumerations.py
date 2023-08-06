a77d85607b566c291975dd896a3d28d29658df33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:43 +0000

    Add package plantpredict/0.8.3/plantpredict-0.8.3.tar.gz

---
+++ packages/plantpredict/plantpredict-0.8.3.tar.gz/plantpredict-0.8.3/plantpredict/enumerations.py
@@ -0,0 +1,52 @@
+class PlantPredictEnum(dict):
+    def __getattr__(self, key):
+        if key not in self:
+            raise AttributeError(key)
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        del self[key]
+
+
+# Air Mass Model
+air_mass_model_type_enum = PlantPredictEnum()
+air_mass_model_type_enum.BIRD_HULSTROM = 0
+air_mass_model_type_enum.KASTEN_SANDIA = 1
+
+# Backtracking Type
+backtracking_type_enum = PlantPredictEnum()
+backtracking_type_enum.TRUE_TRACKING = 0   # no backtracking
+backtracking_type_enum.BACKTRACKING = 1    # shade avoidance
+
+# Cell Technology
+cell_technology_enum = PlantPredictEnum()
+cell_technology_enum.NTYPE_MONO_CSI = 1
+cell_technology_enum.PTYPE_MONO_CSI_PERC = 2
+cell_technology_enum.PTYPE_MONO_CSI_BSF = 3
+cell_technology_enum.POLY_CSI_PERC = 4
+cell_technology_enum.POLY_CSI_BSF = 5
+cell_technology_enum.CDTE = 6
+cell_technology_enum.CIGS = 7
+
+# Cleaning Frequency
+cleaning_frequency_enum = PlantPredictEnum()
+cleaning_frequency_enum.NONE = 0
+cleaning_frequency_enum.DAILY = 1
+cleaning_frequency_enum.MONTHLY = 2
+cleaning_frequency_enum.QUARTERLY = 3
+cleaning_frequency_enum.YEARLY = 4
+
+# Construction Type
+
+
+# Entity Type
+entity_type_enum = PlantPredictEnum()
+
+# Module Orientation
+module_orientation_enum = PlantPredictEnum()
+
+# Prediction Status
+prediction_status_enum = PlantPredictEnum()
