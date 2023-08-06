8f2f12c89a138ec1f972180ab092a19c78bd4ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:24 +0000

    Add package mastercard-product-identifier/1.0.0/mastercard_product_identifier-1.0.0.tar.gz

---
+++ packages/mastercard-product-identifier/mastercard_product_identifier-1.0.0.tar.gz/mastercard_product_identifier-1.0.0/mastercardproductidentifier/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardproductidentifier.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardproductidentifier.hashpanbinvalidation import *
