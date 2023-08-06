7870c2dc26d2d7bf06188d2934ce67b093596209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:59 +0000

    Add package ezWxPython/0.4.7/ezWxPython-0.4.7.tar.gz

---
+++ packages/ezWxPython/ezWxPython-0.4.7.tar.gz/ezWxPython-0.4.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ezWxPython",
+    version="0.4.7",
+    author="zdiv",
+    author_email="zdiv@daum.net",
+    description="Easy wxPython wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="GUI UI wxpython wrapper simple easy beginner novice student graphics", 
+    url="https://github.com/zdivdev/ezWxPython",
+    #packages=setuptools.find_packages(),
+    packages=[ 'ezWxPython', 'ezWxPython/example', 'ezWxPython/demo' ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
