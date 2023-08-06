2916218ecc558c68bdfcf7c93f9ee17803ea4bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:41 +0000

    Add package scrapy-do/0.3.1/scrapy_do-0.3.1-py3-none-any.whl

---
+++ packages/scrapy-do/scrapy_do-0.3.1-py3-none-any.whl/scrapy_do/ui/precache-manifest.446c31093c07b7b90f57f0eb98f64941.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f24f8a630b1130735f33",
+    "url": "/static/css/main.f3c78f7b.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "00520fca34d640eef8fe",
+    "url": "/static/js/2.cd27a249.chunk.js"
+  },
+  {
+    "revision": "f24f8a630b1130735f33",
+    "url": "/static/js/main.8118bfc3.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "00520fca34d640eef8fe",
+    "url": "/static/css/2.f537461d.chunk.css"
+  },
+  {
+    "revision": "cb0d5dc1b7153d435102b4deef628bf7",
+    "url": "/index.html"
+  }
+];
