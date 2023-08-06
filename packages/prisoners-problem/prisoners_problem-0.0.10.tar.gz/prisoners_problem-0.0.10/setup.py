7a67e052de0b2d4dc159273192da2530a3c74e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:36 +0000

    Add package prisoners-problem/0.0.10/prisoners_problem-0.0.10.tar.gz

---
+++ packages/prisoners-problem/prisoners_problem-0.0.10.tar.gz/prisoners_problem-0.0.10/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+# from distutils.core import Extension
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="prisoners_problem",
+    version="0.0.10",
+    description="Simulate the 100 prisoners problem.",
+    # ext_modules=[
+    #     Extension(
+    #         "simulate_fast", sources=["prisoners_problem/simulate_fast.cpp"],
+    #         extra_compile_args=['-std=c++11', '-Wall', '-Wextra'],
+    #         language='c++',
+    #         )
+    #     ],
+    url="https://github.com/tekknolagi/100prisoners",
+    author="Maxwell Bernstein",
+    packages=setuptools.find_packages(),
+    author_email="python@bernsteinbear.com",
+    install_requires=[
+        'numpy==1.16.2',
+        'matplotlib==3.0.3',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
