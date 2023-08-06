f19e274ba48b4ad58067ed0959b002ed494e7337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:45 +0000

    Add package SecureBytes/0.3.1/SecureBytes-0.3.1.tar.gz

---
+++ packages/SecureBytes/SecureBytes-0.3.1.tar.gz/SecureBytes-0.3.1/setup.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+import sys
+excomp=None
+excomp=["-std=c++11"]
+if sys.platform == "win32":
+    excomp=['"-std:c++11"']
+
+setup(
+    name='SecureBytes',
+    version='0.3.1',
+    description='Clears the contents of bytes or integers containing cryptographic material',
+    author=u'Erik Aronesty',
+    long_description=open("README.md").read(),
+    author_email='erik@getvida.io',
+    url='https://github.com/vidaid/pysecbytes',
+    license='MIT',
+    ext_modules=[Extension('SecureBytes', ['pysecbytes.cpp'], extra_compile_args=excomp)],
+)
