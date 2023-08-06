72b17257aa39e241599d55434efc36a93ad22c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:16 +0000

    Add package gbd-mapping/2.0.3/gbd_mapping-2.0.3.tar.gz

---
+++ packages/gbd-mapping/gbd_mapping-2.0.3.tar.gz/gbd_mapping-2.0.3/src/gbd_mapping/__about__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",  "__email__",
+]
+
+__title__ = "gbd_mapping"
+__summary__ = "A programmatically accessible mapping of gbd entities."
+__uri__ = "https://github.com/ihmeuw/gbd_mapping"
+
+__version__ = "2.0.3"
+
+__author__ = "The vivarium developers"
+__email__ = "vivarium.dev@gmail.com"
