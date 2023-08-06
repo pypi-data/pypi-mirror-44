da46342eb2f64168df505cc2d02f867268e87dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:16 +0000

    Add package alpy/0.11.0/alpy-0.11.0.tar.gz

---
+++ packages/alpy/alpy-0.11.0.tar.gz/alpy-0.11.0/alpy/utils.py
@@ -0,0 +1,86 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+import subprocess
+
+
+def configure_logging():
+
+    stream_handler = logging.StreamHandler()
+    stream_formatter = logging.Formatter(
+        fmt="{levelname:8} {name:22} {message}", style="{"
+    )
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(stream_formatter)
+
+    file_handler = logging.FileHandler("debug.log", mode="w")
+    file_formatter = logging.Formatter(
+        fmt="{relativeCreated:7.0f} {levelname:8} {name:22} {message}",
+        style="{",
+    )
+    file_handler.setFormatter(file_formatter)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    root_logger.addHandler(stream_handler)
+    root_logger.addHandler(file_handler)
+
+
+class StepResultLogger:
+    def __init__(self, logger, description):
+        self._logger = logger
+        self._description = description
+
+    def __enter__(self):
+        self._logger.info(self._description + "...")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            self._logger.error(self._description + "... failed")
+        else:
+            self._logger.info(self._description + "... done")
+
+
+def step(description):
+    return StepResultLogger(logging.getLogger("step"), description)
+
+
+class NonZeroExitCode(Exception):
+    pass
+
+
+def close_verbose_process(
+    *, process, process_name, logger, check=True, timeout
+):
+
+    try:
+        stdout_data, stderr_data = process.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        logger.error(f"{process_name} is still running")
+        with StepResultLogger(logger, f"Kill {process_name}"):
+            process.kill()
+            stdout_data, stderr_data = process.communicate()
+
+    for line in stdout_data.splitlines():
+        logger.debug("< " + line)
+
+    for line in stderr_data.splitlines():
+        logger.error("< " + line)
+
+    return_code = process.returncode
+
+    if return_code >= 0:
+        logger.debug(f"{process_name} exited with code {return_code}")
+    else:
+        logger.debug(
+            f"{process_name} " f"was killed by signal {-1 * return_code}"
+        )
+    if check and return_code > 0:
+        raise NonZeroExitCode(
+            f"Process {process.args} exited with non-zero code {return_code}"
+        )
+    if check and return_code < 0:
+        raise NonZeroExitCode(
+            f"Process {process.args} was killed by signal {-1 * return_code}"
+        )
+
+    return return_code
