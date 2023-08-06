7e04492da66969880a6e3858420661701a1cf662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:46 +0000

    Add package wiki-archive-combsl10/0.0.6/wiki-archive-combsl10-0.0.6.tar.gz

---
+++ packages/wiki-archive-combsl10/wiki-archive-combsl10-0.0.6.tar.gz/wiki-archive-combsl10-0.0.6/README.md
@@ -0,0 +1,6 @@
+# Wiki Archive
+## With wiki archive, you can easily:
+* Save historical copies of flask/markdown pages
+* Restore a page to it's original content
+
+You can download my package [here](https://pypi.org/project/wiki-archive-combsl10/).
---
+++ packages/wiki-archive-combsl10/wiki-archive-combsl10-0.0.6.tar.gz/wiki-archive-combsl10-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wiki-archive-combsl10",
+    version="0.0.6",
+    author="Lucas Combs",
+    author_email="l-combs@hotmail.com",
+    description="An easy way to archive flash markdown pages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lcombs15/WikiArchive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
