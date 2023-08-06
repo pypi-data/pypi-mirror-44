dd1f130a1ca3b7a8eefed2e37ffde3268fd0283b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:15 +0000

    Add package molgenis-commander/1.3.1/molgenis_commander-1.3.1-py3-none-any.whl

---
+++ packages/molgenis-commander/molgenis_commander-1.3.1-py3-none-any.whl/mcmd/client/molgenis_client.py
@@ -0,0 +1,84 @@
+import json
+from urllib.parse import urljoin
+
+import requests
+
+from mcmd.client import auth
+from mcmd.client.request_handler import request
+from mcmd.config import config
+
+
+@request()
+def get(url):
+    return requests.get(url,
+                        headers=_get_default_headers())
+
+
+@request()
+def post(url, data):
+    return requests.post(url,
+                         headers=_get_default_headers(),
+                         data=json.dumps(data))
+
+
+@request()
+def post_file(url, file_path, params):
+    return requests.post(url,
+                         headers={'x-molgenis-token': auth.get_token()},
+                         files={'file': open(file_path, 'rb')},
+                         params=params)
+
+
+@request()
+def post_files(files, url):
+    return requests.post(url,
+                         headers={'x-molgenis-token': auth.get_token()},
+                         files=files)
+
+
+@request()
+def post_form(url, data):
+    return requests.post(url,
+                         headers={
+                             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                             'x-molgenis-token': auth.get_token()},
+                         data=data)
+
+
+@request()
+def delete(url):
+    return requests.delete(url,
+                           headers=_get_default_headers())
+
+
+@request()
+def delete_data(url, data):
+    return requests.delete(url,
+                           headers=_get_default_headers(),
+                           data=json.dumps({"entityIds": data}))
+
+
+@request()
+def put(url, data):
+    return requests.put(url=url,
+                        headers=_get_default_headers(),
+                        data=data)
+
+
+@request()
+def import_by_url(params):
+    return requests.post(config.api('import_url'),
+                         headers=_get_default_headers(),
+                         params=params)
+
+
+@request(login=False)
+def get_version():
+    return requests.get(urljoin(config.api('rest2'), 'version'),
+                        headers={'Content-Type': 'application/json'})
+
+
+def _get_default_headers():
+    headers = {'Content-Type': 'application/json',
+               'x-molgenis-token': auth.get_token()}
+    return headers
---
+++ packages/molgenis-commander/molgenis_commander-1.3.1-py3-none-any.whl/mcmd/commands/enable.py
@@ -0,0 +1,74 @@
+import mcmd.config.config as config
+from mcmd import io
+from mcmd.commands._registry import arguments
+from mcmd.client.molgenis_client import post
+from mcmd.utils.resources import one_resource_exists, ensure_resource_exists, ResourceType
+from mcmd.command import command
+from mcmd.io import highlight
+from mcmd.utils.errors import McmdError
+
+
+# =========
+# Arguments
+# =========
+
+@arguments('enable')
+def add_arguments(subparsers):
+    p_enable = subparsers.add_parser('enable',
+                                     help='Enable resources/functionality',
+                                     description="Run 'mcmd enable rls -h' to view the help for those sub-commands")
+    p_enable_subparsers = p_enable.add_subparsers(dest="type")
+
+    p_enable_rls = p_enable_subparsers.add_parser('rls',
+                                                  help='Enables row level security on an entity type')
+    p_enable_rls.set_defaults(func=enable_rls,
+                              write_to_history=True)
+    p_enable_rls.add_argument('entity',
+                              type=str,
+                              help="The entity type to secure")
+
+    p_enable_theme = p_enable_subparsers.add_parser('theme',
+                                                    help='Enables the bootstrap theme which changes the styling of your'
+                                                         ' MOLGENIS')
+    p_enable_theme.set_defaults(func=enable_theme,
+                                write_to_history=True)
+    p_enable_theme.add_argument('theme',
+                                type=str,
+                                help='The bootstrap theme you want to enable, specify the name with or without '
+                                     '(.min).css and with or without bootstrap- prefix.')
+
+
+# =======
+# Methods
+# =======
+
+@command
+def enable_rls(args):
+    io.start('Enabling row level security on entity type %s' % highlight(args.entity))
+
+    ensure_resource_exists(args.entity, ResourceType.ENTITY_TYPE)
+    post(config.api('rls'), data={'id': args.entity,
+                                  'rlsEnabled': True})
+
+
+@command
+def enable_theme(args):
+    """
+    enable_theme enables a bootstrap theme
+    :param args: commandline arguments containing the id of the theme (without .css)
+    :exception McmdError: when applying the theme fails
+    :return None
+    """
+    theme = args.theme.replace('.css', '').replace('.min', '')
+    io.start('Applying theme {}'.format(highlight(theme)))
+    # Resource can be bootstrap-name.min.css (if molgenis theme), or name.min.css (if uploaded .min.css), or
+    # name.css (if uploaded as .css).
+    if one_resource_exists([theme + '.min.css', theme + '.css', 'bootstrap-' + theme + '.min.css'], ResourceType.THEME):
+        # Molgenis themes start with bootstrap- but this is stripped from the name in the theme manager
+        try:
+            post(config.api('set_theme'), theme)
+        except:
+            post(config.api('set_theme'), theme.split('bootstrap-')[1])
+    else:
+        raise McmdError(
+            'Applying theme failed. No themes found containing {} in the name'.format(args.theme, 'sys_set_StyleSheet'))
