8c9d031c9814b372b942a70405f45a488aee6570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:56 +0000

    Add package boycesdk/0.0.3/boycesdk-0.0.3.tar.gz

---
+++ packages/boycesdk/boycesdk-0.0.3.tar.gz/boycesdk-0.0.3/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+from setuptools import setup,find_packages
+
+setup(
+    name = "boycesdk",
+    version = "0.0.3",
+    keyword = ("pip","boyce"),
+    description = "boyce sdk",
+    long_description = "boyce sdk for python",
+    license = "MIT Licence",
+    url = "http://www.keepstudying.club",
+    author = "boyce",
+    author_email = "120674007@qq.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],
+    scripts = [],
+    entry_points = {
+        'console_scripts':[
+            'boycesdk = boycesdk.help:main']
+    }
+)
