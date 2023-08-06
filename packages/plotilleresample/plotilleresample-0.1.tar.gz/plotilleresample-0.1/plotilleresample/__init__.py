1d2f6831ec590452c04eb9d1fdef728f098e9446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:12 +0000

    Add package plotilleresample/0.1/plotilleresample-0.1.tar.gz

---
+++ packages/plotilleresample/plotilleresample-0.1.tar.gz/plotilleresample-0.1/plotilleresample/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from servusresample.servusresample import resample_plot
+from servusresample.servusresample import resample_scatter
---
+++ packages/plotilleresample/plotilleresample-0.1.tar.gz/plotilleresample-0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="plotilleresample",
+    packages=["plotilleresample"],
+    version="0.1",
+    license="GPL3",
+    description="Python3 module to resample datasets"
+            "before plotting with Plotille.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Carlos A. Planchón",
+    author_email="bubbledoloresuruguay2@gmail.com",
+    url="https://github.com/carlosplanchon/plotilleresample",
+    download_url="https://github.com/carlosplanchon/"
+        "plotilleresample/archive/v0.1.tar.gz",
+    keywords=["plotting", "ascii", "math", "resample"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
