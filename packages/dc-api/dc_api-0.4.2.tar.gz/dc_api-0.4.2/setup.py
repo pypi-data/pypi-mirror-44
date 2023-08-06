d56d22ac8a6853d264ba15c1cfe614b015c0e150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package dc-api/0.4.2/dc_api-0.4.2.tar.gz

---
+++ packages/dc-api/dc_api-0.4.2.tar.gz/dc_api-0.4.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+try:
+    with open('README.md') as fh:
+        long_description = fh.read()
+except IOError as e:
+    long_description = ""
+
+setuptools.setup(
+    name="dc_api",
+    version="0.4.2",
+    author="Eunchul, Song",
+    author_email="eunchulsong9@gmail.com",
+    description="Deadly dimple unofficial dcinside api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/song9446/dcinside-python3-api",
+    #packages=setuptools.find_packages(),
+    py_modules=['dc_api'],
+    install_requires=[
+        'lxml',
+        'requests',
+    ],
+    entry_points     = """
+           [console_scripts]
+           dc_api = dc_api:dc_api
+       """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
