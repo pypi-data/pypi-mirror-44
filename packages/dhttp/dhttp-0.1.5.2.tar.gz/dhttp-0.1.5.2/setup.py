170df8836cf9389a9f87a52cf15f91a07e9bdbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:58 +0000

    Add package dhttp/0.1.5.2/dhttp-0.1.5.2.tar.gz

---
+++ packages/dhttp/dhttp-0.1.5.2.tar.gz/dhttp-0.1.5.2/setup.py
@@ -0,0 +1,27 @@
+import os
+import dhttp
+
+from setuptools import setup
+
+
+version = dhttp.DHTTP_VERSION
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "dhttp",
+    version = version,
+    author = "Gustavo Rehermann",
+    author_email = "rehermann6046@gmail.com",
+    description = ("A simple, but dynamic, decorator-based HTTP server inspired by Node.js's Express."),
+    license = "MIT",
+    keywords = "http server httpd express decorator simple easy dynamic",
+    packages=['dhttp'],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
