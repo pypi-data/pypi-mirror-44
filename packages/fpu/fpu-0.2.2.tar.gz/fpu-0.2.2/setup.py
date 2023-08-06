6085f3ceeb83ed85d43bb62d461dc7a5f5e3c3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:35 +0000

    Add package fpu/0.2.2/fpu-0.2.2.tar.gz

---
+++ packages/fpu/fpu-0.2.2.tar.gz/fpu-0.2.2/setup.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import fpu
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+long_description = read('README.md')
+
+setup(name='fpu',
+      version=fpu.__version__,
+      description='Functional Programming Utility',
+      url='https://github.com/johnklee/fpu',
+      author='johnklee',
+      author_email='puremonkey2007@gmail.com',
+      tests_require=['pytest'],
+      long_description=long_description,
+      license='MIT',
+      packages=['fpu'],
+      classifiers = [
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      zip_safe=False)
