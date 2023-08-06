8e31748ddfb26c3bc94a3d3203a6130f01c64fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:06 +0000

    Add package zonar-ds-logger/1.0.0/zonar_ds_logger-1.0.0.tar.gz

---
+++ packages/zonar-ds-logger/zonar_ds_logger-1.0.0.tar.gz/zonar_ds_logger-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setuptools.setup(
+    name="zonar_ds_logger",
+    version="1.0.0",
+    author="Zonar Systems - Data Services Pod",
+    author_email="dspod@zonarsystems.com",
+    description="Python logger helper to log in json and redirect flask logs",
+    long_description=long_description,
+    url="https://github.com/evcallia/zonar_ds_logger.git",
+    packages=setuptools.find_packages(where="."),
+    install_requires=required_packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
