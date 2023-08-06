6e32b7cf0200ff1538415461fa0dc2bff666144d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package seed-services-cli/0.0.13/seed-services-cli-0.0.13.tar.gz

---
+++ packages/seed-services-cli/seed-services-cli-0.0.13.tar.gz/seed-services-cli-0.0.13/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seed-services-cli",
+    version="0.0.13",
+    url='http://github.com/praekelt/seed-services-cli',
+    license='BSD',
+    description="A command-line interface for Seed Services HTTP APIs",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation and Individual Contributors',
+    author_email='support@praekeltfoundation.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'click-config',
+        'seed-services-client==0.37.0',
+        'demands==3.0.0',
+    ],
+    entry_points="""
+        [console_scripts]
+        seed-services-cli=seed_services_cli.main:cli
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
