11d0099e38779966a3239b42e2f6584680fc9fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:14 +0000

    Add package django-docusign/3.3/django-docusign-3.3.tar.gz

---
+++ packages/django-docusign/django-docusign-3.3.tar.gz/django-docusign-3.3/django_docusign/backend.py
@@ -0,0 +1,251 @@
+from __future__ import unicode_literals
+
+import pydocusign
+from django.conf import settings
+from django_anysign import api as django_anysign
+
+
+class DocuSignBackend(django_anysign.SignatureBackend):
+    def __init__(self, name='DocuSign', code='docusign',
+                 url_namespace='anysign', **kwargs):
+        """Setup.
+
+        Additional ``kwargs`` are proxied to
+        :class:`pydocusign.DocuSignClient`.
+
+        """
+        super(DocuSignBackend, self).__init__(
+            name=name,
+            code=code,
+            url_namespace=url_namespace,
+        )
+        client_kwargs = self.get_client_kwargs(**kwargs)
+        #: Instance of :class:`~pydocusign.client.DocuSignClient`
+        self.docusign_client = pydocusign.DocuSignClient(**client_kwargs)
+
+    def get_client_kwargs(self, **kwargs):
+        """Return keyword arguments for use with DocuSign client factory.
+
+        Uses, in order (the latter override the former):
+
+        1. ``settings.DOCUSIGN_*``
+        2. ``kwargs``
+
+        .. note::
+
+           :class:`pydocusign.DocuSignClient` itself uses environment
+           variables ``DOCUSIGN_*`` if available.
+
+        """
+        client_kwargs = {}
+        for key in ['root_url',
+                    'username',
+                    'password',
+                    'integrator_key',
+                    'account_id',
+                    'account_url',
+                    'app_token',
+                    'oauth2_token',
+                    'timeout']:
+            setting_name = 'DOCUSIGN_{0}'.format(key.upper())
+            try:
+                client_kwargs[key] = getattr(settings, setting_name)
+            except AttributeError:
+                pass
+        client_kwargs.update(kwargs)
+        return client_kwargs
+
+    def get_docusign_tabs(self, signer):
+        """Return list of pydocusign's tabs for Signer instance.
+
+        Default implementation returns no tab, i.e. the signer chooses where to
+        sign!
+
+        """
+        return []
+
+    def get_docusign_signers(self, signature):
+        """Return list of pydocusign's Signer for Signature instance.
+
+        Default implementation reads name and email from database.
+
+        """
+        signers = []
+        position = 1
+        for signer in signature.signers.all().order_by('signing_order'):
+            tabs = self.get_docusign_tabs(signer)
+            signer = pydocusign.Signer(
+                email=signer.email,
+                name=signer.full_name,
+                recipientId=signer.pk,
+                clientUserId=signer.pk,
+                routingOrder=position,
+                tabs=tabs,
+            )
+            signers.append(signer)
+            position += 1
+        return signers
+
+    def get_docusign_recipient(self, signer):
+        """
+        Get the recipient (dict) matching the given signer
+        """
+        response = self.docusign_client.get_envelope_recipients(
+            signer.signature.signature_backend_id)
+
+        # get recipient matching the signer
+        for recipient in response['signers']:
+            if recipient['clientUserId'] == str(signer.pk):
+                return recipient
+        else:
+            raise Exception("Failed to retrieve Signer")
+
+    def is_last_signer(self, signer):
+        """Return True if ``signer`` is the last signer for the signature
+        request.
+        """
+        return not signer.signature.signers.filter(
+            signing_order__gt=signer.signing_order).exists()
+
+    def get_docusign_roles(self, signature):
+        """Return list of pydocusign's Role for Signature instance.
+
+        Default implementation reads name, email and role name from database.
+
+        """
+        # Get docusign template definition, to retrieve role names
+        template_definition = self.docusign_client.get_template(
+            signature.signature_type.docusign_template_id)
+        template_roles = template_definition['recipients']['signers']
+        roles = []
+        # Build roles
+        for signer in signature.signers.all().order_by('signing_order'):
+            role = pydocusign.Role(
+                email=signer.email,
+                name=signer.full_name,
+                roleName=template_roles[signer.signing_order - 1]['roleName'],
+                clientUserId=signer.pk,
+            )
+            roles.append(role)
+        return roles
+
+    def get_docusign_documents(self, signature):
+        """Generate list of documents for ``signature`` model instance.
+
+        Ignores special document "certificate".
+
+        Yields file-like objects.
+
+        .. warning:: Close returned documents!
+
+        """
+        envelope_id = signature.signature_backend_id
+        document_list = self.docusign_client \
+                            .get_envelope_document_list(envelope_id)
+        for document_data in document_list:
+            document_id = document_data['documentId']
+            if document_id != 'certificate':
+                document = self.docusign_client \
+                               .get_envelope_document(envelope_id, document_id)
+                yield document
+
+    def create_signature_from_document(self, signature, subject='', blurb='',
+                                       sobo_email=None, **env_params):
+        """Register ``signature`` in DocuSign service, for a signature from
+        document.
+
+        """
+        # Prepare signers.
+        signers = self.get_docusign_signers(signature)
+
+        # Prepare documents.
+        documents = []
+        i = 1
+        for document in signature.signature_documents():
+            documents.append(
+                pydocusign.Document(
+                    name=document.name,
+                    documentId=i,
+                    data=document.bytes,
+                )
+            )
+            i += 1
+        # Create envelope with embedded signing.
+        envelope = pydocusign.Envelope(
+            emailSubject=subject,
+            emailBlurb=blurb,
+            status=pydocusign.Envelope.STATUS_SENT,
+            documents=documents,
+            recipients=signers,
+            sobo_email=sobo_email,
+            **env_params
+        )
+        envelope.envelopeId = self.docusign_client \
+                                  .create_envelope_from_documents(envelope)
+        return envelope
+
+    def create_signature_from_template(self, signature, subject='', blurb='',
+                                       sobo_email=None, **env_params):
+        """Register ``signature`` in DocuSign service, for a signature from
+        document.
+
+        """
+        # Prepare roles.
+        roles = self.get_docusign_roles(signature)
+        # Create envelope with embedded signing.
+        envelope = pydocusign.Envelope(
+            emailSubject=subject,
+            emailBlurb=blurb,
+            status=pydocusign.Envelope.STATUS_SENT,
+            templateId=signature.signature_type.docusign_template_id,
+            templateRoles=roles,
+            sobo_email=sobo_email,
+            **env_params
+        )
+        envelope.envelopeId = self.docusign_client \
+                                  .create_envelope_from_template(envelope)
+        return envelope
+
+    def create_signature(self, signature, subject='', blurb='',
+                         sobo_email=None, **env_params):
+        """Register ``signature`` in DocuSign service, return updated object.
+
+        This method calls ``save()`` on ``signature``.
+
+        """
+        if signature.signature_type.docusign_template_id:
+            envelope = self.create_signature_from_template(
+                signature, subject, blurb, sobo_email, **env_params)
+        else:
+            envelope = self.create_signature_from_document(
+                signature, subject, blurb, sobo_email, **env_params)
+        # Update signature instance with backend's ID.
+        signature.signature_backend_id = envelope.envelopeId
+        signature.save()
+        # Return updated object.
+        return signature
+
+    def post_recipient_view(self, signer, signer_return_url=None):
+        # Prepare signers.
+        docusign_signers = self.get_docusign_signers(signer.signature)
+        # Create envelope with embedded signing.
+        envelope = pydocusign.Envelope(
+            envelopeId=signer.signature.signature_backend_id,
+            recipients=docusign_signers,
+        )
+        docusign_signer = [ds for ds in docusign_signers
+                           if ds.clientUserId == signer.pk][0]
+        if signer_return_url is None:
+            signer_return_url = self.get_signer_return_url(signer)
+        envelope.get_recipients(client=self.docusign_client)
+        return envelope.post_recipient_view(
+            client=self.docusign_client,
+            recipient=docusign_signer,
+            returnUrl=signer_return_url
+        )
+
+    def get_page_image(self, signature, document_id, page_no, dpi=None,
+                       max_width=None, max_height=None):
+        envelope_id = signature.signature_backend_id
+        return self.docusign_client.get_page_image(
+            envelope_id, document_id, page_no, dpi, max_width, max_height)
---
+++ packages/django-docusign/django-docusign-3.3.tar.gz/django-docusign-3.3/setup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    """Test command that runs tox."""
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox  # import here, cause outside the eggs aren't loaded.
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+#: Absolute path to directory containing setup.py file.
+here = os.path.abspath(os.path.dirname(__file__))
+#: Boolean, ``True`` if environment is running Python version 2.
+IS_PYTHON2 = sys.version_info[0] == 2
+
+
+NAME = 'django-docusign'
+DESCRIPTION = 'Django application for DocuSign signature SAAS platform.'
+README = open(os.path.join(here, 'README.rst')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read().strip()
+AUTHOR = 'Benoît Bryon'
+EMAIL = 'novafloss@people-doc.com'
+LICENSE = 'BSD'
+URL = 'https://{name}.readthedocs.io/'.format(name=NAME)
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Framework :: Django',
+    'Framework :: Django :: 1.8',
+    'Framework :: Django :: 1.9',
+    'Framework :: Django :: 1.10',
+    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.0',
+    'Framework :: Django :: 2.1',
+]
+KEYWORDS = [
+    'docusign',
+    'signature',
+    'backend',
+    'pydocusign',
+    'django-anysign',
+]
+PACKAGES = [NAME.replace('-', '_')]
+REQUIREMENTS = [
+    'Django>=1.8',
+    'django-anysign>=1.2,<2.0',
+    'pydocusign>=2.2,<3.0',
+    'setuptools',
+]
+ENTRY_POINTS = {}
+SETUP_REQUIREMENTS = ['setuptools']
+TEST_REQUIREMENTS = ['tox']
+if IS_PYTHON2:
+    TEST_REQUIREMENTS.extend([
+        'mock',
+    ])
+CMDCLASS = {'test': Tox}
+EXTRA_REQUIREMENTS = {
+    'test': TEST_REQUIREMENTS,
+}
+
+
+if __name__ == '__main__':  # Do not run setup() when we import this module.
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=README,
+        classifiers=CLASSIFIERS,
+        keywords=' '.join(KEYWORDS),
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        packages=PACKAGES,
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=REQUIREMENTS,
+        entry_points=ENTRY_POINTS,
+        tests_require=TEST_REQUIREMENTS,
+        cmdclass=CMDCLASS,
+        setup_requires=SETUP_REQUIREMENTS,
+        extras_require=EXTRA_REQUIREMENTS,
+    )
