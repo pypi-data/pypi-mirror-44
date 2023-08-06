c88965c2ba739357de6b500993c836c97401dd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:34 +0000

    Add package autolens/0.18.0/autolens-0.18.0.tar.gz

---
+++ packages/autolens/autolens-0.18.0.tar.gz/autolens-0.18.0/autolens/exc.py
@@ -0,0 +1,45 @@
+from autofit import exc
+
+
+class ScaledArrayException(Exception):
+    pass
+
+
+class ImagingException(Exception):
+    pass
+
+
+class KernelException(Exception):
+    pass
+
+
+class MaskException(Exception):
+    pass
+
+
+class GalaxyException(Exception):
+    pass
+
+
+class RayTracingException(exc.FitException):
+    pass
+
+
+class PixelizationException(exc.FitException):
+    pass
+
+
+class InversionException(exc.FitException):
+    pass
+
+
+class FittingException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class PhaseException(Exception):
+    pass
