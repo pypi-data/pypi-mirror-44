ef3d80da74ca839462988450a7a1575cf33678a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:53 +0000

    Add package django-crosswalk/0.0.11/django_crosswalk-0.0.11-py3-none-any.whl

---
+++ packages/django-crosswalk/django_crosswalk-0.0.11-py3-none-any.whl/crosswalk/validators.py
@@ -0,0 +1,35 @@
+from crosswalk.exceptions import NestedAttributesError, ReservedKeyError
+
+
+def validate_shallow_dict(value):
+    """Only allow shallow dictionaries."""
+    if isinstance(value, dict):
+        for key in value:
+            if isinstance(value[key], dict):
+                raise NestedAttributesError(
+                    "Nested attributes are not allowed."
+                )
+
+
+def validate_no_reserved_keys(value):
+    reserved_keys = [
+        "alias_for",
+        "aliased",
+        "attributes",
+        "created",
+        "domain",
+        "entity",
+        "match_score",
+        "superseded_by",
+        "uuid",
+    ]
+
+    if isinstance(value, dict):
+        for key in value:
+            if key in reserved_keys:
+                raise ReservedKeyError("Reserved key in attributes.")
+
+
+def full_validation(value):
+    validate_shallow_dict(value)
+    validate_no_reserved_keys(value)
