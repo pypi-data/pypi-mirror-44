f4c719c2ba775bafb99ad3ae3a4edc85da85c06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package iotile-transport-websocket/2.0.3/iotile-transport-websocket-2.0.3.tar.gz

---
+++ packages/iotile-transport-websocket/iotile-transport-websocket-2.0.3.tar.gz/iotile-transport-websocket-2.0.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+import version
+# TODO: use 'ws' instead of 'ws2'
+
+setup(
+    name="iotile-transport-websocket",
+    packages=find_packages(exclude=("test",)),
+    version=version.version,
+    license="LGPLv3",
+    install_requires=[
+        "iotile-core>=4.0.0,<5",
+        "msgpack>=0.6.1,<1"
+    ],
+    python_requires=">=3.5,<4",
+    entry_points={
+        'iotile.device_adapter': [
+            'ws2 = iotile_transport_websocket.device_adapter:WebSocketDeviceAdapter'
+        ],
+        'iotile.virtual_interface': [
+            'websocket = iotile_transport_websocket.virtual_websocket:WebSocketVirtualInterface'
+        ],
+        'iotile.gateway_agent': [
+            'websockets2 = iotile_transport_websocket.gateway_agent:WebSocketGatewayAgent'
+        ]
+    },
+    description="IOTile Websocket Transport Plugin",
+    author="Arch",
+    author_email="info@arch-iot.com",
+    url="http://github.com/iotile/coretools",
+    keywords=["iotile", "arch", "embedded", "hardware", "firmware"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    long_description="""\
+IOTile Websocket Transport Plugin
+------
+
+A python plug for IOTile CoreTools that allows interacting with IOTile devices through
+websockets, including providing DeviceAdapters that allow controlling devices.
+"""
+)
