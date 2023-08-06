ac3777e4e4362090ba44f3d6f3f22fa3e7e807a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:47 +0000

    Add package pyhaversion/2.1.0/pyhaversion-2.1.0.tar.gz

---
+++ packages/pyhaversion/pyhaversion-2.1.0.tar.gz/pyhaversion-2.1.0/setup.py
@@ -0,0 +1,22 @@
+"""Setup configuration."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG = fh.read()
+setuptools.setup(
+    name="pyhaversion",
+    version="2.1.0",
+    author="Joakim Sorensen",
+    author_email="ludeeus@gmail.com",
+    description="",
+    long_description=LONG,
+    install_requires=['aiohttp', 'async_timeout'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludeeus/pyhaversion",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
