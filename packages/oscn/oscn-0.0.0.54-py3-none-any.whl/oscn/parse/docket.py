0f816f80483707b840a58f242e43d49040ac3262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:09 +0000

    Add package oscn/0.0.0.54/oscn-0.0.0.54-py3-none-any.whl

---
+++ packages/oscn/oscn-0.0.0.54-py3-none-any.whl/oscn/parse/docket.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from ._helpers import text_values, lists2dict
+
+
+def docket(oscn_html):
+    soup = BeautifulSoup(oscn_html, 'html.parser')
+    docket_table = soup.find('table', 'docketlist')
+    thead = docket_table.find('thead').find_all('th')
+    rows = docket_table.find('tbody').find_all('tr')
+    minutes = []
+
+    # make a lower case list of column headers
+    columns = [hdr for hdr in map(lambda str:str.lower(), text_values(thead))]
+
+    for row in rows:
+        cells = row.find_all('td')
+        values = text_values(cells)
+        minute = lists2dict(columns, values)
+        minute['links'] = list(lnk.decode() for lnk in row.find_all('a'))
+        minutes.append(minute)
+
+    # clean up blank dates
+    def dt_chk(min):
+        dt_chk.saved_date = min['date'] if min['date'] else dt_chk.saved_date
+        min['date'] = dt_chk.saved_date
+        return min
+
+    return [dt_chk(min) for min in minutes]
+
+setattr(docket, 'target', ['Case'])
+setattr(docket, '_default_value', [])
