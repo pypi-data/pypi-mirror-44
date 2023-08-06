380a8a780df0409d29168f6bc97c3f7ac491c057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:28 +0000

    Add package pipecashagents/0.1.1/pipecashagents-0.1.1-py2.7.egg

---
+++ packages/pipecashagents/pipecashagents-0.1.1-py2.7.egg/pipecashagents/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pipecashagents."""
+
+__author__ = """Aleksandar Nikolaev Dinkov"""
+__email__ = 'alexander.n.dinkov@gmail.com'
+__version__ = '0.1.1'
+
+from pipecashagents.Email.Email import *
+from pipecashagents.Exchanges.Binance import *
+from pipecashagents.WalletAgents.WalletAgents import *
+
+from pipecashagents.ExampleAgent import *
+
