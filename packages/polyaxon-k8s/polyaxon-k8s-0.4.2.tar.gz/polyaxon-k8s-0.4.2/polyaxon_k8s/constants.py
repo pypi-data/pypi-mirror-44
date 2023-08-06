7b873bf0fdc1cbcc9af23ca6c75b213d67fe50fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:36 +0000

    Add package polyaxon-k8s/0.4.2/polyaxon-k8s-0.4.2.tar.gz

---
+++ packages/polyaxon-k8s/polyaxon-k8s-0.4.2.tar.gz/polyaxon-k8s-0.4.2/polyaxon_k8s/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+K8S_API_VERSION_V1 = 'v1'
+K8S_API_VERSION_V1_BETA1 = 'extensions/v1beta1'
+K8S_PERSISTENT_VOLUME_KIND = 'PersistentVolume'
+K8S_PERSISTENT_VOLUME_CLAIM_KIND = 'PersistentVolumeClaim'
+K8S_CONFIG_MAP_KIND = 'ConfigMap'
+K8S_SECRET_KIND = 'Secret'
+K8S_POD_KIND = 'Pod'
+K8S_POD_TEMPLATE_KIND = 'PodTemplate'
+K8S_DEPLOYMENT_KIND = 'Deployment'
+K8S_SERVICE_KIND = 'Service'
+K8S_INGRESS_KIND = 'Ingress'
