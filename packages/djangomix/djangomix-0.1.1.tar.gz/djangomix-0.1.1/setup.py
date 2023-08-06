3850989b5edc4f139420c95aa66d39e579680341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:02 +0000

    Add package djangomix/0.1.1/djangomix-0.1.1.tar.gz

---
+++ packages/djangomix/djangomix-0.1.1.tar.gz/djangomix-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+version = '0.1.1'
+
+
+setup(
+    name='djangomix',
+    version=version,
+    keywords='Django Mix',
+    description='Django integration for Laravel Mix',
+    long_description=open('README.md').read(),
+
+    url='https://github.com/m-a-k-o/django-mix',
+
+    author='Marek Racik',
+    author_email='marek@racik.info',
+
+    packages=['django-mix'],
+    py_modules=[],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
