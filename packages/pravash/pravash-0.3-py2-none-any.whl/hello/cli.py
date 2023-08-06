52abdc7466a464f5eb81cd400923b1dff18c4fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:24 +0000

    Add package pravash/0.3/pravash-0.3-py2-none-any.whl

---
+++ packages/pravash/pravash-0.3-py2-none-any.whl/hello/cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+
+import click
+
+# from hello import helloworld
+
+def hello_world(first_name,last_name):
+    """ Log hello world on cli """
+    return "Hello "+first_name+" "+last_name
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command("hello")
+@click.option("-firstname", help="Enter First Name")
+@click.option("-lastname", help="Enter Last Name")
+def list(firstname, lastname):
+    """ Greet with first name and lastname """
+    greeting = hello_world(first_name, last_name)
+    click.echo("{}".format(greeting))
+
+if __name__ == "__main__":
+    cli()
