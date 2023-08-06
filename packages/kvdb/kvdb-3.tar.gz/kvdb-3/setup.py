3c90bcb64dddd67fb50292137e720022c150da32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:11 +0000

    Add package kvdb/3/kvdb-3.tar.gz

---
+++ packages/kvdb/kvdb-3.tar.gz/kvdb-3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='kvdb',
+    version='3',
+    url='https://github.com/rcbensley/mariadb-dba/kvdb',
+    description='Key-Value-Database',
+    packages=['kvdb'],
+    install_requires=['pymysql', ],
+    keywords='kvdb',
+)
