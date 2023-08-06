6cbf8268fc2bc2a3cb3f374eb60304c1b3d3145e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:55 +0000

    Add package Flask-Graphite/0.6.0/Flask-Graphite-0.6.0.tar.gz

---
+++ packages/Flask-Graphite/Flask-Graphite-0.6.0.tar.gz/Flask-Graphite-0.6.0/flask_graphite/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from graphitesend.graphitesend import GraphiteClient, GraphiteSendException
+
+from .hooks import MetricHook
+from .request_hooks import default_hooks
+
+logger = logging.getLogger("flask-graphite")
+
+
+class FlaskGraphite(object):
+    """Register a list of hooks meant to monitor a flask application
+
+    The configuration is read from the namespace `FLASK_GRAPHITE_` of the
+    application configuration.
+
+    The main options are:
+     - FLASK_GRAPHITE_HOST: The Carbon host to send metrics
+     - FLASK_GRAPHITE_PORT: The Carbon port to send metrics
+
+    You can read :ref:`Configuring Flask-Graphite` to learn about the other
+    option configurations.
+
+    :param app: The application to monitor
+    """
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Read config and set the hooks in place to monitor the application
+
+        :param app: The application to monitor
+        """
+        config = app.config.get_namespace("FLASK_GRAPHITE_")
+        logger.info("config: {}".format(config))
+
+        config["graphite_server"] = config.pop("host", "localhost")
+        config["graphite_port"] = config.pop("port", 2023)
+        config.setdefault("group", "flask-graphite")
+        config.setdefault("autoreconnect", True)
+        config.pop("metric_template", None)
+        try:
+            app.graphite = GraphiteClient(**config)
+        except GraphiteSendException as e:
+            logger.error("can't instanciate graphite client: {}".format(e))
+        else:
+            logger.info("graphite client instantiated with config: %s",
+                        config)
+            for hook in default_hooks:
+                hook.register_into(app)
+
+
+__all__ = ["FlaskGraphite", "default_hooks", "MetricHook"]
---
+++ packages/Flask-Graphite/Flask-Graphite-0.6.0.tar.gz/Flask-Graphite-0.6.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def get_description():
+    with open("README.rst") as file:
+        return file.read()
+
+
+setup(
+    name='Flask-Graphite',
+    version='0.6.0',
+    url='https://github.com/numberly/flask-graphite',
+    license='MIT',
+    description="Push useful metrics for each request without effort!",
+    long_description=get_description(),
+    author='numberly',
+    author_email='ramnes@1000mercis.com',
+    packages=['flask_graphite'],
+    include_package_data=True,
+    zip_safe=False,
+    platform='any',
+    install_requires=[
+        'Flask>=0.12',
+        'graphitesend>=0.10.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
