992069f6c95e3aa581a684d71cba9d35e3eb4a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:59 +0000

    Add package pyutil/3.2.0/pyutil-3.2.0.tar.gz

---
+++ packages/pyutil/pyutil-3.2.0.tar.gz/pyutil-3.2.0/pyutil/test/current/json_tests/test_dump.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8-with-signature-unix; fill-column: 77 -*-
+# -*- indent-tabs-mode: nil -*-
+from unittest import TestCase
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from pyutil import jsonutil as json
+
+class TestDump(TestCase):
+    def test_dump(self):
+        sio = StringIO()
+        json.dump({}, sio)
+        self.assertEqual(sio.getvalue(), '{}')
+
+    def test_dumps(self):
+        self.assertEqual(json.dumps({}), '{}')
