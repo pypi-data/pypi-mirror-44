30f1e0a970877bfb28e28a7df0ab52862c4f6702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:05 +0000

    Add package pyxpcm/0.3.1/pyxpcm-0.3.1.tar.gz

---
+++ packages/pyxpcm/pyxpcm-0.3.1.tar.gz/pyxpcm-0.3.1/pyxpcm/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. moduleauthor:: Guillaume Maze <gmaze@ifremer.fr>
+"""
+
+# Import class:
+from .pcmodel import pcm as pcm
+
+# Import
+from . import stats
+from . import plot
+from . import datasets
