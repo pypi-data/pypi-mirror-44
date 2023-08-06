8053ec9cc411ef90f9578de3eb33cfa4c86dfe03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:09 +0000

    Add package Mopidy-RadioNet/0.1.4/Mopidy-RadioNet-0.1.4.tar.gz

---
+++ packages/Mopidy-RadioNet/Mopidy-RadioNet-0.1.4.tar.gz/Mopidy-RadioNet-0.1.4/setup.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(filename):
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
+
+
+setup(
+    name='Mopidy-RadioNet',
+    version=get_version('mopidy_radionet/__init__.py'),
+    url='https://github.com/blackberrymamba/mopidy-radionet',
+    license='Apache License, Version 2.0',
+    author='blackberrymamba',
+    author_email='mariusz@typedef.pl',
+    description='Mopidy extension for radio.net',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    python_requires='>= 2.7, < 3',
+    install_requires=[
+        'setuptools',
+        'tornado >= 4.4, < 5',  # Tornado 5 requires Python >= 2.7.9
+        'Mopidy >= 1.0',
+        'Pykka >= 1.1',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'radionet = mopidy_radionet:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
