f3258cf64726738d8aad3e0055f4b232299097e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:22 +0000

    Add package peachapisec-slack/1.5.39/peachapisec-slack-1.5.39.tar.gz

---
+++ packages/peachapisec-slack/peachapisec-slack-1.5.39.tar.gz/peachapisec-slack-1.5.39/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup
+
+cwd = os.getcwd()
+try:
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    setup(
+        name = 'peachapisec-slack',
+        description = 'Peach API Security Slack Notifications',
+        long_description = open('README.rst').read(),
+        author = 'Peach Tech',
+        author_email = 'contact@peach.tech',
+        url = 'https://peach.tech',
+        version = '1.5.39',
+
+        py_modules = ['peach2slack'],
+
+        # update requirements.txt as well!
+        install_requires = [
+			'click~=6.7', 
+			'peachapisec-api==1.5.39', 
+			'requests>=2.18.4',
+			'pystache'
+			],
+		
+        entry_points = {
+            'console_scripts': ['peach2slack=peach2slack:run'],
+        },
+
+        license = 'Apache License, Version 2.0',
+        keywords = 'peach fuzzing security test rest',
+
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Topic :: Security',
+            'Topic :: Software Development :: Quality Assurance',
+            'Topic :: Software Development :: Testing',
+            'Topic :: Utilities',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3'
+        ])
+
+finally:
+    os.chdir(cwd)
+
+# end
