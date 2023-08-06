9dcadce31b73832f22c3616f29479867b7e7f3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:21 +0000

    Add package friendly-traceback/0.0.2/friendly-traceback-0.0.2.tar.gz

---
+++ packages/friendly-traceback/friendly-traceback-0.0.2.tar.gz/friendly-traceback-0.0.2/tests/raise_name_error.py
@@ -0,0 +1,14 @@
+"""Contains a function that should raise NameError"""
+# flake8: noqa
+
+
+def test():
+    """Should raise NameError"""
+    a = 1
+    b = c
+    d = 3
+
+if __name__ == "__main__":
+    import friendly_traceback
+    friendly_traceback.install()
+    test()
