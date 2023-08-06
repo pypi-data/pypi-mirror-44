a370c87b37dc2655144a83c0b534aeeed60c3ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package phonon0K/0.0.10/phonon0K-0.0.10-py3-none-any.whl

---
+++ packages/phonon0K/phonon0K-0.0.10-py3-none-any.whl/phonon/cell.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 12:00:17 2018
+
+@author: Gabriele Coiana
+"""
+import numpy as np
+import sys,os
+sys.path.insert(0, os.getcwd())
+
+import params
+import dynam
+import plot
+import bz
+import cell
+
+class BaTiO3:
+    def __init__(self,a):
+        self.a = a
+        self.mBa = 250331.80714328878 #[atomic units]
+        self.mTi = 87256.20316855246 #[atomic units]
+        self.mO = 29164.392890585812#[atomic units]
+        return
+        
+    def phaseI(self):
+        #self.Ti = np.array(np.meshgrid(np.arange(0.5,0.5+self.N1*self.a,self.a), np.arange(0.5,0.5+self.N2*self.a,self.a), np.arange(0.5,0.5+self.N3*self.a,self.a)))
+        self.Ti = self.a*np.array([[0.5,0.5,0.5]])
+        self.O = self.a*np.array([[0.5,0.5,0], [0.5,0,0.5], [0,0.5,0.5]])
+        self.Ba = self.a*np.array([[0,0,0]])
+        return
+    
+    def supercell(self, N1, N2, N3):
+        self.N1, self.N2, self.N3 = N1, N2, N3
+        xx,yy,zz = np.meshgrid(np.arange(0.5*self.a,0.5+self.N1*self.a,self.a), np.arange(0.5*self.a,0.5+self.N2*self.a,self.a), np.arange(0.5*self.a,0.5+self.N3*self.a,self.a))
+        xx, yy, zz = xx.reshape(1,N1*N2*N3)[0], yy.reshape(1,N1*N2*N3)[0], zz.reshape(1,N1*N2*N3)[0]
+        self.Ti = np.dstack((zz,xx,yy))[0]
+        
+        xx,yy,zz = np.meshgrid(np.arange(0.0,0.0+self.N1*self.a,self.a), np.arange(0.0,0.0+self.N2*self.a,self.a), np.arange(0.0,0.0+self.N3*self.a,self.a))
+        xx, yy, zz = xx.reshape(1,N1*N2*N3)[0], yy.reshape(1,N1*N2*N3)[0], zz.reshape(1,N1*N2*N3)[0]
+        self.Ba = np.dstack((zz,xx,yy))[0]
+        
+        xx,yy,zz = np.meshgrid(np.arange(0.5*self.a,0.5+self.N1*self.a,self.a), np.arange(0.5*self.a,0.5+self.N2*self.a,self.a), np.arange(0.0,0.0+self.N3*self.a,self.a))
+        xx, yy, zz = xx.reshape(1,N1*N2*N3)[0], yy.reshape(1,N1*N2*N3)[0], zz.reshape(1,N1*N2*N3)[0]
+        self.O3 = np.dstack((zz,xx,yy))[0]
+        
+        xx,yy,zz = np.meshgrid(np.arange(0.5*self.a,0.5+self.N1*self.a,self.a), np.arange(0.0,0.0+self.N2*self.a,self.a), np.arange(0.5*self.a,0.5+self.N3*self.a,self.a))
+        xx, yy, zz = xx.reshape(1,N1*N2*N3)[0], yy.reshape(1,N1*N2*N3)[0], zz.reshape(1,N1*N2*N3)[0]
+        self.O1 = np.dstack((zz,xx,yy))[0]
+        
+        xx,yy,zz = np.meshgrid(np.arange(0.0,0.0+self.N1*self.a,self.a), np.arange(0.5*self.a,0.5+self.N2*self.a,self.a), np.arange(0.5*self.a,0.5+self.N3*self.a,self.a))
+        xx, yy, zz = xx.reshape(1,N1*N2*N3)[0], yy.reshape(1,N1*N2*N3)[0], zz.reshape(1,N1*N2*N3)[0]
+        self.O2 = np.dstack((zz,xx,yy))[0]
+        return 
+        
+#bati = BaTiO3(4)
+#bati.supercell(2,2,2)
+#print(bati.O3)
+
+
+
+
+
+
+
+#Kr, a, epsilon, sigma, m = 15.18, 7.551345593761988, 0.00485678, 4.5, 1451.0
+#const = (Kr, a, epsilon, sigma)
+#R0 = np.concatenate((bati.Ba, bati.Ti, bati.O1, bati.O2, bati.O3))
+#dt, nSteps, T0, gamma, N = 1, 20, 300, 0.0, 5*8
+
+#Rtot, Vtot, Utot, Ktot, Etot, R2tot  = moleculardynamics.langevin(R0, dt, nSteps, T0, m, gamma, N, *const)
+
+#plot.plot(bati)
---
+++ packages/phonon0K/phonon0K-0.0.10-py3-none-any.whl/phonon/main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 18:21:44 2018
+
+@author: Gabriele Coiana
+"""
+import numpy as np
+
+import sys,os
+sys.path.insert(0, os.getcwd())
+
+import params
+import dynam
+import plot
+import bz
+import cell
+
+# =============================================================================
+# Parameters
+a = params.get_parameters()[0]
+mba, mti, mo = params.get_parameters()[1:4]
+N1,N2,N3 = params.get_parameters()[4:7]
+kinput = params.get_parameters()[7::][0]
+interp = params.get_parameters()[8]
+savefreq = params.get_parameters()[9]
+
+N1N2N3 = N1*N2*N3 # Number of cells
+N = N1*N2*N3*5    # Number of atoms
+# =============================================================================
+print('\nPho-NON-py')
+print()
+print('\nCalculating the k points...')
+ks, ks_scaled, kk = bz.getk(N1,N2,N3,kinput)
+print('Ok babe, Im gonna take all of these kpoints (scaled):\n',ks_scaled)
+
+
+print('\nComputing the stiffness matrix...')
+K = dynam.K(N1N2N3)
+print('\tK is equilibrated to within ',np.max(np.abs(np.sum(K,axis=1))))
+
+print('\nCalculating dynamical matrixes...')
+FREQ = []
+EIGVEC = []
+mode = str(sys.argv[1])
+for kpoint in ks:
+    D = dynam.D(kpoint,K,N1,N2,N3,mba,mti,mo,mode)
+    
+    ll = np.matrix(D)
+    print('\t',str(kpoint),' Hermitian: ',np.allclose(ll,ll.getH(),rtol=0.01,atol=0.01))
+    
+    eigvals, eigvec = np.linalg.eig(D)
+    idx = eigvals.argsort()[::-1]   
+    eigvals = eigvals[idx]  
+    eigvec = eigvec[:,idx]
+    freq = (np.sqrt(eigvals)/(2*np.pi))
+    FREQ.append(freq)
+    EIGVEC.append(eigvec)
+print()
+
+
+FREQ = np.array(FREQ).T
+
+if(savefreq):
+    np.savetxt('frequencies',np.column_stack([FREQ.real,FREQ.imag]))
+    np.savetxt('frequencies_niceview',FREQ, fmt='%-1.2d')
+    for i in range(len(EIGVEC)):
+        with open('eigvectors','ab') as f: 
+            np.savetxt(f,np.column_stack([EIGVEC[i].real, EIGVEC[i].imag]))
+
+
+
+plot.plot(ks_scaled,kk, FREQ,interp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
