63c06d4181ab7d876807927df141577903ea163d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:49 +0000

    Add package wxy-MySQL/0.0.6/wxy_MySQL-0.0.6.tar.gz

---
+++ packages/wxy-MySQL/wxy_MySQL-0.0.6.tar.gz/wxy_MySQL-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wxy_MySQL',
+    version='0.0.6',
+    description='MySQL基本增删改查封装',
+    url='https://gitee.com/wang_xing_yuan/wxy-MySQL.git',
+    author='cooper.wang',
+    author_email='',
+    license='MIT',
+    packages=['wxy_MySQL'],
+    install_requires = ["pymysql"]
+)
