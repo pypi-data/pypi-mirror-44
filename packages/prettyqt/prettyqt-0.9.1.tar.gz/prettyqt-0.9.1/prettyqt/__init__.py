059fb1fc510ce0b3977173aa85d65b16db54c1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:26 +0000

    Add package prettyqt/0.9.1/prettyqt-0.9.1.tar.gz

---
+++ packages/prettyqt/prettyqt-0.9.1.tar.gz/prettyqt-0.9.1/prettyqt/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PrettyQt."""
+
+from . import core
+from . import widgets
+
+__author__ = """Philipp Temminghoff"""
+__email__ = 'phil65@kodi.tv'
+__version__ = '0.9.1'
+
+__all__ = ["core", "widgets", "gui"]
