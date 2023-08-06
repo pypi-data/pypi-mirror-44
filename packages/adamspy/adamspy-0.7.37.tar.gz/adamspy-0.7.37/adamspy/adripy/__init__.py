bc9dc1ab2c92c7e1da78ae452347ed55e6c8d963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:15 +0000

    Add package adamspy/0.7.37/adamspy-0.7.37.tar.gz

---
+++ packages/adamspy/adamspy-0.7.37.tar.gz/adamspy-0.7.37/adamspy/adripy/__init__.py
@@ -0,0 +1,20 @@
+"""adripy is a set of python tools for manipulating MSC Adams Drill files and data.
+"""
+from .utilities import * 
+
+import jinja2
+
+TMPLT_ENV = jinja2.Environment(
+    loader=jinja2.PackageLoader('adamspy.adripy', 'templates'),
+    autoescape=False,
+    keep_trailing_newline=True,
+    trim_blocks=True,
+    lstrip_blocks=True
+)
+
+from .tool import DrillTool
+from .event import DrillEvent
+from .string import DrillString
+from .solver_settings import DrillSolverSettings
+from .drillsim import DrillSim
+from .hole import DrillHole
