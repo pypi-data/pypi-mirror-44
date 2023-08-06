51df7fadf1cd87b75077e55e2344725cc6a3cade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:10 +0000

    Add package treeview/1.1.6/treeview-1.1.6.tar.gz

---
+++ packages/treeview/treeview-1.1.6.tar.gz/treeview-1.1.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='treeview',  
+    version='1.1.6',
+    scripts=['treeview'] ,
+    author="Low Yiyiu",
+    author_email="lowyiyiu@gmail.com",
+    description="Folder Strucutre Visualizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lowyiyiu/treeview",
+    packages=setuptools.find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
