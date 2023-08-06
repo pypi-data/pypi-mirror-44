3198e00cdac01b1fa775b3a26d3747b588623290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:03 +0000

    Add package custom_cli/0.1/custom_cli-0.1.tar.gz

---
+++ packages/custom_cli/custom_cli-0.1.tar.gz/custom_cli-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='custom_cli',
+      version='0.1',
+      description='Demo of custom cli',
+      url='https://gitlab.com/cicd-automation/custom_cli.git',
+      author='Long Ly',
+      author_email='lyhoanglong94@gmail.com',
+      license='MIT',
+      packages=['custom_cli'],
+      zip_safe=False)
