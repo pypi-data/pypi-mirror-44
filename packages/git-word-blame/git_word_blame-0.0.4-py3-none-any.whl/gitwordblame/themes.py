ea637e02a64049082562a7011a7810f757211a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package git-word-blame/0.0.4/git_word_blame-0.0.4-py3-none-any.whl

---
+++ packages/git-word-blame/git_word_blame-0.0.4-py3-none-any.whl/gitwordblame/themes.py
@@ -0,0 +1,55 @@
+THEMES = {
+    'solarized-light': """
+body {
+    border: 10px solid #eee8d5;
+    background-color: #fdf6e3;
+    color: #586e75;
+}
+
+#content > a:hover, #content > a.hovered {
+    background: #93a1a1;
+    color: #fdf6e3;
+}
+""",
+   'black-and-white': """
+body {
+    margin: 10px;
+    border: 1px solid #ececec;
+}
+
+#content > a:hover, #content > a.hovered {
+    background: black;
+    color: white;
+}
+""",
+    'solarized-dark': """
+body {
+    background-color: #2d363a;
+    color: #94a3a5;
+}
+
+#content > a:hover, #content > a.hovered {
+    background: #94a3a5;
+    color: #2d363a;
+}
+""",
+}
+
+COMMON_CSS = """
+body {
+    margin: 0;
+    padding: 10px;
+}
+#content > a {
+    color: inherit;
+    text-decoration: none;
+}
+#content {
+    white-space: pre-wrap;
+    word-wrap: anywhere;
+    margin-top: 0;
+} 
+#content > a:hover, #content > a.hovered {
+    cursor: pointer;
+}
+"""
