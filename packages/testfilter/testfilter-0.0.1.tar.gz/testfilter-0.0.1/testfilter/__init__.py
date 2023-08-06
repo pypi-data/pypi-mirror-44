4ccc4775a36712fe5c185d58276ee422ec28e144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:37 +0000

    Add package testfilter/0.0.1/testfilter-0.0.1.tar.gz

---
+++ packages/testfilter/testfilter-0.0.1.tar.gz/testfilter-0.0.1/testfilter/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-  
+# __auth__ = mocobk
+# email: mailmzb@qq.com
+from .filter import tag
+from .filter import Tag
+from .filter import level
+from .filter import Level
+from .filter import Meta
+
+__all__ = ['tag', 'Tag', 'level', 'Level', 'Meta']
+
