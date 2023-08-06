474f12e62d8331f7fcde54faad3ccbff50155255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:31 +0000

    Add package aiohttp-i18n/0.0.4/aiohttp_i18n-0.0.4-py3-none-any.whl

---
+++ packages/aiohttp-i18n/aiohttp_i18n-0.0.4-py3-none-any.whl/aiohttp_i18n/__init__.py
@@ -0,0 +1,9 @@
+from .i18n import _
+from .i18n import babel_middleware
+from .i18n import set_default_locale
+from .i18n import load_gettext_translations
+
+
+__all__ = [
+    '_', 'babel_middleware', 'set_default_locale', 'load_gettext_translations'
+]
