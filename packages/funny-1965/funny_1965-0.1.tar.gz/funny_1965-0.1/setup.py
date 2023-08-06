95f257e0d4631aab2708241f41c21987474ec41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:21 +0000

    Add package funny-1965/0.1/funny_1965-0.1.tar.gz

---
+++ packages/funny-1965/funny_1965-0.1.tar.gz/funny_1965-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funny_1965',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/BeshoyAnwar/funny_1965',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funny_1965'],
+      zip_safe=False)
