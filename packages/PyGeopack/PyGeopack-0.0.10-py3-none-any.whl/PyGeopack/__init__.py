a6a057628a476e4614438fbf10cdc1aca5ce7dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:57 +0000

    Add package PyGeopack/0.0.10/PyGeopack-0.0.10-py3-none-any.whl

---
+++ packages/PyGeopack/PyGeopack-0.0.10-py3-none-any.whl/PyGeopack/__init__.py
@@ -0,0 +1,24 @@
+from . import Globals
+from ._CheckFirstImport import _CheckFirstImport
+_CheckFirstImport()
+
+from .FreeTSData import FreeTSData
+from .GSEtoSMUT import GSEtoSMUT
+from .GSEtoGSMUT import GSEtoGSMUT
+from .MLTtoMLONUT import MLTtoMLONUT
+from .SMtoGSEUT import SMtoGSEUT
+from .GSEtoMAGUT import GSEtoMAGUT
+from .MLONtoMLTUT import MLONtoMLTUT
+from .LoadTSData import LoadTSData
+from .ModelField import ModelField
+from .MAGtoGSEUT import MAGtoGSEUT
+from .SetCustParam import SetCustParam
+from .GSMtoSMUT import GSMtoSMUT
+from .GetModelParams import GetModelParams
+from .MAGtoGEOUT import MAGtoGEOUT
+from .TraceField import TraceField
+from .SMtoGSMUT import SMtoGSMUT
+from .GSMtoGSEUT import GSMtoGSEUT
+from .GEOtoMAGUT import GEOtoMAGUT
+from .__del__ import __del__
+from .UpdateParameters import UpdateParameters
