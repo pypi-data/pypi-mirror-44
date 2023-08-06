16618ac9543bdf3269f1abeec5b969f35e7428d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:30 +0000

    Add package ibm-ai-openscale/2.1.4/ibm_ai_openscale-2.1.4-py3-none-any.whl

---
+++ packages/ibm-ai-openscale/ibm_ai_openscale-2.1.4-py3-none-any.whl/tests/svt/_test_azure_scikit_multiclass_iris_performance_quality_and_explainability_monitoring.py
@@ -0,0 +1,328 @@
+# ----------------------------------------------------------------------------------------------------
+# IBM Confidential
+# OCO Source Materials
+# 5900-A3Q, 5737-J33
+# Copyright IBM Corp. 2018
+# The source code for this program is not published or other-wise divested of its trade 
+# secrets, irrespective of what has been deposited with the U.S.Copyright Office.
+# ----------------------------------------------------------------------------------------------------
+
+import time
+
+import pandas as pd
+import requests
+
+from assertions import *
+from ibm_ai_openscale import APIClient, APIClient4ICP
+from ibm_ai_openscale.engines import *
+from ibm_ai_openscale.supporting_classes import PayloadRecord
+from ibm_ai_openscale.supporting_classes.enums import InputDataType, ProblemType
+from ibm_ai_openscale.utils.training_stats import TrainingStats
+from preparation_and_cleaning import *
+
+
+class TestAIOpenScaleClient(unittest.TestCase):
+    deployment_uid = None
+    model_uid = None
+    subscription_uid = None
+    binding_uid = None
+    scoring_url = None
+    labels = None
+    ai_client = None
+    wml_client = None
+    subscription = None
+    test_uid = str(uuid.uuid4())
+    source_uid = None
+    transaction_id = None
+
+    scoring_records = 10
+    feedback_records = 10
+
+    # Azure configuration
+    credentials = {
+        "client_id": "29f007c5-4c45-4210-8a88-9a40136f0ddd",
+        "client_secret": "e4d8b0fa-73f7-4b77-83a7-0b424d92940f",
+        "subscription_id": "744bca72-2299-451c-b682-ed6fb75fb671",
+        "tenant": "fcf67057-50c9-4ad4-98f3-ffca64add9e9"
+    }
+
+    def score(self, subscription_details):
+        scoring_url = subscription_details['entity']['deployments'][0]['scoring_endpoint']['url']
+
+        data = {
+            "Inputs": {
+                "input1":
+                    [
+                        {
+                            'sepal_length': "1.1",
+                            'sepal_width': "15.5",
+                            'petal_length': "1.4",
+                            'petal_width': "1.2",
+                        }
+                    ],
+            },
+            "GlobalParameters": {
+            }
+        }
+
+        body = str.encode(json.dumps(data))
+
+        token = subscription_details['entity']['deployments'][0]['scoring_endpoint']['credentials']['token']
+        headers = subscription_details['entity']['deployments'][0]['scoring_endpoint']['request_headers']
+        headers['Authorization'] = ('Bearer ' + token)
+
+        start_time = time.time()
+        response = requests.post(url=scoring_url, data=body, headers=headers)
+        response_time = time.time() - start_time
+        result = response.json()
+        print('Scoring results: ' + json.dumps(result, indent=2))
+
+        request = data
+        response = result
+
+        return request, response, response_time
+
+    @classmethod
+    def setUpClass(cls):
+        cls.schema = get_schema_name()
+        cls.aios_credentials = get_aios_credentials()
+        cls.database_credentials = get_database_credentials()
+
+        if "ICP" in get_env():
+            cls.ai_client = APIClient4ICP(cls.aios_credentials)
+        else:
+            cls.ai_client = APIClient(cls.aios_credentials)
+
+        prepare_env(cls.ai_client)
+
+    def test_01_setup_data_mart(self):
+        self.ai_client.data_mart.setup(db_credentials=self.database_credentials, schema=self.schema)
+        details = TestAIOpenScaleClient.ai_client.data_mart.get_details()
+        assert_datamart_details(details, schema=self.schema, state='active')
+
+    def test_02_bind_azure(self):
+        TestAIOpenScaleClient.binding_uid = TestAIOpenScaleClient.ai_client.data_mart.bindings.add("Azure ml engine", AzureMachineLearningInstance(self.credentials))
+        print("Binding uid: {}".format(self.binding_uid))
+
+    def test_03_get_binding_details(self):
+        print("Binding details: {}".format(self.ai_client.data_mart.bindings.get_details(self.binding_uid)))
+        self.ai_client.data_mart.bindings.list()
+
+    def test_04_get_assets(self):
+        assets_uids = TestAIOpenScaleClient.ai_client.data_mart.bindings.get_asset_uids()
+        self.assertGreater(len(assets_uids), 1)
+        print('Assets uids: ' + str(assets_uids))
+
+        TestAIOpenScaleClient.ai_client.data_mart.bindings.list_assets()
+        asset_details = TestAIOpenScaleClient.ai_client.data_mart.bindings.get_asset_details()
+        print('Assets details: ' + str(asset_details))
+
+        for detail in asset_details:
+            if 'Irisclassificati.me2018.12.4.10.40.56.847' in detail['name']:
+                TestAIOpenScaleClient.source_uid = detail['source_uid']
+                print(detail)
+
+        self.assertIsNotNone(TestAIOpenScaleClient.source_uid)
+
+    def test_05_subscribe_azure_asset(self):
+        # https://github.ibm.com/aiopenscale/tracker/issues/5134
+
+        subscription = TestAIOpenScaleClient.ai_client.data_mart.subscriptions.add(
+            AzureMachineLearningAsset(source_uid=TestAIOpenScaleClient.source_uid,
+                                      binding_uid=TestAIOpenScaleClient.binding_uid,
+                                      input_data_type=InputDataType.STRUCTURED,
+                                      problem_type=ProblemType.MULTICLASS_CLASSIFICATION,
+                                      label_column='species',
+                                      feature_columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
+                                      categorical_columns=[],
+                                      prediction_column='prediction'))
+
+        TestAIOpenScaleClient.subscription_uid = subscription.uid
+        print('Subscription details: {}'.format(subscription.get_details()))
+
+    def test_06_get_subscription_details(self):
+        TestAIOpenScaleClient.subscription = TestAIOpenScaleClient.ai_client.data_mart.subscriptions.get(TestAIOpenScaleClient.subscription_uid)
+        details = self.subscription.get_details()
+
+        assert_subscription_details(subscription_details=details, no_deployments=1, text_included='azureml', enabled_monitors=['payload_logging', 'performance_monitoring'])
+
+    def test_07_list_deployments(self):
+        TestAIOpenScaleClient.subscription.list_deployments()
+
+    def test_08_get_payload_logging_details(self):
+        payload_logging_details = self.subscription.payload_logging.get_details()
+        assert_payload_logging_configuration(payload_logging_details=payload_logging_details, dynamic_schema_update=True)
+
+    def test_09_get_performance_monitoring_details(self):
+        performance_monitoring_details = self.subscription.performance_monitoring.get_details()
+        assert_performance_monitoring_configuration(performance_monitoring_details=performance_monitoring_details)
+
+    def test_10_score_model_and_log_payload(self):
+        subscription_details = TestAIOpenScaleClient.subscription.get_details()
+
+        request, response, response_time = self.score(subscription_details)
+        records_list = []
+
+        TestAIOpenScaleClient.scoring_records = 10
+        for i in range(0, self.feedback_records):
+            records_list.append(PayloadRecord(request=request, response=response, response_time=int(response_time)))
+
+        TestAIOpenScaleClient.subscription.payload_logging.store(records=records_list)
+        time.sleep(10)
+
+    def test_11_stats_on_payload_logging_table(self):
+        self.subscription.payload_logging.print_table_schema()
+        self.subscription.payload_logging.show_table()
+        self.subscription.payload_logging.describe_table()
+
+        table_content = self.subscription.payload_logging.get_table_content()
+        assert_payload_logging_pandas_table_content(pandas_table_content=table_content, scoring_records=self.scoring_records)
+
+        python_table_content = self.subscription.payload_logging.get_table_content(format='python')
+        assert_payload_logging_python_table_content(python_table_content=python_table_content, fields=['sepal_length', 'petal_width', 'petal_length', 'sepal_width', 'prediction'])
+
+    def test_12_stats_on_performance_monitoring_table(self):
+        self.subscription.performance_monitoring.print_table_schema()
+        self.subscription.performance_monitoring.show_table()
+        self.subscription.performance_monitoring.describe_table()
+
+        performance_table_pandas = self.subscription.performance_monitoring.get_table_content()
+        assert_performance_monitoring_pandas_table_content(pandas_table_content=performance_table_pandas)
+
+        performance_table_python = self.subscription.performance_monitoring.get_table_content(format='python')
+        assert_performance_monitoring_python_table_content(python_table_content=performance_table_python)
+
+    def test_13_setup_quality_monitoring(self):
+        TestAIOpenScaleClient.subscription.quality_monitoring.enable(threshold=0.8, min_records=5)
+
+    def test_14_get_quality_monitoring_details(self):
+        details = TestAIOpenScaleClient.subscription.quality_monitoring.get_details()
+        assert_quality_monitoring_configuration(quality_monitoring_details=details)
+
+    def test_15_send_feedback_data(self):
+        feedback_records = []
+        fields = ['petal_length', 'sepal_length', 'sepal_width', 'petal_width', 'species']
+
+        TestAIOpenScaleClient.feedback_records = 15
+        for i in range(0, self.feedback_records):
+            feedback_records.append(["1.1", "15.5", "1.4", "1.2", "Setosa"])
+
+        self.subscription.feedback_logging.store(feedback_records, fields=fields)
+        time.sleep(10)
+
+    def test_16_stats_on_feedback_logging_table(self):
+        self.subscription.feedback_logging.show_table()
+        self.subscription.feedback_logging.print_table_schema()
+        self.subscription.feedback_logging.describe_table()
+
+        feedback_pd = self.subscription.feedback_logging.get_table_content(format='pandas')
+        assert_feedback_pandas_table_content(pandas_table_content=feedback_pd, feedback_records=self.feedback_records)
+
+    def test_17_run_quality_monitor(self):
+        run_details = TestAIOpenScaleClient.subscription.quality_monitoring.run()
+
+        status = run_details['status']
+        id = run_details['id']
+        start_time = time.time()
+        elapsed_time = 0
+
+        while status != 'completed' and elapsed_time < 60:
+            time.sleep(10)
+            run_details = TestAIOpenScaleClient.subscription.quality_monitoring.get_run_details(run_uid=id)
+            status = run_details['status']
+            elapsed_time = time.time() - start_time
+            print("Details: {}".format(run_details))
+            self.assertNotIn('failed', run_details)
+
+        self.assertEqual('completed', status)
+
+    def test_17_stats_on_quality_monitoring_table(self):
+        self.subscription.quality_monitoring.print_table_schema()
+        self.subscription.quality_monitoring.show_table()
+        self.subscription.quality_monitoring.show_table(limit=None)
+        self.subscription.quality_monitoring.describe_table()
+
+        quality_monitoring_table = self.subscription.quality_monitoring.get_table_content()
+        assert_quality_monitoring_pandas_table_content(pandas_table_content=quality_monitoring_table)
+
+        quality_metrics = self.subscription.quality_monitoring.get_table_content(format='python')
+        assert_quality_monitoring_python_table_content(python_table_content=quality_metrics)
+
+    # SKIPPED FOR NOW
+    #
+    # def test_18_setup_explainability(self):
+    #
+    #     data_df = pd.read_csv(
+    #         "./datasets/iris/iris_dataset.csv"
+    #     )
+    #
+    #     TestAIOpenScaleClient.subscription.explainability.enable(training_data=data_df)
+    #
+    # def test_21_get_explainability_details(self):
+    #     details = TestAIOpenScaleClient.subscription.explainability.get_details()
+    #     assert_explainability_configuration(explainability_details=details)
+    #
+    # def test_22_get_transaction_id(self):
+    #     python_table_content = self.subscription.payload_logging.get_table_content(format='python')
+    #     no_payloads = len(python_table_content['values'])
+    #
+    #     # select random record from payload table
+    #     import random
+    #     random_record = random.randint(0, no_payloads-1)
+    #     TestAIOpenScaleClient.transaction_id = python_table_content['values'][random_record][0]
+    #
+    #     print("Selected trainsaction id: {}".format(self.transaction_id))
+    #
+    # def test_23_run_explainability(self):
+    #     self.skipTest(reason="Skipped due to issue: https://github.ibm.com/aiopenscale/tracker/issues/5138")
+    #
+    #     explainability_run = TestAIOpenScaleClient.subscription.explainability.run(
+    #         transaction_id=self.transaction_id,
+    #         background_mode=False
+    #     )
+    #     assert_explainability_run(explainability_run_details=explainability_run)
+    #
+    # def test_24_stats_on_explainability_table(self):
+    #     self.skipTest(reason="Skipped due to issue: https://github.ibm.com/aiopenscale/tracker/issues/5138")
+    #
+    #     TestAIOpenScaleClient.subscription.explainability.print_table_schema()
+    #     TestAIOpenScaleClient.subscription.explainability.show_table()
+    #     TestAIOpenScaleClient.subscription.explainability.describe_table()
+    #
+    #     pandas_df = TestAIOpenScaleClient.subscription.explainability.get_table_content()
+    #     assert_explainability_pandas_table_content(pandas_table_content=pandas_df)
+    #
+    #     python_table_content = TestAIOpenScaleClient.subscription.explainability.get_table_content(format='python')
+    #     assert_explainability_python_table_content(python_table_content=python_table_content)
+
+    def test_25_disable_monitors(self):
+        self.subscription.payload_logging.disable()
+        self.subscription.performance_monitoring.disable()
+        self.subscription.quality_monitoring.disable()
+        self.subscription.explainability.disable()
+        self.subscription.fairness_monitoring.disable()
+
+        subscription_details = self.subscription.get_details()
+        assert_monitors_enablement(subscription_details=subscription_details)
+
+    def test_26_get_metrics(self):
+        print(TestAIOpenScaleClient.ai_client.data_mart.get_deployment_metrics())
+        print(TestAIOpenScaleClient.ai_client.data_mart.get_deployment_metrics(deployment_uid=TestAIOpenScaleClient.deployment_uid))
+        print(TestAIOpenScaleClient.ai_client.data_mart.get_deployment_metrics(subscription_uid=TestAIOpenScaleClient.subscription.uid))
+        print(TestAIOpenScaleClient.ai_client.data_mart.get_deployment_metrics(asset_uid=TestAIOpenScaleClient.subscription.source_uid))
+        print(TestAIOpenScaleClient.ai_client.data_mart.get_deployment_metrics(metric_type='quality'))
+
+    def test_27_unsubscribe(self):
+        TestAIOpenScaleClient.ai_client.data_mart.subscriptions.delete(TestAIOpenScaleClient.subscription.uid)
+
+    def test_28_unbind(self):
+        TestAIOpenScaleClient.ai_client.data_mart.bindings.delete(TestAIOpenScaleClient.binding_uid)
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Deleting DataMart.")
+        cls.ai_client.data_mart.delete()
+
+
+if __name__ == '__main__':
+    unittest.main()
