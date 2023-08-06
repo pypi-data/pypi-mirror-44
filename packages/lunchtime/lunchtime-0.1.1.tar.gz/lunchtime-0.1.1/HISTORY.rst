faae7d37f58931919b03113fca7887ad8b027320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:42 +0000

    Add package lunchtime/0.1.1/lunchtime-0.1.1.tar.gz

---
+++ packages/lunchtime/lunchtime-0.1.1.tar.gz/lunchtime-0.1.1/HISTORY.rst
@@ -0,0 +1,13 @@
+=======
+History
+=======
+
+0.1.0 (2019-04-09)
+------------------
+
+* First release on PyPI.
+
+0.1.1 (2019-04-10)
+==================
+
+* Change prompt to user + current working directory.
---
+++ packages/lunchtime/lunchtime-0.1.1.tar.gz/lunchtime-0.1.1/tests/test_lunchtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import pytest
+from click.testing import CliRunner
+# from lunchtime import lunchtime
+from lunchtime import cli
+
+
+# def test_cli():
+#     """Test the CLI."""
+#     runner = CliRunner()
+#     result = runner.invoke(cli.main)
+#     assert result.exit_code == 0
+#     assert 'lunchtime.cli.main' in result.output
+
+
+def test_cli_help():
+    """Test the CLI help."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ["--help"])
+    assert result.exit_code == 0
+    assert "Show this message and exit." in result.output
