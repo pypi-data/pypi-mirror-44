b75425a956eaa19375eb53fdff9e8805c6a5742f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package cfdiclient/1.0.4/cfdiclient-1.0.4-py3-none-any.whl

---
+++ packages/cfdiclient/cfdiclient-1.0.4-py3-none-any.whl/cfdiclient/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from cfdiclient.autenticacion import Autenticacion
+from cfdiclient.solicitadescarga import SolicitaDescarga
+from cfdiclient.verificasolicituddescarga import VerificaSolicitudDescarga
+from cfdiclient.descargamasiva import DescargaMasiva
+from cfdiclient.fiel import Fiel
+
+name = "cfdiclient"
