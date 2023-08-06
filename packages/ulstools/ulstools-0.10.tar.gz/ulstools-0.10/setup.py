2e5045226dbb7c84ca873cf032295ee8234d38e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:19 +0000

    Add package ulstools/0.10/ulstools-0.10.tar.gz

---
+++ packages/ulstools/ulstools-0.10.tar.gz/ulstools-0.10/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jussi (jnu@iki.fi)
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(name='ulstools',
+      version='0.10',
+      description='Miscellaneous tools for ULS',
+      author='Jussi Nurminen',
+      author_email='jnu@iki.fi',
+      license='GPLv3',
+      url='https://github.com/jjnurminen/ulstools',
+      packages=find_packages(),
+      )
