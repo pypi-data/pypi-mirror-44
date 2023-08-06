5ab0f32a84b059d5ca668c75f4566d4e9b1c31ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:33 +0000

    Add package iranlowo/0.0.0/iranlowo-0.0.0.zip

---
+++ packages/iranlowo/iranlowo-0.0.0.zip/iranlowo-0.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+url: https://github.com/Niger-Volta-LTI
+download_url: archive/v{version}.tar.gz
+"""
+
+
+from setuptools import setup
+
+
+setup(
+    name="iranlowo",
+    setup_requires="setupmeta",
+    # versioning="dev",
+    license="MIT",
+    author="Ruoho Ruotsi ruoho.ruotsi@gmail.com",
+    description="Utility package for analysis & (pre)processing of Yorùbá text"
+)
