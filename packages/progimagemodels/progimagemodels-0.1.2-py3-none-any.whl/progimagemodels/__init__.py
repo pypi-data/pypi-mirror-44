a354ee0ecdeda4846d10cd89e8dcd2b874a515f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:50 +0000

    Add package progimagemodels/0.1.2/progimagemodels-0.1.2-py3-none-any.whl

---
+++ packages/progimagemodels/progimagemodels-0.1.2-py3-none-any.whl/progimagemodels/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+name = "progimagebackend"
+__title__ = 'progimage-backend-models'
+__author__ = 'Nasko Grozdanov'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019, Nasko Grozdanov'
+
+from .client import Client
+from .image import Image
