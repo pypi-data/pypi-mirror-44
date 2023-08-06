748dd5b2d71c9770b1a1cd7109755b81e08534c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:33 +0000

    Add package pipomatic/4.1/pipomatic-4.1.tar.gz

---
+++ packages/pipomatic/pipomatic-4.1.tar.gz/pipomatic-4.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name="pipomatic",
+    version="4.1",
+    author="eq8r",
+    author_email="doug.hudgeon@eq8r.com",
+    description="Automating the creation of an installable PIP module from a Jupyter notebook",
+    long_description="One of the challenges with writing code in a Jupyter notebook is that it is hard to re-use functions. Pipomatic allows you to create pip-installable modules from the functions in your Jupyter notebook.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/eq8r/pipomatic",
+    py_modules = ["pipomatic"],
+    install_requires=[
+        'black',
+        'click',
+        'ipython',
+        'nbformat',
+        'nbconvert',
+        'setuptools',
+        'twine',
+        'wheel'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pipomatic=pipomatic:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
