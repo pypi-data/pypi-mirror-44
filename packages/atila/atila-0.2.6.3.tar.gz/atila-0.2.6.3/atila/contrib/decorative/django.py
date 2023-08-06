32c1ab6657b51f339525598a364acb98905dde69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:36 +0000

    Add package atila/0.2.6.3/atila-0.2.6.3.tar.gz

---
+++ packages/atila/atila-0.2.6.3.tar.gz/atila-0.2.6.3/atila/contrib/decorative/django.py
@@ -0,0 +1,6 @@
+#-----------------------------------------------------------
+# This module directory is deprecated 
+# USE atila.contrib.services
+#-----------------------------------------------------------
+
+from ..services.django import auth
