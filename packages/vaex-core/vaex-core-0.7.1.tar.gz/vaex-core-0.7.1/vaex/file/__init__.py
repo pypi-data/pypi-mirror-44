ae098214f3d17cccdc10d8e091508584f45adb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:22 +0000

    Add package vaex-core/0.7.1/vaex-core-0.7.1.tar.gz

---
+++ packages/vaex-core/vaex-core-0.7.1.tar.gz/vaex-core-0.7.1/vaex/file/__init__.py
@@ -0,0 +1,41 @@
+__author__ = 'breddels'
+import logging
+logger = logging.getLogger("vaex.file")
+
+opener_classes = []
+
+def register(cls):
+	opener_classes.append(cls)
+
+import vaex.file.other
+try:
+	import vaex.hdf5 as hdf5
+except ImportError:
+	hdf5 = None
+if hdf5:
+	import vaex.hdf5.dataset
+
+def can_open(path, *args, **kwargs):
+	for name, class_ in list(vaex.file.other.dataset_type_map.items()):
+		if class_.can_open(path, *args):
+			return True
+
+
+def open(path, *args, **kwargs):
+	dataset_class = None
+	openers = []
+	for opener in opener_classes:
+		if opener.can_open(path, *args, **kwargs):
+			return opener.open(path, *args, **kwargs)
+	if hdf5:
+		openers.extend(hdf5.dataset.dataset_type_map.items())
+	openers.extend(vaex.file.other.dataset_type_map.items())
+	for name, class_ in list(openers):
+		logger.debug("trying %r with class %r" % (path, class_))
+		if class_.can_open(path, *args, **kwargs):
+			logger.debug("can open!")
+			dataset_class = class_
+			break
+	if dataset_class:
+		dataset = dataset_class(path, *args, **kwargs)
+		return dataset
