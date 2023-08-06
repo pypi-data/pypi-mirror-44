c0911fa360f96e5b8bc2ce70c04c2421474211f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:55 +0000

    Add package ctodd-python-lib-avro/1.0.4/ctodd-python-lib-avro-1.0.4.tar.gz

---
+++ packages/ctodd-python-lib-avro/ctodd-python-lib-avro-1.0.4.tar.gz/ctodd-python-lib-avro-1.0.4/avro_helpers/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Purpose:
+        Add Libraries to Path for Pip Installing
+"""
+
+from .avro_exceptions import *
+from .avro_general_helpers import *
+from .avro_reading_helpers import *
+from .avro_schema_helpers import *
+from .avro_writing_helpers import *
