7d6b302667af59cc97d9cdcbba3a57cde45cafd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:59 +0000

    Add package pyronn/0.0.3/pyronn-0.0.3-py3-none-any.whl

---
+++ packages/pyronn/pyronn-0.0.3-py3-none-any.whl/examples/ct_reconstruction/example_fan_2d.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+import lme_custom_ops
+import pyconrad as pyc # TODO: get independent of pyconrad
+pyc.setup_pyconrad()
+
+# TODO: better imports
+from pyronn.ct_reconstruction.layers.projection_2d import fan_projection2d
+from pyronn.ct_reconstruction.layers.backprojection_2d import fan_backprojection2d
+from pyronn.ct_reconstruction.geometry.geometry_fan_2d import GeometryFan2D
+from pyronn.ct_reconstruction.helpers.phantoms import shepp_logan
+from pyronn.ct_reconstruction.helpers.trajectories import circular_trajectory
+from pyronn.ct_reconstruction.helpers.filters import filters
+
+
+def example_fan_2d():
+    # ------------------ Declare Parameters ------------------
+
+    # Volume Parameters:
+    volume_size = 512
+    volume_shape = [volume_size, volume_size]
+    volume_spacing = [0.5, 0.5]
+
+    # Detector Parameters:
+    detector_shape = 800
+    detector_spacing = 0.5
+
+    # Trajectory Parameters:
+    number_of_projections = 360
+    angular_range = 2*np.pi
+
+    source_detector_distance = 1200
+    source_isocenter_distance = 750
+
+    # create Geometry class
+    geometry = GeometryFan2D(volume_shape, volume_spacing, detector_shape, detector_spacing, number_of_projections, angular_range, source_detector_distance, source_isocenter_distance)
+    geometry.set_central_ray_vectors(circular_trajectory.circular_trajectory_2d(geometry))
+
+    # Get Phantom
+    phantom = shepp_logan.shepp_logan(volume_shape)
+    pyc.imshow(phantom, 'phantom')
+
+
+    # ------------------ Call Layers ------------------
+    with tf.Session() as sess:
+        result = fan_projection2d(phantom, geometry)
+        sinogram = result.eval()
+        pyc.imshow(sinogram, 'sinogram')
+
+        reco_filter = filters.ram_lak_2D(geometry)
+
+        sino_freq = np.fft.fft(sinogram, axis=1)
+        sino_filtered_freq = np.multiply(sino_freq, reco_filter)
+        sinogram_filtered = np.fft.ifft(sino_filtered_freq, axis=1)
+
+        result_back_proj = fan_backprojection2d(sinogram_filtered, geometry)
+        reco = result_back_proj.eval()
+        pyc.imshow(reco, 'reco')
+
+
+if __name__ == '__main__':
+    example_fan_2d()
---
+++ packages/pyronn/pyronn-0.0.3-py3-none-any.whl/pyronn/ct_reconstruction/layers/projection_3d.py
@@ -0,0 +1,44 @@
+from tensorflow.python.framework import ops
+import pyronn_layers
+
+
+# cone_projection3d
+def cone_projection3d(volume, geometry, hardware_interp=True, step_size=1.0):
+    """
+    Wrapper function for making the layer call.
+    Args:
+        volume:             Input volume to project.
+        geometry:           Corresponding GeometryCone3D Object defining parameters.
+        hardware_interp:    Controls if interpolation is done by GPU.
+        step_size:          step_size along ray direction in voxel.
+    Returns:
+            Initialized lme_custom_ops.cone_projection3d layer.
+    """
+
+    return pyronn_layers.cone_projection3d(volume,
+                                            volume_shape=geometry.volume_shape,
+                                            projection_shape=geometry.sinogram_shape,
+                                            volume_origin=geometry.tensor_proto_volume_origin,
+                                            volume_spacing=geometry.tensor_proto_volume_spacing,
+                                            projection_matrices=geometry.tensor_proto_projection_matrices,
+                                            hardware_interp=hardware_interp,
+                                            step_size=step_size,
+                                            projection_multiplier = geometry.projection_multiplier)
+
+
+@ops.RegisterGradient("ConeProjection3D")
+def _project_grad(op, grad):
+    '''
+    Compute the gradient of the projection op by invoking the backprojector.
+    '''
+    reco = pyronn_layers.cone_backprojection3d(
+        sinogram=grad,
+        sinogram_shape=op.get_attr("projection_shape"),
+        volume_shape=op.get_attr("volume_shape"),
+        volume_origin=op.get_attr("volume_origin"),
+        volume_spacing=op.get_attr("volume_spacing"),
+        projection_multiplier=op.get_attr("projection_multiplier"),
+        projection_matrices=op.get_attr("projection_matrices"),
+        hardware_interp=op.get_attr("hardware_interp"),
+    )
+    return [reco]
