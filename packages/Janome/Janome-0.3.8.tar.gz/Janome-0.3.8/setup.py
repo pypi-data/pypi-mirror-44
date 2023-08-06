60612f9203b5a6175f31178cceb438443427ff6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package Janome/0.3.8/Janome-0.3.8.tar.gz

---
+++ packages/Janome/Janome-0.3.8.tar.gz/Janome-0.3.8/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+import sys
+from io import open
+
+sys.path.append('./janome')
+sys.path.append('./tests')
+
+import os
+from zipfile import ZipFile
+from janome.dic import *
+from janome.version import JANOME_VERSION
+
+dicdir = 'ipadic'
+
+if not os.path.exists('sysdic') and os.path.exists(os.path.join('ipadic', 'sysdic.zip')):
+    print('Unzip dictionary data...')
+    with ZipFile(os.path.join(dicdir, 'sysdic.zip')) as zf:
+        zf.extractall()
+
+fst_data = [data_file for data_file in os.listdir('sysdic') if data_file.startswith('fst.data')]
+
+name = 'janome'
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: Japanese",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7"
+]
+
+setup(
+    name='Janome',
+    version=JANOME_VERSION,
+    description='Japanese morphological analysis engine.',
+    long_description=long_description,
+    author='Tomoko Uchida',
+    author_email='tomoko.uchida.1111@gmail.com',
+    license='AL2',
+    classifiers=classifiers,
+    url='http://mocobeta.github.io/janome/',
+    packages=['janome','sysdic'],
+    package_data={'sysdic': fst_data},
+    py_modules=['janome.dic','janome.fst','janome.lattice','janome.tokenizer','janome.analyzer','janome.charfilter','janome.tokenfilter'],
+    scripts=['bin/janome', 'bin/janome.bat'],
+    test_suite = 'suite'
+)
+
