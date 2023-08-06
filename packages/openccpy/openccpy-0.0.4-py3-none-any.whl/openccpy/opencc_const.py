d2b582b84072a73d2b9b2269f572a61f75553c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:40 +0000

    Add package openccpy/0.0.4/openccpy-0.0.4-py3-none-any.whl

---
+++ packages/openccpy/openccpy-0.0.4-py3-none-any.whl/openccpy/opencc_const.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Desc: Opencc python constant module.
+Author: binbin.hou
+Date: 2019-4-10 13:40:20
+Since: 0.0.2
+"""
+
+# 2019.04.10 binbin.hou created opencc_const.py at version 0.0.2
+
+# 默认编码
+DEFAULT_CHARSET = "UTF-8"
+
+# 空白结果
+EMPTY_RESULT = "\uD86D\uDDF5"
+
+# 数据配置路径相关
+# 简体转繁体字符路径
+ST_CHAR_PATH = "/db/STCharacters.txt"
+
+# 简体转繁体词组路径
+ST_PHRASE_PATH = "/db/STPhrases.txt"
+
+# 繁体转简体字符路径
+TS_CHAR_PATH = "/db/TSCharacters.txt"
+
+# 繁体转简体词组路径
+TS_PHRASE_PATH = "/db/TSPhrases.txt"
