154e3e98a6f5f08b43412d292f8e8ef19ec72f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:06 +0000

    Add package directory-components/9.2.0/directory_components-9.2.0-py3-none-any.whl

---
+++ packages/directory-components/directory_components-9.2.0-py3-none-any.whl/directory_components/mixins.py
@@ -0,0 +1,56 @@
+from directory_constants.constants.choices import COUNTRY_CHOICES
+
+from directory_components import helpers, forms
+
+
+class CountryDisplayMixin:
+    country_form_class = forms.CountryForm
+
+    def get_context_data(self, *args, **kwargs):
+        country_code = helpers.get_user_country(self.request)
+
+        # if there is a country already detected we can hide the selector
+        hide_country_selector = bool(country_code)
+        country_name = dict(COUNTRY_CHOICES).get(country_code, '')
+
+        country = {
+            # used for flag icon css class. must be lowercase
+            'code': country_code.lower(),
+            'name': country_name,
+        }
+
+        country_form_kwargs = self.get_country_form_kwargs()
+
+        return super().get_context_data(
+            hide_country_selector=hide_country_selector,
+            country=country,
+            country_selector_form=self.country_form_class(
+                **country_form_kwargs),
+            *args, **kwargs
+        )
+
+    def get_country_form_kwargs(self, **kwargs):
+        return {
+            'initial': forms.get_country_form_initial_data(self.request),
+            **kwargs,
+        }
+
+
+class LanguageSwitcherMixin:
+    language_form_class = forms.LanguageForm
+
+    def get_context_data(self, *args, **kwargs):
+
+        language_form_kwargs = self.get_language_form_kwargs()
+
+        return super().get_context_data(
+            language_switcher_form=self.language_form_class(
+                **language_form_kwargs),
+            *args, **kwargs
+        )
+
+    def get_language_form_kwargs(self, **kwargs):
+        return {
+            'initial': forms.get_language_form_initial_data(self.request),
+            **kwargs,
+        }
