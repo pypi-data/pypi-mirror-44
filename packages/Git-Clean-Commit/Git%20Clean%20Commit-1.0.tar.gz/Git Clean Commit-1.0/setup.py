97c2aaaa0a413a6f28b2e6dd9f3085bd9a4c1fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:51 +0000

    Add package Git-Clean-Commit/1.0/Git%20Clean%20Commit-1.0.tar.gz

---
+++ packages/Git-Clean-Commit/Git%20Clean%20Commit-1.0.tar.gz/Git Clean Commit-1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Git Clean Commit',  
+    version='1.0',
+    scripts=['git-clean-commit'] ,
+    author="Low Yiyiu",
+    author_email="lowyiyiu@gmail.com",
+    description="Easily clean commit history",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lowyiyiu/git-clean-commit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
