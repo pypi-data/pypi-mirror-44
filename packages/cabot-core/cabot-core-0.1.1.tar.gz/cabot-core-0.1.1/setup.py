1f744c6963ecf97ab25bf90507d7e022d9e4ef27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:57 +0000

    Add package cabot-core/0.1.1/cabot-core-0.1.1.tar.gz

---
+++ packages/cabot-core/cabot-core-0.1.1.tar.gz/cabot-core-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cabot-core',
+    packages=find_packages(),
+    version='0.1.1',
+    license='MIT',
+    description='A modular chat bot.',
+    author='Joseph Hanna',
+    author_email='josephhanna@centennialarts.com',
+    url='https://github.com/centennialarts/cabot',
+    download_url='https://github.com/centennialarts/cabot/archive/master.zip',
+    keywords=['Chatbot', 'IRC', 'API'],
+    install_requires=[
+        'irc',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
