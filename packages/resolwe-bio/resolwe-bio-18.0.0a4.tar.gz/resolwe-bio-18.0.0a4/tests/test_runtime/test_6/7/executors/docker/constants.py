b9c194a8a9982d1b62a6c30384bfd96e1807f6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:16 +0000

    Add package resolwe-bio/18.0.0a4/resolwe-bio-18.0.0a4.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-18.0.0a4.tar.gz/resolwe-bio-18.0.0a4/tests/test_runtime/test_6/7/executors/docker/constants.py
@@ -0,0 +1,8 @@
+"""Constants for the Docker executor."""
+DATA_VOLUME = '/data'
+DATA_ALL_VOLUME = '/data_all'
+UPLOAD_VOLUME = '/upload'
+SECRETS_VOLUME = '/secrets'
+
+# Container image used if no image is specified.
+DEFAULT_CONTAINER_IMAGE = 'resolwe/base:ubuntu-16.04'
