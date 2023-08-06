ca4b8a148471dc31c5bb926cdecba245be20f222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:28 +0000

    Add package rprintlib/1.2/rprintlib-1.2.tar.gz

---
+++ packages/rprintlib/rprintlib-1.2.tar.gz/rprintlib-1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="rprintlib",
+    version="1.2",
+    author="Wirtos_new",
+    author_email="Wirtos.new@gmail.com",
+    description="Pure python custom print implementation",
+    url="https://wirtos.github.io/Rprint/",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/Wirtos/Rprint",
+    },
+    install_requires=[],
+    keywords="print rprint Rprint rprintlib",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
