91eb6173eed7db30514d1df5fdce56b2ec6aa3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:46 +0000

    Add package opencv-draw-tools-fernaperg/1.2.0/opencv_draw_tools_fernaperg-1.2.0-py3-none-any.whl

---
+++ packages/opencv-draw-tools-fernaperg/opencv_draw_tools_fernaperg-1.2.0-py3-none-any.whl/opencv_draw_tools/__init__.py
@@ -0,0 +1,16 @@
+from opencv_draw_tools.SelectZone import *
+import opencv_draw_tools.tags_constraint
+
+name = 'opencv_draw_tools'
+help = '''
+MIT License
+Copyright (c) 2019 Fernando Perez
+For more information visit: https://github.com/fernaper/opencv-draw-tools
+Also you can write complete_help to view full information'''
+__version__ =  '1.2.0'
+
+complete_help = '''
+{} - v{}
+{}
+{}
+'''.format(name, __version__, help, get_complete_help())
