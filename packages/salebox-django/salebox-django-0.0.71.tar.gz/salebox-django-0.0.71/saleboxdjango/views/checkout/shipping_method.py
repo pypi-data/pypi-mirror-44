6ce62db7b2884b5f855a65bb93f4d50628c056b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:37 +0000

    Add package salebox-django/0.0.71/salebox-django-0.0.71.tar.gz

---
+++ packages/salebox-django/salebox-django-0.0.71.tar.gz/salebox-django-0.0.71/saleboxdjango/views/checkout/shipping_method.py
@@ -0,0 +1,42 @@
+from django import forms
+
+from saleboxdjango.lib.shipping_options import SaleboxShippingOptions
+from saleboxdjango.views.checkout.base import SaleboxCheckoutBaseView
+
+
+class SaleboxCheckoutShippingMethodForm(forms.Form):
+    shipping_method = forms.IntegerField()
+
+class SaleboxCheckoutShippingMethodView(SaleboxCheckoutBaseView):
+    shipping_options_class = SaleboxShippingOptions
+    checkout_step = 'shipping_method'
+    form_class = SaleboxCheckoutShippingMethodForm
+    template_name = 'salebox/checkout/shipping_method.html'
+
+    def get_additional_context_data(self, context):
+        return self._get_shipping_options(context)
+
+    def form_valid_pre_redirect(self, form):
+        # retrieve selected option
+        id = form.cleaned_data['shipping_method']
+        options = self._get_shipping_options()['shipping_options']
+        selected = next(o for o in options if o['id'] == id)
+
+        # store in checkout
+        self.sc.set_shipping_method(
+            selected['id'],
+            selected['label'],
+            selected['price']['price'],
+            selected['meta'],
+            self.request
+        )
+
+        return True
+
+    def _get_shipping_options(self, context={}):
+        smc = self.shipping_options_class()
+        return smc.go(
+            self.request,
+            self.sc.get_raw_data(),
+            context
+        )
