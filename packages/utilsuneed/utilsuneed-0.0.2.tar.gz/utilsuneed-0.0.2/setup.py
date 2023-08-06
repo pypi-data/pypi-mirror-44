7f37e9a4c409f8e8ef5161db1ab813d9049df9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package utilsuneed/0.0.2/utilsuneed-0.0.2.tar.gz

---
+++ packages/utilsuneed/utilsuneed-0.0.2.tar.gz/utilsuneed-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="utilsuneed",
+    version="0.0.2",
+    author="Paritosh Sapre",
+    author_email="paritoshsapre@gmail.com",
+    description="A need for many",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
