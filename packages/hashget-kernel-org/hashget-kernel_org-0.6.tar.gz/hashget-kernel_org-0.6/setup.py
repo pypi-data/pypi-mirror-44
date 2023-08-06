934840f36bed414117176e89b029f7ae2c044a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package hashget-kernel-org/0.6/hashget-kernel_org-0.6.tar.gz

---
+++ packages/hashget-kernel-org/hashget-kernel_org-0.6.tar.gz/hashget-kernel_org-0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hashget-kernel_org',
+    version='0.6',
+    description='kernel.org (linux kernel sources) plugin for hashget',
+    long_description='kernel.org (linux kernel sources) plugin for hashget',
+    author='Yaroslav Polyakov',
+    author_email='yaroslaff@gmail.com',
+
+    license='Apache Software License',
+
+    packages=['hashget.heuristics.kernel_org'],
+    zip_safe=False,
+)
+
