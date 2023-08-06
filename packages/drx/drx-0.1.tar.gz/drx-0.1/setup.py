ab01553c33f61f1c3acabcfe1d4fb2574ec9372f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:48 +0000

    Add package drx/0.1/drx-0.1.tar.gz

---
+++ packages/drx/drx-0.1.tar.gz/drx-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='drx',
+      version='0.1',
+      description='Tools for running Docker on your laptop',
+      author='Mat Brown',
+      author_email='matb@dropbox.com',
+      packages=['drx'])
