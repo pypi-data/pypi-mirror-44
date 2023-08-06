c10747e610f69d65ab70bd2eb2c51a35d42a56b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:36 +0000

    Add package asyncsa/1.0.1/asyncsa-1.0.1-py3-none-any.whl

---
+++ packages/asyncsa/asyncsa-1.0.1-py3-none-any.whl/asyncsa/connection/async_pg.py
@@ -0,0 +1,60 @@
+import asyncpg
+from .base import BaseConnection
+
+
+class AsyncPg(BaseConnection):
+    def __init__(self, dsn=None,  **kwargs):
+        self.init_pool = asyncpg.create_pool(dsn=dsn, **kwargs)
+        self.pool = None
+
+    async def connect(self):
+        self.pool = await self.init_pool
+
+    def format_sql_result(self, result=None):
+        if isinstance(result, list):
+            result = [dict(i) for i in result]
+        else:
+            try:
+                result = dict(result)
+            except:
+                result = result
+        return result
+
+    async def get(self, sql=None):
+        try:
+            async with self.pool.acquire() as con:
+                result = await con.fetch(sql)
+                return self.format_sql_result(result=result)
+        except Exception as tmp:
+            raise Exception(tmp)
+
+    async def set(self, sql=None):
+        try:
+            async with self.pool.acquire() as con:
+                result = await con.fetch(sql)
+                return self.format_sql_result(result=result)
+        except Exception as tmp:
+            raise Exception(tmp)
+
+    async def bare_set(self, sql=None, con=None):
+        try:
+            result = await con.execute(sql)
+            return self.format_sql_result(result=result)
+        except Exception as tmp:
+            raise Exception(tmp)
+
+    async def commit(self, con=None, tr=None):
+        await tr.commit()
+        await self.pool.release(con)
+        return True
+
+    async def rollback(self, con=None, tr=None):
+        await tr.rollback()
+        await self.pool.release(con)
+
+    async def get_transaction(self):
+        con = await self.pool.acquire()
+        self._tr_con = con
+        tr = con.transaction()
+        await tr.start()
+        return con, tr
---
+++ packages/asyncsa/asyncsa-1.0.1-py3-none-any.whl/asyncsa/manager/base.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
+
+class BaseManager(metaclass=ABCMeta):
+    @abstractmethod
+    async def connect(self):
+        pass
+
+    @abstractmethod
+    async def get_by_param(self):
+        pass
+
+    @abstractmethod
+    async def get(self):
+        pass
+
+    @abstractmethod
+    async def set(self):
+        pass
+
+    @abstractmethod
+    async def format_query(self):
+        pass
+
+    @abstractmethod
+    async def set_multi(self):
+        pass
+
+    @abstractmethod
+    async def all(self):
+        pass
+
+    @abstractmethod
+    async def instance(self):
+        pass
+
+    @abstractmethod
+    async def execute(self):
+        pass
