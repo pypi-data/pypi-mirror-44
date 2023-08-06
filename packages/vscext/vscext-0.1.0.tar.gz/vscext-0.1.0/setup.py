ffb463786494e725474973e900b4d3e4243e4597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:12 +0000

    Add package vscext/0.1.0/vscext-0.1.0.tar.gz

---
+++ packages/vscext/vscext-0.1.0.tar.gz/vscext-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vscext",
+    version="0.1.0",
+    author="Irakli Nadareishvili",
+    #author_email="author@example.com",
+    description="Gives installed VS Code extensions as a linked markdown list",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inadarei/vscode-extensions-list",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.*',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
