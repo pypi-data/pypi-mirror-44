52d2e09108c754b77969167680ce1321bf9b880f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:26 +0000

    Add package s3-parallel-put/0.4/s3-parallel-put-0.4.tar.gz

---
+++ packages/s3-parallel-put/s3-parallel-put-0.4.tar.gz/s3-parallel-put-0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fh:
+  long_description = fh.read()
+
+setup(name='s3-parallel-put',
+      version='0.4',
+      description='s3-parallel-put Parallel uploads to Amazon AWS S3',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords='s3 parallel',
+      url='https://github.com/mishudark/s3-parallel-put',
+      author='mishudark',
+      # author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        "boto>=2.49.0",
+        "python-magic>=0.4.15"
+       ],
+      entry_points={
+        'console_scripts': ['s3-parallel-put=s3_parallel_put.s3_parallel_put:main']
+      },
+      )
