182d7d06f4bb8b96ff5aebf23b5764768e1c5096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:28 +0000

    Add package predator/0.0.1/predator-0.0.1.tar.gz

---
+++ packages/predator/predator-0.0.1.tar.gz/predator-0.0.1/predator/predator.py
@@ -0,0 +1,10 @@
+"""Routines to search for seeds.
+
+"""
+
+
+def search_seeds(graph_data:str, targets:set=None) -> [str]:
+    """Yield the set of seeds for each found solution.
+
+    """
+    yield from ()  # TODO: implement that
