a30e7d3cd06c4cbabc48b5dd59c4c62e162b7f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:56 +0000

    Add package pigar/0.9.2/pigar-0.9.2-py2.py3-none-any.whl

---
+++ packages/pigar/pigar-0.9.2-py2.py3-none-any.whl/pigar/tests/imports_example/example1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import collections
+
+import foobar
+
+
+eval('import foo')
+
+
+exec("import bar", {}, {})
+
+
+exec("import foobaz")
+
+
+def foo():
+    import json
+    pass
+
+
+class A(object):
+    """
+    >>> import itertools
+    """
+    def baz(self):
+        """
+        >>> import baz
+        """
+
+
+def bar():
+    """
+    >>> import Queue
+    >>> def test():
+    ...     import bisect
+    ...     pass
+    """
+    pass
+
+
+__import__('mod', globals(), locals())
+
+importlib.import_module('name')
+
+importlib.import_module('.name', 'pkg.subpkg')
