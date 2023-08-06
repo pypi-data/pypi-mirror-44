96d9eff90a9e4645248c2d5e587a103dfb3dc95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package dophon/1.3.1.post2/dophon-1.3.1.post2.tar.gz

---
+++ packages/dophon/dophon-1.3.1.post2.tar.gz/dophon-1.3.1.post2/dophon/annotation/__init__.py
@@ -0,0 +1,67 @@
+# coding: utf-8
+from dophon.annotation import AutoWired
+from dophon.annotation import res, req
+from dophon.annotation.AutoWired import *
+from dophon.annotation.description import *
+
+"""
+注解集合(部分)
+author:CallMeE
+date:2018-06-01
+
+
+"""
+__all__ = [
+    'ResponseBody',
+    'ResponseTemplate',
+    'AutoParam',
+    'FullParam',
+    'RequestMapping',
+    'GetRoute',
+    'PostRoute',
+    'Get',
+    'Post',
+    'AutoWired',
+    'AsResBody',
+    'AsResTemp',
+    'AsArgs',
+    'AsJson',
+    'AsFile',
+    'BeanConfig',
+    'bean',
+    'Bean',
+    'Desc',
+    'DefBean'
+]
+
+AutoWired = AutoWired
+
+ResponseBody = AsResBody = res.response_body
+
+ResponseTemplate = AsResTemp = res.response_template
+
+AutoParam = AsArgs = req.auto_param
+
+FullParam = AsJson = req.full_param
+
+FileParam = AsFile = req.file_param
+
+BeanConfig = AutoWired.BeanConfig
+
+bean = AutoWired.bean
+
+Bean = AutoWired.Bean
+
+RequestMapping = req.request_mapping
+
+GetRoute = req.get_route
+
+PostRoute = req.post_route
+
+Get = req.get
+
+Post = req.post
+
+Desc = desc
+
+DefBean = DefBean
