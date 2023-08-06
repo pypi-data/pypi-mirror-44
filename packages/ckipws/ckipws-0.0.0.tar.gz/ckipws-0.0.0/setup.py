178e470eb1c43aa538d338ed4a9a3491e3d09e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package ckipws/0.0.0/ckipws-0.0.0.tar.gz

---
+++ packages/ckipws/ckipws-0.0.0.tar.gz/ckipws-0.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+	name='ckipws',
+	description='CKIP Word-Segmentation',
+	long_description='Use `pyckip <https://pypi.org/project/pyckip/>`_ instead.',
+	url='https://pypi.org/project/pyckip/',
+	install_requires=['pyckip'],
+)
