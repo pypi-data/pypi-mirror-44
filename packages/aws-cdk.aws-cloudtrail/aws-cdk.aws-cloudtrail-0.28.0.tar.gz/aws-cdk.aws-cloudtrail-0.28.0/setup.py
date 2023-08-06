5c322f3e244397d4e8f71479450ea4a0a7eefcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:45 +0000

    Add package aws-cdk.aws-cloudtrail/0.28.0/aws-cdk.aws-cloudtrail-0.28.0.tar.gz

---
+++ packages/aws-cdk.aws-cloudtrail/aws-cdk.aws-cloudtrail-0.28.0.tar.gz/aws-cdk.aws-cloudtrail-0.28.0/setup.py
@@ -0,0 +1,45 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-cloudtrail",
+    "version": "0.28.0",
+    "description": "CDK Constructs for AWS CloudTrail",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_cloudtrail",
+        "aws_cdk.aws_cloudtrail._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_cloudtrail._jsii": [
+            "aws-cloudtrail@0.28.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_cloudtrail": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii",
+        "publication>=0.0.3",
+        "aws-cdk.aws-kms~=0.28.0",
+        "aws-cdk.aws-logs~=0.28.0",
+        "aws-cdk.cdk~=0.28.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
