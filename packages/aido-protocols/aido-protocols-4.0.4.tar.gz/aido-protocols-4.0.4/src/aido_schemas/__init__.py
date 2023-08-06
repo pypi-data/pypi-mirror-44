a0a2c8ab4dc79b93d49e01169fd8c9131f28fa9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:08 +0000

    Add package aido-protocols/4.0.4/aido-protocols-4.0.4.tar.gz

---
+++ packages/aido-protocols/aido-protocols-4.0.4.tar.gz/aido-protocols-4.0.4/src/aido_schemas/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '4.0.4'
+
+from zuper_nodes import InteractionProtocol, particularize, logger as zlogger
+
+logger = zlogger.getChild('aido_schemas')
+logger.info(f'aido_schemas {__version__}')
+
+from zuper_nodes_wrapper import wrap_direct, Context
+
+_ = wrap_direct
+_ = Context
+_ = InteractionProtocol
+_ = particularize
+
+from .protocol_agent import *
+from .protocol_simulator import *
+from .protocols import *
+from .schemas import *
