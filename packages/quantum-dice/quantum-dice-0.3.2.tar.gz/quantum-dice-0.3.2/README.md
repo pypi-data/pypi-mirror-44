127a80c0c299e7f5e58d4fe51d7a9191aab0aef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package quantum-dice/0.3.2/quantum-dice-0.3.2.tar.gz

---
+++ packages/quantum-dice/quantum-dice-0.3.2.tar.gz/quantum-dice-0.3.2/README.md
@@ -0,0 +1,1 @@
+The Quantum Dice leverages the IBM Q quantum computer to generate a truly random set of numbers.
---
+++ packages/quantum-dice/quantum-dice-0.3.2.tar.gz/quantum-dice-0.3.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quantum-dice",
+    version="0.3.2",
+    author="Justin Peter",
+    author_email="planetzzyx@protonmail.com",
+    description="A dice using quantum mechanics.",
+    long_description="The Quantum Dice leverages the IBM Q quantum computer to generate a truly random set of numbers.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheMagicNacho/quantum-dice",
+    packages=setuptools.find_packages(),
+    install_requires = ['qiskit'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
