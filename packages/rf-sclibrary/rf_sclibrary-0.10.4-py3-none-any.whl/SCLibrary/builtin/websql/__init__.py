c1d48168da7610504a9478f5f6709afe6180fafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:10 +0000

    Add package rf-sclibrary/0.10.4/rf_sclibrary-0.10.4-py3-none-any.whl

---
+++ packages/rf-sclibrary/rf_sclibrary-0.10.4-py3-none-any.whl/SCLibrary/builtin/websql/__init__.py
@@ -0,0 +1,8 @@
+from .websql import WebSQL
+import os
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def get_data(path):
+    return os.path.join(_ROOT, 'data', path)
+
+print(get_data('shitf.dll'))
