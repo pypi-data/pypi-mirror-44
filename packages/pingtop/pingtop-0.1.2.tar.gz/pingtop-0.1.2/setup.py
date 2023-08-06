2129623409adda40d35946fc74d7413f35120700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:10 +0000

    Add package pingtop/0.1.2/pingtop-0.1.2.tar.gz

---
+++ packages/pingtop/pingtop-0.1.2.tar.gz/pingtop-0.1.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pingtop",
+    version="0.1.2",
+    description="Top like ping tool.",
+    author="laixintao",
+    author_email="laixintaoo@gmail.com",
+    url="https://github.com/laixintao/pingtop",
+    entry_points={"console_scripts": ["pingtop = pingtop:multi_ping"]},
+    scripts=["pingtop.py"],
+    install_requires=["panwid", "click"],
+)
