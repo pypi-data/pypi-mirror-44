d74ab27443090c96cefa75e371894ffc32358495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:04 +0000

    Add package pynuodb/2.4.1/pynuodb-2.4.1.tar.gz

---
+++ packages/pynuodb/pynuodb-2.4.1.tar.gz/pynuodb-2.4.1/pynuodb/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '2.4.1'
+
+from .connection import *
+from .datatype import *
+from .exception import *
---
+++ packages/pynuodb/pynuodb-2.4.1.tar.gz/pynuodb-2.4.1/tests/nuodb_connect_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import pynuodb
+from .nuodb_base import NuoBase
+from pynuodb.exception import ProgrammingError
+from pynuodb.session import SessionException
+
+
+class NuoDBConnectTest(NuoBase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_nosuchdatabase(self):
+        try:
+            con = pynuodb.connect("nosuchdatabase", self.host, "dba", "dba_password")
+            self.fail()
+        except SessionException:
+            pass
+        except:
+            self.fail()
+
+    def test_nosuchport(self):
+        try:
+            con = pynuodb.connect("pynuodb_test", "localhost:23456", "dba", "dba_password")
+            self.fail()
+        except:
+            pass
+
+    def test_nosuchhost(self):
+        try:
+            con = pynuodb.connect("pynuodb_test", "nosuchhost", "dba", "dba_password")
+            self.fail()
+        except:
+            pass
+
+    def test_nosuchuser(self):
+        try:
+            con = pynuodb.connect("pynuodb_test", self.host, "nosuchuser", "dba_password")
+            self.fail()
+        except ProgrammingError:
+            pass
+        except:
+            self.fail()
+
+    def test_nosuchpassword(self):
+        try:
+            con = pynuodb.connect("pynuodb_test", self.host, "dba", "nosuchpassword")
+            self.fail()
+        except ProgrammingError:
+            pass
+        except:
+            self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
