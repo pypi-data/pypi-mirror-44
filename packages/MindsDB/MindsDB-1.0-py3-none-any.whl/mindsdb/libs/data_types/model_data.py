9feef46f9ac7a406189183f8af2155cf038e8dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package MindsDB/1.0/MindsDB-1.0-py3-none-any.whl

---
+++ packages/MindsDB/MindsDB-1.0-py3-none-any.whl/mindsdb/libs/data_types/model_data.py
@@ -0,0 +1,13 @@
+
+class ModelData():
+
+    def __init__(self):
+
+        self.train_set = {}
+        self.test_set = {}
+        self.validation_set = {}
+
+        self.predict_set = {}
+
+        self.predict_set_map = {}
+
