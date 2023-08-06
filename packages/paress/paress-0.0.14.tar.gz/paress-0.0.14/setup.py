3351267788b50cef5895cc5d3c9798be0e8bfd36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:40 +0000

    Add package paress/0.0.14/paress-0.0.14.tar.gz

---
+++ packages/paress/paress-0.0.14.tar.gz/paress-0.0.14/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+    name="paress",
+    version="0.0.14",
+    author="Jairo Antonio Melo Flórez",
+    author_email="jairom@colmich.edu.mx",
+    description="Utilidad para Web Scrapping en el Portal de Archivos Españoles-PARES",
+    py_modules=["paress"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jairomelo/PARESS",
+    package_dir={'': 'paress'},
+    packages=setuptools.find_packages(),
+    install_requires=['selenium','beautifulsoup4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
