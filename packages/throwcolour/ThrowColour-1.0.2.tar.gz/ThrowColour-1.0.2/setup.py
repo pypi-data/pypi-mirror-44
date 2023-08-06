c09115c84ee146b437a62f5ff94ecca09460c810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:35 +0000

    Add package throwcolour/1.0.2/ThrowColour-1.0.2.tar.gz

---
+++ packages/throwcolour/ThrowColour-1.0.2.tar.gz/ThrowColour-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ThrowColour",
+    version="v1.0.2",
+    author="Jacky Zhao",
+    author_email="j.zhao2k19@gmail.com",
+    description="A small package to make printing pretty things easier!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackyzha0/ThrowColour/archive/v1.0.2.zip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
