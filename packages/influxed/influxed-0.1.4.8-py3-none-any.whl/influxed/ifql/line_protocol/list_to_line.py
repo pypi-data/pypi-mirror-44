4eb75e1de3dad588b80ec494c862d5c5ac1eb4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:29 +0000

    Add package influxed/0.1.4.8/influxed-0.1.4.8-py3-none-any.whl

---
+++ packages/influxed/influxed-0.1.4.8-py3-none-any.whl/influxed/ifql/line_protocol/list_to_line.py
@@ -0,0 +1,5 @@
+from influxed.ifql.line_protocol.dict_to_line import dict_to_line_protocol
+
+
+def to_lines(list_, measurement):
+    return [dict_to_line_protocol(dict_, measurement) for dict_ in list_]
