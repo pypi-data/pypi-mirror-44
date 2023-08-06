226963b9be954bb4db28f219aa18c8c229ff247d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:22 +0000

    Add package arcsecond/0.6.3/arcsecond-0.6.3.tar.gz

---
+++ packages/arcsecond/arcsecond-0.6.3.tar.gz/arcsecond-0.6.3/arcsecond/api/endpoints/__init__.py
@@ -0,0 +1,26 @@
+from .activities import ActivitiesAPIEndPoint
+from .charts import FindingChartsAPIEndPoint
+from .datasets import DatasetsAPIEndPoint, FITSFilesAPIEndPoint
+from .objects import ExoplanetsAPIEndPoint, ObjectsAPIEndPoint
+from .observingruns import NightLogAPIEndPoint, ObservingRunsAPIEndPoint
+from .observingsites import InstrumentsAPIEndPoint, ObservingSitesAPIEndPoint, TelescopesAPIEndPoint
+from .profiles import PersonalProfileAPIEndPoint, ProfileAPIEndPoint, ProfileAPIKeyAPIEndPoint
+from .satellites import SatellitesAPIEndPoint
+from .telegrams import TelegramsATelAPIEndPoint
+
+__all__ = ["ActivitiesAPIEndPoint",
+           "FindingChartsAPIEndPoint",
+           "DatasetsAPIEndPoint",
+           "FITSFilesAPIEndPoint",
+           "ExoplanetsAPIEndPoint",
+           "ObjectsAPIEndPoint",
+           "NightLogAPIEndPoint",
+           "ObservingRunsAPIEndPoint",
+           "InstrumentsAPIEndPoint",
+           "ObservingSitesAPIEndPoint",
+           "TelescopesAPIEndPoint",
+           "PersonalProfileAPIEndPoint",
+           "ProfileAPIEndPoint",
+           "ProfileAPIKeyAPIEndPoint",
+           "SatellitesAPIEndPoint",
+           "TelegramsATelAPIEndPoint"]
