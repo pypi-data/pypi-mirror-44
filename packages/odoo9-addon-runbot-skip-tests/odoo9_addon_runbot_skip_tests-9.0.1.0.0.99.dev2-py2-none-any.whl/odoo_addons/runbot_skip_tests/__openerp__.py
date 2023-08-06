0c420bc55c6f94cfdcdb1e1ac3d1267d6ff92505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:50 +0000

    Add package odoo9-addon-runbot-skip-tests/9.0.1.0.0.99.dev2/odoo9_addon_runbot_skip_tests-9.0.1.0.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo9-addon-runbot-skip-tests/odoo9_addon_runbot_skip_tests-9.0.1.0.0.99.dev2-py2-none-any.whl/odoo_addons/runbot_skip_tests/__openerp__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Copyrigh 2010 - 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Runbot Skip Tests',
+    'category': 'Website',
+    'summary': 'Skip tests on runbot builds',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
+    'depends': ['runbot'],
+    'installable': True,
+}
---
+++ packages/odoo9-addon-runbot-skip-tests/odoo9_addon_runbot_skip_tests-9.0.1.0.0.99.dev2-py2-none-any.whl/odoo_addons/runbot_skip_tests/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Copyrigh 2010 - 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    runbot_build,
+)
