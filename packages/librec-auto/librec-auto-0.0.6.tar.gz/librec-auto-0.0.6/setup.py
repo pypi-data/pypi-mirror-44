513b65d095844ac3f767cddecb617102d8127838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package librec-auto/0.0.6/librec-auto-0.0.6.tar.gz

---
+++ packages/librec-auto/librec-auto-0.0.6.tar.gz/librec-auto-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="librec-auto",
+    version="0.0.6",
+	scripts=['example_pkg\init.py'] ,
+    author="Masoud Mansoury and Robin Burke",
+    author_email="masoodmansoury@gmail.com",
+    description="The librec-auto project aims to automate recommender system experimens using Librec.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/that-recsys-lab/librec-auto",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
