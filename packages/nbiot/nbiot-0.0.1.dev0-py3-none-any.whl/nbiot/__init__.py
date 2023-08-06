217b8e8a6f408787d84d6b2c73d7182916ffb184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:05 +0000

    Add package nbiot/0.0.1.dev0/nbiot-0.0.1.dev0-py3-none-any.whl

---
+++ packages/nbiot/nbiot-0.0.1.dev0-py3-none-any.whl/nbiot/__init__.py
@@ -0,0 +1,18 @@
+import click
+
+
+@click.group()
+def cli():
+    """
+    This is a NB IoT Scanner software built by Palmlund Wahlgren Innovative
+    Technology AB in Sweden for use in finding problems and evaluating network
+    coverage in smart meter rollouts.
+    """
+    pass
+
+
+
+
+
+def main():
+    cli()
