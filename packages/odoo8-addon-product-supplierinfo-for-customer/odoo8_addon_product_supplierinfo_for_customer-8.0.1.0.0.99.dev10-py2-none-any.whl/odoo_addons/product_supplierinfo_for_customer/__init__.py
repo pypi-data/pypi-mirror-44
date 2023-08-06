a36e3f217f18e1c2393adba846d8a5f18c8e4174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:27 +0000

    Add package odoo8-addon-product-supplierinfo-for-customer/8.0.1.0.0.99.dev10/odoo8_addon_product_supplierinfo_for_customer-8.0.1.0.0.99.dev10-py2-none-any.whl

---
+++ packages/odoo8-addon-product-supplierinfo-for-customer/odoo8_addon_product_supplierinfo_for_customer-8.0.1.0.0.99.dev10-py2-none-any.whl/odoo_addons/product_supplierinfo_for_customer/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from . import models
+from .post_init_hook import post_init_hook
