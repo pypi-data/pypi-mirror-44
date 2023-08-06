b3221b05545a0952e91d1326377cd883e13a6472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:28 +0000

    Add package cipcore/1.0.15/cipcore-1.0.15-py3-none-any.whl

---
+++ packages/cipcore/cipcore-1.0.15-py3-none-any.whl/cipcore/__init__.py
@@ -0,0 +1,6 @@
+from .mailer import EmailApi
+from .delivery_ef import DeliveryEfPublishRequestApi
+from .ftp_service import FTPService
+from .file_copy import FileCopyApi
+from .access_token_client import TokenApi
+from .redis_checker import RedisChecker
