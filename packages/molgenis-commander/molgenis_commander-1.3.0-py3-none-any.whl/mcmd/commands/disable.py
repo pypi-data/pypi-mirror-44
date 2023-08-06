38768f8fde107d16cf7c5a97751ea59d0b65c8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:02 +0000

    Add package molgenis-commander/1.3.0/molgenis_commander-1.3.0-py3-none-any.whl

---
+++ packages/molgenis-commander/molgenis_commander-1.3.0-py3-none-any.whl/mcmd/commands/disable.py
@@ -0,0 +1,43 @@
+import mcmd.config.config as config
+from mcmd import io
+from mcmd.client.molgenis_client import post
+from mcmd.utils.resources import ensure_resource_exists, ResourceType
+from mcmd.command import command
+from mcmd.commands._registry import arguments
+from mcmd.io import highlight
+
+
+# =========
+# Arguments
+# =========
+
+@arguments('disable')
+def add_arguments(subparsers):
+    p_disable = subparsers.add_parser('disable',
+                                      help='Disable resources/functionality',
+                                      description="Run 'mcmd disable rls -h' to view the help for those sub-commands")
+    p_disable_subparsers = p_disable.add_subparsers(dest="type")
+
+    p_disable_rls = p_disable_subparsers.add_parser('rls',
+                                                    help='Disables row level security on an entity type')
+    p_disable_rls.set_defaults(func=disable_rls,
+                               write_to_history=True)
+    p_disable_rls.add_argument('entity',
+                               type=str,
+                               help="The entity type to remove the row level security from")
+
+
+# =======
+# Methods
+# =======
+
+@command
+def disable_rls(args):
+    if not io.confirm('Are you sure you want to disable row level security on %s?' % args.entity):
+        return
+
+    io.start('Disabling row level security on entity type %s' % highlight(args.entity))
+
+    ensure_resource_exists(args.entity, ResourceType.ENTITY_TYPE)
+    post(config.api('rls'), data={'id': args.entity,
+                                  'rlsEnabled': False})
