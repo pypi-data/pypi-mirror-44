9ef6677d38dc7a2d9f378147d87dfb9adc4b4f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:41 +0000

    Add package flaskweb/0.0.3/flaskweb-0.0.3.tar.gz

---
+++ packages/flaskweb/flaskweb-0.0.3.tar.gz/flaskweb-0.0.3/setup.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+# Created by Meteorix at 2019/4/11
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file. (replacing from pip.req import parse_requirements)"""
+    lines = (line.strip() for line in open(filename))
+    return [line for line in lines if line and not line.startswith("#")]
+
+
+reqs = parse_requirements('requirements.txt')
+
+
+setup(
+    name='flaskweb',
+    version='0.0.3',
+    author='meteorix',
+    author_email='lxhustauto@gmail.com',
+    description='complete web framework from dev to production',
+    packages=find_packages(exclude=['example', 'simple']),
+    package_data={
+        "flaskweb": ["static/*/*/*", "templates/*"],
+    },
+    include_package_data=True,
+    install_requires=reqs,
+)
