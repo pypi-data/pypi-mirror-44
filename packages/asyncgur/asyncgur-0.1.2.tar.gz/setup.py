6a79812a4df246cbdefb3999bccefe04e6d7de60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:39 +0000

    Add package asyncgur/0.1/asyncgur-0.1.2.tar.gz

---
+++ packages/asyncgur/asyncgur-0.1.2.tar.gz/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='asyncgur',
+    version='0.1.2',
+    packages=['asyncgur',],
+    license='MIT License',
+    install_requires=['aiohttp', 'dacite',],
+    long_description=open('README.md').read(),
+    author='Dylee',
+    author_email='dylee@mewbot.me',
+)
