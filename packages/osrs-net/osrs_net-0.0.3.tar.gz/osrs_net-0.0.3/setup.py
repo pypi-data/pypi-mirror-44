cf214104e02a40a87ed08fb2061a55be7ca76bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:14 +0000

    Add package osrs-net/0.0.3/osrs_net-0.0.3.tar.gz

---
+++ packages/osrs-net/osrs_net-0.0.3.tar.gz/osrs_net-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='osrs_net',
+    version='0.0.3',
+    author='Patrick Seute',
+    author_email='patrick.seute@gmail.com',
+    description='Simple OSBuddy Exchange API Wrapper',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        'requests'
+    ]
+)
