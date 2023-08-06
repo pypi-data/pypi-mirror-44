9ce8927be1f0d1f6cbb3c27603ecd23984994be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:56 +0000

    Add package pyDSA-core/1.0.0/pyDSA_core-1.0.0-py3-none-any.whl

---
+++ packages/pyDSA-core/pyDSA_core-1.0.0-py3-none-any.whl/pyDSA/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#!/bin/env python3
+
+# Copyright (C) 2018 Gaby Launay
+
+# Author: Gaby Launay  <gaby.launay@tutanota.com>
+# URL: https://github.com/gabylaunay/pyDSA
+# Version: 0.1
+
+# This file is part of pyDSA
+
+# pyDSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+from .image import Image
+from .temporalimages import TemporalImages
+from .dropedges import DropEdges
+from .dropfit import DropFit, DropCircleFit, DropCirclesFit, DropEllipseFit, \
+    DropSplineFit
+from .helpers import import_from_video, import_from_image, import_from_images
+
+name = "pyDSA"
