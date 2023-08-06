029b768de56259da5ee07ada7b2e2210e7e7c150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:03 +0000

    Add package ai-demos/0.1.2/ai_demos-0.1.2.tar.gz

---
+++ packages/ai-demos/ai_demos-0.1.2.tar.gz/ai_demos-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ai_demos',
+    version='0.1.2',
+    packages=['ai_demos'],
+    url='https://github.com/JarbasAl/ai_demos',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial api to machine learning demos'
+)
