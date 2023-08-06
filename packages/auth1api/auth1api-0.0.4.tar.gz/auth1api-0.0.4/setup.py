5a579249dbb1c97c8145232872566d51bf3e2ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:50 +0000

    Add package auth1api/0.0.4/auth1api-0.0.4.tar.gz

---
+++ packages/auth1api/auth1api-0.0.4.tar.gz/auth1api-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="auth1api",
+    version="0.0.4",
+    author="Brian Li, Mark Lalor, and Yidi Huang",
+    author_email="auth1@case.edu",
+    description="A library to make requests to your Auth1 instance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
