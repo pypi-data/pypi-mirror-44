3a8752202d4055f3abde7010c4d0b3335622c545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package aws-codedeploy-watcher/0.1.1/aws-codedeploy-watcher-0.1.1.tar.gz

---
+++ packages/aws-codedeploy-watcher/aws-codedeploy-watcher-0.1.1.tar.gz/aws-codedeploy-watcher-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['aws_codedeploy_watcher']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.9,<2.0', 'botocore>=1.12,<2.0', 'pendulum>=2.0,<3.0']
+
+entry_points = \
+{'console_scripts': ['aws-codedeploy-watcher = aws_codedeploy_watcher:main']}
+
+setup_kwargs = {
+    'name': 'aws-codedeploy-watcher',
+    'version': '0.1.1',
+    'description': 'Observe AWS Codedeploy deployments live',
+    'long_description': None,
+    'author': 'Daniel Miranda',
+    'author_email': 'danielkza2@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+}
+
+
+setup(**setup_kwargs)
