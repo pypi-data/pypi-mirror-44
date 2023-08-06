1ab317ee49fd5cb34b4b49cad85b21ba063c79f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:59 +0000

    Add package suanpan/0.8.36/suanpan-0.8.36-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.8.36-py3-none-any.whl/suanpan/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import print_function
+
+from suanpan import objects
+
+<<<<<<< HEAD
+__version__ = "0.8.36"
+=======
+__version__ = "0.8.34"
+>>>>>>> develop
+
+g = objects.Global()
