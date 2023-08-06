4bc3758267de0323ead3748888536f98364b1c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:29 +0000

    Add package citrus/0.0.3/citrus-0.0.3.tar.gz

---
+++ packages/citrus/citrus-0.0.3.tar.gz/citrus-0.0.3/citrus/core.py
@@ -0,0 +1,178 @@
+from functools import reduce
+import pulp
+from .errors import assert_binary, assert_same_problem, MissingProblemReference
+
+
+class Problem(pulp.LpProblem):
+    def __init__(self, *args ,**kwargs):
+        super().__init__(*args, **kwargs)
+        self._synth_var_ix = 0
+
+    def make_var(self, *args, **kwargs):
+        return Variable(*args, **kwargs, problem=self)
+
+    def _synth_var(self):
+        name = str(self._synth_var_ix)
+        self._synth_var_ix += 1
+        return name
+
+    def dicts(self, *args, **kwargs):
+        ds = pulp.LpVariable.dicts(*args, **kwargs)
+        return self._walk_dicts(ds)
+
+    def _walk_dicts(self, ds):
+        if isinstance(ds, pulp.LpVariable):
+            return Variable.from_lp_var(ds, self)
+        if isinstance(ds, dict):
+            return { k: self._walk_dicts(v) for k, v in ds.items() }
+        raise ValueError('Expected a dict or LpVariable. received {}'.format(ds))
+
+class AffineExpression(pulp.LpAffineExpression):
+    def __init__(self, *args, **kwargs):
+        if 'problem' in kwargs:
+            problem = kwargs.pop('problem')
+        elif len(args) > 0 and isinstance(args[0], (Variable, AffineExpression)):
+            problem = args[0]._problem
+        else:
+            raise MissingProblemReference('Cannot create AffineExpression without a reference to the problem')
+        super().__init__(*args, **kwargs)
+        self._problem = problem
+
+    def copy(self):
+        return AffineExpression(self)
+
+    def emptyCopy(self):
+        return AffineExpression(problem=self._problem)
+
+
+class Variable(pulp.LpVariable):
+    def __init__(self, *args, **kwargs):
+        self._problem = kwargs.pop('problem')
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def from_lp_var(cls, lp_var: pulp.LpVariable, problem: Problem):
+        lp_var.__class__ = cls
+        lp_var._problem = problem
+        return lp_var
+
+    def __or__(self, other):
+        return logical_or(self, other)
+
+    def __and__(self, other):
+        return logical_and(self, other)
+
+    def __xor__(self, other):
+        return logical_xor(self, other)
+
+    # -- copied from LpVariable, but with a different constructor
+    def __neg__(self):
+        return - AffineExpression(self)
+
+    def __add__(self, other):
+        return AffineExpression(self) + other
+
+    def __radd__(self, other):
+        return AffineExpression(self) + other
+
+    def __sub__(self, other):
+        return AffineExpression(self) - other
+
+    def __rsub__(self, other):
+        return other - AffineExpression(self)
+
+    def __mul__(self, other):
+        return AffineExpression(self) * other
+
+    def __rmul__(self, other):
+        return AffineExpression(self) * other
+
+    def __div__(self, other):
+        return AffineExpression(self)/other
+
+def negate(x: Variable):
+    assert_binary(x)
+    problem = x._problem
+    y = problem.make_var('(NOT {})_{}'.format(x.name, problem._synth_var()), cat=pulp.LpBinary)
+    problem.addConstraint(y == 1 - x)
+    return y
+
+def logical_and(x: Variable, y: Variable):
+    """
+    produce a variable that represents x && y
+
+    That variable can then be used in constraints and the objective func.
+    """
+    assert_same_problem(x, y)
+    assert_binary(x)
+    assert_binary(y)
+
+    model = x._problem
+
+    z = model.make_var('({} AND {})_{}'.format(x.name, y.name, model._synth_var()), cat=pulp.LpBinary)
+    model.addConstraint(z >= x + y - 1)
+    model.addConstraint(z <= x)
+    model.addConstraint(z <= y)
+    return z
+
+def logical_or(x: Variable, y: Variable):
+    assert_same_problem(x, y)
+    assert_binary(x)
+    assert_binary(y)
+
+    model = x._problem
+
+    z = model.make_var('({} AND {})_{}'.format(x.name, y.name, model._synth_var()), cat=pulp.LpBinary)
+    model.addConstraint(z <= x + y)
+    model.addConstraint(z >= x)
+    model.addConstraint(z >= y)
+    return z
+
+def logical_xor(x: Variable, y: Variable):
+    assert_same_problem(x, y)
+    assert_binary(x)
+    assert_binary(y)
+
+    model = x._problem
+
+    z = model.make_var('({} XOR {})_{}'.format(x.name, y.name, model._synth_var()), cat=pulp.LpBinary)
+    model.addConstraint(z <= x + y)
+    model.addConstraint(z >= x - y)
+    model.addConstraint(z >= y - x)
+    model.addConstraint(z <= 2 - x - y)
+
+    return z
+
+def implies(x: Variable, y: Variable):
+    assert_same_problem(x, y)
+    assert_binary(x)
+    assert_binary(y)
+
+    model = x._problem
+
+    z = model.make_var('({} implies {})_{}'.format(x.name, y.name, model._synth_var()), cat=pulp.LpBinary)
+    model.addConstraint(z <= 1 - x + y)
+    model.addConstraint(z >= 1 - x)
+    model.addConstraint(z >= y)
+    return z
+
+
+def minimum(*xs: Variable, name=None):
+    if len(xs) == 1:
+        return xs[0]
+    reduce(assert_same_problem, xs)
+    model = xs[0]._problem
+    m = model.make_var('{}_{}'.format(name or 'min', model._synth_var()), cat=pulp.LpContinuous)
+    for x in xs:
+        model.addConstraint(m <= x)
+    return m
+
+def maximum(*xs: Variable, name=None):
+    if len(xs) == 1:
+        return xs[0]
+    reduce(assert_same_problem, xs)
+    model = xs[0]._problem
+    m = model.make_var('{}_{}'.format(name or 'max', model._synth_var()), cat=pulp.LpContinuous)
+    for x in xs:
+        model.addConstraint(m >= x)
+    return m
---
+++ packages/citrus/citrus-0.0.3.tar.gz/citrus-0.0.3/citrus/tests.py
@@ -0,0 +1,218 @@
+import pytest
+import pulp
+from .core import Problem, Variable, negate, logical_and, logical_or, minimum, maximum, logical_xor, implies
+from .errors import NonBinaryVariableError, CitrusError
+
+def test_that_negate_produces_negated_variable():
+    p = Problem('negation test', pulp.LpMinimize)
+    x = p.make_var('x', cat=pulp.LpBinary)
+    y = negate(x)
+    z = negate(y)
+    p.setObjective(2 * x + y)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert x.value() == 0
+    assert y.value() == 1
+    assert z.value() == 0
+
+def test_that_logical_and_produces_constrained_value():
+    p = Problem('logical_and test', pulp.LpMinimize)
+    t1 = p.make_var('t1', cat=pulp.LpBinary)
+    t2 = p.make_var('t2', cat=pulp.LpBinary)
+    f1 = p.make_var('f1', cat=pulp.LpBinary)
+    f2 = p.make_var('f2', cat=pulp.LpBinary)
+
+    tt = logical_and(t1, t2)
+    tf = logical_and(t1, f1)
+    ft = logical_and(f1, t1)
+    ff = logical_and(f1, f2)
+    p.addConstraint(t1 == 1)
+    p.addConstraint(t2 == 1)
+    p.addConstraint(f1 == 0)
+    p.addConstraint(f2 == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tt.value() == 1
+    assert tf.value() == 0
+    assert ft.value() == 0
+    assert ff.value() == 0
+
+def test_that_anding_with_itself_is_okay():
+    p = Problem('anding with self', pulp.LpMinimize)
+    t = p.make_var('t', cat=pulp.LpBinary)
+    f = p.make_var('f', cat=pulp.LpBinary)
+
+    tt = logical_and(t, t)
+    tf = logical_and(t, f)
+    ft = logical_and(f, t)
+    ff = logical_and(f, f)
+    p.addConstraint(t == 1)
+    p.addConstraint(f == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tt.value() == 1
+    assert tf.value() == 0
+    assert ft.value() == 0
+    assert ff.value() == 0
+
+def test_that_logical_or_produces_constrained_value():
+    p = Problem('logical_or tests', pulp.LpMinimize)
+    t = p.make_var('t', cat=pulp.LpBinary)
+    f = p.make_var('f', cat=pulp.LpBinary)
+
+    tt = logical_or(t, t)
+    tf = logical_or(t, f)
+    ft = logical_or(f, t)
+    ff = logical_or(f, f)
+    p.addConstraint(t == 1)
+    p.addConstraint(f == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tt.value() == 1
+    assert tf.value() == 1
+    assert ft.value() == 1
+    assert ff.value() == 0
+
+def test_that_funcs_throws_on_non_binary_variable():
+    p = Problem('problem', pulp.LpMinimize)
+    x = p.make_var('x', cat=pulp.LpInteger)
+    y = p.make_var('y', cat=pulp.LpInteger)
+
+    with pytest.raises(NonBinaryVariableError):
+        logical_and(x, y)
+    with pytest.raises(NonBinaryVariableError):
+        logical_or(x, y)
+    with pytest.raises(NonBinaryVariableError):
+        logical_xor(x, y)
+    with pytest.raises(NonBinaryVariableError):
+        implies(x, y)
+
+def test_that_vars_from_diff_problems_raise_error():
+    a = Problem('problem a', pulp.LpMinimize)
+    b = Problem('problem b', pulp.LpMinimize)
+    x = a.make_var('x', cat=pulp.LpBinary)
+    y = b.make_var('y', cat=pulp.LpBinary)
+
+    with pytest.raises(CitrusError):
+        logical_or(x, y)
+    with pytest.raises(CitrusError):
+        logical_and(x, y)
+    with pytest.raises(CitrusError):
+        logical_xor(x, y)
+    with pytest.raises(CitrusError):
+        implies(x, y)
+
+def test_that_from_lp_var_works():
+    p = Problem('anding with self', pulp.LpMinimize)
+    t = pulp.LpVariable('t', cat=pulp.LpBinary)
+    f = p.make_var('f', cat=pulp.LpBinary)
+    t = Variable.from_lp_var(t, p)
+
+    tf = logical_and(t, f)
+    p.addConstraint(t == 1)
+    p.addConstraint(f == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tf.value() == 0
+
+def test_that_minimum_is_truly_min():
+    p = Problem('minimum', pulp.LpMaximize)
+
+    x = p.make_var('x', cat=pulp.LpContinuous)
+    p.addConstraint(x <= 52)
+
+    y = p.make_var('y', cat=pulp.LpContinuous)
+    p.addConstraint(y <= 12)
+
+    z = p.make_var('z', cat=pulp.LpContinuous)
+    p.addConstraint(z <= 15)
+
+    m = minimum(x, y, z)
+    p.setObjective(x + y + z + m)
+
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert m.value() == 12
+
+def test_that_maximum_is_truly_min():
+    p = Problem('maximum', pulp.LpMinimize)
+
+    x = p.make_var('x', cat=pulp.LpContinuous)
+    p.addConstraint(x >= 52)
+
+    y = p.make_var('y', cat=pulp.LpContinuous)
+    p.addConstraint(y >= 12)
+
+    z = p.make_var('z', cat=pulp.LpContinuous)
+    p.addConstraint(z >= 15)
+
+    m = maximum(x, y, z)
+    p.setObjective(x + y + z + m)
+
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert m.value() == 52
+
+def test_logical_xor():
+    p = Problem('logical_xor tests', pulp.LpMinimize)
+    t = p.make_var('t', cat=pulp.LpBinary)
+    f = p.make_var('f', cat=pulp.LpBinary)
+
+    tt = logical_xor(t, t)
+    tf = logical_xor(t, f)
+    ft = logical_xor(f, t)
+    ff = logical_xor(f, f)
+    p.addConstraint(t == 1)
+    p.addConstraint(f == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tt.value() == 0
+    assert tf.value() == 1
+    assert ft.value() == 1
+    assert ff.value() == 0
+
+def test_implies():
+    p = Problem('implies tests', pulp.LpMinimize)
+    t = p.make_var('t', cat=pulp.LpBinary)
+    f = p.make_var('f', cat=pulp.LpBinary)
+
+    tt = implies(t, t)
+    tf = implies(t, f)
+    ft = implies(f, t)
+    ff = implies(f, f)
+    p.addConstraint(t == 1)
+    p.addConstraint(f == 0)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
+    assert tt.value() == 1
+    assert tf.value() == 0
+    assert ft.value() == 1
+    assert ff.value() == 1
+
+def test_addition_retains_problem_reference():
+    """
+    adding two Variables should produce an AffineExpression
+    (not an LpAffineExpression, which loses its reference to the Problem)
+    """
+    p = Problem('works with addition', pulp.LpMinimize)
+    a = p.make_var('a', cat=pulp.LpContinuous)
+    b = p.make_var('b', cat=pulp.LpContinuous)
+
+    c = a + b
+    assert c._problem == p, "c should retain the problem from a, b"
+
+def test_maximum_operates_on_affine_expr():
+    p = Problem('maximum test', pulp.LpMinimize)
+    a = p.make_var('a', cat=pulp.LpContinuous)
+    b = p.make_var('b', cat=pulp.LpContinuous)
+    c = 2 * a + b
+
+    p.addConstraint((a + b + c) <= 12, 'a + b + c<= 12')
+    p.addConstraint(a >= 0, 'pos a')
+    p.addConstraint(b >= 0, 'pos b')
+    p.addConstraint(c >= 0, 'pos c')
+
+    largest = maximum(a, b, c)
+    p.setObjective(largest)
+    p.solve()
+    assert pulp.LpStatus[p.status] == 'Optimal'
