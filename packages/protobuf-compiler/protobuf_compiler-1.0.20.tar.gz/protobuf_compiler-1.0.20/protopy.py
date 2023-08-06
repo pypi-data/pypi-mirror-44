8a136ca4f1f4b12983ae66c0ccdef261c666e29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:15 +0000

    Add package protobuf-compiler/1.0.20/protobuf_compiler-1.0.20.tar.gz

---
+++ packages/protobuf-compiler/protobuf_compiler-1.0.20.tar.gz/protobuf_compiler-1.0.20/protopy.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from protobuf_compiler.main import main
+main()
---
+++ packages/protobuf-compiler/protobuf_compiler-1.0.20.tar.gz/protobuf_compiler-1.0.20/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+from setuptools import setup
+from protobuf_compiler.main import __version__
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='protobuf_compiler',
+    version=__version__,
+    packages=['protobuf_compiler'],
+    url='https://github.com/netsaj/python-protobuf-compiler',
+    license='MIT',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Fabio Moreno',
+    author_email='fabiomoreno@outlook.com',
+    description='compile all protobuf files and create a single package distribution for can be installed with pip',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=[
+        'grpcio==1.18.0',
+        'grpcio-tools==1.18.0',
+        'colorama==0.3.3',
+        'termcolor==1.1.0',
+        'tqdm==4.31.1',
+        'bleach==2.1.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'protopy = protobuf_compiler.main:main',
+            'protobuf-compiler = protobuf_compiler.main:main',
+        ],
+    },
+    scripts=['protopy.py', 'protobuf-compiler.py']
+)
