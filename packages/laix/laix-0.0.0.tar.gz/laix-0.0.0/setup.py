dfa6cbc002c4a36f8d052e8f8adbe7b905fc353b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:14 +0000

    Add package laix/0.0.0/laix-0.0.0.tar.gz

---
+++ packages/laix/laix-0.0.0.tar.gz/laix-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name = "laix",
+    version = "0.0.0",
+    keywords = ('laix'),
+    description = 'a simple egg',
+    license = 'MIT License',
+    url = 'https://github.com/liluo',
+    author = 'liluo',
+    author_email = 'liluorz@gmail.com',
+    packages = find_packages(),
+)
