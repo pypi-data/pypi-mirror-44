c834595f46e936800a6f4484c45bac2c3d2baacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:58 +0000

    Add package prettyqt/0.13.3/prettyqt-0.13.3.tar.gz

---
+++ packages/prettyqt/prettyqt-0.13.3.tar.gz/prettyqt-0.13.3/prettyqt/gui/painter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtGui, QtCore
+
+COLORS = dict(transparent=QtCore.Qt.transparent)
+
+PEN_TYPES = dict(none=QtCore.Qt.NoPen)
+
+
+class Painter(QtGui.QPainter):
+
+    def draw_image(self, point, frame_buffer):
+        self.setCompositionMode(QtGui.QPainter.CompositionMode_SourceAtop)
+        self.drawImage(point, frame_buffer)
+        self.end()
+
+    def use_antialiasing(self):
+        self.setRenderHint(self.Antialiasing, True)
+
+    def fill_rect(self, rect, color):
+        if color not in COLORS:
+            raise ValueError("Invalid value for color.")
+        self.fillRect(rect, COLORS[color])
+
+    def set_pen(self, pen_type):
+        if pen_type not in PEN_TYPES:
+            raise ValueError("Invalid value for pen_type.")
+        self.setPen(PEN_TYPES[pen_type])
