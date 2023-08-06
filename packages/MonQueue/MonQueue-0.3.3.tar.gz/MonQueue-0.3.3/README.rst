33ac0f3dc3f627c128593f683138f38e007d6a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:42 +0000

    Add package MonQueue/0.3.3/MonQueue-0.3.3.tar.gz

---
+++ packages/MonQueue/MonQueue-0.3.3.tar.gz/MonQueue-0.3.3/README.rst
@@ -0,0 +1,35 @@
+.. image:: https://img.shields.io/travis/rexzhang/monqueue.svg
+    :target: https://travis-ci.org/rexzhang/monqueue
+.. image:: https://img.shields.io/coveralls/rexzhang/monqueue.svg
+    :target: https://coveralls.io/github/rexzhang/monqueue?branch=master
+.. image:: https://img.shields.io/pypi/v/monqueue.svg
+    :target: https://pypi.org/project/MonQueue/
+.. image:: https://img.shields.io/pypi/pyversions/monqueue.svg
+    :target: https://pypi.org/project/MonQueue/
+.. image:: https://img.shields.io/pypi/dm/monqueue.svg
+    :target: https://pypi.org/project/MonQueue/
+
+========
+MonQueue
+========
+:Info: MonQueue is a Python library that allows you to use MongoDB as a message queue.
+:Author: Rex Zhang (http://github.com/rexzhang)
+
+Install
+=======
+To install it, just run:
+
+.. code-block:: console
+
+    pip install -U monqueue
+
+Coverage Report
+===============
+
+.. code-block:: console
+
+    py.test --cov=. --cov-report html
+
+Contributing
+============
+The source is available on `GitHub <http://github.com/rexzhang/monqueue>`_. To contribute to the project, fork it on GitHub and send a pull request, all contributions and suggestions are welcome.
---
+++ packages/MonQueue/MonQueue-0.3.3.tar.gz/MonQueue-0.3.3/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+import monqueue as module
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='MonQueue',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=module.__version__,
+
+    description=module.__description__,
+    long_description=long_description,
+
+    # The project's main homepage.
+    url=module.__project_url__,
+
+    # Author details
+    author=module.__author__,
+    author_email=module.__author_email__,
+
+    # Choose your license
+    license='LGPL',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    # What does your project relate to?
+    keywords='mongodb message-queue',
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pymongo'],
+
+    py_modules=['monqueue'],
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Source': module.__source_url__,
+    },
+)
