9f7d91a8699d623530d82befb1bdead9d5e0b3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:04 +0000

    Add package newsreader/0.3.0/newsreader-0.3.0.tar.gz

---
+++ packages/newsreader/newsreader-0.3.0.tar.gz/newsreader-0.3.0/nr/site.py
@@ -0,0 +1,37 @@
+"""
+Newsreader - Site
+
+Copyright (c) 2018 Trevor Bramwell <trevor@bramwell.net>
+SPDX-License-Identifier: Apache-2.0
+"""
+import requests
+from bs4 import BeautifulSoup
+
+from .article import Article
+
+
+class Site():
+    """A site represents a news source and contains a list of articles
+       after being parsed"""
+    name = "NPR"
+
+    def __init__(self, url):
+        self.url = url
+        self.articles = []
+
+    def parse(self):
+        """Parse a site and extract the articles"""
+        # logging.info("Parsing: %s", self.url)
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, 'html5lib')
+        links = soup.body.ul.find_all('li')
+        for link in links:
+            # logging.info("Adding article: %s", link)
+            href = "%s%s" % (self.url, link.a['href'])
+            article = Article(self.name, link.string, href)
+            self.articles.append(article)
+
+    def get_articles(self):
+        """Return articles from the site, requires the site have been
+           parsed first"""
+        return self.articles
---
+++ packages/newsreader/newsreader-0.3.0.tar.gz/newsreader-0.3.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""News Reader setup"""
+
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
