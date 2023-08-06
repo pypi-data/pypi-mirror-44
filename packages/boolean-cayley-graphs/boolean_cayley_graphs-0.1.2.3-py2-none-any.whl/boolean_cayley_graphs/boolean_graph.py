32b66706fe55055d10fef4e811d97c565faa6d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:29 +0000

    Add package boolean-cayley-graphs/0.1.2.3/boolean_cayley_graphs-0.1.2.3-py2-none-any.whl

---
+++ packages/boolean-cayley-graphs/boolean_cayley_graphs-0.1.2.3-py2-none-any.whl/boolean_cayley_graphs/boolean_graph.py
@@ -0,0 +1,167 @@
+r"""
+The ``boolean_graph`` module defines
+the ``BooleanGraph``  class,
+which represents a Graph whose order is a power of 2.
+
+AUTHORS:
+
+- Paul Leopardi (2017-11-11): initial version
+
+"""
+#*****************************************************************************
+#       Copyright (C) 2017 Paul Leopardi paul.leopardi@gmail.com
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+from math import log
+
+from sage.arith.srange import xsrange
+from sage.graphs.graph import Graph
+from sage.matrix.constructor import matrix
+from sage.rings.finite_rings.finite_field_constructor import FiniteField as GF
+from sage.rings.integer import Integer
+
+from boolean_cayley_graphs.integer_bits import base2
+from boolean_cayley_graphs.saveable import Saveable
+
+
+class BooleanGraph(Graph, Saveable):
+    """
+    A Graph whose order is a power of 2.
+
+    EXAMPLES:
+
+    ::
+
+        sage: from boolean_cayley_graphs.boolean_graph import BooleanGraph
+        sage: g16 = BooleanGraph(16)
+        sage: g16.order()
+        16
+
+    TESTS:
+
+    ::
+
+        sage: from boolean_cayley_graphs.boolean_graph import BooleanGraph
+        sage: g16 = BooleanGraph(16)
+        sage: print(g16)
+        Graph on 16 vertices
+
+        sage: from boolean_cayley_graphs.boolean_graph import BooleanGraph
+        sage: g16 = BooleanGraph(16)
+        sage: latex(g16)
+        \begin{tikzpicture}
+        \definecolor{cv0}{rgb}{0.0,0.0,0.0}
+        \definecolor{cfv0}{rgb}{1.0,1.0,1.0}
+        \definecolor{clv0}{rgb}{0.0,0.0,0.0}
+        ...
+        \definecolor{cv15}{rgb}{0.0,0.0,0.0}
+        \definecolor{cfv15}{rgb}{1.0,1.0,1.0}
+        \definecolor{clv15}{rgb}{0.0,0.0,0.0}
+        %
+        \Vertex[style={minimum size=1.0cm,draw=cv0,fill=cfv0,text=clv0,shape=circle},LabelOut=false,L=\hbox{$0$},x=0.0cm,y=2.5cm]{v0}
+        \Vertex[style={minimum size=1.0cm,draw=cv1,fill=cfv1,text=clv1,shape=circle},LabelOut=false,L=\hbox{$1$},x=0.3333cm,y=2.5cm]{v1}
+        \Vertex[style={minimum size=1.0cm,draw=cv2,fill=cfv2,text=clv2,shape=circle},LabelOut=false,L=\hbox{$2$},x=0.6667cm,y=2.5cm]{v2}
+        ...
+        \Vertex[style={minimum size=1.0cm,draw=cv13,fill=cfv13,text=clv13,shape=circle},LabelOut=false,L=\hbox{$13$},x=4.3333cm,y=2.5cm]{v13}
+        \Vertex[style={minimum size=1.0cm,draw=cv14,fill=cfv14,text=clv14,shape=circle},LabelOut=false,L=\hbox{$14$},x=4.6667cm,y=2.5cm]{v14}
+        \Vertex[style={minimum size=1.0cm,draw=cv15,fill=cfv15,text=clv15,shape=circle},LabelOut=false,L=\hbox{$15$},x=5.0cm,y=2.5cm]{v15}
+        %
+        %
+        \end{tikzpicture}
+    """
+
+
+    def __init__(self, *args, **kwargs):
+        """
+        A Graph whose order is a power of 2.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.boolean_graph import BooleanGraph
+            sage: g16 = BooleanGraph(8)
+            sage: g16.order()
+            8
+        """
+        super(BooleanGraph, self).__init__(*args, **kwargs)
+
+        if not log(self.order(),2).is_integer():
+            raise ValueError
+
+
+    def is_linear_isomorphic(self, other, certificate=False):
+        r"""
+        Check that the two BooleanGraphs ``self`` and ``other`` are isomorphic
+        and that the isomorphism is given by a GF(2) linear mapping on the
+        vector space of vertices.
+
+        INPUT:
+
+        - ``self`` -- the current object.
+        - ``other`` -- another object of class BooleanFunctionImproved.
+        - ``certificate`` -- bool (default False). If true, return a GF(2) matrix
+           that defines the isomorphism.
+
+        OUTPUT:
+
+        If ``certificate`` is false, a bool value.
+        If ``certificate`` is true, a tuple consisting of either (False, None)
+        or (True, M), where M is a GF(2) matrix that defines the isomorphism.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.boolean_function_improved import BooleanFunctionImproved
+            sage: from boolean_cayley_graphs.boolean_graph import BooleanGraph
+            sage: bf1 = BooleanFunctionImproved([0,1,0,0])
+            sage: cg1 = BooleanGraph(bf1.cayley_graph())
+            sage: bf2 = BooleanFunctionImproved([0,0,1,0])
+            sage: cg2 = BooleanGraph(bf2.cayley_graph())
+            sage: cg1.is_linear_isomorphic(cg2)
+            True
+            sage: cg2.is_linear_isomorphic(cg1, certificate=True)
+            (
+                  [0 1]
+            True, [1 0]
+            )
+
+
+        """
+
+        # Check the isomorphism via canonical labels.
+        # This is to work around the slow speed of is_isomorphic in some cases.
+        if self.canonical_label() != other.canonical_label():
+            return (False, None)
+
+        # Obtain the mapping that defines the isomorphism.
+        is_isomorphic, mapping = self.is_isomorphic(other, certificate=True)
+
+        # If self is not isomorphic to other, it is not linear isomorphic.
+        if not is_isomorphic:
+            return (False,None) if certificate else False
+
+        # Check that the mapping is linear on each pair of basis vectors.
+        dim = Integer(log(self.order(), 2))
+        for a in xsrange(dim):
+            for b in xsrange(a + 1, dim):
+                if mapping[2**a] ^ mapping[2**b] != mapping[(2**a) ^ (2**b)]:
+                    return (False,None) if certificate else False
+
+        # The mapping is linear.
+        # If the caller does not want a certificate, just return True.
+        if not certificate:
+            return True
+
+        # Create the G(2) matrix corresponding to the mapping.
+        mapping_matrix = matrix(GF(2), [
+            base2(dim, Integer(mapping[2**a]))
+            for a in xsrange(dim)]).transpose()
+        return (True, mapping_matrix)
+
---
+++ packages/boolean-cayley-graphs/boolean_cayley_graphs-0.1.2.3-py2-none-any.whl/boolean_cayley_graphs/strongly_regular_graph.py
@@ -0,0 +1,214 @@
+r"""
+The ``strongly_regular_graph`` module defines the
+``StronglyRegularGraph`` class, which represents
+a strongly regular graph, with some of its properties,
+such as its stronly regular parameters.
+
+AUTHORS:
+
+- Paul Leopardi (2016-10-19): initial version
+
+"""
+#*****************************************************************************
+#       Copyright (C) 2016-2017 Paul Leopardi paul.leopardi@gmail.com
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+from sage.graphs.graph import Graph
+from sage.matrix.constructor import matrix
+from sage.misc.lazy_attribute import lazy_attribute
+from sage.rings.finite_rings.finite_field_constructor import GF
+
+from graph_improved import GraphImproved
+
+
+class StronglyRegularGraph(GraphImproved):
+    r"""
+    A strongly regular graph, with lazy attributes for some computed properties.
+
+    The class inherits from ``GraphImproved``, and is initialized either from a graph
+    or from keyword arguments.
+
+    EXAMPLES:
+
+    ::
+
+        sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+        sage: g = royle_x_graph()
+        sage: g.is_strongly_regular()
+        True
+        sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+        sage: srg = StronglyRegularGraph(g)
+        sage: srg.is_strongly_regular()
+        True
+
+    TESTS:
+
+    ::
+
+        sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+        sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+        sage: g = royle_x_graph()
+        sage: srg = StronglyRegularGraph(g)
+        sage: print(srg)
+        Graph on 64 vertices
+
+        sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+        sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+        sage: g = royle_x_graph()
+        sage: srg = StronglyRegularGraph(g)
+        sage: latex(srg)
+        \begin{tikzpicture}
+        \definecolor{cv0}{rgb}{0.0,0.0,0.0}
+        \definecolor{cfv0}{rgb}{1.0,1.0,1.0}
+        \definecolor{clv0}{rgb}{0.0,0.0,0.0}
+        ...
+        \Vertex[style={minimum size=1.0cm,draw=cv61,fill=cfv61,text=clv61,shape=circle},LabelOut=false,L=\hbox{$61$},x=2.87cm,y=4.6843cm]{v61}
+        \Vertex[style={minimum size=1.0cm,draw=cv62,fill=cfv62,text=clv62,shape=circle},LabelOut=false,L=\hbox{$62$},x=3.3104cm,y=3.6687cm]{v62}
+        \Vertex[style={minimum size=1.0cm,draw=cv63,fill=cfv63,text=clv63,shape=circle},LabelOut=false,L=\hbox{$63$},x=3.9728cm,y=3.5058cm]{v63}
+        %
+        \Edge[lw=0.1cm,style={color=cv0v1,},](v0)(v1)
+        \Edge[lw=0.1cm,style={color=cv0v2,},](v0)(v2)
+        \Edge[lw=0.1cm,style={color=cv0v3,},](v0)(v3)
+        ...
+        \Edge[lw=0.1cm,style={color=cv58v63,},](v58)(v63)
+        \Edge[lw=0.1cm,style={color=cv59v62,},](v59)(v62)
+        \Edge[lw=0.1cm,style={color=cv60v61,},](v60)(v61)
+        %
+        \end{tikzpicture}
+    """
+    def __init__(self, graph=None, **kwargs):
+        r"""
+        Initialize either from a graph or from keyword arguments.
+
+        TESTS::
+
+            sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+            sage: g = royle_x_graph()
+            sage: g.is_strongly_regular()
+            True
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: srg = StronglyRegularGraph(g)
+            sage: srg.is_strongly_regular()
+            True
+        """
+        GraphImproved.__init__(self, graph, **kwargs)
+
+    @lazy_attribute
+    def strongly_regular_parameters(self):
+        r"""
+        The strongly regular parameters of the graph.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+            sage: g = royle_x_graph()
+            sage: g.is_strongly_regular(parameters=True)
+            (64, 35, 18, 20)
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: srg = StronglyRegularGraph(g)
+            sage: srg.is_strongly_regular()
+            True
+            sage: srg.strongly_regular_parameters
+            (64, 35, 18, 20)
+        """
+        return self.is_strongly_regular(parameters=True)
+
+
+    @lazy_attribute
+    def matrix_GF2(self):
+        r"""
+        The adjacency matrix of the graph, over :math:`\mathbb{F}_2`.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.bent_function import BentFunction
+            sage: bentf = BentFunction([0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,0])
+            sage: g = bentf.cayley_graph()
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: sg = StronglyRegularGraph(g)
+            sage: sg.matrix_GF2
+            [0 0 0 1 0 0 0 1 0 0 0 1 1 1 1 0]
+            [0 0 1 0 0 0 1 0 0 0 1 0 1 1 0 1]
+            [0 1 0 0 0 1 0 0 0 1 0 0 1 0 1 1]
+            [1 0 0 0 1 0 0 0 1 0 0 0 0 1 1 1]
+            [0 0 0 1 0 0 0 1 1 1 1 0 0 0 0 1]
+            [0 0 1 0 0 0 1 0 1 1 0 1 0 0 1 0]
+            [0 1 0 0 0 1 0 0 1 0 1 1 0 1 0 0]
+            [1 0 0 0 1 0 0 0 0 1 1 1 1 0 0 0]
+            [0 0 0 1 1 1 1 0 0 0 0 1 0 0 0 1]
+            [0 0 1 0 1 1 0 1 0 0 1 0 0 0 1 0]
+            [0 1 0 0 1 0 1 1 0 1 0 0 0 1 0 0]
+            [1 0 0 0 0 1 1 1 1 0 0 0 1 0 0 0]
+            [1 1 1 0 0 0 0 1 0 0 0 1 0 0 0 1]
+            [1 1 0 1 0 0 1 0 0 0 1 0 0 0 1 0]
+            [1 0 1 1 0 1 0 0 0 1 0 0 0 1 0 0]
+            [0 1 1 1 1 0 0 0 1 0 0 0 1 0 0 0]
+
+        """
+        return matrix(GF(2), self)
+
+
+    @lazy_attribute
+    def rank(self):
+        r"""
+        The 2-rank of the graph.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+            sage: g = royle_x_graph()
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: srg = StronglyRegularGraph(g)
+            sage: srg.rank
+            64
+        """
+        return self.matrix_GF2.rank()
+
+
+    @lazy_attribute
+    def automorphism_group(self):
+        r"""
+        The automorphism group of the graph.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+            sage: g = royle_x_graph()
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: srg = StronglyRegularGraph(g)
+            sage: srg.automorphism_group
+            Permutation Group with generators [(11,21)(12,22)(13,23)(14,24)(15,25)(16,26)(17,27)(18,28)(19,29)(20,30)(36,37)(44,49)(45,50)(46,51)(47,52)(48,53), (5,11)(6,12)(7,13)(8,14)(9,15)(10,16)(27,31)(28,32)(29,33)(30,34)(37,38)(43,44)(50,54)(51,55)(52,56)(53,57), (3,4)(6,7)(8,9)(12,13)(14,15)(17,18)(22,23)(24,25)(27,28)(31,32)(41,42)(47,48)(52,53)(56,57)(59,60)(61,62), (2,3)(5,6)(9,10)(11,12)(15,16)(18,19)(21,22)(25,26)(28,29)(32,33)(40,41)(46,47)(51,52)(55,56)(58,59)(62,63), (2,5)(3,6)(4,7)(14,17)(15,18)(16,19)(24,27)(25,28)(26,29)(34,35)(38,39)(44,45)(49,50)(55,58)(56,59)(57,60), (1,2)(6,8)(7,9)(12,14)(13,15)(19,20)(22,24)(23,25)(29,30)(33,34)(39,40)(45,46)(50,51)(54,55)(59,61)(60,62), (1,20)(2,19)(3,18)(4,17)(5,16)(6,15)(7,14)(8,13)(9,12)(10,11)(37,43)(38,44)(39,45)(40,46)(41,47)(42,48), (0,1)(2,38,4,36)(3,37)(5,57,23,46)(6,53,22,51)(7,48,21,55)(8,17,26,33)(9,27,25,29)(10,31,24,19)(11,56,13,47)(12,52)(14,18,16,32)(15,28)(20,58,34,60)(30,59)(35,39)(40,43,42,49)(41,44)(45,63,54,61)(50,62)]
+        """
+        return Graph.automorphism_group(self)
+
+
+    @lazy_attribute
+    def group_order(self):
+        r"""
+        The order of the automorphism group of the graph.
+
+        EXAMPLES:
+
+        ::
+
+            sage: from boolean_cayley_graphs.royle_x_graph import royle_x_graph
+            sage: g = royle_x_graph()
+            sage: from boolean_cayley_graphs.strongly_regular_graph import StronglyRegularGraph
+            sage: srg = StronglyRegularGraph(g)
+            sage: srg.group_order
+            2580480
+        """
+        return self.automorphism_group.order()
