b0ea3f2d2e354f6bc9a9a7d2726220d66d39f5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:55 +0000

    Add package DBService/1.6/DBService-1.6.tar.gz

---
+++ packages/DBService/DBService-1.6.tar.gz/DBService-1.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: hang.zhang
+# @Email:  hhczy1003@163.com
+# @Date:   2017-08-01 20:37:27
+# @Last Modified by:   hang.zhang
+# @Last Modified time: 2019-04-12 17:32:01
+
+from setuptools import setup
+
+setup(
+    name="DBService",
+    version="1.6",
+    author="yiTian.zhang",
+    author_email="hhczy1003@163.com",
+    packages=["DBService"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "scrapy",
+        "scrapy-redis",
+        "pymongo",
+        "apscheduler",
+        "selenium",
+    ],
+)
