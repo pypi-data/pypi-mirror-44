0f782f53c72adb2c2745a77edb5ed2fc208ac9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:31 +0000

    Add package django-crosswalk/0.0.12/django_crosswalk-0.0.12-py2-none-any.whl

---
+++ packages/django-crosswalk/django_crosswalk-0.0.12-py2-none-any.whl/crosswalk/views/delete_match.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.response import Response
+
+from crosswalk.authentication import AuthenticatedView
+from crosswalk.models import Domain, Entity
+
+
+class DeleteMatch(AuthenticatedView):
+    def post(self, request, domain):
+        """
+        POST searches for an entity based on criteria. If only one entity is
+        returned from query, it is deleted. If more than one, return 403.
+        """
+        block_attrs = request.data.copy()
+
+        try:
+            domain = Domain.objects.get(slug=domain)
+        except ObjectDoesNotExist:
+            return Response(
+                "Domain not found.", status=status.HTTP_404_NOT_FOUND
+            )
+
+        entities = Entity.objects.filter(domain=domain)
+        entities = entities.filter(attributes__contains=block_attrs)
+
+        if entities.count() == 0:
+            return Response(
+                "Entity not found.", status=status.HTTP_404_NOT_FOUND
+            )
+
+        elif entities.count() > 1:
+            return Response(
+                "More than one entity found.", status=status.HTTP_403_FORBIDDEN
+            )
+
+        entities.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
