c94cad74ef50bd0a804c12b6da3b1396c6e3e3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:02 +0000

    Add package refactored/0.3/refactored-0.3.tar.gz

---
+++ packages/refactored/refactored-0.3.tar.gz/refactored-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='refactored',
+      version='0.3',
+      description='refactored unittesting library',
+      url='https://github.com/colaberry/rfvalidator',
+      install_requires=['numpy', 'pandas', 'seaborn'],
+      author='refactored',
+      author_email='refactored@colaberry.com',
+      license='LICENCE',
+      packages=['refactored'],
+      zip_safe=False)
