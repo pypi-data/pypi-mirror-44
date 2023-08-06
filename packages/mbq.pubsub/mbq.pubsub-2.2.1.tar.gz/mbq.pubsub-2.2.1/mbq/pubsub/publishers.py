16276ad291464f81d6b7586892bf1d1aa3474347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package mbq.pubsub/2.2.1/mbq.pubsub-2.2.1.tar.gz

---
+++ packages/mbq.pubsub/mbq.pubsub-2.2.1.tar.gz/mbq.pubsub-2.2.1/mbq/pubsub/publishers.py
@@ -0,0 +1,48 @@
+import json
+from datetime import date, datetime
+from typing import Union
+
+import arrow
+import boto3
+from google.protobuf import message
+
+import mbq.atomiq
+from mbq.pubsub import utils
+
+
+def _json_dumps_with_iso_datetimes(data: Union[dict, list, tuple]):
+    def _default(obj):
+        if isinstance(obj, (datetime, date, arrow.Arrow)):
+            return obj.isoformat()
+        else:
+            raise TypeError("{} is not JSON serializable".format(obj))
+
+    return json.dumps(data, default=_default, sort_keys=True)
+
+
+def _publish(payload: dict, topic_arn: str, use_atomiq: bool):
+    if use_atomiq:
+        mbq.atomiq.sns_publish(topic_arn=topic_arn, payload=payload)
+    else:
+        sns = boto3.client("sns")
+        sns.publish(
+            TargetArn=topic_arn,
+            MessageStructure="json",
+            Message=json.dumps({"default": _json_dumps_with_iso_datetimes(payload)}),
+        )
+
+
+def publish_proto(proto: message.Message, topic_arn: str, use_atomiq: bool = True):
+    message_type = f"proto.{proto.__module__}.{proto.DESCRIPTOR.name}"
+    payload = proto.SerializeToString().decode()
+    envelope = utils.Envelope(message_type, payload, utils.PayloadType.PROTO, arrow.now())
+
+    _publish(envelope.asdict(), topic_arn, use_atomiq)
+
+
+def publish_json(
+    message_type: str, data: Union[dict, list, tuple], topic_arn: str, use_atomiq: bool = True
+):
+    envelope = utils.Envelope(message_type, data, utils.PayloadType.JSON, arrow.now())
+
+    _publish(envelope.asdict(), topic_arn, use_atomiq)
