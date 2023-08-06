77ae87fbeb396574fed6e3c3009c9e715193d73f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package keras-nalu/1.0.0/keras-nalu-1.0.0.tar.gz

---
+++ packages/keras-nalu/keras-nalu-1.0.0.tar.gz/keras-nalu-1.0.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['src']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['keras>=2.2,<3.0']
+
+setup_kwargs = {
+    'name': 'keras-nalu',
+    'version': '1.0.0',
+    'description': 'Keras implementation of a NALU layer',
+    'long_description': None,
+    'author': 'Dennis Torres',
+    'author_email': 'djtorres0@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
