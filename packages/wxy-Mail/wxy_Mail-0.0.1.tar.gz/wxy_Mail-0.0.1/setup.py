f8869c4a7f9e0168a8858938045d0da7ad05f77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:37 +0000

    Add package wxy-Mail/0.0.1/wxy_Mail-0.0.1.tar.gz

---
+++ packages/wxy-Mail/wxy_Mail-0.0.1.tar.gz/wxy_Mail-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wxy_Mail',
+    version='0.0.1',
+    description='发送邮件相关操作',
+    url='https://gitee.com/wang_xing_yuan/wxy-Mail.git',
+    author='cooper.wang',
+    author_email='',
+    license='MIT',
+    packages=['wxy_Mail'],
+    # install_requires = [""]
+)
