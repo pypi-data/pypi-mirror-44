0297e0f55887b38094720787eb4c13c5e0b236b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package irisSeg/0.2/irisSeg-0.2.tar.gz

---
+++ packages/irisSeg/irisSeg-0.2.tar.gz/irisSeg-0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='irisSeg',
+      version='0.2',
+      description='Daugman implementation to segement iris and pupil',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+      ],
+      keywords="Daugman Daugman's integrodifferential operator iris pupil segementation computer vision",
+      url='http://github.com/mzmmoazam/',
+      author='mzm',
+      author_email='mzm.moazam@gmail.com',
+      license='MIT',
+      packages=['irisSeg'],
+      install_requires=[
+          'numpy',
+          'opencv-python',
+          'scipy',
+          'scikit-image',
+          'matplotlib'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['iris-seg=irisSeg.command_line:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
