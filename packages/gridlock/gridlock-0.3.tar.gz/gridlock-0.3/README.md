9dcbc159ac066caadd09c35f3b93507778894011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:58 +0000

    Add package gridlock/0.3/gridlock-0.3.tar.gz

---
+++ packages/gridlock/gridlock-0.3.tar.gz/gridlock-0.3/README.md
@@ -0,0 +1,25 @@
+# Gridlock README
+
+Gridlock is a Python module for drawing on coupled grids.
+
+Gridlock is used primarily for 'painting' shapes in 3D on multiple grids which represent the
+same spatial region, but are offset from each other. It does straightforward natural <-> grid unit
+conversion and can handle non-uniform rectangular grids (the entire grid is generated based on
+the coordinates of the boundary points along each axis).
+
+## Installation
+
+Requirements:
+* python 3 (written and tested with 3.5)
+* numpy
+* [float_raster](https://mpxd.net/code/jan/float_raster)
+* matplotlib (optional, used for visualization functions)
+* mpl_toolkits.mplot3d (optional, used for isosurface visualization)
+* skimage (optional, used for isosurface visualization)
+
+Install with pip, via git:
+
+```bash
+pip install git+https://mpxd.net/code/jan/float_raster.git@release
+pip install git+https://mpxd.net/code/jan/gridlock.git@release
+```
---
+++ packages/gridlock/gridlock-0.3.tar.gz/gridlock-0.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import gridlock
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='gridlock',
+      version=gridlock.version,
+      description='Coupled gridding library',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Jan Petykiewicz',
+      author_email='anewusername@gmail.com',
+      url='https://mpxd.net/code/jan/gridlock',
+      packages=find_packages(),
+      install_requires=[
+            'numpy',
+            'float_raster',
+      ],
+      extras_require={
+          'visualization': ['matplotlib'],
+          'visualization-isosurface': [
+                'matplotlib',
+                'skimage>=0.13',
+                'mpl_toolkits',
+          ],
+      },
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU Affero General Public License v3',
+            'Topic :: Multimedia :: Graphics :: 3D Rendering',
+            'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+            'Topic :: Scientific/Engineering :: Physics',
+            'Topic :: Scientific/Engineering :: Visualization',
+      ],
+      )
+
