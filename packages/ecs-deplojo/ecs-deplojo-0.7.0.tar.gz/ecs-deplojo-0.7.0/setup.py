2780dc3c6649f462890508afc83cee78b886f53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:54 +0000

    Add package ecs-deplojo/0.7.0/ecs-deplojo-0.7.0.tar.gz

---
+++ packages/ecs-deplojo/ecs-deplojo-0.7.0.tar.gz/ecs-deplojo-0.7.0/setup.py
@@ -0,0 +1,58 @@
+import re
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'boto3==1.9.37',
+    'click==6.7',
+    'pyaml==16.12.2',
+    'pytz',
+]
+
+tests_requires = [
+    'flake8==3.3.0',
+    'isort==4.2.5',
+    'moto==0.4.31',
+    'pytest==3.0.7',
+    'pytest-cov==2.4.0',
+    'pytest-capturelog==0.7',
+]
+
+with open('README.rst') as fh:
+    long_description = re.sub(
+        '^.. start-no-pypi.*^.. end-no-pypi', '', fh.read(), flags=re.M | re.S)
+
+setup(
+    name='ecs-deplojo',
+    version='0.7.0',
+    author='Lab Digital B.V.',
+    author_email='opensource@labdigital.nl',
+    url='https://www.github.com/labd/ecs-deplojo/',
+    description="Deployment tool for Amazon ECS",
+    long_description=long_description,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_requires,
+    extras_require={'test': tests_requires},
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': {
+            'ecs-deplojo = ecs_deplojo.main:cli'
+        }
+    },
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ packages/ecs-deplojo/ecs-deplojo-0.7.0.tar.gz/ecs-deplojo-0.7.0/src/ecs_deplojo/task_definitions.py
@@ -0,0 +1,88 @@
+import json
+import os.path
+from string import Template
+
+
+def generate_task_definitions(config, template_vars, base_path,
+                              output_path=None):
+    """Generate the task definitions
+
+    :parameter config: The yaml config contents
+    :parameter template_vars: Key-Value dict with template replacements
+    :parameter base_path: The base path (location of the config file)
+    :parameter output_path: Optional path to write the task definitions to.
+    :rtype dict:
+
+    """
+    task_definitions = {}
+
+    for name, info in config['task_definitions'].items():
+        # Default environment. Always create a new dict instance since it is
+        # mutated.
+        env_items = {}
+        env_items.update(config.get('environment', {}))
+
+        # Environment groups
+        env_group = info.get('environment_group')
+        if env_group:
+            env_items.update(config['environment_groups'][env_group])
+
+        overrides = info.get('overrides', {})
+        definition = generate_task_definition(
+            info['template'],
+            env_items,
+            template_vars,
+            overrides,
+            name=name,
+            base_path=base_path,
+            task_role_arn=info.get('task_role_arn'))
+
+        if output_path:
+            write_task_definition(name, definition, output_path)
+        task_definitions[name] = {
+            'definition': definition,
+        }
+    return task_definitions
+
+
+def generate_task_definition(filename, environment, template_vars, overrides,
+                             name, base_path=None, task_role_arn=None):
+
+    """Generate the task definitions"""
+    if base_path:
+        filename = os.path.join(base_path, filename)
+
+    with open(filename, 'r') as fh:
+        data = json.load(fh)
+
+        data['family'] = name
+        if task_role_arn:
+            data['taskRoleArn'] = task_role_arn
+
+        num_containers = len(data['containerDefinitions'])
+        for container in data['containerDefinitions']:
+            container['image'] = Template(container['image']).substitute(template_vars)
+            container['environment'] = environment
+
+            hostname = name
+            if num_containers > 1:
+                hostname += '-%s' % container['name'].replace('_', '-')
+            container.setdefault('hostname', hostname)
+
+            if overrides:
+                container_overrides = overrides.get(container['name'], {})
+                for key, value in container_overrides.items():
+                    if key in container and isinstance(container[key], list):
+                        container[key].extend(value)
+                    elif key in container and isinstance(container[key], dict):
+                        container[key].update(value)
+                    else:
+                        container[key] = value
+
+        return data
+
+
+def write_task_definition(name, definition, output_path):
+    filename = os.path.join(output_path, '%s.json' % name)
+    with open(filename, 'w') as fh:
+        json.dump(definition, fh, indent=4)
