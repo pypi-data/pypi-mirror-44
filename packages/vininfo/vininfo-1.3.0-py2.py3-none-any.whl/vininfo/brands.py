aa6e5d6d15b8ab00bff9dc690c37c0cbc9ccd366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package vininfo/1.3.0/vininfo-1.3.0-py2.py3-none-any.whl

---
+++ packages/vininfo/vininfo-1.3.0-py2.py3-none-any.whl/vininfo/brands.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .common import Brand
+from .details import *
+
+
+class Lada(Brand):
+
+    extractor = AvtoVazDetails
+
+
+class Nissan(Brand):
+
+    extractor = NissanDetails
+
+
+class Opel(Brand):
+
+    extractor = OpelDetails
+
+
+class Renault(Brand):
+
+    extractor = RenaultDetails
---
+++ packages/vininfo/vininfo-1.3.0-py2.py3-none-any.whl/vininfo/common.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+
+
+class Annotatable(object):
+
+    annotate_titles = {}
+
+    def annotate(self):
+
+        annotations = {}
+        no_attr = set()
+
+        for attr_name, label in self.annotate_titles.items():
+            value = getattr(self, attr_name, no_attr)
+
+            if value is no_attr:
+                continue
+
+            if isinstance(value, list):
+                value = ', '.join('%s' % val for val in value)
+
+            annotations[label] = '%s' % value
+
+        return OrderedDict((title, value) for title, value in sorted(annotations.items(), key=lambda item: item[0]))
+
+
+class Brand(object):
+
+    __slots__ = ['manufacturer']
+
+    extractor = None
+
+    def __init__(self, manufacturer=None):
+        self.manufacturer = manufacturer or self.title
+
+    @property
+    def title(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return '%s (%s)' % (self.title, self.manufacturer)
+
+
+class UnsupportedBrand(Brand):
+    """Unsupported brand."""
