0a54e6d3d129f5615c9ab9543b498346053df0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:23 +0000

    Add package imagedt/0.4.5/imagedt-0.4.5.tar.gz

---
+++ packages/imagedt/imagedt-0.4.5.tar.gz/imagedt-0.4.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+	name = 'imagedt',
+	version = '0.4.5',
+	keywords = ('imagedt'),
+	description = 'a python lib for neural networks, file and image processing etc. ',
+	long_description="##[Details](https://github.com/hanranCode/ImageDT)",
+	long_description_content_type="text/markdown",
+	license = 'Apache License 2.0',
+
+	url = 'https://github.com/hanranCode/ImageDT',
+	author = 'pytorch_fans11',
+	author_email = 'zhengyzms@163.com',
+	packages = find_packages(),
+	include_package_data = True,
+	platforms = 'any',
+	install_requires = [],
+)
