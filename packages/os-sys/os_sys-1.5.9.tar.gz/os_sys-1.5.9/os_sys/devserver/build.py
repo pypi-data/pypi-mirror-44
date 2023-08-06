1399a788e467ce7caf235afad99e3cda8306c525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package os-sys/1.5.9/os_sys-1.5.9.tar.gz

---
+++ packages/os-sys/os_sys-1.5.9.tar.gz/os_sys-1.5.9/os_sys/devserver/build.py
@@ -0,0 +1,14 @@
+try:
+    from . import server
+except:
+    import server
+__all__ = ['path', 'config', 'HTTPResponse', 'paths']
+def paths(port, **programs):
+
+    server.run_(port, **programs)
+def config(port=9999, **programs):
+    paths(port, **programs)
+def HTTPResponse(response):
+    return response
+def path(program, name, *args, **kwargs):
+    return name, program
