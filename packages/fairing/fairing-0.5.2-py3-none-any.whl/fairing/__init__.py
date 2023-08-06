c435e667a4b733be933e6c47d0b3b87ec853c1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:23 +0000

    Add package fairing/0.5.2/fairing-0.5.2-py3-none-any.whl

---
+++ packages/fairing/fairing-0.5.2-py3-none-any.whl/fairing/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+if os.getenv('FAIRING_RUNTIME', None) is not None:
+    from fairing.runtime_config import config
+else:
+    from fairing.config import config
+
+name = "fairing"
+from .ml_tasks.tasks import TrainJob, PredictionEndpoint
