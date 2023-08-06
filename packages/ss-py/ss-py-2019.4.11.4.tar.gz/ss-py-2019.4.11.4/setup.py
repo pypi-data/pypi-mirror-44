5646474ecb4e8ff38ea6164389b312346245e707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:22 +0000

    Add package ss-py/2019.4.11.4/ss-py-2019.4.11.4.tar.gz

---
+++ packages/ss-py/ss-py-2019.4.11.4.tar.gz/ss-py-2019.4.11.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ss-py',
+    version='2019.4.11.4',
+    license="MIT Licence",
+    description="SS Tool",
+
+    author='YaronH',
+    author_email="yaronhuang@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["aigpy >= 2019.3.29.2", "netfilter"],
+
+    entry_points={'console_scripts': [
+        'ss-py = ss_py:main', ]}
+)
