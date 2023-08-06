db906fd75705181e5b6e452e93c9fa593c2b8808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:07 +0000

    Add package exabyte-json-include/2.0.0/exabyte_json_include-2.0.0.tar.gz

---
+++ packages/exabyte-json-include/exabyte_json_include-2.0.0.tar.gz/exabyte_json_include-2.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='exabyte_json_include',
+    version='2.0.0',
+    description='An extension for json_include to support file inclusion',
+    url='https://github.com/Exabyte-io/json_include',
+    author='Exabyte Inc.',
+    author_email='info@exabyte.io',
+    py_modules=["json_include"],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ]
+)
