c9ebbdc91ccedf4f94272f96ff088ca3041a0144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:25 +0000

    Add package nbprogress/0.1.0/nbprogress-0.1.0.tar.gz

---
+++ packages/nbprogress/nbprogress-0.1.0.tar.gz/nbprogress-0.1.0/README.md
@@ -0,0 +1,8 @@
+# nbprogress
+
+Progressbar for Jupyter notebooks.
+
+Important to enable enable ipythonwidgets by running
+```Python
+jupyter nbextension enable --py widgetsnbextension
+```
---
+++ packages/nbprogress/nbprogress-0.1.0.tar.gz/nbprogress-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbprogress",
+    version="0.1.0",
+    python_requires='>=3.*',
+    author="saberd",
+    author_email="mail@saberd.com",
+    description="A lightweigth progressbar for jupyter notebook",
+    long_description="Important to enable enable ipythonwidgets by running \
+        ```Python \
+        jupyter nbextension enable --py widgetsnbextension \
+        ```",
+    long_description_content_type="text/markdown",
+    url="https://github.com/saberd/nbprogress",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'ipywidgets',
+        'IPython',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
