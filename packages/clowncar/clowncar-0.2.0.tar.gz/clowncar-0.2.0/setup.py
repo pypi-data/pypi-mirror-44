9af9f68e239dd09c6ee987c16a778d45db57e237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:59 +0000

    Add package clowncar/0.2.0/clowncar-0.2.0.tar.gz

---
+++ packages/clowncar/clowncar-0.2.0.tar.gz/clowncar-0.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+exec(open('clowncar/version.py').read())
+
+kwargs = {
+    "name": "clowncar",
+    "version": str(__version__),
+    "packages": ["clowncar"],
+    "description": "Simple client side load balancing module.",
+    # PyPi, despite not parsing markdown, will prefer the README.md to the
+    # standard README. Explicitly read it here.
+    "long_description": open("README").read(),
+    "author": "Gary M. Josack",
+    "maintainer": "Gary M. Josack",
+    "author_email": "gary@byoteki.com",
+    "maintainer_email": "gary@byoteki.com",
+    "license": "MIT",
+    "url": "https://github.com/gmjosack/clowncar",
+    "download_url": "https://github.com/gmjosack/clowncar/archive/master.tar.gz",
+    "classifiers": [
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+}
+
+setup(**kwargs)
