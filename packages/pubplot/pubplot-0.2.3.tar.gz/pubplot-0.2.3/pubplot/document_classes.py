c9a9476398a8194764dbd1dadbb78568a7419f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package pubplot/0.2.3/pubplot-0.2.3.tar.gz

---
+++ packages/pubplot/pubplot-0.2.3.tar.gz/pubplot-0.2.3/pubplot/document_classes.py
@@ -0,0 +1,79 @@
+# coding=utf-8
+# ISC License
+# Copyright (c) 2017, Hugo Sadok
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# document_classes.py
+
+from pylatex import Package, NoEscape
+
+ieee_infocom = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['10pt', 'conference', 'letterpaper'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_conf = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['conference'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_conf_compsoc = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['conference', 'compsoc'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_jrnl = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['journal'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_jrnl_compsoc = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['10pt', 'journal', 'compsoc'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_jrnl_comsoc = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['journal', 'comsoc'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+ieee_jrnl_transmag = {
+    'documentclass': 'IEEEtran',
+    'document_options': ['journal', 'transmag'],
+    'packages': [Package(NoEscape('times'))],
+}
+
+acm_sigconf = {
+    'documentclass': 'acmart',
+    'document_options': 'sigconf'
+}
+
+usenix = {
+    'documentclass': 'article',
+    'document_options': ['letterpaper','twocolumn','10pt'],
+    'packages': [Package(NoEscape('usenix'))],
+}
+
+sbc = {
+    'documentclass': 'article',
+    'document_options': ['12pt'],
+    'packages': [Package(NoEscape('sbc-template'))],
+    'data': [NoEscape(r'\address{a}')]
+}
---
+++ packages/pubplot/pubplot-0.2.3.tar.gz/pubplot-0.2.3/setup.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='pubplot',
+    version='0.2.3',
+    description='Seamless LaTeX and Matplotlib integration for publication plots',
+    long_description=readme,
+    packages=find_packages(),
+    url='',
+    download_url='https://github.com/hugombarreto/pubplot',
+    license='ISC',
+    author='Hugo Sadok',
+    author_email='hugo@sadok.com.br',
+    keywords=['matplotlib', 'latex', 'pgf'],
+    include_package_data=True,
+    install_requires=[
+        'matplotlib',
+        'pylatex',
+        'numpy'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
