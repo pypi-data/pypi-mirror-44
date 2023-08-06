c392538cf5a76261f2636c3642e2a81357155d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package django-perms-provisioner/0.0.3/django-perms-provisioner-0.0.3.tar.gz

---
+++ packages/django-perms-provisioner/django-perms-provisioner-0.0.3.tar.gz/django-perms-provisioner-0.0.3/tests/conftest.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+
+
+def pytest_configure():
+    settings.configure(
+        MIDDLEWARE_CLASSES=[],
+        INSTALLED_APPS=[
+            "django.contrib.admin",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.messages",
+            "django.contrib.sessions",
+            "django.contrib.sites",
+            "django.contrib.staticfiles",
+            "django_perms_provisioner",
+        ],
+        CACHES={
+            "default": {
+                "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+                "LOCATION": "unique-snowflake",
+            }
+        },
+        DATABASES={
+            "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite"}
+        },
+    )
