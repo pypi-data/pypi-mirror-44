0912a411f7ed1d84097b4ebe688e564d4349b0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:41 +0000

    Add package jupyterlab-sql/0.1.8rc1/jupyterlab_sql-0.1.8rc1.tar.gz

---
+++ packages/jupyterlab-sql/jupyterlab_sql-0.1.8rc1.tar.gz/jupyterlab_sql-0.1.8rc1/jupyterlab_sql/handlers.py
@@ -0,0 +1,61 @@
+import json
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+from tornado.escape import json_decode
+import tornado.ioloop
+
+from .query_executor import QueryExecutor
+
+
+class SqlHandler(IPythonHandler):
+    def initialize(self, query_executor):
+        self._query_executor = query_executor
+
+    def execute_query(self, connection_url, query):
+        result = self._query_executor.execute_query(connection_url, query)
+        return result
+
+    def error_response(self, message):
+        response = {
+            "responseType": "error",
+            "responseData": {"message": message},
+        }
+        return response
+
+    async def post(self):
+        data = json_decode(self.request.body)
+        query = data["query"]
+        connection_url = data["connectionString"]
+        ioloop = tornado.ioloop.IOLoop.current()
+        try:
+            result = await ioloop.run_in_executor(
+                None, self.execute_query, connection_url, query
+            )
+            if result.has_rows:
+                response = {
+                    "responseType": "success",
+                    "responseData": {
+                        "hasRows": True,
+                        "keys": result.keys,
+                        "rows": result.rows,
+                    },
+                }
+            else:
+                response = {
+                    "responseType": "success",
+                    "responseData": {"hasRows": False},
+                }
+        except Exception as e:
+            response = self.error_response(str(e))
+        self.finish(json.dumps(response))
+
+
+def register_handlers(nbapp):
+    web_app = nbapp.web_app
+    host_pattern = ".*$"
+    route_pattern = url_path_join(
+        web_app.settings["base_url"], "/jupyterlab-sql/query"
+    )
+    executor = QueryExecutor()
+    handlers = [(route_pattern, SqlHandler, {"query_executor": executor})]
+    web_app.add_handlers(host_pattern, handlers)
---
+++ packages/jupyterlab-sql/jupyterlab_sql-0.1.8rc1.tar.gz/jupyterlab_sql-0.1.8rc1/src/index.ts
@@ -0,0 +1,84 @@
+import * as uuid from 'uuid';
+
+import {
+  JupyterLab,
+  JupyterLabPlugin,
+  ILayoutRestorer
+} from '@jupyterlab/application';
+
+import {
+  ICommandPalette,
+  MainAreaWidget,
+  InstanceTracker
+} from '@jupyterlab/apputils';
+
+import { IEditorServices } from '@jupyterlab/codeeditor';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { JupyterLabSqlWidget } from './widget';
+
+import { createTracker } from './tracker';
+
+import '../style/index.css';
+
+function activate(
+  app: JupyterLab,
+  palette: ICommandPalette,
+  launcher: ILauncher | null,
+  editorServices: IEditorServices,
+  restorer: ILayoutRestorer
+) {
+  const tracker: InstanceTracker<
+    MainAreaWidget<JupyterLabSqlWidget>
+  > = createTracker();
+  const command: string = 'jupyterlab-sql:open';
+
+  restorer.restore(tracker, {
+    command: command,
+    args: widget => ({
+      name: widget.content.name,
+      connectionString: widget.content.toolbarModel.connectionString,
+      sqlStatement: widget.content.sqlStatementValue
+    }),
+    name: widget => widget.content.name
+  });
+
+  app.commands.addCommand(command, {
+    label: ({ isPalette }) => (isPalette ? 'New SQL session' : 'SQL'),
+    iconClass: 'p-Sql-DatabaseIcon',
+    execute: ({ name, connectionString, sqlStatement }) => {
+      const widgetName = <string>(name || uuid.v4());
+      const initialConnectionString = <string>(
+        (connectionString || 'postgres://localhost:5432/postgres')
+      );
+      const initialSqlStatement = <string>(sqlStatement || '');
+      const widget = new JupyterLabSqlWidget(editorServices.factoryService, {
+        name: widgetName,
+        initialConnectionString,
+        initialSqlStatement
+      });
+      const main = new MainAreaWidget({ content: widget });
+      app.shell.addToMainArea(main);
+      tracker.add(main);
+    }
+  });
+
+  palette.addItem({ command, category: 'SQL', args: { isPalette: true } });
+
+  if (launcher) {
+    launcher.add({ command, category: 'Other' });
+  }
+}
+
+/**
+ * Initialization data for the jupyterlab-sql extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyterlab-sql',
+  autoStart: true,
+  requires: [ICommandPalette, ILauncher, IEditorServices, ILayoutRestorer],
+  activate
+};
+
+export default extension;
