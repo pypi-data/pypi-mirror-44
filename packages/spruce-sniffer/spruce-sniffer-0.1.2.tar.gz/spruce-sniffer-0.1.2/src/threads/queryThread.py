11aab539bf1827387b13f07f9c31529b9e569cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:01 +0000

    Add package spruce-sniffer/0.1.2/spruce-sniffer-0.1.2.tar.gz

---
+++ packages/spruce-sniffer/spruce-sniffer-0.1.2.tar.gz/spruce-sniffer-0.1.2/src/threads/queryThread.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from PyQt5 import QtCore
+
+
+# class QueryThread(QtCore.QObject):
+class QueryThread(QtCore.QThread):
+    finishSignal = QtCore.pyqtSignal(bool)
+    jsonSignal = QtCore.pyqtSignal(str)
+    infoSignal = QtCore.pyqtSignal(str, str)
+
+    def __init__(self, ip, token='80977e50c0ef36'):
+        super().__init__()
+        self.token = token
+        self.queryIP = ip
+
+    def __del__(self):
+        # Destroyed while thread is still running
+        # Solution https://blog.csdn.net/suli_fly/article/details/21627535
+        self.quit()
+        self.wait()
+
+    def run(self):
+        self.finishSignal.emit(False)
+        url = 'http://ipinfo.io/{searchIP}?token={token}'.format(
+            searchIP=self.queryIP, token=self.token)
+        try:
+            ipJSON = requests.get(url, timeout=1.0).text
+        except (requests.ConnectionError, requests.exceptions.ReadTimeout):
+            self.infoSignal.emit('Search Info',
+                                 'Make sure your network is stable')
+        else:
+            self.jsonSignal.emit(ipJSON)
+        finally:
+            self.finishSignal.emit(True)
