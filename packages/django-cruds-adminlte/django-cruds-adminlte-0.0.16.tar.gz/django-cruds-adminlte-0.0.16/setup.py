33a1a75482e8f84679e30b0656235e37e37af763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:00 +0000

    Add package django-cruds-adminlte/0.0.16/django-cruds-adminlte-0.0.16.tar.gz

---
+++ packages/django-cruds-adminlte/django-cruds-adminlte-0.0.16.tar.gz/django-cruds-adminlte-0.0.16/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = open('VERSION').read()
+readme = open('README.rst').read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='django-cruds-adminlte',
+    version=version,
+    description="""django-cruds-adminlte is simple drop-in django app that creates CRUD for faster prototyping.""",  # noqa
+    test_suite="runtests.run_tests",
+    long_description=readme,
+    author='Óscar M. Lage',
+    author_email='info@oscarmlage.com',
+    url='https://github.com/oscarmlage/django-cruds-adminlte',
+    packages=[
+        'cruds_adminlte',
+        'cruds_adminlte.templatetags',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'django-crispy-forms',
+        'djangoajax',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django-cruds-adminlte',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
