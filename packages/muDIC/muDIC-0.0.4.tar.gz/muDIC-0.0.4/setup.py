2f0516a3ea46d774884375ac4ac7815e1100cafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:41 +0000

    Add package muDIC/0.0.4/muDIC-0.0.4.tar.gz

---
+++ packages/muDIC/muDIC-0.0.4.tar.gz/muDIC-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="muDIC",
+    version="0.0.4",
+    author="PolymerGuy",
+    author_email="sindre.n.olufsen@ntnu.no",
+    description="A digital image correlation toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PolymerGuy/muDIC",
+    install_requires=[
+        'numba',
+        'scipy >= 1.2.1',
+        'matplotlib',
+        'numpy',
+        'Pillow',
+        'dill',
+        'nose',
+        'scikit-image',
+        'muDIC'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
