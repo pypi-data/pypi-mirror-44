6b3a9eb62162f69fe8aca45fbc95d65d11f9e15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:49 +0000

    Add package doitlive/4.3.0/doitlive-4.3.0-py2.py3-none-any.whl

---
+++ packages/doitlive/doitlive-4.3.0-py2.py3-none-any.whl/doitlive/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+doitlive
+~~~~~~~~
+
+A tool for "live" presentations in the terminal.
+"""
+from .cli import SessionState
+from .python_consoles import PythonRecorderConsole, PythonPlayerConsole
+from .styling import THEMES, Style, TermString, TTY, echo, echo_prompt, format_prompt
+from .keyboard import wait_for, magictype, magicrun, ESC, BACKSPACE, CTRLC, RETURNS
+from .version_control import (
+    get_current_git_branch,
+    get_current_hg_branch,
+    get_current_vcs_branch,
+)
+from .exceptions import DoItLiveError, ConfigurationError, SessionError
+from .__version__ import __version__
+
+__author__ = "Steven Loria"
+__license__ = "MIT"
+__version__ = __version__
+__all__ = [
+    "SessionState",
+    "PythonRecorderConsole",
+    "PythonPlayerConsole",
+    "THEMES",
+    "Style",
+    "TermString",
+    "TTY",
+    "echo",
+    "echo_prompt",
+    "format_prompt",
+    "wait_for",
+    "magictype",
+    "magicrun",
+    "ESC",
+    "BACKSPACE",
+    "CTRLC",
+    "RETURNS",
+    "get_current_git_branch",
+    "get_current_hg_branch",
+    "get_current_vcs_branch",
+    "DoItLiveError",
+    "ConfigurationError",
+    "SessionError",
+]
