dbe13b4df1187c7edec60b75a3b9a2184da0dfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:48 +0000

    Add package python-gratisdns/0.3/python-gratisdns-0.3.tar.gz

---
+++ packages/python-gratisdns/python-gratisdns-0.3.tar.gz/python-gratisdns-0.3/gratisdns/__init__.py
@@ -0,0 +1,11 @@
+from .gratisdns import (AAAARecord, ARecord, CNAMERecord, GratisDNS,
+                        GratisDNSError, MXRecord, SRVRecord, TXTRecord)
+
+__all__ = ['GratisDNS',
+           'GratisDNSError',
+           'ARecord',
+           'AAAARecord',
+           'CNAMERecord',
+           'MXRecord',
+           'TXTRecord',
+           'SRVRecord']
---
+++ packages/python-gratisdns/python-gratisdns-0.3.tar.gz/python-gratisdns-0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='python-gratisdns',
+    version='0.3',
+    author='Mads Sülau Jørgensen, Kasper Laudrup',
+    author_email='mads@sulau.dk, laudrup@stacktrace.dk',
+    url='https://github.com/laudrup/python-gratisdns',
+    packages=[
+        'gratisdns',
+    ],
+    license='MIT',
+    description='Unofficial library for interfacing with the gratisdns.dk web interface',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests-html',
+        'wheel',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose'
+    ],
+    keywords='gratisdns, dns',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
