b278f8dbc7324a5a1c41db574f42b0ad60c324c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package alt-tabpy/0.0.1/alt-tabpy-0.0.1.tar.gz

---
+++ packages/alt-tabpy/alt-tabpy-0.0.1.tar.gz/alt-tabpy-0.0.1/alt_tabpy/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+
+import tornado.ioloop
+import tornado.web
+
+from alt_tabpy.handlers import EvaluateHandler
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=9004)
+    args = parser.parse_args()
+
+    app = tornado.web.Application(
+        [
+            (r'/evaluate', EvaluateHandler),
+        ])
+    app.listen(args.port)
+    print(f"Listening on port {args.port}!")
+    tornado.ioloop.IOLoop.current().start()
