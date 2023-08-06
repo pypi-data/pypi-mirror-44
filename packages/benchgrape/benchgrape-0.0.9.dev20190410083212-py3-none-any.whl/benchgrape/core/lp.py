b1e2340e826f4b4f5f80998ff4d67212c157350b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:29 +0000

    Add package benchgrape/0.0.9.dev20190410083212/benchgrape-0.0.9.dev20190410083212-py3-none-any.whl

---
+++ packages/benchgrape/benchgrape-0.0.9.dev20190410083212-py3-none-any.whl/benchgrape/core/lp.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import six
+import uuid
+from locust import TaskSet
+
+from benchgrape.core.statement import login
+
+logger = logging.getLogger(__name__)
+
+
+class LongPollingTaskSet(TaskSet):
+    def on_start(self):
+        self.org = int(os.getenv('ORG_ID', 1))
+        logger.info('configured organization: %i', self.org)
+        self.auth_token = login(self.client)
+        self.user_id = six.text_type(uuid.uuid4())
+
+        if not self.parent.host:
+            raise AssertionError(
+                '--host must be appended to determine the websocket location.'
+            )
