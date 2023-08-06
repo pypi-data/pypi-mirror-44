30723af6a3bfef49b2c7cdb92b4a657944272bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:26 +0000

    Add package vision-tile-query/0.0.2.1/vision_tile_query-0.0.2.1.tar.gz

---
+++ packages/vision-tile-query/vision_tile_query-0.0.2.1.tar.gz/vision_tile_query-0.0.2.1/vision_tile_query/__init__.py
@@ -0,0 +1,10 @@
+from .tile_creator import VisionBaseTileProcessor
+from .contrib.model_creator import TableManager
+
+version = '0.0.2.1'
+
+__all__ = [
+    VisionBaseTileProcessor,
+    TableManager,
+    version
+]
