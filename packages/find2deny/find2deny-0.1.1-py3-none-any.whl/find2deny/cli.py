26ac43ecbbd11413a70ced68b3d763069992a397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:17 +0000

    Add package find2deny/0.1.1/find2deny-0.1.1-py3-none-any.whl

---
+++ packages/find2deny/find2deny-0.1.1-py3-none-any.whl/find2deny/cli.py
@@ -0,0 +1,9 @@
+
+
+# work-flow
+# 1. suche alle IP in Logfile nach Merkmale eines Angriff
+# 2. generiert UFW Befehlen aus der IPs im Schritte 1
+# 3. gebe die Befehlen aus, oder sonstiges weiter Verarbeitung
+
+def main():
+    print("TODO")
