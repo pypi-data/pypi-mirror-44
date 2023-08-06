685073e939cc5548d3f22a12d405e27d76f344af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:28 +0000

    Add package python-dev-tools/2019.4.3/python_dev_tools-2019.4.3-py2.py3-none-any.whl

---
+++ packages/python-dev-tools/python_dev_tools-2019.4.3-py2.py3-none-any.whl/python_dev_tools/linters/lint.py
@@ -0,0 +1,22 @@
+"""Definition of lint function, calling all linters."""
+
+from python_dev_tools.linters.mccabe import MccabeLinter
+from python_dev_tools.linters.pycodestyle import PycodestyleLinter
+from python_dev_tools.linters.pydocstyle import PydocstyleLinter
+from python_dev_tools.linters.pyflakes import PyflakesLinter
+
+linters = [PyflakesLinter, PycodestyleLinter, MccabeLinter, PydocstyleLinter]
+
+
+def lint(file, all_warnings=False):
+    """Lint the file with known linters."""
+    messages = set()
+    for linter in linters:
+        messages.update(linter.lint(file))
+        if len(messages) >= 10 and not all_warnings:
+            break
+
+    messages = sorted(list(messages))
+    if all_warnings:
+        return messages
+    return messages[:10]
