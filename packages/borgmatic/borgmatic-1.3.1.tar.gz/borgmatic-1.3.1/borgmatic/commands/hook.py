c3f42c5c03a1af8f242f68dd95977befafb158fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package borgmatic/1.3.1/borgmatic-1.3.1.tar.gz

---
+++ packages/borgmatic/borgmatic-1.3.1.tar.gz/borgmatic-1.3.1/borgmatic/commands/hook.py
@@ -0,0 +1,24 @@
+import logging
+import subprocess
+
+
+logger = logging.getLogger(__name__)
+
+
+def execute_hook(commands, config_filename, description):
+    if not commands:
+        logger.debug('{}: No commands to run for {} hook'.format(config_filename, description))
+        return
+
+    if len(commands) == 1:
+        logger.info('{}: Running command for {} hook'.format(config_filename, description))
+    else:
+        logger.info(
+            '{}: Running {} commands for {} hook'.format(
+                config_filename, len(commands), description
+            )
+        )
+
+    for command in commands:
+        logger.debug('{}: Hook command: {}'.format(config_filename, command))
+        subprocess.check_call(command, shell=True)
