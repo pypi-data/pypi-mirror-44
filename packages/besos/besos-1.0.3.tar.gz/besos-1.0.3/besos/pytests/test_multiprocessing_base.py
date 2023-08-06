5faa8d4fac72c80834350bf1435002aa1687e150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:28 +0000

    Add package besos/1.0.3/besos-1.0.3.tar.gz

---
+++ packages/besos/besos-1.0.3.tar.gz/besos-1.0.3/besos/pytests/test_multiprocessing_base.py
@@ -0,0 +1,41 @@
+import pytest
+
+import eppy_funcs as ef
+from evaluator import EvaluatorEP, EvaluatorSR
+from parameters import RangeParameter, CategoryParameter, Parameter, FieldSelector, expand_plist
+from problem import EPProblem, Problem
+import sampling
+
+
+@pytest.fixture
+def building():
+    return ef.get_building()
+
+
+@pytest.fixture
+def parameters():
+    parameters = [
+        Parameter(FieldSelector(object_name='NonRes Fixed Assembly Window',
+                field_name='Solar Heat Gain Coefficient'),
+                value_descriptor=RangeParameter(0.01,0.99)),
+        Parameter(FieldSelector('Lights', '*', 'Watts per Zone Floor Area'),
+                value_descriptor=RangeParameter(8, 12),
+                name='Lights Watts/Area')]
+    return parameters
+
+
+@pytest.fixture
+def problem(parameters):
+    objectives = ['Electricity:Facility']
+    problem = EPProblem(parameters, objectives)
+    return problem
+
+
+def test_eval(building, problem):
+    evaluator = EvaluatorEP(problem, building)
+    samples = sampling.dist_sampler(sampling.seeded_sampler, problem, 20)
+
+    outputs = evaluator.df_apply(samples, keep_input=True)
+    print()
+    print(outputs)
+    assert 1
