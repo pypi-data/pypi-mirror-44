7efd44b407090bda82963fed5a6ad07b2ceed085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:27 +0000

    Add package betabernsum/0.0.10/betabernsum-0.0.10-py3-none-any.whl

---
+++ packages/betabernsum/betabernsum-0.0.10-py3-none-any.whl/betabernsum/bbs_test.py
@@ -0,0 +1,56 @@
+#===============================================================================
+# bbs_test.py
+#===============================================================================
+
+# Imports ======================================================================
+
+from betabernsum.bbs import bbs_pmf, bbs_cdf
+
+
+
+
+# Functions ====================================================================
+
+def bbs_test(
+    x,
+    n,
+    a,
+    b,
+    independent=True,
+    alternative='two-sided',
+    processes=1
+):
+    """Perform a hypothesis test using a BBS distribution
+    
+    Parameters
+    ----------
+    x : int
+        the number of successes
+    n
+        iterable giving the number of trials for each group
+    a
+        iterable giving the first shape parameter for each group
+    b
+        iterable giving the second shape parameter for each group
+    alternative : str
+        alternative hypothesis {'less', 'greater', 'two-sided'}
+
+    Returns
+    -------
+    float
+        the p-value of the hypothesis test
+    """
+    
+    lower_tail_area = bbs_cdf(
+        x, n, a, b, independent=independent, processes=processes
+    )
+    if alternative == 'less':
+        return lower_tail_area
+    else:
+        upper_tail_area = 1 - lower_tail_area + bbs_pmf(
+            x, n, a, b, independent=independent
+        )
+    if alternative == 'two-sided':
+        return min(1, 2 * min(lower_tail_area, upper_tail_area))
+    elif alternative == 'greater':
+        return upper_tail_area
