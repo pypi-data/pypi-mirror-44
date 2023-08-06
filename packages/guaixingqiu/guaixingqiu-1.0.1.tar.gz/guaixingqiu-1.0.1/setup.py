d6781b942f6338d337bd0790672c3e9573166b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:45 +0000

    Add package guaixingqiu/1.0.1/guaixingqiu-1.0.1.tar.gz

---
+++ packages/guaixingqiu/guaixingqiu-1.0.1.tar.gz/guaixingqiu-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name= "guaixingqiu" , 
+    version = "1.0.1",
+    py_modules=["guaixingqiu"],
+    author="liupei",
+    author_email="lp138879@163.com",
+    description="no",
+)
