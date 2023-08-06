3c0fc1595c813ef3a3ad853ca55acf4bcaa031f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package pyclts/1.2.0/pyclts-1.2.0.tar.gz

---
+++ packages/pyclts/pyclts-1.2.0.tar.gz/pyclts-1.2.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+
+setup(
+    name='pyclts',
+    version="1.2.0",
+    description='A python library to check phonetic transcriptions',
+    long_description=codecs.open("README.md", 'r', 'utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Johann-Mattis List, Cormac Anderson, Tiago Tresoldi, Christoph Rzymskie, Simon Greenhill, and Robert Forkel',
+    author_email='mattis.list@lingpy.org',
+    url='https://github.com/cldf/clts',
+    install_requires=[
+        'attrs>=18.2',
+        'clldutils>=1.13.6',
+        'csvw>=1.0',
+        'uritemplate',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'pytest>=3.6',
+            'pytest-mock',
+            'mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license="GPL",
+    zip_safe=False,
+    keywords='',
+    classifiers=[
+        ],
+    entry_points={
+        'console_scripts': [
+            'clts=pyclts.__main__:main',
+        ]
+    },
+)
