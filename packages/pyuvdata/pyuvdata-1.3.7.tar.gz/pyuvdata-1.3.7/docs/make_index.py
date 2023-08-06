5449dd4df40aa54cd3f25761cc0d8f1873c340b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package pyuvdata/1.3.7/pyuvdata-1.3.7.tar.gz

---
+++ packages/pyuvdata/pyuvdata-1.3.7.tar.gz/pyuvdata-1.3.7/docs/make_index.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""
+Format the readme.md file into the sphinx index.rst file.
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import os
+import inspect
+import re
+import pypandoc
+from astropy.time import Time
+
+
+def write_index_rst(readme_file=None, write_file=None):
+    t = Time.now()
+    t.out_subfmt = 'date'
+    out = ('.. pyuvdata documentation master file, created by\n'
+           '   make_index.py on {date}\n\n').format(date=t.iso)
+
+    if readme_file is None:
+        main_path = os.path.dirname(os.path.dirname(os.path.abspath(inspect.stack()[0][1])))
+        readme_file = os.path.join(main_path, 'README.md')
+
+    readme_md = pypandoc.convert_file(readme_file, 'md')
+
+    travis_str = 'https://travis-ci.org/RadioAstronomySoftwareGroup/pyuvdata.svg'
+    regex_travis = re.compile(travis_str)
+    loc_travis_start = re.search(regex_travis, readme_md).start()
+    loc_travis_end = re.search(regex_travis, readme_md).end()
+    end_branch_str = r'\)\]'
+    regex_end = re.compile(end_branch_str)
+    loc_branch_end = re.search(regex_end, readme_md).start()
+    branch_str = readme_md[loc_travis_end:loc_branch_end]
+
+    cover_str = 'https://coveralls.io/repos/github/RadioAstronomySoftwareGroup/pyuvdata/badge.svg'
+    regex_cover = re.compile(cover_str)
+    loc_cover_start = re.search(regex_cover, readme_md).start()
+    loc_cover_end = re.search(regex_cover, readme_md).end()
+
+    readme_text = pypandoc.convert_file(readme_file, 'rst')
+
+    rst_status_badge = '.. image:: ' + travis_str + branch_str + '\n    :target: https://travis-ci.org/RadioAstronomySoftwareGroup/pyuvdata'
+    status_badge_text = '|Build Status|'
+    readme_text = readme_text.replace(status_badge_text, rst_status_badge + '\n\n')
+
+    rst_status_badge = '.. image:: ' + cover_str + branch_str + '\n    :target: https://coveralls.io/github/RadioAstronomySoftwareGroup/pyuvdata' + branch_str
+    status_badge_text = '|Coverage Status|'
+    readme_text = readme_text.replace(status_badge_text, rst_status_badge)
+
+    readme_text = readme_text.replace(' ' + rst_status_badge, rst_status_badge)
+
+    end_text = 'parameters descriptions'
+    regex = re.compile(end_text.replace(' ', r'\s+'))
+    loc = re.search(regex, readme_text).start()
+
+    out += readme_text[0:loc] + end_text + '.'
+    out += ('\n\nFurther Documentation\n====================================\n'
+            '.. toctree::\n'
+            '   :maxdepth: 1\n\n'
+            '   tutorial\n'
+            '   uvdata_parameters\n'
+            '   uvdata\n'
+            '   uvcal_parameters\n'
+            '   uvcal\n'
+            '   uvbeam_parameters\n'
+            '   uvbeam\n'
+            '   cst_settings_yaml\n'
+            '   developer_docs\n')
+
+    out.replace(u"\u2018", "'").replace(u"\u2019", "'").replace(u"\xa0", " ")
+
+    if write_file is None:
+        write_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+        write_file = os.path.join(write_path, 'index.rst')
+    F = open(write_file, 'w')
+    F.write(out)
+    print("wrote " + write_file)
---
+++ packages/pyuvdata/pyuvdata-1.3.7.tar.gz/pyuvdata-1.3.7/setup.py
@@ -0,0 +1,76 @@
+# -*- mode: python; coding: utf-8 -*
+# Copyright (c) 2018 Radio Astronomy Software Group
+# Licensed under the 2-clause BSD License
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup, Extension
+import glob
+import os
+import io
+import numpy as np
+import json
+
+# When setting up, the binary extension modules haven't yet been built, so
+# without a workaround we can't use the pyuvdata code to get the version.
+os.environ['PYUVDATA_IGNORE_EXTMOD_IMPORT_FAIL'] = '1'
+from pyuvdata import version  # noqa (pycodestyle complains about import below code)
+
+data = [version.git_origin, version.git_hash, version.git_description, version.git_branch]
+with open(os.path.join('pyuvdata', 'GIT_INFO'), 'w') as outfile:
+    json.dump(data, outfile)
+
+with io.open('README.md', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+
+global_c_macros = [
+    ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),
+]
+
+setup_args = {
+    'name': 'pyuvdata',
+    'author': 'Radio Astronomy Software Group',
+    'url': 'https://github.com/RadioAstronomySoftwareGroup/pyuvdata',
+    'license': 'BSD',
+    'description': 'an interface for astronomical interferometeric datasets in python',
+    'long_description': readme,
+    'long_description_content_type': 'text/markdown',
+    'package_dir': {'pyuvdata': 'pyuvdata'},
+    'packages': ['pyuvdata', 'pyuvdata.tests'],
+    'ext_modules': [
+        Extension(
+            'pyuvdata._miriad',
+            sources=[
+                'pyuvdata/src/miriad_wrap.cpp',
+                'pyuvdata/src/uvio.c',
+                'pyuvdata/src/hio.c',
+                'pyuvdata/src/pack.c',
+                'pyuvdata/src/bug.c',
+                'pyuvdata/src/dio.c',
+                'pyuvdata/src/headio.c',
+                'pyuvdata/src/maskio.c',
+            ],
+            define_macros=global_c_macros,
+            include_dirs=[
+                np.get_include(),
+                'pyuvdata/src',
+            ]
+        )
+    ],
+    'scripts': glob.glob('scripts/*'),
+    'version': version.version,
+    'include_package_data': True,
+    'setup_requires': ['numpy>=1.14', 'six>=1.10'],
+    'install_requires': ['numpy>=1.14', 'six>=1.10', 'scipy', 'astropy>=2.0'],
+    'classifiers': ['Development Status :: 5 - Production/Stable',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: BSD License',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3.6',
+                    'Topic :: Scientific/Engineering :: Astronomy'],
+    'keywords': 'radio astronomy interferometry'
+}
+
+if __name__ == '__main__':
+    setup(**setup_args)
