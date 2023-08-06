cd3404a7d7968ee8200c131bb1dd9ba2758b17aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package webnotifier/0.1.0/webnotifier-0.1.0.tar.gz

---
+++ packages/webnotifier/webnotifier-0.1.0.tar.gz/webnotifier-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webnotifier",
+    version="0.1.0",
+    author="Pol MP",
+    author_email="pol.moreno30@gmail.com",
+    description="A package for notifying any webpage change",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/polmp/webnotifier",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests>=2.21.0',
+          'lxml>=4.3.3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
