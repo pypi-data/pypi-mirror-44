247a25b50bf91d1d94b1db6aa5fb2341e33d7017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:10 +0000

    Add package bayescache/0.1.0/bayescache-0.1.0.tar.gz

---
+++ packages/bayescache/bayescache-0.1.0.tar.gz/bayescache-0.1.0/test/test_bayescache.py
@@ -0,0 +1,19 @@
+"""
+Tests for `bayescache` module.
+"""
+import pytest
+from bayescache import bayescache
+
+
+class TestBayescache(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
