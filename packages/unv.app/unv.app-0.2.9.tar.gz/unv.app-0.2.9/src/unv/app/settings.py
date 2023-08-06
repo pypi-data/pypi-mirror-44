21bfe7467d07ff8c33f37a258ef57af2789a476e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:23 +0000

    Add package unv.app/0.2.9/unv.app-0.2.9.tar.gz

---
+++ packages/unv.app/unv.app-0.2.9.tar.gz/unv.app-0.2.9/src/unv/app/settings.py
@@ -0,0 +1,35 @@
+from unv.utils.os import get_homepath
+
+from .core import create_component_settings
+
+
+SCHEMA = {
+    'root': {
+        'empty': False,
+        'type': 'string',
+        'required': True
+    },
+    'env': {
+        'type': 'string',
+        'allowed': ['production', 'development', 'testing'],
+        'required': True
+    },
+    'components': {
+        'type': 'list',
+        'empty': True,
+        'schema': {'type': 'string'},
+        'required': True
+    }
+}
+
+DEFAULTS = {
+    'root': str(get_homepath()),
+    'env': 'development',
+    'components': [],
+}
+
+SETTINGS = create_component_settings('app', DEFAULTS, SCHEMA)
+
+DEVELOPMENT = SETTINGS['env'] == 'development'
+PRODUCTION = SETTINGS['env'] == 'production'
+TESTING = SETTINGS['env'] == 'testing'
