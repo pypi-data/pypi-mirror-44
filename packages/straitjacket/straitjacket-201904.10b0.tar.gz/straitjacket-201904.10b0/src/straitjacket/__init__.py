275dc59099831ed84f859d7732f4d0981f982056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package straitjacket/201904.10b0/straitjacket-201904.10b0.tar.gz

---
+++ packages/straitjacket/straitjacket-201904.10b0.tar.gz/straitjacket-201904.10b0/src/straitjacket/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of the straitjacket project
+# https://gitlab.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+__version__ = "v201904.0010-beta"
+
+from . import sjfmt  # noqa
