e9b0912f3e4842df0a838abd2d743e74dfc97413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:56 +0000

    Add package apronpy/0.5.2/apronpy-0.5.2-py3-none-any.whl

---
+++ packages/apronpy/apronpy-0.5.2-py3-none-any.whl/apronpy/__init__.py
@@ -0,0 +1,15 @@
+
+__all__ = [
+    "abstract1",
+    "box",
+    "coeff",
+    "environment",
+    "interval",
+    "lincons1",
+    "linexpr1",
+    "polka",
+    "scalar",
+    "tcons1",
+    "texpr1",
+    "var"
+]
