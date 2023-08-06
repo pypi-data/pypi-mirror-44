d8defc421a1703d4f1ba02e7ede8729b244b4dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:46 +0000

    Add package polyaxon-cli/0.4.3/polyaxon_cli-0.4.3-py2.py3-none-any.whl

---
+++ packages/polyaxon-cli/polyaxon_cli-0.4.3-py2.py3-none-any.whl/tests/test_schemas/test_global_configuration.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from unittest import TestCase
+
+from polyaxon_cli.schemas.global_configuration import GlobalConfigurationConfig
+
+
+class TestGlobalConfigs(TestCase):
+    def test_global_config(self):
+        config_dict = {'verbose': True,
+                       'host': 'localhost',
+                       'http_port': '80',
+                       'ws_port': '1338',
+                       'verify_ssl': True,
+                       'use_https': False}
+        config = GlobalConfigurationConfig.from_dict(config_dict)
+        assert config.to_dict() == config_dict
